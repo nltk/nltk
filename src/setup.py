97871b02c47dbfac01653353853dff77a8a099fd
Edward Loper <edloper@ldc.upenn.edu>
2001-05-28 03:48:48 +0000

    Updated makefiles to correctly build source and built distributions and to add them to the web page; added a distutils setup program to build distributions.
    svn/trunk/nltk@48

---
+++ src/setup.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python
+#
+# Distutils setup script for the Natural Language
+# Processing Toolkit
+#
+# Created [05/27/01 09:04 PM]
+# Edward Loper
+#
+
+from distutils.core import setup
+
+setup(name="nltk",
+      version="0.1",
+      description="Natural Language Processing Toolkit",
+      author="Edward Loper",
+      author_email="ed@loper.org",
+      url="http://www.cis.upenn.edu/~edloper/nltk/",
+      packages=['nltk'])
+
