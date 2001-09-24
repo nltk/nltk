6c861dd79098a5e1771f8f3c810bfc1dc061d40e
Edward Loper <edloper@ldc.upenn.edu>
2001-09-24 04:11:20 +0000

    Added a few test cases for SampleEvent
    svn/trunk/nltk@217

--- src/nltk/test/probability.py
+++ src/nltk/test/probability.py
@@ -8,7 +8,7 @@
 # $Id$
 
 
-from nltk.set import *
+from nltk.probability import *
 
 ##//////////////////////////////////////////////////////
 ##  Test code
@@ -26,16 +26,65 @@ class SampleEventTestCase(unittest.TestCase):
         y = SampleEvent('a')
         z = SampleEvent( (1,2,3, (4,5)) )
 
+    def testContains(self):
+        "nltk.probability.SampleEvent: containership tests"
+        x = SampleEvent(1)
+        y = SampleEvent('a')
+        z = SampleEvent( (1,2,3, (4,5)) )
+
         self.failUnless(1 in x)
         self.failUnless('a' in y)
         self.failUnless( (1,2,3, (4,5)) in z)
 
-        self.failf(0 in x)
-        self.failf(0 in x)
-        self.failf('A' in y)
-        self.failf('z' in y)
-        self.failf('a' in z)
-        self.failf(1 in z)
+        self.failIf(0 in x)
+        self.failIf(0 in x)
+        self.failIf('A' in y)
+        self.failIf('z' in y)
+        self.failIf('a' in z)
+        self.failIf(1 in z)
+
+        self.failUnless(x.contains(1))
+        self.failUnless(y.contains('a'))
+        self.failUnless(z.contains( (1,2,3, (4,5)) ))
+
+        self.failIf(x.contains(0))
+        self.failIf(x.contains(0))
+        self.failIf(y.contains('A'))
+        self.failIf(y.contains('z'))
+        self.failIf(z.contains('a'))
+        self.failIf(z.contains(1))
+
+    def testEquals(self):
+        "nltk.probability.SampleEvent: equality tests"
+        x = SampleEvent(1)
+        y = SampleEvent('a')
+        a = SampleEvent('a')
+        b = SampleEvent(0)
+
+        self.failIf(x == y)
+        self.failIf(x == a)
+        self.failIf(x == b)
+        self.failIf(y == x)
+
+        self.failUnless(y == a)
+        self.failUnless(a == y)
+        
+        self.failIf(y == b)
+        self.failIf(a == x)
+        self.failIf(a == b)
+        self.failIf(b == x)
+        self.failIf(b == y)
+        self.failIf(b == a)
+
+        self.failIf(y != a)
+        self.failIf(a != y)
+        
+        self.failUnless(y != b)
+        self.failUnless(a != x)
+        self.failUnless(a != b)
+        self.failUnless(b != x)
+        self.failUnless(b != y)
+        self.failUnless(b != a)
 
 
 def testsuite():
