d5ca02d583e224ac25e4b00aaa4d53a0e95c3ea2
Edward Loper <edloper@ldc.upenn.edu>
2001-12-11 08:11:46 +0000

    - Added lots of docstrings - Added SelectedFDList - Added AttestedFeatureSelector (not sure if that's the name I want)
    svn/trunk/nltk@427

--- src/nltk/classifier/featureselection.py
+++ src/nltk/classifier/featureselection.py
@@ -10,74 +10,150 @@
 from nltk.classifier import *
 from nltk.classifier.feature import *
 
-class FilteredFDList(AbstractFDList):
+class FeatureSelectorI:
     """
-    Only include features that mean something.
+    A processing interface for selecting which features are relevant
+    to a given classification problem.  Feature selectors must
+    implement a single method, C{select}.  This method takes a
+    C{FeatureDetectorList}, and returns a new C{FeatureDetectorList}
+    that implements the features from the given C{FeatureDetectorList}
+    that are relevant to the classificaiton task.
     """
-    def __init__(self, fdlist, labeled_toks, labels):
-        self._fdlist = fdlist
+    def select(self, fdlist):
+        """
+        Decide which features are relevant for classification; and
+        return a new C{FeatureDetectorList} implementing those
+        features.  All features must be selected from C{fdlist}.  In
+        particular, if M{new_fdlist} is the C{FeatureDetectorList}
+        returned by C{select}, then the following must be true for all
+        labeled texts M{lt} and feature ids M{id}, and for some
+        function M{f()}::
+
+            new_fdlist[id].detect(lt) = fdlist[f(id)].detect(lt)
         
-        useful = Numeric.zeros(len(fdlist))
-        for tok in labeled_toks:
-            text = tok.type().text()
-            fvlist = fdlist.detect(tok.type())
-            for (id, val) in fvlist.assignments():
-                useful[id] = 1
-        self._len = Numeric.sum(useful)
-        self._idmap = Numeric.zeros(len(fdlist))
-        src = 0
-        for dest in range(len(useful)):
-            if useful[dest]:
-                self._idmap[dest] = src
-                src += 1
-            else:
-                self._idmap[dest] = -1
+        @return: a C{FeatureDetectorList} that implements the features
+            from C{fdlist} that are relevant.
+        @rtype: C{FeatureDetectorListI}
+        @param fdlist: The C{FeatureDetectorList} from which features
+            detectors should be chosen.
+        @type fdlist: C{FeatureDetectorListI}
+        """
+        raise AssertionError()
+
+class SelectedFDList(AbstractFDList):
+    """
+    A feature detector list that implements a subset of the features
+    defined by a X{base C{FeatureDetectorList}}.  C{SelectedFDList} is
+    used by C{FeatureSelector}s to select the set of features that are
+    relevant for a given classification task.
+    """
+    def __init__(self, sub_fdlist, selected_ids):
+        """
+        Construct a new C{SelectedFDList}.  This feature detector list
+        implements the features from C{sub_fdlist} that are indicated
+        by C{selected_ids}.  In particular, the following is true for
+        all feature ids M{0<=id<len(self)} and C{LabeledText}s M{lt}::
+
+          self[id].detect(lt) = sub_fdlist[selected_ids.index(id)].detect(lt)
+
+        @type sub_fdlist: C{FeatureDetectorListI}
+        @param sub_fdlist: The C{FeatureDetectorList} that this
+            C{SelectedFDList} is based on.
+        @type selected_ids: C{sequence} of C{int}
+        @param selected_ids: The feature ids for the feature detectors
+            that should be included in the C{SelectedFDList}.  This
+            list should not contain duplicate feature ids.
+        """
+        N = 0
+        idmap = {}
+        for id in selected_ids:
+            if not idmap.has_key(id):
+                idmap[id] = N
+                N += 1
                 
+        self._N = N
+        self._idmap = idmap
+        self._sub_fdlist = sub_fdlist
+            
     def __len__(self):
-        return self._len
+        # Inherit docs from AbstractFDList
+        return self._N
 
-    def detect(self, ltext):
-        assignments = [(self._idmap[id], val) for (id, val)
-                       in self._fdlist.detect(ltext).assignments()
-                       if self._idmap[id] >= 0]
-        #if len(assignments) > 0:
-        #    print ltext, assignments
-        #    if randint(0, 10) == 0: raise ValueError()
-        return SimpleFeatureValueList(assignments, self._len)
+    def detect(self, labeled_text):
+        # Inherit docs from AbstractFDList
+        fvlist = self._sub_fdlist.detect(labeled_text)
+        assignments = [(self._idmap.get(id), val) for (id, val)
+                       in fvlist.assignments()
+                       if self._idmap.has_key(id)]
+        return SimpleFeatureValueList(assignments, self._N,
+                                      fvlist.default())
 
-class FilteredFDList2(AbstractFDList):
+class AttestedFeatureSelector(FeatureSelectorI):
     """
-    Only include features that mean something.
+    A feature selector that selects all features that could possibly
+    apply to any of the texts in a given training set.  A feature can
+    apply to a text M{t} if the feature value for C{LabeledText(l,t)}
+    is not the default value for any label M{l}.
     """
-    def __init__(self, fdlist, labeled_toks, labels):
-        self._fdlist = fdlist
+    def __init__(self, training_data, **kwargs):
+        """
+        Construct a new C{AttestedFeatureSelector}.  Given a
+        C{FeatureDetectorList} M{fdlist}, this feature selector will
+        select any feature with id M{id} such that::
+
+            fdlist[id].detect(LabeledText(l, t)) != (default)
+
+        For any text M{t} from training_data, and any label M{l}.
         
-        useful = Numeric.zeros(len(fdlist))
-        for tok in labeled_toks:
-            text = tok.type().text()
-            for label in labels:
+        @param kwargs: Keyword arguments.
+          - C{labels}: The set of labels that should be considered
+            by this C{AttestedFeatureSelector} to decide whether a
+            feature can apply to a text.  If none is given, then the
+            set of all labels attested in the training data will be
+            used instead.  (type=C{list} of (immutable)).             
+          - C{min_count}: The minimum number of C{LabeledText}s to 
+            which a feature must apply, in order to be included in the
+            feature value list.  Default=1.  (type=C{int})
+        """
+        self._training_data = training_data
+        
+        # Process the keyword arguments.
+        self._min_count = 1
+        self._labels = None
+        for (key, val) in kwargs.items():
+            if key == 'min_count':
+                self._min_count = val
+            elif key == 'labels':
+                self._labels = val
+            else:
+                raise TypeError('Unknown keyword arg %s' % key)
+            
+        # Find the labels, if necessary.
+        if self._labels is None:
+            self._labels = find_labels(training_data)
+
+    def select(self, fdlist):
+        # Count the number of times each feature is attested.
+        attested = {}
+        for labeled_token in self._training_data:
+            text = labeled_token.type().text()
+            for label in self._labels:
                 fvlist = fdlist.detect(LabeledText(text, label))
+                default = fvlist.default()
                 for (id, val) in fvlist.assignments():
-                    useful[id] = 1
-        self._len = Numeric.sum(useful)
-        self._idmap = Numeric.zeros(len(fdlist))
-        src = 0
-        for dest in range(len(useful)):
-            if useful[dest]:
-                self._idmap[dest] = src
-                src += 1
-            else:
-                self._idmap[dest] = -1
-                
-    def __len__(self):
-        return self._len
+                    if val != default:
+                        attested[id] = attested.get(id, 0) + 1
 
-    def detect(self, ltext):
-        assignments = [(self._idmap[id], val) for (id, val)
-                       in self._fdlist.detect(ltext).assignments()
-                       if self._idmap[id] >= 0]
-        #if len(assignments) > 0:
-        #    print ltext, assignments
-        #    if randint(0, 10) == 0: raise ValueError()
-        return SimpleFeatureValueList(assignments, self._len)
+        # Construct the list of selected ids.  This is easy if
+        # min_count = 1.  Otherwise, loop through the entries of
+        # attested. 
+        if self._min_count == 1:
+            selected_ids = attested.keys()
+        else:
+            selected_ids = []
+            for (id, count) in attested.items():
+                if count >= self._min_count:
+                    selected_ids.append(id)
 
+        # Return the selected feature detector list.
+        return SelectedFDList(fdlist, selected_ids)
