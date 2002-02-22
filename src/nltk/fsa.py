e04ee46c43f41d49262d5459cbfc0d76cddd9ee5
Steven Bird <stevenbird1@gmail.com>
2002-02-22 16:17:44 +0000

    Added remove_epsilon function
    svn/trunk/nltk@463

--- src/nltk/fsa.py
+++ src/nltk/fsa.py
@@ -4,6 +4,7 @@ FSA class - deliberately simple so that the operations are easily understood.
 
 from nltk.srparser import *
 from nltk.token import CharTokenizer
+from nltk.set import *
 
 _grammar = (
     Rule('S', ('S', '*')),
@@ -47,44 +48,55 @@ class FSA:
         elif tree[1] == Tree('*'):
             start = self._final
             self._build(tree[0])
-            self.star(start)
+            self.insert_transition(self._final, epsilon, Set(start))
+            self.insert_transition(start, epsilon, Set(self._final))
         elif tree[1] == Tree('+'):
             start = self._final
             self._build(tree[0])
-            self.plus(start)
+            self.insert_transition(self._final, epsilon, Set(start))
         elif tree[1] == Tree('?'):
             start = self._final
             self._build(tree[0])
-            self.optional(start)
+            self.insert_transition(start, epsilon, Set(self._final))
         else:
             self._build(tree[0])
             self._build(tree[1])
         
-    def add_transition(self, s1, label, s2):
+    def insert_transition(self, s1, label, s2):
         if self._table.has_key((s1,label)):
-            if s2 not in self._table[(s1,label)]:
-                self._table[(s1,label)].append(s2)
+            self._table[(s1,label)].union(s2)
         else:
-            self._table[(s1,label)] = [s2]
+            self._table[(s1,label)] = s2
+
+    def delete_transition(self, s1, label, s2):
+        if self._table.has_key((s1,label)):
+            new = self._table[(s1,label)].difference(s2)
+            if len(new) > 0:
+                self._table[(s1,label)] = new
+            else:
+                del self._table[(s1,label)]
+        else:
+            print "Error: attempt to delete non-existent transition"
 
     def transitions(self):
-        return self._table.items()
+        return [(s1,label,s2)
+                for ((s1,label),s2) in self._table.items()]
+
+    def epsilon_transitions(self):
+        return [(s1,label,s2)
+                for (s1, label, s2) in self.transitions()
+                if label == epsilon]
+
+    def outgoing_transitions(self, node):
+        return [(s1,label,s2)
+                for (s1, label, s2) in self.transitions()
+                if s1 == node]
 
     def _atomic(self, char):
         state = self._final
-        self.add_transition(state, char, state+1)
+        self.insert_transition(state, char, Set(state+1))
         self._final += 1
         
-    def optional(self, start):
-        self.add_transition(start, epsilon, self._final)
-
-    def plus(self, start):
-        self.add_transition(self._final, epsilon, start)
-
-    def star(self, start):
-        self.plus(start)
-        self.optional(start)
-
 #    # add num to every state identifier
 #    def add(self, num):
 #        newtable = {}
@@ -97,19 +109,25 @@ class FSA:
 #
 #        # TODO - add epsilon transition from finals to initials
 #        for final in self._finals:
-#            self.add_transition(final, epsilon, self._count)
+#            self.insert_transition(final, epsilon, self._count)
 #        self._table.extend(fsa._table)
 
-    def remove_epsilons():
-        pass
+    def remove_epsilons(self):
+        while self.epsilon_transitions() != []:
+            print self.epsilon_transitions()
+            for (s1, label1, s2) in self.epsilon_transitions():
+                for state in s2.elements():
+                    for (s3, label2, s4) in self.outgoing_transitions(state):
+                        self.insert_transition(s1, label2, s4)
+                self.delete_transition(s1, label1, s2)
     
-    def minimize():
+    def minimize(self):
         pass
 
     def pp(self):
         t = self.transitions()
         t.sort()
-        for ((s1, label), s2) in t:
+        for (s1, label, s2) in t:
             print s1, ':', label, '->', s2
         print "Final:", self._final
 
