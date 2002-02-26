bf99ab3ed58d92835d74c6f8c498ccb932df59b1
Steven Bird <stevenbird1@gmail.com>
2002-02-26 04:29:34 +0000

    Changed the way final states are represented. Fixed the treatment of final states in epsilon transition removal. Re-organized class and created kleene functions. Added a generate function.
    svn/trunk/nltk@465

--- src/nltk/fsa.py
+++ src/nltk/fsa.py
@@ -30,7 +30,8 @@ epsilon = 0
 
 class FSA:
     def __init__(self, re):
-        self._final = 0
+        self._num = 0
+        self._finals = Set(0)
         self._table = {}
         re_list = _tokenizer.tokenize(re)
         tree = _parser.parse(re_list)
@@ -38,26 +39,29 @@ class FSA:
         self.remove_epsilons()
         self.minimize()
 
+    def _new_state(self):
+        self._num += 1
+        return self._num
+
     def _build(self, tree):
         if len(tree) == 0:
-            self._atomic(tree.node())
+            self.concat_char(tree.node())
         elif len(tree) == 1:
             self._build(tree[0])
         elif tree[0] == Tree('(') and tree[2] == Tree(')'):
             self._build(tree[1])
         elif tree[1] == Tree('*'):
-            start = self._final
+            start_states = self._finals
             self._build(tree[0])
-            self.insert_transition(self._final, epsilon, Set(start))
-            self.insert_transition(start, epsilon, Set(self._final))
+            self.kleene_star(start_states)
         elif tree[1] == Tree('+'):
-            start = self._final
+            start_states = self._finals
             self._build(tree[0])
-            self.insert_transition(self._final, epsilon, Set(start))
+            self.kleene_plus(start_states)
         elif tree[1] == Tree('?'):
-            start = self._final
+            start = self._finals
             self._build(tree[0])
-            self.insert_transition(start, epsilon, Set(self._final))
+            self.optional(start_states)
         else:
             self._build(tree[0])
             self._build(tree[1])
@@ -67,6 +71,10 @@ class FSA:
             self._table[(s1,label)].union(s2)
         else:
             self._table[(s1,label)] = s2
+
+    def insert_transitions(self, state_set, label, s2):
+        for s1 in state_set.elements():
+            self.insert_transition(s1, label, s2)
 
     def delete_transition(self, s1, label, s2):
         if self._table.has_key((s1,label)):
@@ -87,16 +95,29 @@ class FSA:
                 for (s1, label, s2) in self.transitions()
                 if label == epsilon]
 
+    # CONSIDER CHANGING DATA STRUCTURE SO THAT THIS IS EFFICIENT
     def outgoing_transitions(self, node):
         return [(s1,label,s2)
                 for (s1, label, s2) in self.transitions()
                 if s1 == node]
 
-    def _atomic(self, char):
-        state = self._final
-        self.insert_transition(state, char, Set(state+1))
-        self._final += 1
+    def concat_char(self, char):
+        new_final = Set(self._new_state())
+        for state in self._finals.elements():
+            self.insert_transition(state, char, new_final)
+        self._finals = new_final
         
+    def optional(self, start = Set(0)):
+#        self._finals = self._finals.intersection(start)
+        self.insert_transitions(start, epsilon, self._finals)
+
+    def kleene_plus(self, start = Set(0)):
+        self.insert_transitions(self._finals, epsilon, start)
+
+    def kleene_star(self, start = Set(0)):
+        self.kleene_plus(start)
+        self.optional(start)
+
 #    # add num to every state identifier
 #    def add(self, num):
 #        newtable = {}
@@ -108,14 +129,16 @@ class FSA:
 #        fsa.add(self._count) # relabel states for uniqueness
 #
 #        # TODO - add epsilon transition from finals to initials
-#        for final in self._finals:
+#        for final in self._finalss:
 #            self.insert_transition(final, epsilon, self._count)
 #        self._table.extend(fsa._table)
 
+    # this algorithm breaks when there are two epsilons in a row
     def remove_epsilons(self):
         while self.epsilon_transitions() != []:
-            print self.epsilon_transitions()
             for (s1, label1, s2) in self.epsilon_transitions():
+                if self._finals.intersection(s2):
+                    self._finals.insert(s1)
                 for state in s2.elements():
                     for (s3, label2, s4) in self.outgoing_transitions(state):
                         self.insert_transition(s1, label2, s4)
@@ -124,19 +147,29 @@ class FSA:
     def minimize(self):
         pass
 
+    # generate all strings in the language up to length maxlen
+    def generate(self, maxlen, state=0, prefix=""):
+        if maxlen > 0:
+            if self._finals.contains(state):
+                print prefix
+            for (s1, label, states) in self.outgoing_transitions(state):
+                for s2 in states.elements():
+                    self.generate(maxlen-1, s2, prefix+label)
+
     def pp(self):
         t = self.transitions()
         t.sort()
         for (s1, label, s2) in t:
             print s1, ':', label, '->', s2
-        print "Final:", self._final
+        print "Final:", self._finals
 
     # TODO - epsilon removal, minimization
 
 def demo():
-    re = 'a(b*)c'
+    re = 'a(b+)(c+)d'
     print 'Regular Expression:', re
     fsa = FSA(re)
     fsa.pp()
+    fsa.generate(10)
 
 if __name__ == '__main__': demo()
