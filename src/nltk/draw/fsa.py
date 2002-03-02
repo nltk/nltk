1465d6b060f69138f29c4fb0beddd34d1b1ed6e0
Edward Loper <edloper@ldc.upenn.edu>
2002-03-02 03:11:42 +0000

    - Added classes FSAFrame, which creates a frame with a canvas &   scrollbars, and puts a CanvasFSA in it. - Added class FSAWindow, which gives an easy way to create a window   with an FSAFrame in it. - Added constants VIEWBORDER and SCROLLBORDER to control how much   space is put around the fsa on the canvas - Fixed node-dragging with scrollbars; added bounds checking to   node-dragging - Added _curve attribute to edges, to prevent edge overlap. - Lower edge tags, so the tag_binds of nodes take precedence (makes   node-dragging much easier) - Added code to CanvasEdge to handle self-looping edges - Added a collapse_edge param to CanvasFSA: this causes it to display   mutliple edges between the same nodes as one edge, with their labels   separated by commas. - Improved node-arranging code somewhat: now placing nodes into levels   using depth-first, rather than breadth-first, algorithm. - Added code to properly handle arrangement for cycles: we select the   node which can reach the most unexpanded nodes as the node to add to   level 0. - Got rid of _LevelNode
    svn/trunk/nltk@474

--- src/nltk/draw/fsa.py
+++ src/nltk/draw/fsa.py
@@ -1,10 +1,25 @@
 """
 Draw a finite state automoton
+
+  - CanvasNode: a node, displayed on a canvas.
+  - CanvasEdge: an edge, displayed on a canvas.
+  - CanvasFSA: an FSA, displayed on a canvas.
+  - FSAView: A widget showing an FSA
+  
 """
 
 import Tkinter, random, math, nltk.fsa
 
-class _Node:
+# How much extra space should we add around the FSA, to make it
+# viewable.  If this were zero, then all nodes at the edges would be
+# only half-visible.
+VIEWBORDER = 70
+
+# How much extra scroll-space should we provide, to let the user move
+# nodes around?  This is in addition to the view border. 
+SCROLLBORDER = 50
+
+class CanvasNode:
     def __init__(self, x, y, label, canvas):
         self._x = x
         self._y = y
@@ -60,48 +75,28 @@ class _Node:
     def _press_cb(self, event):
         self._mcb_id = self._canvas.bind('<Motion>', self._motion_cb)
         self._rcb_id = self._canvas.bind('<ButtonRelease-1>', self._release_cb)
+        self._scrollregion = [int(n) for n in self._canvas['scrollregion'].split()]
 
     def _motion_cb(self, event):
-        (x,y) = (event.x, event.y)
+        x = self._canvas.canvasx(event.x)
+        y = self._canvas.canvasy(event.y)
+
+        # Bounds checking:
+        x = max(self._scrollregion[0], min(self._scrollregion[2], x))
+        y = max(self._scrollregion[1], min(self._scrollregion[3], y))
+        
         self.move(x, y)
 
     def _release_cb(self, event):
         self._canvas.unbind('<Motion>', self._mcb_id)
         self._canvas.unbind('<Motion>', self._rcb_id)
 
-class _LevelNode(_Node):
-    """
-    A node whose position is specified (initially, at least) by a
-    level and levelpos within that detph..
-    """
-    XWIDTH = 80
-    YWIDTH = 70
-    def __init__(self, level, levelpos, label, canvas):
-        self._level = level
-        self._levelpos = levelpos
-        (x,y) = self._coords()
-        _Node.__init__(self, x, y, label, canvas)
-
-    def _coords(self):
-        if self._level is None or self._levelpos is None:
-            return (0, 0)
-        else:
-            return (self._level * _LevelNode.XWIDTH + 20,
-                    self._levelpos * _LevelNode.YWIDTH + 20)
-
-    def level(self): return self._level
-    def levelpos(self): return self._levelpos
-    def levelmove(self, level, levelpos):
-        self._level = level
-        self._levelpos = levelpos
-        _Node.move(self, *self._coords())
-
     def __repr__(self):
-        return '<Node %s level=%s, lpos=%s>' % (self.label(), self._level,
-                                                self._levelpos)
+        return '<Node %s @%sx%s>' % (self._label, self._x, self._y)
+    def __str__(self):
+        return '<Node %s>' % (self._label)
 
-class _Edge:
-    CURVE = 0.15
+class CanvasEdge:
     def __init__(self, node1, node2, label, canvas):
         self._node1 = node1
         self._node2 = node2
@@ -110,6 +105,13 @@ class _Edge:
         self._tags = None
         self._linecolor = self._textcolor = 'black'
         self._font = None
+        self._collapse_edges = 0
+
+        # How curvey should we be?
+        other_edges = 1
+        for edge in node1.outgoing():
+            if edge.to_node() is node2: other_edges += 1
+        self._curve = 0.2 * other_edges
 
     def draw(self):
         if self._tags is not None: return
@@ -122,6 +124,10 @@ class _Edge:
                                         anchor='c', fill=self._textcolor)
         if self._font: self._canvas.itemconfig(tag2, font=self._font)
         self._tags = [tag1, tag2]
+
+        # Lower the edge, so the tag_binds of nodes take precedence
+        self._canvas.lower(tag1)
+        self._canvas.lower(tag2)
 
     def erase(self):
         if self._tags is None: return
@@ -144,29 +150,47 @@ class _Edge:
         y1 = self._node1.y()
         x2 = self._node2.x()
         y2 = self._node2.y()
-        r = math.sqrt((x1-x2)**2 + (y1-y2)**2)
-        textx = (x1+x2)*0.5 + (y2-y1)*(_Edge.CURVE/2) + (y2-y1) * 8 / r
-        texty = (y1+y2)*0.5 - (x2-x1)*(_Edge.CURVE/2) - (x2-x1) * 8 / r
-        return (textx, texty)
+        if self._node1 is self._node2:
+            # Self-loops.
+            return (x1, y1+150*self._curve)
+        else:
+            # Normal edges.
+            r = max(math.sqrt((x1-x2)**2 + (y1-y2)**2), 1)
+            textx = (x1+x2)*0.5 + (y2-y1)*(self._curve/2) + (y2-y1) * 8 / r
+            texty = (y1+y2)*0.5 - (x2-x1)*(self._curve/2) - (x2-x1) * 8 / r
+            return (textx, texty)
     
     def _line_coords(self):
         x1 = self._node1.x()
         y1 = self._node1.y()
         x2 = self._node2.x()
         y2 = self._node2.y()
-        x3 = (x1+x2)*0.5 + (y2-y1)*_Edge.CURVE
-        y3 = (y1+y2)*0.5 - (x2-x1)*_Edge.CURVE
-        # Adjust endpoints..
-        r = max(math.sqrt((x1-x2)**2 + (y1-y2)**2), 1)
-        x2 -= (x2-x1) * 5 / r
-        y2 -= (y2-y1) * 5 / r
-        x1 -= (x1-x2) * 5 / r
-        y1 -= (y1-y2) * 5 / r
-        return (x1, y1, x3, y3, x2, y2)
+        
+        if self._node1 is self._node2:
+            # Self-loops
+            x3 = x1 - 70*self._curve
+            y3 = y1 + 70*self._curve
+            x4 = x1
+            y4 = y1 + 140*self._curve
+            x5 = x1 + 70*self._curve
+            y5 = y1 + 70*self._curve
+            return (x1, y1, x3, y3, x4, y4, x5, y5, x2, y2)
+        else:
+            # Normal edges.
+            x3 = (x1+x2)*0.5 + (y2-y1)*self._curve
+            y3 = (y1+y2)*0.5 - (x2-x1)*self._curve
+            # Adjust endpoints so they end at the node parimeter.
+            r = max(math.sqrt((x1-x2)**2 + (y1-y2)**2), 1)
+            x2 -= (x2-x1) * 5 / r
+            y2 -= (y2-y1) * 5 / r
+            x1 -= (x1-x2) * 5 / r
+            y1 -= (y1-y2) * 5 / r
+            return (x1, y1, x3, y3, x2, y2)
 
     def from_node(self): return self._node1
     def to_node(self): return self._node2
     def label(self): return self._label
+    def set_label(self, newlabel): self._label = newlabel
 
     def update(self):
         """
@@ -176,22 +200,37 @@ class _Edge:
             self._canvas.coords(self._tags[0], self._line_coords())
             self._canvas.coords(self._tags[1], self._text_coords())
 
-class FSADraw:
-    def __init__(self, fsa, canvas):
+    def __repr__(self):
+        return '%s-->%s' % (self._node1, self._node2)
+
+class CanvasFSA:
+    def __init__(self, fsa, canvas, collapse_edges = 1):
         self._fsa = fsa
         self._canvas = canvas
+        self._collapse_edges = collapse_edges
 
         nodes = self._nodes = {}
         edges = self._edges = []
 
-        if canvas['scrollregion']:
-            (self._left, self._top, self._right, self._bot) = canvas['scrollregion']
-        else:
-            self._left = self._top = 0
-            self._right = int(canvas['width'])
-            self._bot = int(canvas['height'])
-
         self._add_fsa(fsa)
+
+        # Adjust the canvas size...
+        minx = miny = maxx = maxy = 0
+        for node in self._nodes.values():
+            (x, y) = (node.x(), node.y())
+            minx = min(x, minx)
+            miny = min(y, miny)
+            maxx = max(x, maxx)
+            maxy = max(y, maxy)
+
+        minx -= VIEWBORDER
+        miny -= VIEWBORDER
+        maxx += VIEWBORDER
+        maxy += VIEWBORDER
+        canvas['scrollregion'] = (minx-SCROLLBORDER, miny-SCROLLBORDER,
+                                  maxx+SCROLLBORDER, maxy+SCROLLBORDER)
+        canvas.xview('moveto', (SCROLLBORDER / (2.0 * SCROLLBORDER + maxx-minx)))
+        canvas.yview('moveto', (SCROLLBORDER / (2.0 * SCROLLBORDER + maxy-miny)))
 
     def _add_fsa(self, fsa):
         nodes = self._nodes
@@ -203,20 +242,32 @@ class FSADraw:
                 if not nodes.has_key(s2): nodes[s2] = self._make_node(s2)
 
                 # Add the edge
-                edges.append(self._make_edge(nodes[s1], nodes[s2], label))
+                if not self._collapse_edges:
+                    edges.append(self._make_edge(nodes[s1], nodes[s2], label))
+                    # Tell the nodes about it the edge.
+                    nodes[s1].add_outgoing(edges[-1])
+                    nodes[s2].add_incoming(edges[-1])
+                else:
+                    edge_collapsed = 0
+                    for edge in nodes[s1].outgoing():
+                        if edge.to_node() is nodes[s2]:
+                            edge.set_label(edge.label()+', '+label)
+                            edge_collapsed = 1
+                            break
+                    if not edge_collapsed:
+                        edges.append(self._make_edge(nodes[s1], nodes[s2], label))
+                        # Tell the nodes about it the edge.
+                        nodes[s1].add_outgoing(edges[-1])
+                        nodes[s2].add_incoming(edges[-1])
 
-                # Tell the nodes about it the edge.
-                nodes[s1].add_outgoing(edges[-1])
-                nodes[s2].add_incoming(edges[-1])
-
-        self._arrange()
+        self.arrange()
 
         # Draw them
         for node in self._nodes.values(): node.draw()
         for edge in edges: edge.draw()
 
     def _make_node(self, state):
-        node = _LevelNode(None, None, str(state), self._canvas)
+        node = CanvasNode(None, None, str(state), self._canvas)
         if state in self._fsa._finals: node.color('red')
         else: node.color('green3')
         return node
@@ -224,64 +275,209 @@ class FSADraw:
     def _make_edge(self, node1, node2, label):
         # Handle epsilon transitions
         if label != nltk.fsa.epsilon:
-            edge = _Edge(node1, node2, label, self._canvas)
+            edge = CanvasEdge(node1, node2, label, self._canvas)
+            edge.color('cyan4', 'blue4')
         else:
-            edge = _Edge(node1, node2, 'e', self._canvas)
+            edge = CanvasEdge(node1, node2, 'e', self._canvas)
             edge.font(('symbol', 12))
-        edge.color('blue4', 'blue3')
+            edge.color('gray50', 'gray40')
         return edge
     
-    def _arrange(self):
-        nodes = self._nodes
-        edges = self._edges
+    def arrange(self):
+        """
+        Set the node positions.  This routine should attempt to
+        minimize the number of crossing edges, in order to make the
+        graph easier to read.
+        """
+        self._arrange_into_levels()
 
-        total_nodes = len(self._nodes)
-        nodes_processed = 0
-        
-        # Get the 1st-level nodes.
-        levels = [[]]
-        for (state, node) in nodes.items():
-            if len(node.incoming()) == 0:
-                levels[0].append(node)
-                node.levelmove(0, None)
-                nodes_processed += 1
-
-        # If there were none..
-        if levels[-1] == []:
-            levels[-1] = nodes.values()[:1]
-
-        # Put the remaining nodes into levels
-        levelnum = 0
-        while (nodes_processed < total_nodes):
-            if levelnum > 10: raise ValueError()
-            levels.append([])
-            for node1 in levels[levelnum]:
-                for edge in node1.outgoing():
-                    node2 = edge.to_node()
-                    if node2.level() is None:
-                        levels[-1].append(node2)
-                        node2.levelmove(levelnum+1, None)
-                        nodes_processed += 1
-            levelnum += 1
+        self._arrange_levels()
 
         # Now we want to minimize crossing edges.. how, again? :)
-        for i in range(len(levels)):
-            for j in range(len(levels[i])):
-                levels[i][j].levelmove(i, j)
+        for i in range(len(self._levels)):
+            for j in range(len(self._levels[i])):
+                self._levels[i][j].move(i*80, j*70)
+
+    def _arrange_levels(self):
+        """
+        Re-arrange each level to (locally) minimize the number of
+        crossing edges.
+        """
+        # For now, put nodes with more incoming edges further down.
+        for level in self._levels:
+            level.sort(lambda n1,n2:cmp(len(n1.incoming()), len(n2.incoming())))
+
+    def _arrange_into_levels(self):
+        """
+        Assign a level to each node.
+        """
+        # Mapping from node to level.
+        self._nodelevel = {}
+        self._levels = []
+
+        nodelist = self._nodes.values()
+        
+        # Find any nodes that have no incoming edges; put all of these
+        # in level 0.
+        toplevel = []
+        for node in nodelist:
+            if len(node.incoming()) == 0:
+                toplevel.append(node)
+                self._nodelevel[node] = 0
+
+        # Expand all of their children.
+        self._levels = [toplevel]
+        self._add_descendants(toplevel, 1)
+
+        # If we didn't get all the nodes, we'll have to start picking
+        # nodes that do have incoming transitions.  Pick the ones that
+        # have the most reachable nodes.  (n.b., this implementation
+        # isn't terribly efficient, but we dont' expect to be
+        # displaying huge FSAs, so it should be ok)
+        while len(self._nodelevel) < len(self._nodes):
+            expand_node = None
+            max_reachable = -1
+
+            for node in nodelist:
+                reachable = self._reachable(node)
+                if reachable >= max_reachable:
+                    max_reachable = reachable
+                    expand_node = node
+            
+            # Expand the new node's children.
+            self._levels[0].append(expand_node)
+            self._nodelevel[expand_node] = 0
+            self._add_descendants(toplevel, 1)
+    
+    def _reachable(self, node, reached=None):
+        """
+        How many *unexpanded* nodes can be reached from the given node?
+        """
+        if self._nodelevel.has_key(node): return 0
+        if reached is None: reached = {}
+        if not reached.has_key(node):
+            reached[node] = 1
+            for edge in node.outgoing():
+                self._reachable(edge.to_node(), reached)
+        return len(reached)
+
+    def _add_descendants(self, parent_level, levelnum):
+        """
+        Add all the descendants of the nodes in the list parent_level
+        to the structures self._level and self._nodelevel.
+        """
+        self._add_descendants_dfs(parent_level, levelnum)
+
+    def _add_descendants_dfs(self, parent_level, levelnum):
+        if levelnum >= len(self._levels): self._levels.append([])
+        for parent_node in parent_level:
+            # Add the parent node
+            if not self._nodelevel.has_key(parent_node):
+                self._levels[levelnum-1].append(parent_node)
+                self._nodelevel[parent_node] = levelnum-1
+
+            # Recurse to its children
+            child_nodes = [edge.to_node() for edge in parent_node.outgoing()
+                           if not self._nodelevel.has_key(edge.to_node())]
+            if len(child_nodes) > 0:
+                self._add_descendants_dfs(child_nodes, levelnum+1)
+
+    def _add_descendants_bfs(self, parent_level, levelnum):
+        all_child_nodes = []
+        if levelnum >= len(self._levels): self._levels.append([])
+        for parent_node in parent_level:
+            print ('  '*levelnum), parent_node
+            child_nodes = [edge.to_node() for edge in parent_node.outgoing()
+                           if not self._nodelevel.has_key(edge.to_node())]
+            for node in child_nodes:
+                self._levels[levelnum].append(node)
+                self._nodelevel[node] = levelnum
+            all_child_nodes += child_nodes
+        if len(all_child_nodes) > 0:
+            self._add_descendants_bfs(all_child_nodes, levelnum+1)
+
+class FSAFrame:
+    "Widget to view FSA's."
+
+    MINX = MINY = 60
+    MAXX = 800
+    MAXY = 400
+    
+    def __init__(self, parent, fsa, **kw):
+        self._parent = parent
+        self._fsa = fsa
+
+        self._frame = frame = Tkinter.Frame(parent)
+        self._canvas = canvas = Tkinter.Canvas(frame, closeenough='10.0', **kw)
+        xscrollbar = Tkinter.Scrollbar(parent, orient='horizontal')
+        yscrollbar = Tkinter.Scrollbar(parent, orient='vertical')
+        xscrollbar['command'] = canvas.xview
+        yscrollbar['command'] = canvas.yview
+        canvas['xscrollcommand'] = xscrollbar.set
+        canvas['yscrollcommand'] = yscrollbar.set
+        
+        canvas.pack(expand=1, fill='both', side='left')
+        yscrollbar.pack(fill='y', side='right')
+        xscrollbar.pack(fill='x', side='bottom')
+        
+        self._canvas_fsa = CanvasFSA(fsa, canvas)
+
+    def pack(self, cnf={}, **kw):
+        self._frame.pack(cnf, **kw)
+        (minx, miny, maxx, maxy) = self._canvas['scrollregion'].split()
+        self._canvas['width'] = max(FSAFrame.MINX, min(FSAFrame.MAXX,
+                                            int(maxx)-int(minx)-2*SCROLLBORDER))
+        self._canvas['height'] = max(FSAFrame.MINY, min(FSAFrame.MAXY,
+                                             int(maxy)-int(miny)-2*SCROLLBORDER))
+
+class FSAWindow:
+    "Window to view FSA's."
+    def __init__(self, fsa):
+        self._top = Tkinter.Tk()
+        self._top.bind('q', self.destroy)
+
+        # Ok button.
+        buttonframe = Tkinter.Frame(self._top)
+        buttonframe.pack(fill='x', side='bottom')
+        ok = Tkinter.Button(buttonframe, text='Done', command=self.destroy)
+        ok.pack(side='left')
+        help = Tkinter.Button(buttonframe, text='Help')
+        help.pack(side='right')
+
+        # FSAFrame.
+        fsaframe = FSAFrame(self._top, fsa, background='white')
+        fsaframe.pack(side='bottom', expand=1, fill='both')
+
+    def destroy(self, *e):
+        if self._top is not None: self._top.destroy()
+        self._top = None
+
+    def mainloop(self, n=0):
+        self._top.mainloop(n)
 
 def show_fsa(fsa):
     top = Tkinter.Tk()
     def destroy(e, top=top): top.destroy()
     top.bind('q', destroy)
-    c = Tkinter.Canvas(top, closeenough='3.0', width=500, height=300)
+    c = Tkinter.Canvas(top, closeenough='10.0', width=500, height=300)
     c.pack()
-    f = FSADraw(fsa, c)
+    f = CanvasFSA(fsa, c)
     top.mainloop()
                 
-# Main
+# Test
 if __name__ == '__main__':
+    from nltk.set import Set
+    #re = 'ab'
+    #re = '(ab(c*)c)*dea(b*)'
     re = '(ab(c*)c)*'
-    print 'Regular Expression:', re
-    fsa = nltk.fsa.FSA(re)
-    print fsa.pp()
-    show_fsa(fsa)
+    fsa = nltk.fsa.FSA("abcde")
+    nltk.fsa.re2nfa(fsa, re)
+    fsa = fsa.dfa()
+    fsa.prune()
+    #fsa.insert_transition(3,'hi',0)
+    #fsa.delete_transition(0, nltk.fsa.epsilon, Set(1))
+    w = FSAWindow(fsa)
+
+    # n.b., this is optional, as long as you're running this from an
+    # interactive Python shell..
+    #w.mainloop()
