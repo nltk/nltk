89fc7958f2c38466aa10295e3afd45bd8fc90f69
Edward Loper <edloper@ldc.upenn.edu>
2002-05-10 04:11:57 +0000

    - Added lots of docstrings
    svn/trunk@572

--- src/nltk/cfg.py
+++ src/nltk/cfg.py
@@ -9,7 +9,82 @@
 # $Id$
 
 """
-Context free grammars.  This module will eventually replace C{Rule}.
+Basic data classes for representing context free grammars.  A
+X{grammar} specifies which trees can represent the structure of a
+given text.  Each of these trees is called a X{parse tree} for the
+text (or simply a X{parse}).  In a X{context free} grammar, the set of
+parse trees for any piece of a text can depend only on that piece, and
+not on the rest of the text (i.e., the piece's context).  Context free
+grammars are often used to find possible syntactic structures for
+sentences.  In this context, the leaves of a parse tree are word
+tokens; and the node values are phrasal categories, such as C{"NP"}
+and C{"VP"}.
+
+The C{CFG} class is used to encode context free grammars.  Each C{CFG}
+consists of a start symbol and a set of rules.  The X{start symbol}
+specifies the root node value for parse trees.  For example, the start
+symbol for syntactic parsing is usually C{"S"}.  Start symbols are
+encoded using the C{Nonterminal} class, which is discussed below.
+
+A CFG's X{rules} specify what parent-child relationships a parse tree
+can contain.  Each rule specifies that a particular node can be the
+parent of a particular set of children.  For example, the rule C{"<S>
+-> <NP> <VP>"} specifies that an C{"S"} node can be the parent of an
+C{"NP"} node and a C{"VP"} node.
+
+CFG rules are implemented by the C{CFG_Rule} class.  Each C{CFG_Rule}
+consists of a left hand side and a right hand side.  The X{left hand
+side} is a C{Nonterminal} that specifies the node type for a potential
+parent; and the X{right hand side} is a list that specifies allowable
+children for that parent.  This lists consists of C{Nonterminals} and
+text tokens: each C{Nonterminal} indicates that the corresponding
+child may be a C{TreeToken} with the specified node type; and each
+text type indicates that the corresponding child may be a C{Token}
+with the with that type.
+
+The C{Nonterminal} class is used to distinguish node values from leaf
+values.  This prevents the grammar from accidentally using a leaf
+value (such as the English word "A") as the node of a subtree.  Within
+a C{CFG}, all node values are wrapped in the C{Nonterminal} class.
+Note, however, that the trees that are specified by the grammar do
+E{not} include these C{Nonterminal} wrappers.
+
+CFGs can also be given a more procedural interpretation.  According to
+this interpretation, a CFG specifies any tree structure M{tree} that
+can be produced by the following procedure:
+
+    - Set M{tree} to the start symbol
+    - Repeat until M{tree} contains no more nonterminals:
+      - Choose a rule M{rule} with whose left hand side M{lhs} is a
+        nonterminal in M{tree}.
+      - Replace one occurance of M{lhs} with a subtree, whose node
+        value is the value wrapped by the nonterminal M{lhs}, and
+        whose children are the right hand side of {rule}.
+
+The operation of replacing the left hand side (M{lhs}) of a rule with
+the right hand side (M{rhs}) in a tree (M{tree}) is known as
+X{expanding} M{lhs} to M{rhs} in {tree}.
+
+Probablistic CFGs
+=================
+
+
+String Representaitons
+======================
+A C{CFG_Rule} whose left hand side is C{M{lhs}} and whose right hand
+side is C{M{rhs}} is written "C{M{lhs}->M{rhs}}".  A C{Nonterminal}
+with symbol M{s} is written "C{<M{s}>}" Thus, for example, the
+C{CFG_Rule} that specifies that a tree with node value C{"NP"} can
+have children that are subtrees with node values C{"Det"} and C{"N"}
+is written::
+
+    <NP> -> Det N
+
+And the rule that specifies that a tree with node value C{"N"} can
+have a leaf with text type C{"dog"} is::
+
+    <N> -> 'dog'
+
 """
 
 from nltk.token import *
@@ -24,35 +99,34 @@ from types import IntType as _IntType
 
 class Nonterminal:
     """
-    A non-terminal symbol for a grammar rule.  Non-terminals are used
-    to represent elements that can be expanded to zero or more
-    terminals.  
+    A non-terminal symbol for a context free grammar.  C{Nonterminal}
+    is a wrapper class for node values; it is used by C{CFG_Rule}s to
+    distinguish node values from leaf values.  The node value that is
+    wrapped by a C{Nonterminal} is known as its X{symbol}.  Symbols
+    are typically strings representing phrasal categories (such as
+    C{"NP"} or C{"VP"}).  However, more complex symbol types are
+    sometimes used (e.g., for lexicalized grammars).  Since symbols
+    are node values, they must be immutable and hashable.  Two
+    C{Nonterminal}s are considered equal if their symbols are equal.
 
-    Each non-terminal is based on a X{symbol}, which must be immutable
-    and hashable.  Two non-terminals are considered equal if their
-    symbols are equal.  Symbols are typically strings representing
-    phrasal categories (such as C{"NP"} or C{"VP"}).  However, more
-    complex symbol types are sometimes used (e.g., for lexicalized
-    grammars).
-
-    @type _symbol: immutable
-    @ivar _symbol: This non-terminal's base symbol.  Two nonterminals
-        are considered equal if their symbols are equal.
+    @type _symbol: (any)
+    @ivar _symbol: The node value corresponding to this
+        C{Nonterminal}.  This value must be immutable and hashable. 
     """
     def __init__(self, symbol):
         """
         Construct a new non-terminal from the given symbol.
 
         @type symbol: (any)
-        @param symbol: The new non-terminal's symbol.  The new
-            non-terminal is considered equal to another non-terminal
-            if their symbols are equal.
+        @param symbol: The node value corresponding to this
+            C{Nonterminal}.  This value must be immutable and
+            hashable. 
         """
         self._symbol = symbol
 
     def symbol(self):
         """
-        @return: This non-terminal's symbol.
+        @return: The node value corresponding to this C{Nonterminal}. 
         @rtype: (any)
         """
         return self._symbol
@@ -86,10 +160,19 @@ class Nonterminal:
         return hash(self._symbol)
 
     def __repr__(self):
+        """
+        @return: A verbose representation for this C{Nonterminal}.
+            The verbose representation for a C{Nonterminal} whose
+            symbol is C{M{s}} is C{<M{s}>}.
+        @rtype: C{string}
+        """
         return '<%s>' % self._symbol
 
     def __str__(self):
-        return str(self._symbol)
+        """
+        TEMPORARY
+        """
+        return '%s' % self._symbol
 
 #################################################################
 # CFGRule and CFG
@@ -147,9 +230,9 @@ class CFG_Rule:
         @return: A verbose string representation of the C{Rule}.
         @rtype: C{string}
         """
-        str = '<%s> ->' % self._lhs
+        str = '%r ->' % self._lhs
         for elt in self._rhs:
-            if isinstance(elt, Nonterminal): str += ' <%s>' % elt
+            if isinstance(elt, Nonterminal): str += ' %r' % elt
             else: str += ' %r' % elt
         return str
 
