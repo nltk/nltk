9fdea660bb6f00a6b8d39c409ddcaf7243f15f3b
Edward Loper <edloper@ldc.upenn.edu>
2002-11-12 15:57:09 +0000

    - Added a BLT-based implementation of plot.  It's used if BLT & PWM are   installed.
    svn/trunk@851

--- src/nltk/draw/plot.py
+++ src/nltk/draw/plot.py
@@ -11,6 +11,10 @@
 A simple tool for plotting functions.
 """
 
+# Delegate to BLT?
+#   <http://sourceforge.net/projects/blt/>
+#   
+
 #from chktype import chktype as _chktype 
 from types import FunctionType as _FunctionType
 from types import MethodType as _MethodType
@@ -18,9 +22,142 @@ from types import BuiltinFunctionType as _BuiltinFunctionType
 from types import IntType as _IntType
 from types import FloatType as _FloatType
 from math import log
-import Tkinter
+import Tkinter, sys
 
-class Plot:
+class BLTPlot:
+    def __init__(self, vals, rng=None, **kwargs):
+        self._dragging = 0
+        self._x0 = self._y0 = self._x1 = self._y1 = 0
+        
+        self._root = Tkinter.Tk()
+        self._root.bind('q', self.destroy)
+        try:
+            import Pmw
+            # This import is needed to prevent an error if we create
+            # more than 1 graph in the same interactive session:
+            reload(Pmw.Blt)
+            
+            Pmw.initialise()
+            self._graph = Pmw.Blt.Graph(self._root)
+            self._graph.pack(expand=1, fill='both')
+        except:
+            self._root.destroy()
+            raise
+            raise AssertionError('Pwm not installed!')
+
+        self._graph.configure(title='Left: zoom in.  Right: zoom out.')
+        
+        # Derive vals and range.
+        if type(vals) in (_FunctionType, _BuiltinFunctionType,
+                            _MethodType):
+            if rng == None: rng = [x*0.1 for x in range(-100, 100)]
+            vals = [vals(i) for i in rng]
+        elif len(vals) > 0 and type(vals[0]) in (type(()), type([])):
+                rng = [x for (x,y) in vals]
+                vals = [y for (x,y) in vals]
+        elif rng == None:
+            rng = range(len(vals))
+        self._vals = tuple(vals)
+        self._rng = tuple(rng)
+
+        self._graph.line_create(kwargs.get('title', 'Plot'),
+                                xdata=self._rng,
+                                ydata=self._vals,
+                                symbol='')
+        self._graph.grid_on()
+        if not kwargs.has_key('title'):
+            print self._graph.legend_configure(hide=1)
+        
+        scale = kwargs.get('scale', 'linear')
+        self._xscale = self._yscale = 'linear'
+        if scale in ('log-linear', 'log_linear', 'log'):
+            self._graph.xaxis_configure(logscale=1)
+            self._xscale = 'log'
+        if scale in ('linear-log', 'linear_log', 'log'):
+            self._graph.yaxis_configure(logscale=1)
+            self._yscale = 'log'
+
+        # Find max/min's.
+        self._xmax = max(rng)
+        self._xmin = min(rng)
+        self._ymax = max(vals)
+        self._ymin = min(vals)
+
+        # Set up zooming
+        self._graph.bind(sequence="<ButtonPress>", func=self.mouseDown)
+        self._graph.bind(sequence="<ButtonRelease>", func=self.mouseUp)
+        
+    def zoom(self, x0, y0, x1, y1):
+        if x0 < self._xmin:
+            x1 += (self._xmin - x0)
+            x0 = self._xmin
+        if y0 < self._ymin:
+            y1 += (self._ymin - y0)
+            y0 = self._ymin
+        x1 = min(self._xmax, x1)
+        y1 = min(self._ymax, y1)
+        self._graph.xaxis_configure(min=x0, max=x1)
+        self._graph.yaxis_configure(min=y0, max=y1)
+
+    def mouseDrag(self, event):
+        if not self._dragging: return
+        (x0, y0) = (self._x0, self._y0)
+        (x1, y1) = self._graph.invtransform(event.x, event.y)
+        (self._x1, self._y1) = (x1, y1)
+        
+        self._graph.marker_configure("zoom", 
+                                     coords = (x0, y0, x1, y0, x1,
+                                               y1, x0, y1, x0, y0))
+    
+    def mouseUp(self, event):
+        (x0, y0) = (self._x0, self._y0)
+        (x1, y1) = (self._x1, self._y1)
+        if self._dragging:
+            self._graph.unbind("<Motion>", self._bindid)
+            self._graph.marker_delete("zoom")
+            
+            if x0 != x1 and y0 != y1:
+                # make sure the coordinates are sorted
+                if x0 > x1: x0, x1 = x1, x0
+                if y0 > y1: y0, y1 = y1, y0
+         
+                if event.num == 1:
+                   self.zoom(x0, y0, x1, y1) # zoom in
+                else:
+                   (X0, X1) = self._graph.xaxis_limits()
+                   k  = (X1-X0)/(x1-x0)
+                   x0 = X0 -(x0-X0)*k
+                   x1 = X1 +(X1-x1)*k
+                   
+                   (Y0, Y1) = self._graph.yaxis_limits()
+                   k  = (Y1-Y0)/(y1-y0)
+                   y0 = Y0 -(y0-Y0)*k
+                   y1 = Y1 +(Y1-y1)*k
+                   
+                   self.zoom(x0, y0, x1, y1) # zoom out
+                           
+    def mouseDown(self, event):
+        self._dragging = 0
+        
+        if self._graph.inside(event.x, event.y):
+            self._dragging = 1
+            (x0, y0) = self._graph.invtransform(event.x, event.y)
+            (self._x0, self._y0) = (x0, y0)
+            
+            self._graph.marker_create("line", name="zoom",
+                                      dashes=(2, 2))
+            self._bindid = self._graph.bind("<Motion>", self.mouseDrag)
+    
+    def destroy(self, *args):
+        if self._root is None: return
+        self._root.destroy()
+        self._root = None
+
+    def mainloop(self, *varargs, **kwargs):
+        self._root.mainloop(*varargs, **kwargs)
+        
+
+class SimplePlot:
     def __init__(self, vals, rng=None, **kwargs):
         """
         @type vals: C{function} or (C{list} of C{number}s) or (C{list} of
@@ -157,8 +294,8 @@ class Plot:
         self._c.postscript(file=filename, x=float(x0), y=float(y0),
                            width=float(w)+2, height=float(h)+2)
         
-
     def destroy(self, *args):
+        if self._root is None: return
         self._root.destroy()
         self._root = None
 
@@ -211,8 +348,23 @@ class Plot:
             line.append( (x,y) )
         self._c.create_line(line)
 
+    def mainloop(self, *varargs, **kwargs):
+        self._root.mainloop(*varargs, **kwargs)
 
-if __name__ == '__main__':        
-    Plot(lambda x:x*x*x, range(-30,30))
+def Plot(vals, rng=None, **kwargs):
+    """
+    A factory function that creates a Plot graph.  The Plot graph will
+    either be a L{BLTPlot} (if BLT is installed), or a L{SimplePlot}
+    (if BLT is not installed).
+    """
+    return BLTPlot(vals, rng, **kwargs)
+    try: return BLTPlot(vals, rng, **kwargs)
+    except: return SimplePlot(vals, rng, **kwargs)
+
+if __name__ == '__main__':
+    Plot(lambda x:x**2-x**3,
+         [0.01*x for x in range(1,100)],
+         scale='log').mainloop()
+    
 
     
