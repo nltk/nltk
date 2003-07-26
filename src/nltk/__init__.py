02dff48c6ef84335236632d22c171150803b725a
Edward Loper <edloper@ldc.upenn.edu>
2003-07-26 18:26:38 +0000

    - Rewrote the package docstring.
    svn/trunk@936

--- src/nltk/__init__.py
+++ src/nltk/__init__.py
@@ -8,127 +8,72 @@
 # $Id$
 
 """
-The Natural Language Toolkit is a package intended to simplify the
-task of programming natural language systems.  It is intended to be
-used as a teaching tool, not as a basis for building production
-systems.
+NLTK, the Natural Language Toolkit, is a collection of modules that
+simplify the task of creating natural language systems.  NLTK covers
+symbolic and statistical natural language processing, and is
+interfaced to annotated corpora.
 
-@author: Edward Loper
-@version: 1.0
-"""
+NLTK is primarily intended for use as a teaching tool.  Along with the
+tutorials and problem sets available from U{the NLTK
+webpage<http://nltk.sourceforge.net}, NLTK provides ready-to-use
+computational linguistics courseware.  Students can augment and
+replace existing components, learning structured programming by
+example, and manipulating sophisticated models from the outset.
 
-# Leave this out for now... It's out of date.
-"""
-The natural language toolkit is under active development.
+NLTK can also be used to implement small research systems, and to
+quickly prototype larger research or production systems.  
+
+Modules
+=======
+NLTK is implemented as a collection of independent modules, each of
+which defines a specific data structure or task. A set of core modules
+defines basic data types and processing systems that are used
+throughout the toolkit.
+
+  - The L{token} module provides basic classes for processing
+  individual elements of text, such as words or sentences.
+
+  - The L{tree} module defines data structures for representing tree
+  structures over text, such as syntax trees and morphological trees.
+
+  - TThe L{probability} module implements classes that encode
+  frequency distributions and probability distributions, including a
+  variety of statistical smoothing techniques.
+
+The remaining modules define data structures and interfaces for
+performing specific natural language processing tasks.  This list of
+modules will grow over time, as we add new tasks and algorithms to the
+toolkit.
 
 Interfaces
-==========
+----------
+Natural language processing task (such as parsing or classifying
+texts) are defined by interfaces.  An X{interface} is a special type
+of base class that specifies a set of methods that must be supported.
+For example, the L{ParserI<nltk.parser.ParserI>} interface specifies
+that parsers must support the L{parse()<nltk.parser.ParserI.parse>}
+method.
 
-The Natural Language Toolkit is implemented as a set of interfaces and
-classes.  Interfaces are a concept loosely borrowed from Java.  They
-are essentially a specification of a set of methods.  Any class that
-implements all of an interface's methods according to the interface's
-specification are said to \"implement\" that interface.
+Each subclass of an interface provides an implementation for the
+natural language processing task defined by the interface.  For
+example, L{ShiftReduceParser<nltk.parser.ShiftReduceParser>},
+L{RecursiveDescentParser<nltk.parser.RecursiveDescentParser>}, and
+L{ChartParser<nltk.parser.chart.ChartParser>} are all implementations
+of the C{ParserI} interface.
 
-In the context of this toolkit, an interface is implemented as a
-class, all of whose methods simply raise AssertionError.  The
-__doc__ strings of these methods, together with the methods'
-declarations,  provide specifications for the methods.
-
-Interface classes are named with a trailing \"I\", such as
-C{TokenizerI} or C{EventI}.
-
-Interface and Class Hierarchy
-=============================
-
-The classes defined by the Natural Language Toolkit can be divided
-into two basic categories: Data classes; and Processing (or
-Task-Oriented) Classes. 
-
-Data Classes
-------------
-
-Data classes are used to store several different types of information
-that are relavant to natural language processing.  Data classes can
-generally be grouped into small clusters, with minimal interaction
-between the clusters.  The clusters that are currently defined by the
-Natural Language Toolkit are listed below.  Under each cluster, the
-top-level classes and interfaces contained in that cluster are given.
-
-  - B{Sets}: Encodes the mathmatical notion of a \"finite set\".
-    - Set: A finite set.
-    
-  - B{Tokens}: Encodes units of text such as words.
-      - Location: A span over indices in a text
-      - Type: A unit of text.  This is actually not implemented as a
-        class or an interface -- (almost) anything can be a Type.
-      - Token: An occurance of a unit of text.
-         
-  - B{Syntax Trees}: Encodes syntax trees.
-      - Tree: A hierarchical structure spanning a text.
-      - TreeToken: An occurance of a Tree.
-        
-  - B{Probability}: Encodes data structures associated with
-    the mathmatical notion of probability, such as events and
-    frequency distributions.
-      - Sample: Encodes the mathmatical notion of a
-        \"sample\".  This is actually not implemented as a class or
-        an interface -- (almost) anything can be a Sample.
-      - EventI: A (possibly infinite) set of samples.
-      - FreqDistI: The frequency distribution of a collection of
-        samples. 
-      - ProbDistI: A probability distribution, typically derived from
-        a frequency distribution (e.g., using ELE).
-          
-Processing Classes
-------------------
-
-Processing classes are used to perform a variety of tasks that are
-relavant to natural language processing.  Processing classes can
-generally be grouped into small clusters, with minimial interaction
-between the clusters.  Each cluster typically makes use of several
-data-class clusters.  The processing clusters that are currently
-defined by the Natural Language Toolkit are listed below.  Under each
-cluster, the interfaces contained in that cluster are given.
-
-  - B{Tokenizers}: Separate a string of text into a list of
-    Tokens. 
-       - TokenizerI
-  - B{Taggers}: Assign tags to each Token in a list of Tokens.
-       - TaggerI
-  - B{Parser}: Produce Trees that represent the internal structure of
-    a text.
-
-@author: Edward Loper
+@author: L{Edward Loper<edloper@gradient.cis.upenn.edu>}
 @version: 1.0
-"""
 
-"""
-Open Questions
-==============
+@contributor: Steven Bird
+@contributor: Edward Loper
 
-The following is a list of currently unresolved questions, pertaining
-to currently implemented interfaces and classes.
-  - Terminology/Naming Questions
-    - Is \"Token Type\" too easily confusable with the notion of
-         type in python?  E.g., names like SimpleTokenType suggest
-         that they are similar to StringType, IntType, etc. when they
-         are very different.  I could use \"TokenTyp\" to distinguish, 
-         but this also seems somewhat confusing to the uninitiated.
-  - Is the token/token type/text location system too complex?
-       Often, one only cares about the token type.  E.g., a tokenizer
-       could be defined as mapping string to list of TokenType, and a
-       tagger as mapping list of SimpleTokenType to TaggedTokenType.
-       But sometimes we really need to be able to distinguish tokens,
-       not just token types.. e.g., to test the chunk parser from the
-       chunk parsing problem set.
-  - Should FreqDist.max() and FreqDist.cond_max() be merged, with
-       the condition as an optional argument?  Same for
-       FreqDist.freq() and FreqDist.cond_freq().
-  - What cross-toolkit policies on how to use __str__ 
-       and __repr__ should there be?
-  - What cross-toolkit policies on __cmp__, __eq__, __gt__, etc.
-       should there be? 
+@gropu Core Modules: token, tree, probability
+@group Data Modules: set, cfg, fsa
+@group Task Modules: classifier, parser, speech, stemmer, tagger
+@group Visualization: draw
+@group Debugging: chktype, test
+@sort: token, tree, probability, set, cfg, fsa, classifier, parser,
+       speech, stemmer, tagger
 """
 
 ##//////////////////////////////////////////////////////
@@ -169,28 +114,3 @@ __contributors__ = """\
 Edward Loper <edloper@gradient.cis.upenn.edu>
 Steven Bird <sb@cs.mu.oz.au>
 """
-
-# Used for epydoc sorting.  Also used for "from nltk import *"
-__all__ = [
-    # Base modules
-    'token',
-    'probability',
-    'set',
-
-    # Data types
-    'tree',
-    'cfg',
-    'fsa',
-
-    # Tasks
-    'tagger',
-    'parser',
-    'classifier',
-
-    # Visualization
-    'draw',
-
-    # Debugging/testing
-    'chktype',
-    'test',
-    ]
