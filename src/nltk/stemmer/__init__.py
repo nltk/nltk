81d16c1c1033121fd151a36dd1858c9c5c998d09
Edward Loper <edloper@ldc.upenn.edu>
2003-07-29 00:56:11 +0000

    - Changed the definition of the StemmerI interface:   - define a single method stem(token) -> token   - included a commented-out method stem_type(type) -> type
    svn/trunk@989

--- src/nltk/stemmer/__init__.py
+++ src/nltk/stemmer/__init__.py
@@ -8,16 +8,16 @@
 # $Id$
 
 """
-Interfaces used to remove morphological affixes from words, leaving only the
-word stem. Stemming algorithms aim to remove those affixes required for
-eg. grammatical role, tense, derivational morphology leaving only the stem of
-the word. This is a difficult problem due to irregular words (eg. common verbs
-in English), complicated morphological rules and part-of-speech and sense
-ambiguities (eg. C{ceil-} is not the stem of C{ceiling}).
+Interfaces used to remove morphological affixes from words, leaving
+only the word stem.  Stemming algorithms aim to remove those affixes
+required for eg. grammatical role, tense, derivational morphology
+leaving only the stem of the word.  This is a difficult problem due to
+irregular words (eg. common verbs in English), complicated
+morphological rules, and part-of-speech and sense ambiguities
+(eg. C{ceil-} is not the stem of C{ceiling}).
 
-C{StemmerI} defines an interface to which all stemmers must adhere. This
-interface provides simple methods for stemming words.
-
+C{StemmerI} defines an interface to which all stemmers must
+adhere. This interface provides simple methods for stemming words.
 """
 
 ##//////////////////////////////////////////////////////
@@ -26,31 +26,34 @@ interface provides simple methods for stemming words.
 
 class StemmerI:
     """
-    A processing interface for stemming strings.  Either single words or
-    unrestricted text may be stemmed, producing the stemmed word or the text
-    with each distinct word (whitespace separated) stemmed respectively.
+    A processing interface for stemming strings.  Either single words
+    or unrestricted text may be stemmed, producing the stemmed word or
+    the text with each distinct word (whitespace separated) stemmed
+    respectively.
     """
-    def stem_word(self, word):
-        """
-        Stem a single word, returning a stemmed variant of the word.
 
-        @return: the stemmed word.
-        @rtype: C{string}
-        @param word: the word to be stemmed.
-        @type word: C{string}
+    def stem(self, token):
         """
-        raise AssertionError()
-    
-    def stem_text(self, text):
-        """
-        Stem a text consisting of many whitespace separated words. Each word
-        will be stemmed and whitespace preserved. The stemmed text is
-        returned.
+        Remove any morphological affixes from the given word token.
 
-        @return: the stemmed test.
-        @rtype: C{string}
-        @param word: the text to be stemmed.
-        @type word: C{string}
+        @param token: The word token that should be stemmed.
+        @type token: C{token}
+        @rtype: C{Token}
+        @return: A new token whose type is the morphological stem of
+            Ctoken.type()}, and whose location is C{token.loc()}.
         """
-        raise AssertionError()
+        raise AssertionError, 'StemmerI is an abstract interface'
 
+## This function, or one much like it, will be included in a future
+## version of this interface:
+#    def stem_type(self, type):
+#        """
+#        Remove any morphological affixes from the given word type.
+#
+#        @param type: The word type that should be stemmed.  Typically,
+#            C{type} is a C{string}; but depending on the stemmer, it
+#            can be any immutable type.
+#        @type type: any
+#        @rtype: any
+#        """
+#        raise AssertionError, 'StemmerI is an abstract interface'
