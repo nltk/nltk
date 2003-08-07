c07baf9abe64e8deda0dccfc0fb4e51cc505b60a
Edward Loper <edloper@ldc.upenn.edu>
2003-08-07 04:34:14 +0000

    project file for nltk_contrib.upenn.cis530
    svn/trunk@1112

---
+++ contrib/nltk_contrib/upenn/cis530/__init__.py
@@ -0,0 +1,12 @@
+# Natural Language Toolkit: Third-Party Contributions
+# Contributions from the University of Pennsylvania: CIS-530
+#
+# Copyright (C) 2003 The original contributors
+# URL: <http://nltk.sf.net>
+#
+# $Id$
+
+"""
+Student projects from the course CIS-530 at the University of
+Pennsylvania.
+"""
