4cd96a80039781f6a4ecdbefd89c975c58a34089
Edward Loper <edloper@ldc.upenn.edu>
2003-08-07 04:35:21 +0000

    project file for nltk_contrib.upenn.cis530.fall2001
    svn/trunk/nltk@1113

---
+++ contrib/nltk_contrib/upenn/cis530/fall2001/__init__.py
@@ -0,0 +1,17 @@
+# Natural Language Toolkit: Third-Party Contributions
+# Contributions from the University of Pennsylvania: CIS-530/fall2001
+#
+# Copyright (C) 2003 The original contributors
+# URL: <http://nltk.sf.net>
+#
+# $Id$
+
+"""
+Student projects from the course CIS-530, taught in Fall of 2001 at
+the University of Pennsylvania.
+"""
+
+# Add all subdirectories to our package contents path.  This lets us
+# put modules in separate subdirectories without making them packages.
+import nltk_contrib
+nltk_contrib._add_subdirectories_to_package(__path__)
