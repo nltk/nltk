5b6e10d328c5caab3a85310894887f430861739e
Edward Loper <edloper@ldc.upenn.edu>
2003-08-11 17:07:28 +0000

    - Fixed import statements to work with new location (nltk_contrib)
    svn/trunk/nltk@1223

--- contrib/nltk_contrib/misc/festival/weather.py
+++ contrib/nltk_contrib/misc/festival/weather.py
@@ -17,7 +17,8 @@ is very sensitive to changes in the location and layout of the target web
 page.
 """
 
-import urllib, re, nltk.speech.tts as tts
+import urllib, re
+import nltk_contrib.misc.festival.tts as tts
 
 URL	= "http://www.bom.gov.au/cgi-bin/wrap_fwo.pl?IDV10450.txt"
 REGEXP	= "(Forecast for Melbourne .*)Suburban"
