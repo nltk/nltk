ab31639954d66bc271b084424d49048698253794
Edward Loper <edloper@ldc.upenn.edu>
2003-10-29 20:46:28 +0000

    added docstring
    svn/trunk@1364

---
+++ contrib/nltk_contrib/unimelb/__init__.py
@@ -0,0 +1,12 @@
+# Natural Language Toolkit: Third-Party Contributions
+# Contributions from the University of Melbourne
+#
+# Copyright (C) 2003 The original contributors
+# URL: <http://nltk.sf.net>
+#
+# $Id$
+
+"""
+Contributions to NLTK made by students at the University of
+Melbourne.
+"""
