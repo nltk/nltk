668cbe12fc0d10b3d0c01bdc8c3ee948edfca3ab
Edward Loper <edloper@ldc.upenn.edu>
2003-10-29 20:46:47 +0000

    initial import
    svn/trunk@1365

---
+++ contrib/nltk_contrib/unimelb/sb/__init__.py
@@ -0,0 +1,11 @@
+# Natural Language Toolkit: Third-Party Contributions
+# Contributions from the University of Melbourne: Steven Bird
+#
+# Copyright (C) 2003 The original contributors
+# URL: <http://nltk.sf.net>
+#
+# $Id$
+
+"""
+Contributions made by Steven Bird.
+"""
