801c30d916b328e54d80bcc60c34d00fe9f493da
Edward Loper <edloper@ldc.upenn.edu>
2003-11-03 03:17:31 +0000

    - Improvements to the efficiency of the location object:   - Define a LocationContext object, that's used to store a     <unit,source> pair.  These are shared among locations, so each     location only needs one pointer for unit/source, not 2 (saves     ~1 word/location); and makes several operations faster (since     we can check for pointer equality between LocationContext     objects before doing a full equality check).   - Intern unit strings.   - added a new implementation, nltkLen1Location, which is used for     locations with a length of 1.  These objects don't store an     end index; only a start index.  (saves 1 word/location for the     common case of length-1 locations).   These improvements take the average space/location down from 6   words/location (=24 bytes) to 4 words/location (=16 bytes).  c.f.   to ~35-40 words (140-160 bytes) that are used by the Python   implementation.
    - Improvements to the efficiency of the type object:
      - use a single list for properties, with alternating property
        names and property values (saves 1 word/type).
    
    - misc bug fixes
    
    (note that the Token implementation is not yet functional)
    
    
    svn/trunk@1384

--- src/extensions/ctoken.h
+++ src/extensions/ctoken.h
@@ -27,29 +27,102 @@
 #define CTOKEN_H
 
 /*********************************************************************
+ *  Location Context
+ *********************************************************************/
+
+/* This struct is used to pair together a unit and a source.  Since
+ * we're often dealing with many tokens that all have the same unit
+ * and source, we can save 1 word/location by having each location
+ * point to a single nltkLocationContext object.
+ *
+ * We maintain a cache of the most recently used location contexts,
+ * which allows us to reuse them when new locations are defined.  Use
+ * getLocationContext to get a new location context for a location;
+ * and freeLocationContext to free a location context.
+ *
+ * nltkLocationContext objects are entirely internal to the ctoken
+ * implementation; they are not visible from the python interface. */
+typedef struct {
+    int refcount;
+    PyObject *unit;
+    PyObject *source;
+} nltkLocationContext;
+
+/* Location Constructor & Destructor */
+nltkLocationContext *getLocationContext(PyObject *unit, PyObject *source);
+int destroy_location_context(nltkLocationContext *lc);
+
+/* Increase the given location context's reference count by one.
+ * It is safe to call locationContext_INCREF(NULL). */
+#define locationContext_INCREF(lc) (void)(((lc)==NULL) || ((lc)->refcount++))
+
+/* Decrease the given location context's reference count by one.  If
+ * the reference count goes to zero, then free it.  It is safe to call
+ * locationContext_DECREF(NULL). */
+#define locationContext_DECREF(lc) (void)(((lc)==NULL) || \
+                                          (--((lc)->refcount)) || \
+                                          destroy_location_context(lc))
+
+/* Equality check (for comparing locations) */
+int check_location_contexts_eq(nltkLocationContext *c1,
+                               nltkLocationContext *c2);
+
+/* A faster version of check_location_contexts_eq, that first checks if
+ * c1==c2.  This should actually be the case most of the time, because
+ * the location context cache lets us reuse location context
+ * objects. */
+#define CHECK_LOCATION_CONTEXTS_EQ(c1, c2) \
+      (((c1)==(c2)) || check_location_contexts_eq((c1), (c2)))
+
+/* Helper functions */
+PyObject *normalizeUnitCase(PyObject *string);
+
+/*********************************************************************
  *  Location
  *********************************************************************/
 
-/* The struct that is used to encode Location instances. */
+/* We define two implementations of the Location type.  The first
+ * implementation (struct=nltkLocation, type=nltkLocationType) stores
+ * both the start and the end index; and the second implementation
+ * (struct=nltkLen1Location, type=nltkLen1LocationType) stores only
+ * the start index.  This second implementation is used for the common
+ * case of locations with a length of exactly 1 (saving 1
+ * word/location of memory).
+ *
+ * The length-1 location implementation is automatically used by
+ * Location.__new__, when appropriate.  nltkLen1LocationType is a
+ * subclass of nltkLocation, so isinstance(loc, Location) will work
+ * correctly. */
+
+/* The object struct for the arbitrary-length location object */
 typedef struct {
-    PyObject_HEAD      /* Object head: refcount & type */
-    long int start;    /* The start index */
-    long int end;      /* The end index */
-    PyObject *unit;    /* The unit */
-    PyObject *source;  /* The source */
+    PyObject_HEAD                 /* Object head: refcount & type */
+    nltkLocationContext *context; /* The unit and the source */
+    long int start;               /* The start index */
+    long int end;                 /* The end index */
 } nltkLocation;
 
-/* The Location type. */
+/* The object struct for the length-1 location object */
+typedef struct {
+    PyObject_HEAD                 /* Object head: refcount & type */
+    nltkLocationContext *context; /* The unit and the source */
+    long int start;               /* The start index */
+} nltkLen1Location;
+
+/* The Location types. */
 static PyTypeObject nltkLocationType;
+static PyTypeObject nltkLen1LocationType;
 
 /* A macro to test if something's a Location. */
 #define is_nltkLocation(v) ((v)->ob_type == &nltkLocationType)
+#define is_nltkLen1Location(v) ((v)->ob_type == &nltkLen1LocationType)
+#define nltkLocation_END(ob) (is_nltkLen1Location(ob) ? \
+                                  ((ob)->start+1) :\
+                                  ((ob)->end))
 
 /* Location Constructor & Destructor */
 static PyObject *
   nltkLocation__new__(PyTypeObject* type, PyObject *args, PyObject *keywords);
-static int
-  nltkLocation__init__(nltkLocation* self, PyObject *args, PyObject *keywords);
 static void
   nltkLocation__del__(nltkLocation* self);
 
@@ -91,11 +164,14 @@ static nltkLocation *
 
 /* The struct that is used to encode Type instances. */
 typedef struct {
-    PyObject_HEAD          /* Object head: refcount & type */
-    int num_props;         /* num properties defined by this token */
-    PyObject **prop_names;  /* names of properties */
-    PyObject **prop_values; /* values of properties */
+    PyObject_HEAD            /* Object head: refcount & type */
+    int num_props;           /* num properties defined by this token */
+    PyObject **properties;   /* alternating list of (value/name) */
 } nltkType;
+
+/* Use these macros to access nltkType.properties. */
+#define nltkType_PROP_NAME(ob, n) ((ob)->properties[(n)*2])
+#define nltkType_PROP_VALUE(ob, n) ((ob)->properties[(n)*2+1])
 
 /* The Type type. */
 static PyTypeObject nltkTypeType;
@@ -142,26 +218,25 @@ static long
  *    - nltkArrayToken -- array-based token.
  *    - nltkLen1ArrayToken -- array-based token with len=1. */
 
+/* The array-based implementation of tokens.  N.b. that this is (for
+ * the most part) binary-compatible with nltkType; so we can reuse
+ * methods. */
 typedef struct {
-    PyObject_HEAD
-} nltkAbstractToken;
+    PyObject_HEAD            /* Object head: refcount & type */
+    int num_props;           /* num properties defined by this token */
+    PyObject **properties;   /* alternating list of (value/name) */
+    long int start;          /* location's start index */
+    long int end;            /* location's end index */
+    PyObject *unit;          /* location's unit */
+    PyObject *source;        /* location's source */
+} nltkToken;
 
-/* The array-based implementation of tokens */
-typedef struct {
-    PyObject_HEAD          /* Object head: refcount & type */
-    int num_properties;    /* # properties defined by this token */
-    char **names;          /* names of properties */
-    PyObject *values;      /* values of properties */
-    int start;             /* location's start index */
-    int end;               /* location's end index */
-    PyObject *unit;        /* location's unit */
-    PyObject *source;      /* location's source */
-    PyTypeObject *real_type;/* Hmmm */
-} nltkArrayToken;
+/* Use these macros to access nltkToken.properties. */
+#define nltkToken_PROP_NAME(ob, n) ((ob)->properties[(n)*2])
+#define nltkToken_PROP_VALUE(ob, n) ((ob)->properties[(n)*2+1])
 
-/* The Location types. */
-static PyTypeObject nltkAbstractTokenType;
-static PyTypeObject nltkArrayTokenType;
+/* The Location type. */
+static PyTypeObject nltkTokenType;
 
 /*********************************************************************
  *  Module Initialization
