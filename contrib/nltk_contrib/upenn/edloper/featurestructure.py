94134307b91507ae014edfa38f4aaac381b6746f
Edward Loper <edloper@ldc.upenn.edu>
2004-02-16 04:29:20 +0000

    - Fixed FeatureStructure's str and repr to work correctly for feature   structures that are reentrant at the root. - Improved docstrings - Added FeatureStructureVariableBinding.variables() - Added some chktype checks - Changed FeatureStructureVariableBinding.__repr__ - Added FeatureStructureVariableBinding.__cmp__ - Simplified FeatureStructure._find_reentrances
    svn/trunk/nltk@1422

--- contrib/nltk_contrib/upenn/edloper/featurestructure.py
+++ contrib/nltk_contrib/upenn/edloper/featurestructure.py
@@ -54,34 +54,18 @@ types of information about variables:
     the same value.
 """
 
-OPEN_QUESTIONS = """
-Should there be a feature object?
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-The current implementation doesn't actually include an object to
-encode a "feature" (i.e., a name/value pair).  This makes the code
-simpler -- one less class to deal with, and you can directly query for
-feature values, rather than going through a feature structure.  But
-there might be use cases for adding a Feature object.  E.g., if we
-wanted to assign properties (like is_required) to features.  But I'd
-like to see some compelling use cases before we add it.
-
-NullCategory and StarCategory
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-Rob's code included 2 special feature values: star and null.  Star
-unifies with everything, and null unifies with (almost) nothing.  In
-Rob's code, null does unify with star and with itself; although I'm
-not sure what the justification for that is.
-
-This version doesn't include anything like the star & null values; and
-I'm not convinced that we need them.  In particular, a feature with a
-value of star acts just like an unspecified feature.  And I'm not sure
-what the use cases for the Null value are.  It's possible that the
-star value would be more meaningful if we also implemented
-feature.is_required.  But for both values, I'd like to see some use
-cases before we decide whether and how to add them.
-"""
+# Open question: should there be a "feature" object?
+#
+# The current implementation doesn't actually include an object to
+# encode a "feature" (i.e., a name/value pair).  This makes the code
+# simpler -- one less class to deal with, and you can directly query
+# for feature values, rather than going through a feature object.  But
+# there might be use cases for adding a Feature object.  E.g., if we
+# wanted to assign properties (like is_required) to features.  But I'd
+# like to see some compelling use cases before we add it.
 
 import re, copy
+from nltk.chktype import chktype as _chktype
 
 #//////////////////////////////////////////////////////////////////////
 # Variables and variable bindings
@@ -90,11 +74,15 @@ import re, copy
 class FeatureStructureVariable:
     """
     A variable that can stand for a single feature value in a feature
-    structure.  A feature value can either be a base value (immutable
-    and hashable) or a C{FeatureStructure}.  Each variable is uniquely
-    identified by a case sensitive identifier.  Any two
-    C{FeatureStructureVariable} objects with the same identifier are
-    treated as the same variable.
+    structure.  Each variable is uniquely identified by a case
+    sensitive identifier.  Any two C{FeatureStructureVariable} objects
+    with the same identifier are treated as the same variable.
+
+    Variables do not directly contain values; instead, the mapping
+    from variables to values is encoded externally as a set of
+    bindings, using L{FeatureStructureVariableBinding}.
+
+    @see: L{FeatureStructure}
     """
     def __init__(self, identifier):
         """
@@ -104,6 +92,7 @@ class FeatureStructureVariable:
             Any two C{FeatureStructureVariable} objects with the
             same identifier are treated as the same variable.
         """
+        assert _chktype(1, identifier, str)
         self._identifier = identifier
 
     def identifier(self):
@@ -114,6 +103,11 @@ class FeatureStructureVariable:
         return self._identifier
 
     def __repr__(self):
+        """
+        @return: A string representation of this feature structure
+           variable.  A feature structure variable with identifier
+           C{I{x}} is represented as C{'?I{x}'}.
+        """
         return '?%s' % self._identifier
 
     def __cmp__(self, other):
@@ -123,17 +117,14 @@ class FeatureStructureVariable:
     def __hash__(self):
         return self._identifier.__hash__()
 
-# This is used to keep track of a set of bound variables.  We can't
-# get away with just using a dictionary, because we might want to
-# "merge" unbound variables together (see docstring), and that's not
-# convenient to represent in a dictionary.
 class FeatureStructureVariableBinding:
     """
     A set of bindings for feature structure variables.  Each variable
-    is either bound to a value, or unbound.  Two or more unbound
-    variables can be set equal by X{merging} them together.  When an
-    unbound variable is bound to a value, any variables that have been
-    merged with it will also be bound to that value.
+    is either X{bound} (i.e., assigned a value), or X{unbound} (i.e.,
+    left unspecified).  Two or more unbound variables can be set equal
+    by X{merging} them together.  When an unbound variable is assigned
+    a value, any variables that have been merged with it will also be
+    assigned that value.
 
     Feature structure variable bindings are monotonic: once a value is
     bound, it cannot be unbound or rebound.
@@ -155,16 +146,26 @@ class FeatureStructureVariableBinding:
             the two variables will be merged.
         """
         self._bindings = {}
-        self._merged_vars = {}
+        self._merges = {}
 
-        # Register all the bindings and merged variables listed in
-        # initial_bindings.
+        # Look for variables to merge.
         for (src, dst) in initial_bindings.items():
             if isinstance(dst, FeatureStructureVariable):
                 self.merge(src, dst)
+
+        # Look for variables to bind.  (n.b.: this is intentionally
+        # done *after* looking for variables to merge.)
         for (src, dst) in initial_bindings.items():
             if not isinstance(dst, FeatureStructureVariable):
                 self.bind(src, dst)
+
+    def variables(self):
+        """
+        @return: A list of all variables that have been bound
+            to other values or merged with other variables.
+        @rtype: C{list} of L{FeatureStructureVariable}
+        """
+        return self._bindings.keys() + self._merges.keys()
 
     def bound_variables(self):
         """
@@ -180,13 +181,14 @@ class FeatureStructureVariableBinding:
             with other variables.
         @rtype: C{list} of L{FeatureStructureVariable}
         """
-        return self._bindings.keys()
+        return self._merges.keys()
 
     def is_bound(self, variable):
         """
         @return: true if the given variable is bound to a value.
         @rtype: C{bool}
         """
+        assert _chktype(1, variable, FeatureStructureVariable)
         return self._bindings.has_key(variable)
 
     def lookup(self, variable):
@@ -194,53 +196,64 @@ class FeatureStructureVariableBinding:
         @return: The value that the given variable is bound to; or
             the variable itself, if it's not bound.
         """
+        assert _chktype(1, variable, FeatureStructureVariable)
         return self._bindings.get(variable, variable)
     
     def merge(self, var1, var2):
         """
-        Merge two unbound variables together.  After C{var1} and
-        C{var2} have been merged, binding either variable to a value
-        will cause the other variable to be bound to the same value.
-        @raise ValueError: if C{var1} or C{var2} is bound.
+        Merge two unbound variables together.
+        @raise ValueError: if C{var1} or C{var2} is already bound.
         """
+        assert _chktype(1, var1, FeatureStructureVariable)
+        assert _chktype(1, var2, FeatureStructureVariable)
         if self._bindings.has_key(var1) or self._bindings.has_key(var2):
             raise ValueError('Only unbound variables may be merged')
-        varlist = (self._merged_vars.get(var1,[var1]) +
-                   self._merged_vars.get(var2,[var2]))
-        self._merged_vars[var1] = varlist
-        self._merged_vars[var2] = varlist
+        varlist = (self._merges.get(var1,[var1]) +
+                   self._merges.get(var2,[var2]))
+        self._merges[var1] = varlist
+        self._merges[var2] = varlist
 
-    def bind(self, var, value):
+    def bind(self, variable, value):
         """
         Bind an unbound variable to a value.
-        @raise ValueError: If C{var} is already bound.
+        @raise ValueError: If C{variable} is already bound.
         @raise ValueError: If C{value} is a variable.  (Variables
             cannot be bound to other variables; use L{merge()}
             instead.)
         """
-        if isinstance(value, FeatureVariableBinding):
+        assert _chktype(1, variable, FeatureStructureVariable)
+        if isinstance(value, FeatureStructureVariable):
             raise ValueError('Variables cannot be bound to other '+
                              'variables; use merge() instead.')
-        if self._bindings.has_key(var):
+        if self._bindings.has_key(variable):
             raise ValueError('Bound variables may not be rebound')
-        for var in self._merged_vars.get(var,[var]):
-            self._bindings[var] = value
+        for variable in self._merges.get(variable,[variable]):
+            self._bindings[variable] = value
 
-    def _rebind(self, var, new_value):
+    def _rebind(self, variable, new_value):
         """
         Replace the value that a variable is bound to with an equal
         value.  This is used by L{FeatureStructure.
         _cleanup_binding_forwards} to preserve reentrance when
         unifying feature structures.
-        @precondition: The old binding for C{var} is equal to
+        @precondition: The old binding for C{variable} is equal to
             C{new_value}.
-        @precondition: C{var} is bound.
+        @precondition: C{variable} is bound.
         """
-        self._bindings[var] = new_value
+        self._bindings[variable] = new_value
 
     def __repr__(self):
-        return '<Bindings: %s; Merged=%s>' % (self._bindings,
-                                              self._merged_vars)
+        vars = self.variables()
+        if self._bindings:
+            return '<Bindings: %s>' % repr(self._bindings)[1:-1]
+        else:
+            return '<Bindings (empty)>'
+
+    def __cmp__(self, other):
+        if not isinstance(other, FeatureStructureVariable): return -1
+        return cmp((self._bindings, self._merges),
+                   (other._bindings, other._merges))
+        
 
 # This corresponds to "Category" in Rob Speer's code.
 class FeatureStructure:
@@ -480,14 +493,14 @@ class FeatureStructure:
         Display a single-line representation of this feature structure,
         suitable for embedding in other representations.
         """
-        return self._repr(self._find_reentrances(), {})
+        return self._repr(self._find_reentrances({}), {})
 
     def __str__(self):
         """
         Display a multi-line representation of this feature structure
         as an FVM (feature value matrix).
         """
-        return '\n'.join(self._str(self._find_reentrances(), {}))
+        return '\n'.join(self._str(self._find_reentrances({}), {}))
 
     def _repr(self, reentrances, reentrance_ids):
         """
@@ -502,6 +515,13 @@ class FeatureStructure:
             it.
         """
         segments = []
+
+        # If this is the first time we've seen a reentrant structure,
+        # then assign it a unique identifier.
+        if reentrances[id(self)]:
+            assert not reentrance_ids.has_key(id(self))
+            reentrance_ids[id(self)] = `len(reentrance_ids)+1`
+
         items = self._features.items()
         items.sort() # sorting note: keys are unique strings, so we'll
                      # never fall through to comparing values.
@@ -512,19 +532,15 @@ class FeatureStructure:
                 segments.append('%s -> (%s)' % (fname,
                                                 reentrance_ids[id(fval)]))
             else:
-                is_reentrant = reentrances[id(fval)]
-                if is_reentrant:
-                    # Pick a new id.
-                    reentrance_ids[id(fval)] = len(reentrance_ids)+1
-                    
                 fval_repr = fval._repr(reentrances, reentrance_ids)
-                if is_reentrant:
-                    segments.append('%s = (%d) %s' % 
-                                    (fname, reentrance_ids[id(fval)],
-                                     fval_repr))
-                else:
-                    segments.append('%s = %s' % (fname, fval_repr))
-        return '[' + ', '.join(segments) + ']'
+                segments.append('%s = %s' % (fname, fval_repr))
+
+        # If it's reentrant, then add on an identifier tag.
+        if reentrances[id(self)]:
+            return '(%s) [%s]' % (reentrance_ids[id(self)],
+                                ', '.join(segments))
+        else:
+            return '[%s]' % (', '.join(segments))
 
     def _str(self, reentrances, reentrance_ids):
         """
@@ -539,12 +555,21 @@ class FeatureStructure:
             displayed, an identifier is added to reentrance_ids for
             it.
         """
+        # If this is the first time we've seen a reentrant structure,
+        # then tack on an id string.
+        if reentrances[id(self)]:
+            assert not reentrance_ids.has_key(id(self))
+            reentrance_ids[id(self)] = `len(reentrance_ids)+1`
+
         # Special case:
         if len(self._features) == 0:
-            return ['[]']
+            if reentrances[id(self)]:
+                return ['(%s) []' % reentrance_ids[id(self)]]
+            else:
+                return ['[]']
         
         # What's the longest feature name?  Use this to align names.
-        maxfnamelen = max([len(fname) for fname in self._features.keys()])
+        maxfnamelen = max([len(k) for k in self._features.keys()])
 
         lines = []
         items = self._features.items()
@@ -566,12 +591,6 @@ class FeatureStructure:
                 # other values by a blank line.
                 if lines and lines[-1] != '': lines.append('')
 
-                # Is it reentrant?  If so, assign it an id number.
-                is_reentrant = reentrances[id(fval)]
-                if is_reentrant:
-                    # Pick a new id.
-                    reentrance_ids[id(fval)] = len(reentrance_ids)+1
-                    
                 # Recursively print the feature's value (fval).
                 fval_lines = fval._str(reentrances, reentrance_ids)
                 
@@ -581,21 +600,7 @@ class FeatureStructure:
                 # Pick which line we'll display fname on.
                 nameline = (len(fval_lines)-1)/2
                 
-                # Is it reentrant?  If it is, display its id
-                # (along with fname) on the name line.
-                if is_reentrant:
-                    # Indent each line to make room for the id.
-                    fid = ' (%s)' % reentrance_ids[id(fval)]
-                    fval_lines = [(' '*len(fid))+l for l in fval_lines]
-                    # Display the id and the name.
-                    fval_lines[nameline] = (
-                        fname.ljust(maxfnamelen)+' ='+str(fid)+
-                        fval_lines[nameline][maxfnamelen+len(fid)+2:])
-
-                # If it's not reentrant, then just display fname
-                # on the name line.
-                else:
-                    fval_lines[nameline] = (
+                fval_lines[nameline] = (
                         fname.ljust(maxfnamelen)+' ='+
                         fval_lines[nameline][maxfnamelen+2:])
 
@@ -612,9 +617,19 @@ class FeatureStructure:
         maxlen = max([len(line) for line in lines])
         lines = ['[ %s%s ]' % (line, ' '*(maxlen-len(line))) for line in lines]
 
+        # If it's reentrant, then add on an identifier tag.
+        if reentrances[id(self)]:
+            idstr = '(%s) ' % reentrance_ids[id(self)]
+            lines = [(' '*len(idstr))+l for l in lines]
+            idline = (len(lines)-1)/2
+            lines[idline] = idstr + lines[idline][len(idstr):]
+
         return lines
 
-    def _find_reentrances(self, reentrances=None, visited=None):
+    # Walk through the feature tree.  The first time we see a feature
+    # value, map it to False (not reentrant).  If we see a feature
+    # value more than once, then map it to C{True} (reentrant).
+    def _find_reentrances(self, reentrances):
         """
         Find all of the feature values contained by self that are
         reentrant (i.e., that can be reached by multiple paths through
@@ -622,24 +637,17 @@ class FeatureStructure:
         C{reentrances} that maps from the C{id} of each feature value
         to a boolean value, indicating whether it is reentrant or not.
         """
-        if reentrances is None: reentrances={}
-
-        # Visit each node only once.
-        if visited is None: visited={}
-        if visited.has_key(id(self)): return
-        visited[id(self)] = 1
-
-        # Walk through the feature tree.  The first time we see a
-        # feature value, map it to False (not reentrant).  If we see a
-        # feature value more than once, then map it to C{True}
-        # (reentrant).
-        for fval in self._features.values():
-            # False the first time we see it; True if we see it more
-            # than once.
-            reentrances[id(fval)] = reentrances.has_key(id(fval))
+        if reentrances.has_key(id(self)):
+            # We've seen it more than once.
+            reentrances[id(self)] = True
+        else:
+            # This is the first time we've seen it.
+            reentrances[id(self)] = False
+        
             # Recurse to contained feature structures.
-            if isinstance(fval, FeatureStructure):
-                fval._find_reentrances(reentrances, visited)
+            for fval in self._features.values():
+                if isinstance(fval, FeatureStructure):
+                    fval._find_reentrances(reentrances)
 
         return reentrances
 
