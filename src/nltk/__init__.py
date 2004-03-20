edc0b3ab5df2e34538cdae05b20bb3002ea6e39c
Edward Loper <edloper@ldc.upenn.edu>
2004-03-20 15:59:05 +0000

    - Updated version numbers to 1.3
    svn/trunk@1664

--- src/nltk/__init__.py
+++ src/nltk/__init__.py
@@ -61,7 +61,7 @@ L{RecursiveDescentParser<nltk.parser.RecursiveDescentParser>}, and
 L{ChartParser<nltk.parser.chart.ChartParser>} are all implementations
 of the C{ParserI} interface.
 
-@version: 1.2
+@version: 1.3
 
 @newfield developer: Developer, Developers, short
 @developer: U{Edward Loper<edloper@gradient.cis.upenn.edu>}
@@ -94,7 +94,7 @@ __extra_epydoc_fields__ = [
 
 # Version.  For each new release, the version number should be updated
 # here and in the Epydoc comment (above).
-__version__ = "1.2"
+__version__ = "1.3"
 
 # Copyright notice
 __copyright__ = """\
