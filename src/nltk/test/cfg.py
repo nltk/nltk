25ad2175b94d96af5c8ae3372bb122a1a5ae0f1d
Edward Loper <edloper@ldc.upenn.edu>
2004-04-07 20:14:59 +0000

    - Lots more test cases (test coverage for nltk.cfg up to 100%)
    svn/trunk@1711

--- src/nltk/test/cfg.py
+++ src/nltk/test/cfg.py
@@ -12,9 +12,9 @@ Unit testing for L{nltk.cfg}.
 """
 
 from nltk.cfg import *
+from nltk.util import mark_stdout_newlines
 
-def test_Nonterminal():
-    """
+def test_Nonterminal(): """
 Unit testing for L{Nonterminal}.
 
 Nonterminals are constructed from node values (or 'symbols'):
@@ -88,8 +88,7 @@ whitespace:
     <A B> <C D> <E F>
 """
 
-def test_CFGProduction():
-    """
+def test_CFGProduction(): """
 Unit tests for L{CFGProduction}.
     
 A CFG production is constructed from a left-hand side element and
@@ -150,6 +149,187 @@ Productions are hashable, and so can be used as dictionary keys:
 
     >>> {prod1:0}
     {[Production: S -> NP]: 0}
+"""
+
+def test_CFG(): """
+Unit tests for L{CFG.
+    
+A context free grammar is constructed from a start symbol and a list
+of productions:
+
+    >>> S,NP,VP,PP,P,N,V = nonterminals('S,NP,VP,PP,P,N,V')
+    >>> prods = [CFGProduction(S), CFGProduction(S, NP),
+    ...          CFGProduction(S, NP, VP), CFGProduction(S, NP, VP, PP),
+    ...          CFGProduction(N, 'dog'), CFGProduction(PP, 'in', NP)]
+    >>> cfg = CFG(S, prods)
+    >>> print cfg
+    CFG with 6 productions (start state = S)
+        S ->
+        S -> NP
+        S -> NP VP
+        S -> NP VP PP
+        N -> 'dog'
+        PP -> 'in' NP
+        
+    >>> print repr(cfg)
+    <CFG with 6 productions>
+
+The start symbol and production list are accessed via methods:
+
+    >>> print `cfg.start()`
+    <S>
+    >>> for prod in cfg.productions():
+    ...     print `prod`
+    [Production: S ->]
+    [Production: S -> NP]
+    [Production: S -> NP VP]
+    [Production: S -> NP VP PP]
+    [Production: N -> 'dog']
+    [Production: PP -> 'in' NP]
+
+CFGs can be constructed from strings with the L{CFG.parse} static
+method.  The start state is taken to be the LHS of the first
+production.
+
+    >>> cfg = CFG.parse('''
+    ...     S   -> NP VP
+    ...     VP  -> V | V NP | VP PP
+    ...     PP  -> P NP
+    ...     NP  -> Det N | 'John' | 'Mary'
+    ...     N   -> 'dog' | 'cat'
+    ...     Det -> 'the' | 'a' | 'an'
+    ...     V   -> 'saw' | 'met'
+    ...     ''')
+    >>> print cfg
+    CFG with 15 productions (start state = S)
+        S -> NP VP
+        VP -> V
+        VP -> V NP
+        VP -> VP PP
+        PP -> P NP
+        NP -> Det N
+        NP -> 'John'
+        NP -> 'Mary'
+        N -> 'dog'
+        N -> 'cat'
+        Det -> 'the'
+        Det -> 'a'
+        Det -> 'an'
+        V -> 'saw'
+        V -> 'met'
+
+It is an error to give C{parse} an empty string:
+
+    >>> CFG.parse('')
+    Traceback (most recent call last):
+      [...]
+    ValueError: No productions found!
+
+Or a mal-formed string:
+
+    >>> CFG.parse('A <- B')
+    Traceback (most recent call last):
+      [...]
+    ValueError: Unable to parse line 0
+"""
+
+def test_PCFGProduction(): """
+Unit tests for L{PCFGProduction}.
+    
+PCFGProduction is a probabilistic version of CFGProduction:
+
+    >>> S,NP,VP,PP,P,N,V = nonterminals('S,NP,VP,PP,P,N,V')
+    >>> PCFGProduction(0.1, S)
+    [Production: S -> (p=0.1)]
+    >>> PCFGProduction(0.8, S, NP, VP)
+    [Production: S -> NP VP (p=0.8)]
+
+It is hashable and comparable:
+
+    >>> prod1 = PCFGProduction(0.1, S)
+    >>> prod2 = PCFGProduction(0.2, S)
+    >>> prod1 == prod2
+    False
+    >>> {prod1: 1}
+    {[Production: S -> (p=0.1)]: 1}
+"""
+
+def test_PCFG(): """
+Unit tests for L{PCFG}.
+
+PCFG is a probabilistic version of CFG.  It uses PCFGProductions
+instead of CFGProductions.
+
+    >>> S,NP,VP,PP,P,N,V = nonterminals('S,NP,VP,PP,P,N,V')
+    >>> prods = [PCFGProduction(0.1, S),
+    ...          PCFGProduction(0.2, S, NP),
+    ...          PCFGProduction(0.3, S, NP, VP),
+    ...          PCFGProduction(0.4, S, NP, VP, PP),
+    ...          PCFGProduction(1.0, N, 'dog'),
+    ...          PCFGProduction(1.0, PP, 'in', NP)]
+    >>> cfg = PCFG(S, prods)
+    >>> print cfg
+    CFG with 6 productions (start state = S)
+        S -> (p=0.1)
+        S -> NP (p=0.2)
+        S -> NP VP (p=0.3)
+        S -> NP VP PP (p=0.4)
+        N -> 'dog' (p=1.0)
+        PP -> 'in' NP (p=1.0)
+
+The probability distribution defined by a PCFG is required to be
+well-defined.  I.e., the probabilities of the productions with a given
+LHS must sum to 1.
+
+    >>> prods[0] = PCFGProduction(0.5, S)
+    >>> PCFG(S, prods)
+    Traceback (most recent call last):
+      [...]
+    ValueError: CFGProductions for <S> do not sum to 1
+"""
+
+def test_demo(): """
+Unit test for L{nltk.cfg.demo}.
+
+    >>> mark_stdout_newlines(demo)
+    Some nonterminals: [<S>, <NP>, <VP>, <PP>, <N>, <V>, <P>, <Det>, <VP/NP>]
+        S.symbol() => 'S'
+    <--BLANKLINE-->
+    A CFG production: [Production: NP -> Det N]
+        prod.lhs() => <NP>
+        prod.rhs() => (<Det>, <N>)
+    <--BLANKLINE-->
+    A CFG grammar: <CFG with 14 productions>
+        cfg.start()       => <S>
+        cfg.productions() => ([Production: S -> NP VP],
+                              [Production: PP -> P NP],
+                              [Production: NP -> Det N],
+                              [Production: NP -> NP PP],
+                              [Production: VP -> V NP],
+                              [Production: VP -> VP PP],
+                              [Production: Det -> 'a'],
+                              [Production: Det -> 'the'],
+                              [Production: N -> 'dog'],
+                              [Production: N -> 'cat'],
+                              [Production: V -> 'chased'],
+                              [Production: V -> 'sat'],
+                              [Production: P -> 'on'],
+                              [Production: P -> 'in'])
+    <--BLANKLINE-->
+    A PCFG production: [Production: B -> B 'b' (p=0.5)]
+        pcfg_prod.lhs()  => <B>
+        pcfg_prod.rhs()  => (<B>, 'b')
+        pcfg_prod.prob() => 0.5
+    <--BLANKLINE-->
+    A PCFG grammar: <CFG with 6 productions>
+        pcfg.start()       => <S>
+        pcfg.productions() => ([Production: A -> B B (p=0.3)],
+                               [Production: A -> C B C (p=0.7)],
+                               [Production: B -> B 'b' (p=0.5)],
+                               [Production: B -> C (p=0.5)],
+                               [Production: C -> 'a' (p=0.1)],
+                               [Production: C -> 'b' (p=0.9)])
+    <--BLANKLINE-->
 """
 
 #######################################################################
