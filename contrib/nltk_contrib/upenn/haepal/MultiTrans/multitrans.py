ad91a9d25acb1d6470964d5a38b7af9ec66689eb
Haejoong Lee <haejoong@ldc.upenn.edu>
2004-06-10 16:20:54 +0000

    a batch commit as of now
    svn/trunk@1847

--- contrib/nltk_contrib/upenn/haepal/MultiTrans/multitrans.py
+++ contrib/nltk_contrib/upenn/haepal/MultiTrans/multitrans.py
@@ -1,14 +1,15 @@
 
 import sys
-import qt, qtcanvas
+import qtcanvas
 import QWave
 from seglistview import *
+import os.path
 
 class Waveform(QWave.QWave):
 
-    def __init__(self, audiofile, parent=None, name=None):
+    def __init__(self, filename, parent=None, name=None):
         QWave.QWave.__init__(self, parent, name)
-        self.openAudio(audiofile)
+        if not self.openAudio(filename): return
 
         self.toolbar = QWave.QWaveToolBar(self)
         self.addLayout(self.toolbar,0,1)
@@ -26,10 +27,10 @@ class Waveform(QWave.QWave):
             self.addWidget(vr,i,0)
             self.vrs.append(vr)
 
-            sl = qt.QSlider(self)
-            sl.setSizePolicy(qt.QSizePolicy.Maximum,qt.QSizePolicy.Minimum)
+            sl = QSlider(self)
+            sl.setSizePolicy(QSizePolicy.Maximum,QSizePolicy.Minimum)
             sl.setRange(1,10);
-            self.connect(sl,qt.SIGNAL("valueChanged(int)"),self.sl_changed)
+            self.connect(sl,SIGNAL("valueChanged(int)"),self.sl_changed)
             self.addWidget(sl,i,2)
             self.slmap[sl] = id
 
@@ -47,44 +48,95 @@ class Waveform(QWave.QWave):
         sl = self.sender()
         self.castWaveformAmplitude(self.slmap[sl],v)
 
-
-if __name__ == '__main__':
-    app = qt.QApplication(sys.argv)
-    
-    mw = qt.QMainWindow()
-
-    grid = QGrid(1, mw)
-
-    annset = Seglist()
-    annset.load(sys.argv[1])
-    text = SeglistView(annset, grid)
-
-    wave = Waveform(sys.argv[2], grid)
-    print wave.height()
-    wave.setFixedHeight(240)
-
-    ###
-    def emitCastSetRegion(a,b,c):
-        x = wave.time2frm(a)
-        y = wave.time2frm(b)
-        wave.castSetRegion(x, y, wave.ch2id[c])
-
-    def createAnnotation(a,b,c):
-        x = wave.frm2time(a)
-        y = wave.frm2time(b)
-        chchr = chr(ord('A')+c)
-        seg = Segment(start=x,end=y,SPKR=chchr,CHANNEL=c)
-        annset.add(seg)
-        print x,y,c
         
-    wave.connect(text, PYSIGNAL("currentParagraphRegion(int,int,int)"),
-                 emitCastSetRegion)
-    wave.connect(wave, SIGNAL("createAnnotation(int,int,int)"),
-                 createAnnotation)
-    ###
+class MyGrid(QWidget):
+    def __init__(self, *args):
+        QWidget.__init__(self, *args)
+        self._layout = QGridLayout(self)
+
     
-    mw.setCentralWidget(grid)
+class MultiTrans(QMainWindow):
+    def __init__(self, *args):
+        QMainWindow.__init__(self, *args)
+
+        self._buildMenu()
+
+        self._grid = QVBox(self)
+        self.annset = None
+        self.text = SeglistView(parent=self._grid)
+        self.wave = None
+        self._grid.setStretchFactor(self.text, 1)
+
+        self.connect(self.text,
+                     PYSIGNAL("currentParagraphRegion(int,int,int)"),
+                     self._emitCastSetRegion)
+
+        self.setCentralWidget(self._grid)
+
+    def _buildMenu(self):
+        fileMenu = QPopupMenu(self)
+        fileMenu.insertItem("New Transcript", self._fileMenu_NewTrans)
+        fileMenu.insertItem("Open Transcript", self._fileMenu_OpenTrans)
+        fileMenu.insertItem("Open Audio", self._fileMenu_OpenAudio)
+        fileMenu.insertItem("Exit", self._fileMenu_Exit)
+        self.menuBar().insertItem("File", fileMenu)
+
+    def _emitCastSetRegion(self, a,b,c):
+        if self.wave is not None:
+            x = self.wave.time2frm(a)
+            y = self.wave.time2frm(b)
+            self.wave.castSetRegion(x, y, self.wave.ch2id[c])
+
+    def _createAnnotation(self, a,b,c):
+        if self.text is not None:
+            x = self.wave.frm2time(a)
+            y = self.wave.frm2time(b)
+            chchr = chr(ord('A')+c)
+            seg = Segment(start=x,end=y,SPKR=chchr,CHANNEL=c)
+            self.annset.add(seg)
+            print x,y,c
+
+    def _changeAnnotationSpan(self, a, b, c):
+        if self.text is not None:
+            x = self.wave.frm2time(a)
+            y = self.wave.frm2time(b)
+            self.text.setCurrentAnnotationSpan(x, y, c)
+
+    def _fileMenu_NewTrans(self):
+        self.annset = Seglist()
+        self.text.setAnnotationSet(self.annset)
     
+    def _fileMenu_OpenTrans(self):
+        filename = QFileDialog.getOpenFileName().ascii()
+        if filename is None: return     # canceled
+
+        self.annset = Seglist()
+        self.text.setAnnotationSet(self.annset)
+        self.annset.load(filename)
+        
+    def _fileMenu_OpenAudio(self):
+        filename = QFileDialog.getOpenFileName().ascii()
+        if filename is None: return     # canceled
+
+        del self.wave  # this also releases the audio device,
+                       # avoiding a deadlock condition
+        self.wave = Waveform(filename, self._grid)
+        self.wave.show()
+
+        self.connect(self.wave, SIGNAL("createAnnotation(int,int,int)"),
+                     self._createAnnotation)
+        self.connect(self.wave, SIGNAL("setRegion(int,int,int)"),
+                     self._changeAnnotationSpan)
+
+        
+    def _fileMenu_Exit(self):
+        QApplication.exit(0)
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    
+    mw = MultiTrans()
+
     app.setMainWidget(mw);
     mw.show()
     app.exec_loop()
