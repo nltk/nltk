d91d3127504994610c57ebbdf130f470904468d6
Trevor Cohn <tacohn@cs.mu.oz.au>
2004-06-15 01:19:39 +0000

    Added bias to EM clusterer. Cleaned up dendogram display. Added trace to cluster() methods.
    svn/trunk@1851

--- src/nltk/clusterer.py
+++ src/nltk/clusterer.py
@@ -27,7 +27,7 @@ vector to the cluster with the closest mean. It then recalculates the means of
 each cluster as the centroid of the vectors in the cluster. This process
 repeats until the cluster memberships stabilise. This is a hill-climbing
 algorithm which may converge to a local maximum. Hence the clustering is
-often repeated with random initial means and the most commonly occuring
+often repeated with random initial means and the most commonly occurring
 output means are chosen.
 
 The GAAC clusterer starts with each of the M{N} vectors as singleton clusters.
@@ -58,7 +58,7 @@ The current existing classifiers also extend VectorSpaceClusterer, an
 abstract class which allows for singular value decomposition (SVD) and vector
 normalisation. SVD is used to reduce the dimensionality of the vector space in
 such a manner as to preserve as much of the variation as possible, by
-reparametising the axes in order of variability and discarding all bar the
+reparameterising the axes in order of variability and discarding all bar the
 first d dimensions. Normalisation ensures that vectors fall in the unit
 hypersphere.
 
@@ -191,9 +191,10 @@ class VectorSpaceClusterer(ClustererI):
         self._should_normalise = normalise
         self._svd_dimensions = svd_dimensions
     
-    def cluster(self, tokens, assign_clusters=False):
+    def cluster(self, tokens, assign_clusters=False, trace=False):
         assert chktype(1, tokens, [Token])
         assert chktype(2, assign_clusters, bool)
+        assert chktype(3, trace, bool)
         assert len(tokens) > 0
         vectors = map(lambda tk: tk['FEATURES'], tokens)
 
@@ -213,14 +214,14 @@ class VectorSpaceClusterer(ClustererI):
             self._Tt = Numeric.transpose(T)
             
         # call abstract method to cluster the vectors
-        self.cluster_vectorspace(vectors)
+        self.cluster_vectorspace(vectors, trace)
 
         # assign the tokens to clusters
         if assign_clusters:
             for token in tokens:
                 self.classify(token)
 
-    def cluster_vectorspace(self, vectors):
+    def cluster_vectorspace(self, vectors, trace):
         """
         Finds the clusters using the given set of vectors.
         """
@@ -262,6 +263,20 @@ class VectorSpaceClusterer(ClustererI):
         if cluster == predicted: return 1.0
         else:                    return 0.0
 
+    def vector(self, token):
+        """
+        Returns the vector after normalisation and dimensionality reduction
+        for the given token's FEATURES.
+        """
+        assert chktype(1, token, Token)
+        vector = token['FEATURES']
+        #assert chktype('features', vector, Numeric.array([]), SparseArray)
+        if self._should_normalise:
+            vector = self._normalise(vector)
+        if self._Tt != None:
+            vector = Numeric.matrixmultiply(self._Tt, vector)
+        return vector
+
     def _normalise(self, vector):
         """
         Normalises the vector to unit length.
@@ -286,7 +301,8 @@ class KMeansClusterer(VectorSpaceClusterer):
 
     def __init__(self, num_means, distance, repeats=1,
                        conv_test=1e-6, initial_means=None,
-                       normalise=False, svd_dimensions=None):
+                       normalise=False, svd_dimensions=None,
+                       rng=None):
         """
         @param  num_means:  the number of means to use (may use fewer)
         @type   num_means:  int
@@ -304,6 +320,8 @@ class KMeansClusterer(VectorSpaceClusterer):
         @param svd_dimensions: number of dimensions to use in reducing vector
                                dimensionsionality with SVD
         @type svd_dimensions: int 
+        @param  rng:        random number generator (or None)
+        @type   rng:        Random
         """
         assert chktype(1, num_means, int)
         #assert chktype(2, distance, ...)
@@ -321,19 +339,22 @@ class KMeansClusterer(VectorSpaceClusterer):
         assert repeats >= 1
         assert not (initial_means and repeats > 1)
         self._repeats = repeats
+        if rng: self._rng = rng
+        else:   self._rng = random.Random()
 
-    def cluster_vectorspace(self, vectors):
+    def cluster_vectorspace(self, vectors, trace=False):
         if self._means and self._repeats > 1:
             print 'Warning: means will be discarded for subsequent trials'
 
         meanss = []
         for trial in range(self._repeats):
+            if trace: print 'k-means trial', trial
             if not self._means or trial > 1:
-                self._means = random.sample(vectors, self._num_means)
-            self._cluster_vectorspace(vectors)
+                self._means = self._rng.sample(vectors, self._num_means)
+            self._cluster_vectorspace(vectors, trace)
             meanss.append(self._means)
 
-        if len(meanss) > 0:
+        if len(meanss) > 1:
             # sort the means first (so that different cluster numbering won't
             # effect the distance comparison)
             for means in meanss:
@@ -352,7 +373,7 @@ class KMeansClusterer(VectorSpaceClusterer):
             # use the best means
             self._means = min_means
 
-    def _cluster_vectorspace(self, vectors):
+    def _cluster_vectorspace(self, vectors, trace=False):
         if self._num_means < len(vectors):
             # perform k-means clustering
             converged = False
@@ -363,6 +384,10 @@ class KMeansClusterer(VectorSpaceClusterer):
                 for vector in vectors:
                     index = self.classify_vectorspace(vector)
                     clusters[index].append(vector)
+
+                if trace: print 'iteration'
+                #for i in range(self._num_means):
+                    #print '  mean', i, 'allocated', len(clusters[i]), 'vectors'
 
                 # recalcluate the cluster means by computing the centroid of
                 # each cluster
@@ -600,16 +625,21 @@ class GroupAverageAgglomerativeClusterer(VectorSpaceClusterer):
         self._dendogram = None
         self._groups_values = None
 
-    def cluster_vectorspace(self, vectors):
+    def cluster(self, tokens, assign_clusters=False, trace=False):
+        # stores the merge order
+        self._dendogram = Dendogram([Numeric.array(tk['FEATURES'],
+        Numeric.Float64) for tk in tokens])
+            
+        return VectorSpaceClusterer.cluster(self, tokens,
+                                            assign_clusters, trace)
+
+    def cluster_vectorspace(self, vectors, trace=False):
         # create a cluster for each vector
         clusters = [[vector] for vector in vectors]
 
-        # the sum vector
+        # the sum vectors
         vector_sum = copy.copy(vectors)
 
-        # stores the merge order
-        self._dendogram = Dendogram(vectors)
-            
         while len(clusters) > max(self._num_clusters, 1):
             # find the two best candidate clusters to merge, based on their
             # S(union c_i, c_j)
@@ -625,6 +655,7 @@ class GroupAverageAgglomerativeClusterer(VectorSpaceClusterer):
             # merge them and replace in cluster list
             i, j = best[1:]
             sum = clusters[i] + clusters[j]
+            if trace: print 'merging %d and %d' % (i, j)
 
             clusters[i] = sum
             del clusters[j]
@@ -640,9 +671,15 @@ class GroupAverageAgglomerativeClusterer(VectorSpaceClusterer):
         self._centroids = []
         for cluster in clusters:
             assert len(cluster) > 0
-            centroid = cluster[0]
+            if self._should_normalise:
+                centroid = self._normalise(cluster[0])
+            else:
+                centroid = Numeric.array(cluster[0])
             for vector in cluster[1:]:
-                centroid += vector
+                if self._should_normalise:
+                    centroid += self._normalise(vector)
+                else:
+                    centroid += vector
             centroid /= float(len(cluster))
             self._centroids.append(centroid)
         self._num_clusters = len(self._centroids)
@@ -654,7 +691,7 @@ class GroupAverageAgglomerativeClusterer(VectorSpaceClusterer):
             sim = self._average_similarity(vector, 1, centroid, 1)
             if not best or sim > best[0]:
                 best = (sim, i)
-        return best[i]
+        return best[1]
 
     def dendogram(self):
         """
@@ -690,7 +727,7 @@ class ExpectationMaximizationClusterer(VectorSpaceClusterer):
     """
 
     def __init__(self, initial_means, priors=None, covariance_matrices=None,
-                       conv_threshold=1e-6, normalise=False,
+                       conv_threshold=1e-6, bias=0.1, normalise=False,
                        svd_dimensions=None):
         """
         Creates an EM clusterer with the given starting parameters,
@@ -705,6 +742,9 @@ class ExpectationMaximizationClusterer(VectorSpaceClusterer):
         @param  conv_threshold: maximum change in likelihood before deemed
                     convergent
         @type   conv_threshold: int or float
+        @param  bias: variance bias used to ensure non-singular covariance
+                      matrices
+        @type   bias: float
         @param  normalise:  should vectors be normalised to length 1
         @type   normalise:  boolean
         @param  svd_dimensions: number of dimensions to use in reducing vector
@@ -723,14 +763,15 @@ class ExpectationMaximizationClusterer(VectorSpaceClusterer):
         self._conv_threshold = conv_threshold
         self._covariance_matrices = covariance_matrices
         self._priors = priors
+        self._bias = bias
 
     def num_clusters(self):
         return self._num_clusters
         
-    def cluster_vectorspace(self, vectors):
+    def cluster_vectorspace(self, vectors, trace=False):
         assert len(vectors) > 0
 
-        # set the parameters
+        # set the parameters to initial values
         dimensions = len(vectors[0])
         means = self._means
         priors = self._priors
@@ -744,24 +785,19 @@ class ExpectationMaximizationClusterer(VectorSpaceClusterer):
                   for i in range(self._num_clusters) ]
             
         # do the E and M steps until the likelihood plateaus
-        lastl = 0.0
-        for vector in vectors:
-            for j in range(self._num_clusters):
-                lastl += priors[j] * \
-                         self._gaussian(means[j], covariances[j], vector)
+        lastl = self._loglikelihood(vectors, priors, means, covariances)
         converged = False
 
         while not converged:
+            if trace: print 'iteration; loglikelihood', lastl
             # E-step, calculate hidden variables, h[i,j]
             h = Numeric.zeros((len(vectors), self._num_clusters),
                 Numeric.Float64)
             for i in range(len(vectors)):
-                s = 0.
                 for j in range(self._num_clusters):
                     h[i,j] = priors[j] * self._gaussian(means[j],
-                                            covariances[j], vectors[i])
-                    s += h[i,j]
-                h[i,:] /= s
+                                               covariances[j], vectors[i])
+                h[i,:] /= sum(h[i,:])
 
             # M-step, update parameters - cvm, p, mean
             for j in range(self._num_clusters):
@@ -780,18 +816,12 @@ class ExpectationMaximizationClusterer(VectorSpaceClusterer):
                 means[j] = new_mean / sum_hj
                 priors[j] = sum_hj / len(vectors)
 
-                # if the matrix is tiny (i.e. only one point)... better to
-                # test if less than 'n' items in each cluster (say 3?)... bit
-                # dodgy, as this may affect the other hidden variables...
-                if LinearAlgebra.determinant(covariances[j]) < 1e-10:
-                    covariances[j] = covariance_before
+                # bias term to stop covariance matrix being singular
+                covariances[j] += self._bias * \
+                    Numeric.identity(dimensions, Numeric.Float64)
 
-            # calculate likelihood
-            l = 0
-            for vector in vectors:
-                for j in range(self._num_clusters):
-                    l += priors[j] * \
-                         self._gaussian(means[j], covariances[j], vector)
+            # calculate likelihood - FIXME: may be broken
+            l = self._loglikelihood(vectors, priors, means, covariances)
 
             # check for convergence
             if abs(lastl - l) < self._conv_threshold:
@@ -817,16 +847,27 @@ class ExpectationMaximizationClusterer(VectorSpaceClusterer):
         assert cvm.shape == (m, m), \
             'bad sized covariance matrix, %s' % str(cvm.shape)
         try:
-            a = 1. / Numeric.power(Numeric.power(2. * Numeric.pi, m) \
-                     * LinearAlgebra.determinant(cvm), 0.5)
+            det = LinearAlgebra.determinant(cvm)
+            inv = LinearAlgebra.inverse(cvm)
+            a = det ** -0.5 * (2 * Numeric.pi) ** (-m / 2.0) 
             dx = x - mean
-            b = math.exp(-0.5 * Numeric.matrixmultiply( \
-                    Numeric.matrixmultiply(dx, LinearAlgebra.inverse(cvm)), dx))
-            return a * b
+            b = -0.5 * Numeric.matrixmultiply( \
+                    Numeric.matrixmultiply(dx, inv), dx)
+            return a * Numeric.exp(b) 
         except OverflowError:
             # happens when the exponent is negative infinity - i.e. b = 0
             # i.e. the inverse of cvm is huge (cvm is almost zero)
             return 0
+
+    def _loglikelihood(self, vectors, priors, means, covariances):
+        llh = 0.0
+        for vector in vectors:
+            p = 0
+            for j in range(len(priors)):
+                p += priors[j] * \
+                         self._gaussian(means[j], covariances[j], vector)
+            llh += Numeric.log(p)
+        return llh
 
     def __repr__(self):
         return '<ExpectionMaximizationClusterer means=%s>' % list(self._means)
@@ -869,14 +910,13 @@ def demo():
     print 'classify(%s)' % token,
     clusterer.classify(token)
     print token
-    # save the means for later (EM needs them)
-    print
 
-    # test the GAAC clusterer with 2 clusters
-    clusterer = GroupAverageAgglomerativeClusterer(2)
-    clusterer.cluster(tokens, True)
+    # test the GAAC clusterer with 4 clusters
+    clusterer = GroupAverageAgglomerativeClusterer(4)
     print 'using clusterer', clusterer
-    print 'clustered', str(tokens)[:60], '...'
+    clusterer.cluster(tokens, True)
+    #print 'clustered', str(tokens)[:60], '...'
+    print 'clustered', tokens
     # show the dendogram
     clusterer.dendogram().show()
     # classify a new token
@@ -908,5 +948,49 @@ def demo():
         print '%s => %.0f%%' % (sample,
                     token['CLUSTER_PROBDIST'].prob(sample) *100)
 
+def demo_kmeans():
+    # example from figure 14.9, page 517, Manning and Schutze
+    tokens = [Token(FEATURES=Numeric.array(f))
+              for f in [[2, 1], [1, 3], [4, 7], [6, 7]]]
+    means = [[4, 3], [5, 5]]
+
+    clusterer = KMeansClusterer(2, euclidean_distance, initial_means=means)
+    clusterer.cluster(tokens, True, trace=True)
+
+    print 'clustered', tokens
+    print 'means', clusterer.means()
+
+def demo_em():
+    # example from figure 14.10, page 519, Manning and Schutze
+    tokens = [Token(FEATURES=Numeric.array(f))
+              for f in [[0.5, 0.5], [1.5, 0.5], [1, 3]]]
+    means = [[4, 2], [4, 2.01]]
+
+    clusterer = ExpectationMaximizationClusterer(means, bias=0.1)
+    clusterer.cluster(tokens, True, trace=True)
+
+    print 'clustered', tokens
+    for c in range(2):
+        print 'cluster %d' % c
+        print 'prior', clusterer._priors[c]
+        print 'mean ', clusterer._means[c]
+        print 'covar', clusterer._covariance_matrices[c]
+
+    # classify a new token
+    token = Token(FEATURES=Numeric.array([2, 2]))
+    print 'classify(%s)' % token,
+    clusterer.classify(token)
+    print token
+
+    # show the classification probabilities
+    token = Token(FEATURES=Numeric.array([2, 2]))
+    print 'classification_probdist(%s)' % token
+    clusterer.classification_probdist(token)
+    for sample in token['CLUSTER_PROBDIST'].samples():
+        print '%s => %.0f%%' % (sample,
+                    token['CLUSTER_PROBDIST'].prob(sample) *100)
+
 if __name__ == '__main__':
     demo()
+    #demo_em()
+    #demo_kmeans()
