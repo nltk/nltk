175d2a53861c8aaee91c0b633a3ac9470793263b
Edward Loper <edloper@ldc.upenn.edu>
2004-06-30 23:04:26 +0000

    - Moved BagOfWordsFeatureDetector to nltk.feature.word - Added SetOfWordsFeatureDetector - Simplified TextFeatureDetector and StemFeatureDetector
    svn/trunk/nltk@1894

--- src/nltk/feature/word.py
+++ src/nltk/feature/word.py
@@ -4,25 +4,69 @@ Feature extractors for tokens that encode single words.
 
 from nltk.feature import *
 
-class TextFeatureExtractor(FeatureStringFeatureExtractor):
+class TextFeatureDetector(PropertyFeatureDetector):
     """
-    A binary feature for each word text.
+    A feature detector that extracts a token's text from the
+    C{TEXT} property, and stores it in the C{TEXT} feature.
     """
-    def extract_feature_strings(self, token):
-        TEXT = self.property_name('TEXT')
-        return token[TEXT]
-    
-    def feature_description(self, fid):
-        return 'Word is %r' % self.fid2string(fid)
+    def __init__(self, **property_names):
+        PropertyFeatureDetector.__init__(self, 'TEXT', **property_names)
 
-class StemFeatureExtractor(FeatureStringFeatureExtractor):
+class StemFeatureDetector(PropertyFeatureDetector):
     """
-    A binary feature for each stem.
+    A feature detector that extracts a token's stem from the
+    C{STEM} property, and stores it in the C{STEM} feature.
     """
-    def extract_feature_strings(self, token):
-        STEM = self.property_name('STEM')
-        return token[STEM]
-    
-    def feature_description(self, fid):
-        return 'Stem is %r' % self.fid2string(fid)
+    def __init__(self, **property_names):
+        PropertyFeatureDetector.__init__(self, 'TEXT', **property_names)
 
+class BagOfWordsFeatureDetector(AbstractFeatureDetector):
+    """
+    A feature detector that extracts the text of the words in a
+    token's context, preserving duplicates, and stores it in the
+    C{BOW} feature.  In particular, C{BOW} is mapped to a list
+    containing the C{TEXT} feature of each word in a fixed-size window
+    centered on the token.
+    """
+    
+    def __init__(self, window=5, **property_names):
+        """
+        @param window: The number of words in the context to add to
+            the bag of words.
+        """
+        AbstractFeatureDetector.__init__(self, **property_names)
+        self._window = window
+        
+    def raw_detect_features(self, token):
+        bow = [tok['TEXT'] for tok in
+               token['CONTEXT'].getrange(-self._window, 0)+
+               token['CONTEXT'].getrange(1, 1+self._window)]
+        return {'BOW': bow}
+    
+    def features(self):
+        return ['BOW']
+
+class SetOfWordsFeatureDetector(AbstractFeatureDetector):
+    """
+    A feature detector that extracts the text of the words in a
+    token's context, preserving duplicates, and stores it in the
+    C{SOW} feature.  In particular, C{SOW} is mapped to a set
+    containing the C{TEXT} feature of each word in a fixed-size window
+    centered on the token.
+    """
+    def __init__(self, window=5, **property_names):
+        """
+        @param window: The number of words in the context to add to
+            the bag of words.
+        """
+        AbstractFeatureDetector.__init__(self, **property_names)
+        self._window = window
+        
+    def raw_detect_features(self, token):
+        bow = [tok['TEXT'] for tok in
+               token['CONTEXT'].getrange(-self._window, 0)+
+               token['CONTEXT'].getrange(1, 1+self._window)]
+        return {'SOW': bow}
+    
+    def features(self):
+        return ['SOW']
