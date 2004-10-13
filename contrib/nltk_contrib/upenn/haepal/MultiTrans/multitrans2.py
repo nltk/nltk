f761469b89ef49f686cb2269354c5e3faf2eed1a
Haejoong Lee <haejoong@ldc.upenn.edu>
2004-10-13 19:35:57 +0000

    sorry i forgot what i've done
    svn/trunk/nltk@2137

--- contrib/nltk_contrib/upenn/haepal/MultiTrans/multitrans2.py
+++ contrib/nltk_contrib/upenn/haepal/MultiTrans/multitrans2.py
@@ -2,17 +2,21 @@
 import sys
 from waveform import Waveform, QWave
 from seglistview2 import *
+from boxview import *
+from format.callhome2 import *
 import os.path
 
-class PlayerControl(QHBoxLayout):
-    def __init__(self, qwave):
-        QHBoxLayout.__init__(self)
+class PlayerControl(QWidget):
+    def __init__(self, qwave, parent=None, name=None):
+        QWidget.__init__(self, parent, name)
+        
+        layout = QHBoxLayout(self)
 
-        lb1 = QWave.QWaveTimeLabel(qwave)
-        lb2 = QWave.QWaveTimeLabel(qwave)
-        lb3 = QWave.QWaveTimeLabel(qwave)
-        lb4 = QWave.QWaveTimeLabel(qwave)
-        lb5 = QWave.QWaveTimeLabel(qwave)
+        lb1 = QWave.QWaveTimeLabel(qwave, self)
+        lb2 = QWave.QWaveTimeLabel(qwave, self)
+        lb3 = QWave.QWaveTimeLabel(qwave, self)
+        lb4 = QWave.QWaveTimeLabel(qwave, self)
+        lb5 = QWave.QWaveTimeLabel(qwave, self)
 
         lb1.setFrameStyle(QFrame.Box | QFrame.Sunken)
         lb2.setFrameStyle(QFrame.Box | QFrame.Sunken)
@@ -31,10 +35,10 @@ class PlayerControl(QHBoxLayout):
         lb3.setFixedWidth(80)
         lb4.setFixedWidth(80)
 
-        btn1 = QPushButton(">", qwave)
-        btn2 = QPushButton("R", qwave)
-        btn3 = QPushButton("||", qwave)
-        btn4 = QPushButton("X", qwave)
+        btn1 = QPushButton(">", self)
+        btn2 = QPushButton("R", self)
+        btn3 = QPushButton("||", self)
+        btn4 = QPushButton("X", self)
 
         btn1.setFixedSize(20,20)
         btn2.setFixedSize(20,20)
@@ -46,22 +50,23 @@ class PlayerControl(QHBoxLayout):
         btn3.setFocusPolicy(QWidget.NoFocus)
         btn4.setFocusPolicy(QWidget.NoFocus)
 
-        self.addWidget(lb1)
-        self.addWidget(lb2)
-        self.addWidget(lb3)
-        self.addWidget(lb4)
-        self.addWidget(lb5)
+        layout.addWidget(lb1)
+        layout.addWidget(lb2)
+        layout.addWidget(lb3)
+        layout.addWidget(lb4)
+        layout.addWidget(lb5)
 
-        self.addWidget(btn1)
-        self.addWidget(btn2)
-        self.addWidget(btn3)
-        self.addWidget(btn4)
+        layout.addWidget(btn1)
+        layout.addWidget(btn2)
+        layout.addWidget(btn3)
+        layout.addWidget(btn4)
 
         self.connect(qwave, SIGNAL("newCursorPos(int)"),lb1.setTime)
         self.connect(qwave, SIGNAL("setRegion(int,int,int)"),self.setRegion)
         self.connect(qwave, SIGNAL("playerCursorPos(int)"),
                      self.setPlayerCursorPos)
-        
+
+        self._layout = layout
         self._qwave = qwave
         self._lb1 = lb1
         self._lb2 = lb2
@@ -120,14 +125,19 @@ class MultiTrans(QMainWindow):
 
         self._buildMenu()
 
-        self._grid = QVBox(self)
-        self._text = CallhomeMixedView(parent=self._grid)
-        self._grid.setStretchFactor(self._text, 1)
+        w = QWidget(self)
+        self.setCentralWidget(w)
 
+        self._grid = QGridLayout(w)
+        self._grid.setRowStretch(0,1)
+
+        #self._text = CallhomeMixedView(w)
+        self._text = None
         self._wave = None
         self._playerControl = None
-
-        self.setCentralWidget(self._grid)
+        self._data = None
+        
+        #self._grid.addWidget(self._text,0,0)
 
         self._setKeyControls()
         self._eventFilter = MultiTrans.ShiftFilter(self)
@@ -139,20 +149,45 @@ class MultiTrans(QMainWindow):
         fileMenu.insertItem("New Transcript", self._fileMenu_NewTrans)
         fileMenu.insertItem("Open Transcript", self._fileMenu_OpenTrans)
         fileMenu.insertItem("Open Audio", self._fileMenu_OpenAudio)
+        fileMenu.insertItem("Save", self._fileMenu_SaveTrans)
+        fileMenu.insertItem("Save As", self._fileMenu_SaveTransAs)
         fileMenu.insertItem("Exit", self._fileMenu_Exit)
         self.menuBar().insertItem("File", fileMenu)
 
+        viewMenu = QPopupMenu(self)
+        viewMenu.insertItem("Fonts", self._viewMenu_Fonts)
+        self.menuBar().insertItem("View", viewMenu)
+        
     def _fileMenu_NewTrans(self):
         # ask if user wants to save the file
-        self._text.newFile()
+
+        self._data = Callhome()
+
+        self._text = CallhomeMixedView(self._grid.mainWidget())
+        self._text.setFile(self._data)
+        self._grid.addWidget(self._text,0,0)
+        self._text.show()
+
+        if self._wave is not None:
+            self._text.setQWave(self._wave)
     
     def _fileMenu_OpenTrans(self):
         filename = QFileDialog.getOpenFileName().ascii()
         if filename is None: return     # canceled
 
-        self._text.load(filename)
+        #self._text.load(filename)
 
+        self._data = Callhome()
+        self._data.load(filename)
         
+        self._text = CallhomeMixedView(self._grid.mainWidget())
+        self._text.setFile(self._data)
+        self._grid.addWidget(self._text,0,0)
+        self._text.show()
+        
+        if self._wave is not None:
+            self._text.setQWave(self._wave)
+       
     def _fileMenu_OpenAudio(self):
         filename = QFileDialog.getOpenFileName().ascii()
         if filename is None: return     # canceled
@@ -160,12 +195,12 @@ class MultiTrans(QMainWindow):
         if self._wave is not None:
             self._wave.closeAudio()
 
+        w = self._grid.mainWidget()
         # load waveform
-        self._wave = Waveform(filename, self._grid)
+        self._wave = Waveform(filename, w)
 
         # set up player control
-        self._playerControl = PlayerControl(self._wave)
-        self._wave.addLayout(self._playerControl, 0, 1)
+        self._playerControl = PlayerControl(self._wave, self._wave)
         self.connect(self._playerControl.playButton, SIGNAL("clicked()"),
                      self._waveformPlay)
         self.connect(self._playerControl.repeatButton, SIGNAL("clicked()"),
@@ -174,16 +209,36 @@ class MultiTrans(QMainWindow):
                      self._waveformTogglePause)
         self.connect(self._playerControl.stopButton, SIGNAL("clicked()"),
                      self._waveformStop)
-        
+
+        self._wave.addWidget(self._playerControl, 0, 1)
+        self._grid.addWidget(self._wave, 2, 0)
+
+        self._playerControl.show()
         self._wave.show()
 
         # inform text widget of creatioin of new waveform
-        self._text.setQWave(self._wave)
+        if self._text is not None:
+            self._text.setQWave(self._wave)
+
+            self._boxview = CallhomeBoxView(self._wave)
+            self._boxview.setFile(self._data)
+            self._wave.addWidget(self._boxview, self._wave.getNextRow(), 0)
         
 
+    def _fileMenu_SaveTrans(self):
+        self._data.save()
+    
+    def _fileMenu_SaveTransAs(self):
+        utf = QFileDialog.getSaveFileName()
+        if utf is not None:
+            self._data.save(utf.ascii())
+    
     def _fileMenu_Exit(self):
         QApplication.exit(0)
 
+    def _viewMenu_Fonts(self):
+        self._text.setFont(QFontDialog.getFont(self._text.font())[0])
+    
     # user interface
     def _setKeyControls(self):
         a = QAccel(self)
@@ -206,8 +261,13 @@ class MultiTrans(QMainWindow):
         self._paused = False
       
     def _createSegment(self):
-        if self._text is not None:
-            self._text.newSegment()
+        if self._wave is not None:
+            c, a, b = self._wave.getSelectedRegionS()
+            seg = self._data.new(c, a, b)
+            self._data.add(seg)
+            
+        #if self._text is not None:
+        #    self._text.newSegment()
 
     def _createSegment2(self):
         if self._segmentStarted:
@@ -221,7 +281,7 @@ class MultiTrans(QMainWindow):
             c = self._wave.getSelectedRegionWaveformId()
             self._wave.castSetRegion(a, a, c)
             self._segmentStarted = True
-            
+
     def _waveformZoomIn(self):
         if self._wave is not None:
             self._wave.castZoomIn(self._wave.startf() + self._wave.widthf()/2)
