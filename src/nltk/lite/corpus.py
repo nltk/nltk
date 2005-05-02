50df3012d555f68f3f2a38d2c4d72f3dad3ad8a0
Steven Bird <stevenbird1@gmail.com>
2005-05-02 04:12:05 +0000

    tree module and treebank corpus reader
    svn/trunk@2269

--- src/nltk/lite/corpus.py
+++ src/nltk/lite/corpus.py
@@ -104,6 +104,50 @@ def brown(files = list('abcdefghjklmnpr')):
                 yield (t, None)
 
 
+
+from tokenizer import blanklineTokenize
+import tree
+
+def treebank_parsed(files = 'wsj_00_parsed'):
+
+    """
+    Read trees from the Penn Treebank corpus sample.
+
+    This is a ~5% fragment of Penn Treebank, (C) LDC 1995.  It is made
+    available under fair use for the purposes of illustrating NLTK
+    tools for tokenizing, tagging, chunking and parsing.  This data is
+    for non-commercial use only.
+
+    Contents: parsed data from Wall Street Journal for 1650 sentences, e.g.:
+
+    ( (S 
+      (NP-SBJ 
+        (NP (NNP Pierre) (NNP Vinken) )
+        (, ,) 
+        (ADJP 
+          (NP (CD 61) (NNS years) )
+          (JJ old) )
+        (, ,) )
+      (VP (MD will) 
+        (VP (VB join) 
+          (NP (DT the) (NN board) )
+          (PP-CLR (IN as) 
+            (NP (DT a) (JJ nonexecutive) (NN director) ))
+          (NP-TMP (NNP Nov.) (CD 29) )))
+      (. .) ))
+    """       
+
+    path = corpus_path('treebank')
+
+    # Just one file to process?  If so convert to a tuple so we can iterate
+    if type(files) is str: files = (files,)
+
+    for file in files:
+        s = open(os.path.join(path, file)).read()
+        for t in blanklineTokenize(s):
+            yield tree.parse(t)
+
+
 def demo():
     """
     Demonstrate corpus access for each of the defined corpora.
@@ -111,10 +155,21 @@ def demo():
     set_basedir('/home/sb/nltk/data/')   # location for modified corpus
 #    set_basedir('/data/nltk/data/')   # location for modified corpus
 
+    i=0
     for token in brown(files='a'):
         print token
+        i+=1
+        if i>50: break
+
+    i=0
+    for tree in treebank_parsed():
+        print tree.pp()
+        i+=1
+        if i>3: break
     
 if __name__ == '__main__':
     demo()
 
 # create modified corpus with "cat ca* > a" etc
+
+
