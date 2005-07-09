ff1d1621b3a7e01b33341f394528c681da8fddf2
Steven Bird <stevenbird1@gmail.com>
2005-07-09 11:21:12 +0000

    *** empty log message ***
    svn/trunk/nltk@2401

---
+++ lite/nltk_lite/misc/__init__.py
@@ -0,0 +1,6 @@
+# Natural Language Toolkit: Miscellaneous modules
+#
+# Copyright (C) 2001-2005 University of Pennsylvania
+# Author: Steven Bird <sb@csse.unimelb.edu.au>
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
