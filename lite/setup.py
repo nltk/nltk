0a926054ee750e5b3afd1b5fd00c35ea3ace6807
Steven Bird <stevenbird1@gmail.com>
2005-07-09 11:24:47 +0000

    added misc package
    svn/trunk@2402

--- lite/setup.py
+++ lite/setup.py
@@ -32,6 +32,7 @@ setup(
     ## Package List
     packages = ['nltk_lite', 'nltk_lite.corpora',
                 'nltk_lite.tokenize', 'nltk_lite.tag',
-                'nltk_lite.parse', 'nltk_lite.chat', 'nltk_lite.draw'],
+                'nltk_lite.parse', 'nltk_lite.chat', 'nltk_lite.draw',
+                'nltk_lite.misc'],
 
     )
