bfeac81a927f5131bdfb32b82264c900b85ddd64
Steven Bird <stevenbird1@gmail.com>
2005-08-28 02:21:35 +0000

    added ProbabilisticMixIn
    svn/trunk@2547

--- lite/nltk_lite/probability.py
+++ lite/nltk_lite/probability.py
@@ -1333,6 +1333,105 @@ def sum_logs(logs):
         return reduce(add_logs, logs[1:], logs[0])
 
 ##//////////////////////////////////////////////////////
+##  Probabilistic Mix-in
+##//////////////////////////////////////////////////////
+
+class ProbabilisticMixIn:
+    """
+    A mix-in class to associate probabilities with other classes
+    (trees, rules, etc.).  To use the C{ProbabilisticMixIn} class,
+    define a new class that derives from an existing class and from
+    ProbabilisticMixIn.  You will need to define a new constructor for
+    the new class, which explicitly calls the constructors of both its
+    parent classes.  For example:
+
+        >>> class A:
+        ...     def __init__(self, x, y): self.data = (x,y)
+        ... 
+        >>> class ProbabilisticA(A, ProbabilisticMixIn):
+        ...     def __init__(self, x, y, **prob_kwarg):
+        ...         A.__init__(self, x, y)
+        ...         ProbabilisticMixIn.__init__(self, **prob_kwarg)
+
+    See the documentation for the ProbabilisticMixIn
+    L{constructor<__init__>} for information about the arguments it
+    expects.
+
+    You should generally also redefine the string representation
+    methods, the comparison methods, and the hashing method.
+    """
+    def __init__(self, **kwargs):
+        """
+        Initialize this object's probability.  This initializer should
+        be called by subclass constructors.  C{prob} should generally be
+        the first argument for those constructors.
+
+        @kwparam prob: The probability associated with the object.
+        @type prob: C{float}
+        @kwparam logprob: The log of the probability associated with
+            the object.
+        @type logrpob: C{float}
+        """
+        if 'prob' in kwargs:
+            if 'logprob' in kwargs:
+                raise TypeError('Must specify either prob or logprob '
+                                '(not both)')
+            else:
+                ProbabilisticMixIn.set_prob(self, kwargs['prob'])
+        elif 'logprob' in kwargs:
+            ProbabilisticMixIn.set_logprob(self, kwargs['logprob'])
+        else:
+            self.__prob = self.__logprob = None
+            
+    def set_prob(self, prob):
+        """
+        Set the probability associated with this object to C{prob}.
+        @param prob: The new probability
+        @type prob: C{float}
+        """
+        self.__prob = prob
+        self.__logprob = None
+
+    def set_logprob(self, logprob):
+        """
+        Set the log probability associated with this object to
+        C{logprob}.  I.e., set the probability associated with this
+        object to C{exp(logprob)}.
+        @param logprob: The new log probability
+        @type logprob: C{float}
+        """
+        self.__logprob = prob
+        self.__prob = None
+
+    def prob(self):
+        """
+        @return: The probability associated with this object.
+        @rtype: C{float}
+        """
+        if self.__prob is None:
+            if self.__logprob is None: return None
+            self.__prob = math.exp(self.__logprob)
+        return self.__prob
+
+    def logprob(self):
+        """
+        @return: C{log(p)}, where C{p} is the probability associated
+        with this object.
+        
+        @rtype: C{float}
+        """
+        if self.__logprob is None:
+            if self.__prob is None: return None
+            self.__logprob = math.log(self.__prob)
+        return self.__logprob
+
+class ImmutableProbabilisticMixIn(ProbabilisticMixIn):
+    def set_prob(self, prob):
+        raise ValueError, '%s is immutable' % self.__class__.__name__
+    def set_logprob(self, prob):
+        raise ValueError, '%s is immutable' % self.__class__.__name__
+
+##//////////////////////////////////////////////////////
 ##  Demonstration
 ##//////////////////////////////////////////////////////
 
