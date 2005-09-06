21bf02bee3a76302f678ff5e4d390f4d9658abfe
Baden Hughes <badenh@cs.mu.oz.au>
2005-09-06 11:33:16 +0000

    updated nltk-lite ISO build script to support dynamic versioning
    svn/trunk/nltk@2600

--- contrib/nltk_contrib/unimelb/badenh/nltk-lite/build_lite_iso.sh
+++ contrib/nltk_contrib/unimelb/badenh/nltk-lite/build_lite_iso.sh
@@ -7,8 +7,9 @@
 #
 # 0.1 20050803 made for 0.2
 # 0.2 20050905 updated for 0.4
+# 0.3 20050906 now supports dynamic versions
 #
-# Still to be done: dynamic versioning for software, Python docs (?), NLTK site (?)
+# Still to be done: Python docs (?), NLTK site (?)
 #
 
 #
@@ -38,6 +39,9 @@ echo "ISO Image will be placed in $ISODIR"
 export SFNETMIRROR=http://optusnet.dl.sourceforge.net/sourceforge	#change this to suit your system
 echo "Using $SFNETMIRROR"
 
+export VERSION=0.4			# change this to suit	
+echo "NLTK Lite version $VERSION"
+
 #
 # Mac
 #
@@ -45,8 +49,8 @@ echo "Starting download of software for Mac OS X ..."
 
 wget -O $MACWORKDIR/MacPython-OSX-2.4.1-1.dmg http://undefined.org/python/MacPython-OSX-2.4.1-1.dmg
 wget -O $MACWORKDIR/numarray-1.1.1-py2.4-macosx10.3.zip http://www.pythonmac.org/packages/numarray-1.1.1-py2.4-macosx10.3.zip
-wget -O $MACWORKDIR/nltk_lite-0.4.tar.gz $SFNETMIRROR/nltk/nltk_lite-0.4.tar.gz
-wget -O $MACWORKDIR/nltk_lite-corpora-0.4.zip $SFNETMIRROR/nltk/nltk_lite-corpora-0.4.zip
+wget -O $MACWORKDIR/nltk_lite-$VERSION.tar.gz $SFNETMIRROR/nltk/nltk_lite-$VERSION.tar.gz
+wget -O $MACWORKDIR/nltk_lite-corpora-$VERSION.zip $SFNETMIRROR/nltk/nltk_lite-corpora-$VERSION.zip
 
 echo "Finished download of software for Mac OS X ..."
 
@@ -58,8 +62,8 @@ echo "Starting download of software for Windows ..."
 
 wget -O $WINWORKDIR/Python-2.4.1.msi http://www.python.org/ftp/python/2.4.1/Python-2.4.1.msi
 wget -O $WINWORKDIR/numarray-1.3.3.exe http://prdownloads.sourceforge.net/numpy/numarray-1.3.3.exe?download
-wget -O $WINWORKDIR/nltk_lite-0.4.exe $SFNETMIRROR/nltk_lite-0.4.exe
-wget -O $WINWORKDIR/nltk_lite-corpora-0.4.zip $SFNETMIRROR/nltk/nltk_lite-corpora-0.4.zip
+wget -O $WINWORKDIR/nltk_lite-$VERSION.exe $SFNETMIRROR/nltk_lite-$VERSION.exe
+wget -O $WINWORKDIR/nltk_lite-corpora-$VERSION.zip $SFNETMIRROR/nltk/nltk_lite-corpora-$VERSION.zip
 
 echo "Finished download of software for Windows ..."
 
@@ -71,8 +75,8 @@ echo "Starting download of software for Unix (Source) ..."
 
 wget -O $UNXWORKDIR/Python-2.4.1.tgz http://www.python.org/ftp/python/2.4.1/Python-2.4.1.tgz
 wget -O $UNXWORKDIR/numarray-1.3.3.tar.gz $SFNETMIRROR/numpy/numarray-1.3.3.tar.gz
-wget -O $UNXWORKDIR/nltk_lite-0.4.tar.gz $SFNETMIRROR/nltk/nltk_lite-0.4.tar.gz
-wget -O $UNXWORKDIR/nltk_lite-corpora-0.4.zip $SFNETMIRROR/nltk/nltk_lite-corpora-0.4.zip
+wget -O $UNXWORKDIR/nltk_lite-$VERSION.tar.gz $SFNETMIRROR/nltk/nltk_lite-$VERSION.tar.gz
+wget -O $UNXWORKDIR/nltk_lite-corpora-$VERSION.zip $SFNETMIRROR/nltk/nltk_lite-corpora-$VERSION.zip
 
 echo "Finished download of software for Unix (Source) ..."
 
@@ -82,7 +86,7 @@ echo "Finished download of software for Unix (Source) ..."
 
 echo "Making ISO image in $ISODIR ..."
 
-mkisofs -o $ISODIR/NLTK-Lite-0.4.iso $WORKDIR
+mkisofs -o $ISODIR/NLTK-Lite-$VERSION.iso $WORKDIR
 
 echo "Finished making ISO image in $ISODIR ..."
 ls -la $ISODIR
