b049dda5559c8e3ac0947fe03640dbc429ced5e4
Steven Bird <stevenbird1@gmail.com>
2006-02-14 02:48:12 +0000

    fixed bug in indexing CFG productions, for empty right-hand-sides
    svn/trunk@2827

--- lite/nltk_lite/parse/cfg.py
+++ lite/nltk_lite/parse/cfg.py
@@ -318,10 +318,11 @@ class Grammar(object):
         for prod in self._productions:
             if prod._lhs not in self._lhs_index:
                 self._lhs_index[prod._lhs] = []
-            if prod._rhs[0] not in self._rhs_index:
+            if prod._rhs and prod._rhs[0] not in self._rhs_index:
                 self._rhs_index[prod._rhs[0]] = []
             self._lhs_index[prod._lhs].append(prod)
-            self._rhs_index[prod._rhs[0]].append(prod)
+            if prod._rhs:
+                self._rhs_index[prod._rhs[0]].append(prod)
         
     def start(self):
         return self._start
