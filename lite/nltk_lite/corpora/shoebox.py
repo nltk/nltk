a9cc2c3bb073f7ebac2fc65d92b9e7d855bee286
Steven Bird <stevenbird1@gmail.com>
2006-03-01 12:48:34 +0000

    expanded support for shoebox format, developed with Stuart Robinson
    svn/trunk/nltk@2831

--- lite/nltk_lite/corpora/shoebox.py
+++ lite/nltk_lite/corpora/shoebox.py
@@ -11,7 +11,8 @@
 
 """
 Read entries from the Shoebox corpus sample, returning a list
-of pairs, where each pair consists of a field name and value
+of pairs (where each pair consists of a field name and value),
+or a list of dictionaries (one per Shoebox entry).
 
 Contents: lexicons for various languages, with entries of the form:
 
@@ -30,37 +31,56 @@ Contents: lexicons for various languages, with entries of the form:
 """
 
 from nltk_lite.corpora import get_basedir
-from nltk_lite import tokenize
 from string import split
 from itertools import imap
 import os
 
-def _parse_entry(str):
-    for field in tokenize.line(str):
-        field = field[1:].strip()   # remove leading backslash and trailing whitespace
+def _parse_entry(s):
+    s = "\n" + s
+    for field in split(s.rstrip(), sep="\n\\")[1:]:
         field = tuple(split(field, sep=" ", maxsplit=1))
         if len(field) == 1:
-            field = (field[0], None)
+            field = (field[0], '')
         yield field
 
-def raw(files = 'rotokas'):
+def raw(files='rotokas', include_header=False):
     """
-    @param files: One or more treebank files to be processed
+    @param files: One or more shoebox files to be processed
     @type files: L{string} or L{tuple(string)}
+    @param include_header: treat header as entry?
+    @type include_header: boolean
     @rtype: iterator over L{list(string)}
     """       
 
     # Just one file to process?  If so convert to a tuple so we can iterate
-    if type(files) is str: files = (files,)
+    if type(files) is str : files = (files,)
 
     for file in files:
         path = os.path.join(get_basedir(), "shoebox", file + ".dic")
-        f = open(path).read()
-        for entry in tokenize.blankline(f):
-            yield list(_parse_entry(entry))
+        f = open(path, "U").read()
+        (header, entries) = split(f, sep="\n\n", maxsplit=1)
 
-def dictionary(files = 'rotokas'):
-    return imap(dict, raw(files))
+        # Deal with head field marker
+        ff = split(entries, sep="\n", maxsplit=1)[0] # first field
+        hfm = split(ff, sep=" ", maxsplit=1)[0] # raw marker of first field
+
+        # Handle header
+        if include_header: yield list(_parse_entry(header))
+
+        # Parse entries
+        for entry in split("\n\n"+entries, sep="\n\n"+hfm)[1:]:
+            yield list(_parse_entry(hfm+entry))
+
+# assumes headwords are unique
+def dictionary(files='rotokas', include_header=False) :
+    """
+    @param files: One or more shoebox files to be processed
+    @type files: L{string} or L{tuple(string)}
+    @param include_header: treat header as entry?
+    @type include_header: boolean
+    @rtype: iterator over L{dict}
+    """       
+    return imap(dict, raw(files, include_header))
 
 def _dict_list_entry(entry):
     d = {}
@@ -72,11 +92,20 @@ def _dict_list_entry(entry):
             d[name].append(value)
     return d
 
-def dict_list(files = 'rotokas'):
-    # Just one file to process?  If so convert to a tuple so we can iterate
-    if type(files) is str: files = (files,)
+# if two entries have the same headword this key maps to a list of entries
+def dict_list(files='rotokas', include_header=False) :
+    """
+    @param files: One or more shoebox files to be processed
+    @type files: L{string} or L{tuple(string)}
+    @param include_header: treat header as entry?
+    @type include_header: boolean
+    @rtype: iterator over L{dict}
+    """       
 
-    for entry in raw(files):
+    # Just one file to process?  If so convert to a tuple so we can iterate
+    if type(files) is str : files = (files,)
+
+    for entry in raw(files, include_header) :
         yield _dict_list_entry(entry)
 
 def demo():
@@ -93,7 +122,17 @@ def demo():
     print 'Dictionary-List:'
     pprint(list(islice(shoebox.dict_list(), 3)))
 
+    print 'Complex test cases, no header'
+    pprint(list(shoebox.raw("test")))
+
+    print 'Complex test cases, no header, dictionary'
+    pprint(list(shoebox.dictionary("test")))
+
+    print 'Complex test cases, no header, dictionary list'
+    pprint(list(shoebox.dict_list("test")))
+
+    print 'Complex test cases, with header'
+    pprint(list(shoebox.raw("test", include_header=True)))
+
 if __name__ == '__main__':
     demo()
-
-
