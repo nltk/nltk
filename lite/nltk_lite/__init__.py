b50d1e9a8bcbb2ad8b9971e7160a9530ded0cab0
Steven Bird <stevenbird1@gmail.com>
2006-03-09 10:22:33 +0000

    updates for 0.6.3
    svn/trunk/nltk@2868

--- lite/nltk_lite/__init__.py
+++ lite/nltk_lite/__init__.py
@@ -1,6 +1,6 @@
 # Natural Language Toolkit (NLTK-Lite)
 #
-# Copyright (C) 2001-2005 University of Pennsylvania
+# Copyright (C) 2001-2006 University of Pennsylvania
 # Authors: Steven Bird <sb@csse.unimelb.edu.au>
 #          Edward Loper <edloper@gradient.cis.upenn.edu>
 # URL: <http://nltk.sf.net>
@@ -23,7 +23,7 @@ having limited programming experience.  The focus is on teaching
 Python together with the help of NLP recipes, instead of teaching
 students to use a large set of specialized classes.
 
-@version: 0.6.2
+@version: 0.6.3
 
 """
 
@@ -33,11 +33,11 @@ students to use a large set of specialized classes.
 
 # Version.  For each new release, the version number should be updated
 # here and in the Epydoc comment (above).
-__version__ = "0.6.2"
+__version__ = "0.6.3"
 
 # Copyright notice
 __copyright__ = """\
-Copyright (C) 2001-2005 University of Pennsylvania.
+Copyright (C) 2001-2006 University of Pennsylvania.
 
 Distributed and Licensed under provisions of the GNU Public
 License, which is included by reference.
