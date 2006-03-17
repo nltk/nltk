91c402d6b718a5b3231dda4e60b0c1e6836eb592
Haejoong Lee <haejoong@ldc.upenn.edu>
2006-03-17 15:33:02 +0000

    - added spkrinfo table - more comprehensive demo
    svn/trunk@2925

--- lite/nltk_lite/corpora/timit.py
+++ lite/nltk_lite/corpora/timit.py
@@ -22,13 +22,14 @@ from nltk_lite import tokenize
 from itertools import islice
 import os, re
 
-__all__ = ["speakers", "items", "raw", "phonetic", "dictionary"]
+__all__ = ["items", "raw", "phonetic", "speakers", "dictionary", "spkrinfo"]
 
 PREFIX = os.path.join(get_basedir(),"timit")
 
 speakers = []
 items = []
 dictionary = {}
+spkrinfo = {}
 
 for f in os.listdir(PREFIX):
     if re.match("^dr[0-9]-[a-z]{4}[0-9]$", f):
@@ -45,18 +46,25 @@ for l in open(os.path.join(PREFIX,"timitdic.txt")):
     a = l.strip().split('  ')
     dictionary[a[0]] = a[1].strip('/').split()
 
-
+# read spkrinfo
+for l in open(os.path.join(PREFIX,"spkrinfo.txt")):
+    if l[0] == ';': continue
+    rec = l[:54].split() + [l[54:].strip()]
+    key = "dr%s-%s%s" % (rec[2],rec[1].lower(),rec[0].lower())
+    spkrinfo[key] = rec
+    
 def _prim(ext, sentences=items, offset=False):
     for sent in sentences:
         fnam = os.path.sep.join([PREFIX] + sent.split(':')) + ext
+        r = []
         for l in open(fnam):
             if not l.strip(): continue
             a = l.split()
             if offset:
-                yield (a[2],int(a[0]),int(a[1]))
+                r.append((a[2],int(a[0]),int(a[1])))
             else:
-                yield a[2]
-
+                r.append(a[2])
+        yield r
 
 def raw(sentences=items, offset=False):
     return _prim(".wrd", sentences, offset)
@@ -68,12 +76,50 @@ def phonetic(sentences=items, offset=False):
 
 def demo():
     from nltk_lite.corpora import timit
-    from itertools import islice
 
-    for x in timit.phonetic(sentences=items[:1]):
-        print x
-    print dictionary['again']
+    print "sentence 5"
+    print "----------"
+    itemid = timit.items[5]
+    spkrid, sentid = itemid.split(':')
+    print "  item id:    ", itemid
+    print "  speaker id: ", spkrid
+    print "  sentence id:", sentid
+    print
+    record = timit.spkrinfo[spkrid]
+    print "  speaker information:"
+    print "    TIMIT speaker id: ", record[0]
+    print "    speaker sex:      ", record[1]
+    print "    dialect region:   ", record[2]
+    print "    data type:        ", record[3]
+    print "    recording date:   ", record[4]
+    print "    date of birth:    ", record[5]
+    print "    speaker height:   ", record[6]
+    print "    speaker race:     ", record[7]
+    print "    speaker education:", record[8]
+    print "    comments:         ", record[9]
+    print
 
+    print "  words of the sentence:"
+    print "   ", timit.raw(sentences=[itemid]).next()
+    print
+
+    print "  words of the sentence with offsets (first 3):"
+    print "   ", timit.raw(sentences=[itemid], offset=True).next()[:3]
+    print
+    
+    print "  phonemes of the sentence (first 10):"
+    print "   ", timit.phonetic(sentences=[itemid]).next()[:10]
+    print
+    
+    print "  phonemes of the sentence with offsets (first 3):"
+    print "   ", timit.phonetic(sentences=[itemid], offset=True).next()[:3]
+    print
+    
+    print "  looking up dictionary for words of the sentence..."
+    words = timit.raw(sentences=[itemid]).next()
+    for word in words:
+        print "    %-5s:" % word, timit.dictionary[word]
+    print
 
 if __name__ == '__main__':
     demo()
