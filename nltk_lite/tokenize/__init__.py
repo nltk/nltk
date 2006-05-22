3f54947f4ec9055c646f6349e6f123ba1db98048
Steven Bird <stevenbird1@gmail.com>
2006-05-22 19:18:03 +0000

    Added some package-level docs
    svn/trunk@3229

--- nltk_lite/tokenize/__init__.py
+++ nltk_lite/tokenize/__init__.py
@@ -1,4 +1,14 @@
-# Basics: Tokenizers and Stemmers
+# Natural Language Toolkit: Tokenizers
+#
+# Copyright (C) 2001-2006 University of Pennsylvania
+# Author: Steven Bird <sb@ldc.upenn.edu> (minor additions)
+#         Edward Loper <edloper@gradient.cis.upenn.edu>
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
+
+"""
+Functions for tokenizing text.
+"""
 
 from simple import *
 from regexp import *
