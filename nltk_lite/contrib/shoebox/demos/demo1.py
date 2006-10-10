ad7275b1ded92525c217be57fc5998348a9b87ff
Greg Aumann <greg.aumann@gmail.com>
2006-10-10 04:03:55 +0000

    add element tree parsing of toolbox data and demo code
    svn/trunk@3471

---
+++ nltk_lite/contrib/shoebox/demos/demo1.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+# Natural Language Toolkit: Shoebox Data demonstration
+#
+# Copyright (C) 2001-2006 University of Pennsylvania
+# Author: Greg Aumann <greg_aumann@sil.org>
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
+
+"""
+corresponds to 
+12.3.1   Accessing Shoebox Data
+in http://nltk.sourceforge.net/lite/doc/en/data.html
+"""
+
+from nltk_lite.corpora import get_basedir
+import os.path
+from nltk_lite.contrib.shoebox.data import Data
+from elementtree import ElementTree
+import sys
+
+db = Data()
+db.open(os.path.join(get_basedir(), 'shoebox', 'rotokas.dic'))
+lexicon = db.flat_parse('lx')
+
+sum_size = num_entries = 0
+for entry in lexicon.findall('record'):
+    num_entries += 1
+    sum_size += len(entry)
+print sum_size/num_entries
+
+forth_entry = lexicon.findall('record')[3]
+tree = ElementTree.ElementTree(forth_entry)
+tree.write(sys.stdout)
---
+++ nltk_lite/contrib/shoebox/demos/demo2.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+# Natural Language Toolkit: Shoebox Data demonstration
+#
+# Copyright (C) 2001-2006 University of Pennsylvania
+# Author: Greg Aumann <greg_aumann@sil.org>
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
+
+"""
+corresponds to 
+12.3.1   Accessing Shoebox Data
+in http://nltk.sourceforge.net/lite/doc/en/data.html
+"""
+
+from nltk_lite.corpora import get_basedir
+import os.path
+from nltk_lite.contrib.shoebox.data import Data
+
+db = Data()
+db.open(os.path.join(get_basedir(), 'shoebox', 'rotokas.dic'))
+lexicon = db.flat_parse('lx')
+lexemes = []
+for lexeme in lexicon.findall('record/lx'):
+    normalised_lexeme = lexeme.text.lower()
+    lexemes.append(normalised_lexeme)
+
+# list comprehension approach
+lexemes2=[lexeme.text.lower() for lexeme in lexicon.findall('record/lx')]
+
+##if lexemes != lexemes2:
+##    print 'error two lists not equal'
+##else:
+##    print repr(lexemes)
+
+import re
+def cv(s):
+    s = s.lower()
+    s = re.sub(r'[^a-z]',     r'_', s)
+    s = re.sub(r'[aeiou]',    r'V', s)
+    s = re.sub(r'[^V_]',     r'C', s)
+    return (s)
+    
+for field in lexicon[50].getchildren():
+    print "\\%s %s" % (field.tag, field.text)
+    if field.tag == "lx":
+        print "\\cv %s" % cv(field.text)
