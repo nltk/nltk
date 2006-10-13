eaa0c1d5ec803617e9538c19eeb08898a6bc9424
Ewan Klein <ewan.klein@gmail.com>
2006-10-13 00:24:10 +0000

    more functions
    svn/trunk@3490

--- nltk_lite/contrib/grammartest.py
+++ nltk_lite/contrib/grammartest.py
@@ -9,13 +9,15 @@ import nltk_lite.contrib.models
 from nltk_lite import tokenize
 from nltk_lite.parse.category import GrammarCategory
 from nltk_lite.parse.tree import Tree
-import sys
 
-mygramdir = '/Users/ewan/svn/nltk/doc/en'
-sys.path.append(mygramdir)
+mygramdir = '/Users/ewan/svn/nltk/doc/en/'
 
-trace = 1
+# specify the level of tracing in chart parser
+trace = 0
+# specify whether semantic output should undergo beta-reduction
 beta = True
+# specify the start symbol for the parse tree
+start = 'S'
 
 def gtest(input, filename):
     grammar = GrammarFile.read_file(filename)
@@ -23,21 +25,21 @@ def gtest(input, filename):
     for sent in input:
         tokens = list(tokenize.whitespace(sent))
         parser = grammar.earley_parser(trace=trace)
-        tree = parser.parse(tokens)
-        parses.append((sent, tree))
+        trees = parser.parse(tokens)
+        parses.append((sent, trees))
     return parses
 
-def get_root_node(tree):
+def get_root_node(tree,start='S'):
     # check that we have a tree
     assert isinstance(tree, Tree)
     # in Speer's chart parser, the root node is '[INIT]'
     # so go down to the first child if necessary
-    if tree.node.head() == 'S':
+    if tree.node.head() == start:
         return tree.node
-    elif tree[0].node.head() == 'S':
+    elif tree[0].node.head() == start:
         return tree[0].node
     else:
-        raise ValueError("Tree not rooted in 'S' node")
+        raise ValueError("Tree not rooted in %s node" % start)
 
 
 def get_sem_val(node, beta_reduce=True):
@@ -56,19 +58,23 @@ def demo():
          'you claim that you like girls', 'who do you claim that you like']
     sents1 = ['Kim sleeps', 'I like Kim']
     sents2 = ['Fido barks', 'John loves Mary', 'every boy loves Mary']
-    sents3 = ['a boy loves all girls']
-    parses = gtest(input=sents3, filename='semtest.cfg')
+    sents3 = ['John sees a boy with Fido']
+    parses = gtest(input=sents3, filename=mygramdir+'semtest.cfg')
     for (sent, tree) in parses:
         print sent
         if tree:
-            node = get_root_node(tree)
+            node = get_root_node(tree, start)
             val = get_sem_val(node, beta_reduce=beta)
             print str(val)
         else:
             print "[No semantics available]"
 
 
-demo()
+#demo()
+
+parses = gtest(input=['John sees a boy with Fido'], filename=mygramdir+'semtest.cfg')
+
+print parses
 
 
 
