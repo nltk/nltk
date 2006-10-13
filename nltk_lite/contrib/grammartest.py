e99ec3df3d726055e60a1c57fe8557cbc7c9a640
Ewan Klein <ewan.klein@gmail.com>
2006-10-13 04:32:46 +0000

    now produces semantic values as well as semreps
    svn/trunk/nltk@3495

--- nltk_lite/contrib/grammartest.py
+++ nltk_lite/contrib/grammartest.py
@@ -5,76 +5,128 @@ Some prototype code to input sentences and output semantic values.
 """
 
 from nltk_lite.contrib.grammarfile import GrammarFile
-import nltk_lite.contrib.models 
+import nltk_lite.contrib.models as models
 from nltk_lite import tokenize
 from nltk_lite.parse.category import GrammarCategory
 from nltk_lite.parse.tree import Tree
+import sys
 
-mygramdir = '/Users/ewan/svn/nltk/doc/en/'
+mygramdir = '/Users/ewan/svn/nltk/doc/en'
+sys.path.append(mygramdir)
 
-# specify the level of tracing in chart parser
 trace = 0
-# specify whether semantic output should undergo beta-reduction
 beta = True
-# specify the start symbol for the parse tree
-start = 'S'
 
-def gtest(input, filename):
-    grammar = GrammarFile.read_file(filename)
-    parses = []
-    for sent in input:
+
+def text_parse(inputs, grammar):
+    
+    parses = {}
+    for sent in inputs:
         tokens = list(tokenize.whitespace(sent))
         parser = grammar.earley_parser(trace=trace)
-        trees = parser.parse(tokens)
-        parses.append((sent, trees))
+        syntrees = parser.get_parse_list(tokens)
+        parses[sent] = syntrees
     return parses
 
-def get_root_node(tree,start='S'):
+def get_root_node(syntree, start='S'):
     # check that we have a tree
-    assert isinstance(tree, Tree)
+    assert isinstance(syntree, Tree)
     # in Speer's chart parser, the root node is '[INIT]'
     # so go down to the first child if necessary
-    if tree.node.head() == start:
-        return tree.node
-    elif tree[0].node.head() == start:
-        return tree[0].node
+    if syntree.node.head() == start:
+        return syntree.node
+    elif syntree[0].node.head() == start:
+        return syntree[0].node
     else:
         raise ValueError("Tree not rooted in %s node" % start)
 
 
-def get_sem_val(node, beta_reduce=True):
+def get_semrep(node, beta_reduce=True):
     # check that we have a GrammarCategory
     assert isinstance(node, GrammarCategory)
     try:
-        sem_val = node.get_feature('sem')
+        semrep = node.get_feature('sem')
         if beta_reduce:
-            sem_val = sem_val.simplify()
-        return sem_val
+            semrep = semrep.simplify()
+        return semrep
     except KeyError:
         print "Node has no 'sem' feature specification"
-    
+        raise
+
+def get_root_semrep(syntree, beta_reduce=True):
+    node = get_root_node(syntree)
+    return get_semrep(node, beta_reduce=beta_reduce)
+
+def text_interpret(inputs, grammar):
+    parses = text_parse(inputs, grammar)
+    semreps = {}
+    for sent in inputs:
+        syntrees = parses[sent]
+        syn_sem = [(syn, get_root_semrep(syn)) for syn in syntrees]
+        semreps[sent] = syn_sem
+    return semreps
+        
+def text_evaluate(inputs, grammar, model, assignment):
+    semreps = text_interpret(inputs, grammar)
+    evaluations = {}
+    for sent in inputs:
+        syn_sem_val = [(syn, sem, model.evaluate(str(sem), g)) for (syn, sem) in semreps[sent]]
+        evaluations[sent] = syn_sem_val
+    return evaluations
+
+
+def model_init():
+    """
+    Sample model to accompany semtest.cfg grammar
+    """
+    global m, g 
+    val = models.Valuation()
+    v = [('john', 'b1'),
+         ('mary', 'g1'),
+         ('fido', 'd1'),
+         ('noosa', 'n'),
+         ('girl', set(['g1', 'g2'])),
+         ('boy', set(['b1', 'b2'])),
+         ('dog', set(['d1'])),
+         ('bark', set(['d1'])),
+         ('walk', set(['b1', 'g2', 'd1'])),
+         ('love', set([('b1', 'g1'), ('b2', 'g2'), ('g1', 'b1'), ('g2', 'b1')])),
+         ('see', set([('b1', 'g1'), ('b2', 'd2'), ('g1', 'b1'), ('d2', 'b1'), ('g2', 'n')])),
+         ('in', set([('b1', 'n'), ('b2', 'n'), ('d2', 'n')])),
+         ('with', set([('b1', 'g1'), ('g1', 'b1'), ('d1', 'b1'), ('b1', 'd1')]))]
+    val.parse(v)
+    dom = val.domain
+    m = models.Model(dom, val)
+    g = models.Assignment(dom)
+
+
+sents = ['Fido sees a boy with Mary',
+         'John sees Mary',
+         'John walks with a girl in Noosa'
+         ]
+
 def demo():
-    sents0 = ['who do you like', 'girls you like', 'you do like girls',
-         'you claim that you like girls', 'who do you claim that you like']
-    sents1 = ['Kim sleeps', 'I like Kim']
-    sents2 = ['Fido barks', 'John loves Mary', 'every boy loves Mary']
-    sents3 = ['John sees a boy with Fido']
-    parses = gtest(input=sents3, filename=mygramdir+'semtest.cfg')
-    for (sent, tree) in parses:
-        print sent
-        if tree:
-            node = get_root_node(tree, start)
-            val = get_sem_val(node, beta_reduce=beta)
-            print str(val)
-        else:
-            print "[No semantics available]"
+    model_init()
+    SPACER = '-' * 30
+    filename = 'semtest.cfg'
+    model = m
+    assignment = g
+    grammar = GrammarFile.read_file(filename)
+    inputs = sents
+    evaluations = text_evaluate(inputs, grammar, model, assignment)
 
-
-#demo()
-
-parses = gtest(input=['John sees a boy with Fido'], filename=mygramdir+'semtest.cfg')
-
-print parses
+    for sent in evaluations:
+        n = 1
+        print
+        print 'Sentence: %s' % sent
+        print SPACER
+        for (syntree, semrep, value) in evaluations[sent]:
+            print '%d:  %s' % (n, semrep)
+            print '%9s in Model m' % value
+            n += 1
+    
+if __name__ == "__main__":
+    demo()
 
 
 
