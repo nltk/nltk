f8e188687da2de0f8b8510e21253f9dd3bc16c86
Ewan Klein <ewan.klein@gmail.com>
2006-10-27 02:19:22 +0000

    bugfixes for syn2sem demo
    svn/trunk/nltk/nltk_lite@3634

--- semantics/__init__.py
+++ semantics/__init__.py
@@ -3,3 +3,15 @@
 # Author: Ewan Klein <ewan@inf.ed.ac.uk>
 # URL: <http://nltk.sf.net>
 # For license information, see LICENSE.TXT
+
+"""
+This package contains classes for representing semantic structure in
+formulas of first-order logic and for evaluating such formulas in
+set-theoretic models.
+
+"""
+
+
+from evaluate import *
+from logic import *
+from utilities import *
