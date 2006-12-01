410bb411f9e46cf46630dc68555fd9dbcfc4ec3e
Steven Bird <stevenbird1@gmail.com>
2006-12-01 02:10:34 +0000

    new package for chunkers
    svn/trunk/nltk/nltk_lite@3768

---
+++ chunk/__init__.py
@@ -0,0 +1,12 @@
+# Natural Language Toolkit: Chunkers
+#
+# Copyright (C) 2001-2006 University of Pennsylvania
+# Author: Steven Bird <sb@csse.unimelb.edu.au>
+#         Edward Loper <edloper@gradient.cis.upenn.edu>
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
+#
+
+"""
+"""
+
