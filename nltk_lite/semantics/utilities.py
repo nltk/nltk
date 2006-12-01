90ed4f9b0d2a7d177bf7d74ea21c74425348327b
Ewan Klein <ewan.klein@gmail.com>
2006-12-01 12:18:12 +0000

    hooks for tracing
    svn/trunk/nltk@3783

--- nltk_lite/semantics/utilities.py
+++ nltk_lite/semantics/utilities.py
@@ -72,12 +72,12 @@ def root_semrep(syntree, beta_reduce=True, start='S'):
     node = root_node(syntree, start=start)
     return semrep(node, beta_reduce=beta_reduce)
 
-def text_interpret(inputs, grammar, beta_reduce=True, start='S'):
+def text_interpret(inputs, grammar, beta_reduce=True, start='S', syntrace=0):
     """
     Add the semantic representation to each syntactic parse tree
     of each input sentence.
     """
-    parses = text_parse(inputs, grammar)
+    parses = text_parse(inputs, grammar, trace=syntrace)
     semreps = {}
     for sent in inputs:
         syntrees = parses[sent]
@@ -86,7 +86,7 @@ def text_interpret(inputs, grammar, beta_reduce=True, start='S'):
         semreps[sent] = syn_sem
     return semreps
 
-def text_evaluate(inputs, grammar, model, assignment, trace=0):
+def text_evaluate(inputs, grammar, model, assignment, semtrace=0):
     """
     Add the truth-in-a-model value to each semantic representation
     for each syntactic parse of each input sentences.
@@ -97,7 +97,7 @@ def text_evaluate(inputs, grammar, model, assignment, trace=0):
     evaluations = {}
     for sent in inputs:
         syn_sem_val = \
-          [(syn, sem, m.evaluate(str(sem), g, trace=trace)) for (syn, sem) in semreps[sent]]
+          [(syn, sem, m.evaluate(str(sem), g, trace=semtrace)) for (syn, sem) in semreps[sent]]
         evaluations[sent] = syn_sem_val
     return evaluations
     
