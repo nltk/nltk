899e5bd3514f3d9727bf18bbf0086f31df91e870
Steven Bird <stevenbird1@gmail.com>
2006-12-05 10:46:23 +0000

    added docstring, copyright, and import
    svn/trunk/nltk/nltk_lite@3815

---
+++ wordnet/__init__.py
@@ -0,0 +1,17 @@
+# Natural Language Toolkit: Wordnet Interface
+#
+# Copyright (C) 2001-2006 University of Pennsylvania
+# Author: Oliver Steele <steele@osteele.com>
+#         David Ormiston Smith <daosmith@csse.unimelb.edu.au>>
+#         Steven Bird <sb@csse.unimelb.edu.au>
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
+
+"""
+Wordnet interface, based on Oliver Steele's Pywordnet, together
+with an implementation of Ted Pedersen's Wordnet::Similarity package.
+"""
+
+from wordnet import *
+
+
