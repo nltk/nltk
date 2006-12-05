33d15eae0cd1c47bea670a204ad4b3818029bafc
Ewan Klein <ewan.klein@gmail.com>
2006-12-05 00:00:54 +0000

    added command line options
    svn/trunk/nltk@3803

--- examples/semantics/parse_chat.py
+++ examples/semantics/parse_chat.py
@@ -34,18 +34,54 @@ g = evaluate.Assignment(dom)
 
 
 
-
 sents = ['France is a country', 'Paris is the capital of France', 'what is the capital of France']
 
-sents = ['which country borders France', 'which sea borders France']
-sents = ['which country that borders the_Mediterranean borders Turkey']
+# sents = ['which country borders France', 'which sea borders France']
+# sents = ['which country that borders the_Mediterranean borders Turkey']
 
 
 
 
-def main(inputs = sents, evaluate=1, filename = 'chat_80.cfg'):
+def main():
+    import sys
+    from optparse import OptionParser
+    description = \
+    """
+Parse and evaluate some sentences in the Chat-80 model. The valuation
+for the model is read in from a shelved database, 'chat.db'. If the
+option '-g' is not set, the file 'chat-80.cfg' is used. If the option
+'-s' is not set, some example sentences are provided automatically.
+    """
+
+    opts = OptionParser(description=description)
+    opts.set_defaults(evaluate=True, beta=True, syntrace=0, semtrace=0,
+                      grammar='chat_80.cfg', sentences=None)
+
+    opts.add_option("-e", "--no-eval", action="store_false", dest="evaluate",
+                    help="just do a syntactic analysis")
+    opts.add_option("-b", "--no-beta-reduction", action="store_false",
+                    dest="beta", help="don't carry out beta-reduction")
+    opts.add_option("-t", "--syntrace", action="count", dest="syntrace",
+                    help="set syntactic tracing on; requires '-e' option")
+    opts.add_option("-T", "--semtrace", action="count", dest="semtrace",
+                    help="set semantic tracing on")
+    opts.add_option("-g", "--gram", dest="grammar",
+                    help="read in grammar G", metavar="G")
+    opts.add_option("-s", "--sentences", dest="sentences",
+                        help="read in a file of test sentences F", metavar="F")
+
+
+    (options, args) = opts.parse_args()
     SPACER = '-' * 30
-    
+    filename = options.grammar
+
+    if options.sentences:
+        inputs = [l.rstrip() for l in open(options.sentences)]
+        # get rid of blank lines
+        inputs = [l for l in inputs if len(l) > 0]
+    else:
+        inputs = sents
+        
     model = m
     assignment = g
     
@@ -56,16 +92,18 @@ def main(inputs = sents, evaluate=1, filename = 'chat_80.cfg'):
         n = 1
         print '\nSentence: %s' % sent
         print SPACER
-        if evaluate: 
-            evaluations = text_evaluate(inputs, grammar, model, assignment, semtrace=0)
+        if options.evaluate: 
+            evaluations = \
+            text_evaluate(inputs, grammar, model, assignment, semtrace=options.semtrace)
             for (syntree, semrep, value) in evaluations[sent]:
                 if isinstance(value, dict):
                     value = set(value.keys())
                 print '%d:  %s' % (n, semrep.infixify())
-                print '%9s in Model m' % value
+                print value
                 n += 1
         else:
-            semreps = text_interpret(inputs, grammar, beta_reduce=True, start='S', syntrace=0)
+            semreps = \
+            text_interpret(inputs, grammar, beta_reduce=options.beta, start='S', syntrace=options.syntrace)
             for (syntree, semrep) in semreps[sent]:
                 print '%d:  %s' % (n, semrep.infixify())
                 n += 1
