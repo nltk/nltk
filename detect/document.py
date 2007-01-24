f96fa8307241d97866024c2342175b180f76a465
Steven Bird <stevenbird1@gmail.com>
2007-01-24 04:27:10 +0000

    direct port; omitting feature encoders
    svn/trunk/nltk/nltk_lite@4059

--- detect/document.py
+++ detect/document.py
@@ -1,52 +1,46 @@
 # Natural Language Toolkit: Feature Extraction for Documents
 #
-# Copyright (C) 2004 University of Pennsylvania
+# Copyright (C) 2001-2007 University of Pennsylvania
 # Author: Edward Loper <edloper@gradient.cis.upenn.edu>
+#         Steven Bird <sb@csse.unimelb.edu.au> (porting)
 # URL: <http://nltk.sf.net>
 # For license information, see LICENSE.TXT
 #
 # $Id: document.py 2975 2006-04-03 07:00:11Z stevenbird $
 
 """
-Feature extractors for tokens that encode documents.
+Feature extractors for documents.
 """
 
-from nltk.feature import *
+from nltk_lite.detect import *
 
-class BagOfContainedWordsFeatureDetector(AbstractFeatureDetector):
+class BagOfContainedWordsFeatureDetector(FeatureDetectorI):
     """
-    A feature detector that extracts the C{TEXT} of each subtoken in a
+    A feature detector that extracts the C{TEXT} of each token in a
     document, and stores them (preserving duplicates) in the C{BOW}
     feature.
     """
-    def __init__(self, **property_names):
-        AbstractFeatureDetector.__init__(self, **property_names)
+    def __init__(self):
+        pass
         
-    def get_features(self, token):
-        BOW = self.property('BOW')
-        TEXT = self.property('TEXT')
-        SUBTOKENS = self.property('SUBTOKENS')
-        return {BOW: [tok[TEXT] for tok in token[SUBTOKENS]]}
+    def get_features(self, tokens):
+        return {'bow': tokens}
     
     def features(self):
-        BOW = self.property('BOW')
-        return [BOW]
+        return ['bow']
 
-class SetOfContainedWordsFeatureDetector(AbstractFeatureDetector):
+class SetOfContainedWordsFeatureDetector(FeatureDetectorI):
     """
     A feature detector that extracts the C{TEXT} of each subtoken in a
     document, and stores them (discarding duplicates) in the C{SOW}
     feature.
     """
-    def __init__(self, **property_names):
-        AbstractFeatureDetector.__init__(self, **property_names)
+    def __init__(self):
+        pass
         
-    def get_features(self, token):
-        SOW = self.property('SOW')
-        TEXT = self.property('TEXT')
-        SUBTOKENS = self.property('SUBTOKENS')
-        return {SOW: Set([tok[TEXT] for tok in token[SUBTOKENS]])}
+    def get_features(self, tokens):
+        return {'sow': set(tokens)}
     
     def features(self):
-        return [SOW]
+        return ['sow']
 
