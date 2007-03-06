676484fbe61bc9e060d1a52c52984057e1a04892
Rob Speer <rspeer@mit.edu>
2007-03-06 06:01:03 +0000

    Added a centralized YAML tag system, and used it to make sure Brill taggers are loaded correctly (closes bug #1662048).
    Also, fixed a minor bug in chunking where the name Tree was undefined.
    
    
    
    svn/trunk@4197

---
+++ nltk_lite/yamltags.py
@@ -0,0 +1,32 @@
+import yaml
+
+"""
+Register YAML tags in the NLTK namespace with the YAML loader, by telling it
+what module and class to look for.
+
+NLTK uses simple '!' tags to mark the types of objects, but the fully-qualified
+"tag:nltk.sourceforge.net,2007:" prefix is also accepted in case anyone ends up
+using it.
+"""
+
+def custom_import(name):
+    components = name.split('.')
+    module_path = '.'.join(components[:-1])
+    mod = __import__(module_path)
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
+
+def metaloader(classpath):
+    def loader(*args, **kwds):
+        classref = custom_import(classpath)
+        return classref.from_yaml(*args, **kwds)
+    return loader
+
+def register_tag(tag, classpath):
+    yaml.add_constructor(u'!'+tag, metaloader(classpath))
+    yaml.add_constructor(u'tag:nltk.sourceforge.net,2007:'+tag, metaloader(classpath))
+
+register_tag(u'tag.Unigram', 'nltk_lite.tag.unigram.Unigram')
+register_tag(u'tag.Brill', 'nltk_lite.tag.brill.Brill')
+
