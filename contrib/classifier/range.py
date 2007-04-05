aa2b9d476865c3d60c78d0232635ada42db002bf
Sumukh Ghodke <sumukh.ghodke@gmail.com>
2007-04-05 07:08:35 +0000

    added splitting functionality to ranges
    svn/trunk/nltk/nltk_lite@4312

--- contrib/classifier/range.py
+++ contrib/classifier/range.py
@@ -10,7 +10,7 @@
 DELTA = 0.000001
 
 class Range:
-    def __init__(self, lower, upper, upper_includes_max=True):
+    def __init__(self, lower = 0, upper = 0, upper_includes_max=False):
         """
         any number within this range should be greater than or equal to self.lower and 
         less than (or less than equal to depending on whether it includes the max) self.upper
@@ -25,12 +25,28 @@ class Range:
     def include(self, number):
         if number < self.lower:
             self.lower = number
-        elif number > self.upper:
+        elif number >= self.upper:
             self.upper = number + DELTA
             self.__delta_added = True
             
     def includes(self, number):
         return self.lower <= number and self.upper > number
+    
+    def split(self, parts):
+        if self.lower == self.upper: return None
+        len = self.upper - self.lower
+        max = self.upper
+        if self.__delta_added:
+            len -= DELTA
+            max -= DELTA
+        each = len / parts
+        if each < DELTA: return None
+        lower, ranges = self.lower, []
+        for i in range(parts):
+            ranges.append(Range(lower, lower + each))
+            lower += each
+        ranges[parts - 1].include(max)
+        return ranges
 
     def __eq__(self, other):
         if other is None: return False
