bf88ee3a5dfcc8b9fad2f53d06b748d529bc8758
Ewan Klein <ewan.klein@gmail.com>
2007-04-11 16:17:21 +0000

    added contrib.dependency
    svn/trunk@4353

---
+++ nltk_lite/contrib/dependency/__init__.py
@@ -0,0 +1,7 @@
+# Natural Language Toolkit: Dependency Trees
+#
+# Author: Ewan Klein <ewan@inf.ed.ac.uk>
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
+
+from deptree import *
