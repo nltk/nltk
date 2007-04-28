b171f89e02b2355b8223ac8708376af6db375fbb
Steven Bird <stevenbird1@gmail.com>
2007-04-28 13:21:19 +0000

    /nltk_lite/corpora/indian.py * new corpus reader for tagged Indian languages corpus
    /doc/en/*.txt
    * ordered exercises in terms of difficulty
    
    /doc/en/tag.txt
    * added exercise for unknown word problem
    
    /doc/en/preface-nltk.txt
    * merged ease-of-use with simplicity
    
    /doc/en/programming.txt
    * started new section on character encoding and unicode
    
    /nltk_lite/utilities.py
    * added function to guess character encoding
    
    
    svn/trunk/nltk/nltk_lite@4448

--- utilities.py
+++ utilities.py
@@ -421,3 +421,64 @@ class Trie:
     def __repr__(self):
         return "Trie(%r)" % self._root
 
+##########################################################################
+# Guess Character Encoding
+##########################################################################
+
+# adapted from io.py in the docutils extension module (http://docutils.sourceforge.net)
+# http://www.pyzine.com/Issue008/Section_Articles/article_Encodings.html
+
+import locale
+def guess_encoding(data):
+    """
+    Given a byte string, attempt to decode it.
+    Tries the standard 'UTF8' and 'latin-1' encodings,
+    Plus several gathered from locale information.
+
+    The calling program *must* first call 
+        locale.setlocale(locale.LC_ALL, '')
+
+    If successful it returns 
+        (decoded_unicode, successful_encoding)
+    If unsuccessful it raises a ``UnicodeError``
+    """
+    successful_encoding = None
+    # we make 'utf-8' the first encoding
+    encodings = ['utf-8']
+    #
+    # next we add anything we can learn from the locale
+    try:
+        encodings.append(locale.nl_langinfo(locale.CODESET))
+    except AttributeError:
+        pass
+    try:
+        encodings.append(locale.getlocale()[1])
+    except (AttributeError, IndexError):
+        pass
+    try:
+        encodings.append(locale.getdefaultlocale()[1])
+    except (AttributeError, IndexError):
+        pass
+    #
+    # we try 'latin-1' last
+    encodings.append('latin-1')
+    for enc in encodings:
+        # some of the locale calls 
+        # may have returned None
+        if not enc:
+            continue
+        try:
+            decoded = unicode(data, enc)
+            successful_encoding = enc
+
+        except (UnicodeError, LookupError):
+            pass
+        else:
+            break
+    if not successful_encoding:
+         raise UnicodeError(
+        'Unable to decode input data.  Tried the following encodings: %s.'
+        % ', '.join([repr(enc) for enc in encodings if enc]))
+    else:
+         return (decoded, successful_encoding)
+
