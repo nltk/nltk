3a06332ffaddaa91d6a8d0552b4751e385700811
Ewan Klein <ewan.klein@gmail.com>
2007-05-09 10:23:58 +0000

    Extended reader to handle Spanish data
    svn/trunk@4491

--- nltk_lite/corpora/conll2002.py
+++ nltk_lite/corpora/conll2002.py
@@ -3,7 +3,7 @@
 # Copyright (C) 2001-2007 University of Pennsylvania
 # Author: Steven Bird <sb@ldc.upenn.edu>
 #              Edward Loper <edloper@gradient.cis.upenn.edu>
-#              Ewan Klein <ewan@iinf.ed.ac.uk>
+#              Ewan Klein <ewan@inf.ed.ac.uk>
 # URL: <http://nltk.sf.net>
 # For license information, see LICENSE.TXT
 
@@ -16,38 +16,45 @@ from nltk_lite import tokenize, chunk
 from nltk_lite.parse import tree
 import os
 
-items = ['ned.train', 'ned.testa', 'ned.testb']
+esp = ['esp.train', 'esp.testa', 'esp.testb']	
+ned = ['ned.train', 'ned.testa', 'ned.testb']
+items = esp + ned
 
 item_name = {
     'ned.train': 'Dutch training set',
-    'ned.testa':  'Dutch test set a',
-    'ned.testb':  'Dutch test set b'
+    'ned.testa': 'Dutch test set a',
+    'ned.testb': 'Dutch test set b',
+    'esp.train': 'Spanish training set',
+    'esp.testa': 'Spanish test set a',
+    'ned.testb': 'Spanish test set b',
     }
 
 def _list_sent(sent):
     return [tokenize.whitespace(line) for line in tokenize.line(sent)]
 
-def raw(files = ['ned.train']):
+def raw(files = ['ned.train', 'esp.train']):
     if type(files) is str: files = (files,)
     for file in files:
         path = os.path.join(get_basedir(), "conll2002", file)
         s = open(path).read()
         # remove initial -DOCSTART- -DOCSTART- O
-        s = s[23:]
+        if s.startswith('-DOCSTART'):
+            s = s[23:]
         for sent in tokenize.blankline(s):
             yield [word for (word, tag, ner) in _list_sent(sent)]
 
-def pos_tagged(files = ['ned.train']):
+def pos_tagged(files = ['ned.train', 'esp.train']):
     if type(files) is str: files = (files,)
     for file in files:
         path = os.path.join(get_basedir(), "conll2002", file)
         s = open(path).read()
         # remove initial -DOCSTART- -DOCSTART- O
-        s = s[23:]
+        if s.startswith('-DOCSTART-'):
+            s = s[23:]
         for sent in tokenize.blankline(s):
             yield [(word, tag) for (word, tag, ner) in _list_sent(sent)]
 
-def ne_chunked(files = ['ned.train'], chunk_types=('LOC','ORG','PER')):
+def ne_chunked(files = ['ned.train', 'esp.train'], chunk_types=('LOC','ORG','PER')):
     """
     MISC has been omitted
     """
@@ -56,7 +63,8 @@ def ne_chunked(files = ['ned.train'], chunk_types=('LOC','ORG','PER')):
         path = os.path.join(get_basedir(), "conll2002", file)
         s = open(path).read()
         # remove initial -DOCSTART- -DOCSTART- O
-        s = s[23:]
+        if s.startswith('-DOCSTART'):
+            s = s[23:]
         for sent in tokenize.blankline(s):
             yield chunk.conllstr2tree(sent, chunk_types)
 
@@ -64,20 +72,35 @@ def demo():
     from nltk_lite.corpora import conll2002
     from itertools import islice
 
-    print "CONLL Chunked data\n"
+    print "CONLL2002 NE data\n"
     
-    print "Raw text:"
-    for sent in islice(conll2002.raw(), 0, 5):
+    print "Raw text -- Dutch:"
+    for sent in islice(conll2002.raw(files = ['ned.train']), 0, 5):
+        print sent
+    print
+    
+    print "Raw text --Spanish:"
+    for sent in islice(conll2002.raw(files = ['esp.train']), 0, 5):
         print sent
     print
 
-    print "POS Tagged text:"
-    for sent in islice(conll2002.pos_tagged(), 0, 5):
+    print "POS Tagged text -- Dutch:"
+    for sent in islice(conll2002.pos_tagged(files = ['ned.train']), 0, 5):
+        print sent
+    print
+    
+    print "POS Tagged text --Spanish:"
+    for sent in islice(conll2002.pos_tagged(files = ['esp.train']), 0, 5):
         print sent
     print
 
-    print "Named Entity Chunked  text:"
-    for tree in islice(conll2002.ne_chunked(), 0, 5):
+    print "Named Entity chunked text -- Dutch:"
+    for tree in islice(conll2002.ne_chunked(files = ['ned.train']), 0, 5):
+        print tree.pp()
+    print
+    
+    print "Named Entity chunked text --Spanish:"
+    for tree in islice(conll2002.ne_chunked(files = ['esp.train']), 0, 5):
         print tree.pp()
     print
 
