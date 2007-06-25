c47b7672f9d3b90f8959f0790a7474f86e05f66e
Steven Bird <stevenbird1@gmail.com>
2007-06-25 10:53:22 +0000

    moved contrib
    svn/trunk/nltk@4778

--- contrib/dependency/__init__.py
+++
@@ -1,7 +0,0 @@
-# Natural Language Toolkit: Dependency Trees
-#
-# Author: Ewan Klein <ewan@inf.ed.ac.uk>
-# URL: <http://nltk.sf.net>
-# For license information, see LICENSE.TXT
-
-from deptree import *
