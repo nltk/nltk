ba48fad48675142bdb7f3ea6ee876efb2986081b
Steven Bird <stevenbird1@gmail.com>
2007-07-15 19:21:24 +0000

    /nltk/treetransforms.py * added whitespace after backslashes to stop epydoc ignoring newlines
    /doc/en/data.txt
    * moved references to further reading section
    
    /nltk/olac.py
    * expanded code including new function read_olac and improved demo code
    
    /nltk/utilities.py
    * added windowdiff metric
    
    /doc/book.bib
    * several new references
    
    
    svn/trunk@4902

--- nltk/olac.py
+++ nltk/olac.py
@@ -5,15 +5,40 @@
 # URL: <http://nltk.sf.net>
 # For license information, see LICENSE.TXT
 
+
+from StringIO import StringIO
+
+def read_olac(xml):
+    """
+    Read an OLAC XML record and return a list of attributes.
+
+    @param xml: XML string for conversion    
+    @type xml: C{string}
+    @rtype: C{list} of C{tuple}
+    """
+    from lxml import etree
+
+    root = etree.parse(StringIO(xml)).getroot()
+    return [(element.tag, element.attrib, element.text) for element in root.getchildren()]
+
+def pprint_olac(xml):
+    for tag, attrib, text in read_olac(xml):
+        print "%-12s" % tag + ':',
+        if text:
+            print text,
+        if attrib:
+            print "(%s=%s)" % (attrib['type'], attrib['code']),
+        print
+
 def demo():
     from lxml import etree
     from nltk.corpus import find_corpus_file
+    
     file = find_corpus_file('treebank', 'olac', '.xml')
-    root = etree.parse(file).getroot()
-    for element in root.getchildren():
-        print element.tag, element.attrib, element.text
+    xml = open(file).read()
+    pprint_olac(xml)
 
 if __name__ == '__main__':
     demo()
 
-__all__ = []
+__all__ = ['read_olac', 'pprint_olac']
