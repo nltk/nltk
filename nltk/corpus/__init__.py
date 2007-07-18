73220a650144c458efdea8831ec898aafdf9c493
Edward Loper <edloper@ldc.upenn.edu>
2007-07-18 23:30:29 +0000

    use corpus readers to access corpora
    svn/trunk@4919

--- nltk/corpus/__init__.py
+++ nltk/corpus/__init__.py
@@ -6,11 +6,69 @@
 # URL: <http://nltk.sf.net>
 # For license information, see LICENSE.TXT
 
+"""
+NLTK corpus readers.  The modules in this package provide functions
+that can be used to read corpus files in a variety of formats.  These
+functions can be used to read both the corpus files that are
+distributed in the NLTK corpus package, and corpus files that are part
+of external corpora.
+
+Corpus Reader Functions
+-----------------------
+Each corpus module defines one or more X{corpus reader functions},
+which can be used to read documents from that corpus.  These functions
+take an argument, C{item}, which is used to indicate which document
+should be read from the corpus:
+
+  - If C{item} is one of the unique identifiers listed in the corpus
+    module's C{items} variable, then the corresponding document will
+    be loaded from the NLTK corpus package.
+
+  - If C{item} is a filename, then that file will be read.
+
+Additionally, corpus reader functions can be given lists of item
+names; in which case, they will return a concatenation of the
+corresponding documents.
+
+Corpus reader functions are named based on the type of information
+they return.  Some common examples, and their return types, are:
+
+  - I{corpus}.words(): list of str
+  - I{corpus}.sents(): list of (list of str)
+  - I{corpus}.paras(): list of (list of (list of str))
+  - I{corpus}.tagged_words(): list of (str,str) tuple
+  - I{corpus}.tagged_sents(): list of (list of (str,str))
+  - I{corpus}.tagged_paras(): list of (list of (list of (str,str)))
+  - I{corpus}.chunked_sents(): list of (Tree w/ (str,str) leaves)
+  - I{corpus}.parsed_sents(): list of (Tree with str leaves)
+  - I{corpus}.parsed_paras(): list of (list of (Tree with str leaves))
+  - I{corpus}.xml(): A single xml ElementTree
+  - I{corpus}.raw(): unprocessed corpus contents
+
+For example, to read a list of the words in the Brown Corpus, use
+L{nltk.corpus.brown.words()}:
+
+    >>> from nltk.corpus import brown
+    >>> print brown.words()
+    ['The', 'Fulton', 'County', 'Grand', 'Jury', 'said', ...]
+
+Corpus Metadata
+---------------
+Metadata about the NLTK corpora, and their individual documents, is
+stored using L{Open Language Archives Community (OLAC)
+<http://www.language-archives.org/>} metadata records.  These records
+can be accessed using C{nltk.corpus.I{corpus}.olac()}.
+"""
+
 from util import *
-import abc, brown, chat80, cmudict, conll2000, conll2002, genesis, gutenberg
-import ieer, inaugural, indian, names, ppattach, senseval, shakespeare
-import sinica_treebank, state_union, stopwords, timit, toolbox, treebank
-import udhr, web, webtext, words, ycoe
+from reader import *
+
+# Load all corpora from the NLTK corpus package:
+globals().update(load_nltk_corpora())
+
+# Allow the user to reload the corpora:
+def reload_corpora():
+    globals().update(load_nltk_corpora())
 
 def demo():
     abc.demo()
@@ -41,5 +99,6 @@ def demo():
 #    ycoe.demo()
 
 if __name__ == '__main__':
-    demo()
+    pass
+    #demo()
     
