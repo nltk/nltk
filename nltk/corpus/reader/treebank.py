090e77c717cf13b83a11b0fbdb9d2ed1675021df
Edward Loper <edloper@ldc.upenn.edu>
2007-07-20 21:51:52 +0000

    moved chunked corpus reader to its own module
    svn/trunk@4941

--- nltk/corpus/reader/treebank.py
+++ nltk/corpus/reader/treebank.py
@@ -6,61 +6,23 @@
 # URL: <http://nltk.sf.net>
 # For license information, see LICENSE.TXT
 
-from nltk.corpus.reader.util import *
-from nltk.corpus.reader.api import *
-from nltk.corpus.reader.bracket_parse import BracketParseCorpusReader
-from nltk.tree import Tree
-from nltk import tokenize, chunk
-import os.path
-
 """
 Penn Treebank corpus sample: tagged, NP-chunked, and parsed data from
 Wall Street Journal for 3700 sentences.
 
 This is a ~10% fragment of the Wall Street Journal section of the Penn
-Treebank, (C) LDC 1995.  It is distributed with the Natural Language Toolkit
-under the terms of the Creative Commons Attribution-NonCommercial-ShareAlike License
+Treebank, (C) LDC 1995.  It is distributed with the Natural Language
+Toolkit under the terms of the Creative Commons
+Attribution-NonCommercial-ShareAlike License
 [http://creativecommons.org/licenses/by-nc-sa/2.5/].
-
-Raw:
-
-    Pierre Vinken, 61 years old, will join the board as a nonexecutive
-    director Nov. 29.
-
-Tagged:
-
-    Pierre/NNP Vinken/NNP ,/, 61/CD years/NNS old/JJ ,/, will/MD join/VB 
-    the/DT board/NN as/IN a/DT nonexecutive/JJ director/NN Nov./NNP 29/CD ./.
-
-NP-Chunked:
-
-    [ Pierre/NNP Vinken/NNP ]
-    ,/, 
-    [ 61/CD years/NNS ]
-    old/JJ ,/, will/MD join/VB 
-    [ the/DT board/NN ]
-    as/IN 
-    [ a/DT nonexecutive/JJ director/NN Nov./NNP 29/CD ]
-    ./.
-
-Parsed:
-
-    ( (S 
-      (NP-SBJ 
-        (NP (NNP Pierre) (NNP Vinken) )
-        (, ,) 
-        (ADJP 
-          (NP (CD 61) (NNS years) )
-          (JJ old) )
-        (, ,) )
-      (VP (MD will) 
-        (VP (VB join) 
-          (NP (DT the) (NN board) )
-          (PP-CLR (IN as) 
-            (NP (DT a) (JJ nonexecutive) (NN director) ))
-          (NP-TMP (NNP Nov.) (CD 29) )))
-      (. .) ))
 """
+from nltk.corpus.reader.util import *
+from nltk.corpus.reader.api import *
+from nltk.corpus.reader.bracket_parse import BracketParseCorpusReader
+from nltk.corpus.reader.chunk import ChunkedCorpusReader
+from nltk.tree import Tree
+from nltk import tokenize, chunk
+import os.path
 
 class TreebankCorpusReader(CorpusReader):
     """
@@ -77,8 +39,10 @@ class TreebankCorpusReader(CorpusReader):
         self._root = root
         self._mrg_reader = BracketParseCorpusReader(
             os.path.join(root, 'combined'), '.*', '.mrg')
-        self._pos_reader = BracketChunkCorpusReader(
-            os.path.join(root, 'tagged'), '.*', '.pos')
+        self._pos_reader = ChunkedCorpusReader(
+            os.path.join(root, 'tagged'), '.*', '.pos',
+            sent_tokenizer=tagged_treebank_sent_tokenizer,
+            para_block_reader=tagged_treebank_para_block_reader)
 
         # Make sure we have a consistent set of items:
         if set(self._mrg_reader.items) != set(self._pos_reader.items):
@@ -125,151 +89,29 @@ class TreebankCorpusReader(CorpusReader):
         return concat([re.sub(r'\A\s*\.START\s*', '', open(filename).read())
                        for filename in filenames])
 
-class BracketChunkCorpusReader(CorpusReader):
-    """
-    Reader for corpora that consist of treebank-style chunked&tagged
-    text (i.e., '*.pos' files).  For reading the Treebank corpus
-    itself, you may wish to use L{TreebankCorpusReader}, which
-    combines this reader with readers for the other formats available
-    in the treebank.
-    """
-    def __init__(self, root, items, extension=''):
-        """
-        @param root: The root directory for this corpus.
-        @param items: A list of items in this corpus.
-        @param extension: File extension for items in this corpus.
-        """
-        if isinstance(items, basestring):
-            items = find_corpus_items(root, items, extension)
-        self._root = root
-        self.items = tuple(items)
-        self._extension = extension
+def tagged_treebank_sent_tokenizer(para):
+    return tokenize.regexp(para, r'(?<=/\.)\s', gaps=True)
 
-    def raw(self, items=None):
-        return concat([open(filename).read()
-                       for filename in self._item_filenames(items)])
-
-    def words(self, items=None):
-        return concat([BracketChunkCorpusView(filename, 0, 0, 0, 0)
-                       for filename in self._item_filenames(items)])
-
-    def sents(self, items=None):
-        return concat([BracketChunkCorpusView(filename, 0, 1, 0, 0)
-                       for filename in self._item_filenames(items)])
-
-    def paras(self, items=None):
-        return concat([BracketChunkCorpusView(filename, 0, 1, 1, 0)
-                       for filename in self._item_filenames(items)])
-
-    def tagged_words(self, items=None):
-        return concat([BracketChunkCorpusView(filename, 1, 0, 0, 0)
-                       for filename in self._item_filenames(items)])
-
-    def tagged_sents(self, items=None):
-        return concat([BracketChunkCorpusView(filename, 1, 1, 0, 0)
-                       for filename in self._item_filenames(items)])
-
-    def tagged_paras(self, items=None):
-        return concat([BracketChunkCorpusView(filename, 1, 1, 1, 0)
-                       for filename in self._item_filenames(items)])
-
-    def chunked_words(self, items=None):
-        return concat([BracketChunkCorpusView(filename, 1, 0, 0, 1)
-                       for filename in self._item_filenames(items)])
-
-    def chunked_sents(self, items=None):
-        return concat([BracketChunkCorpusView(filename, 1, 1, 0, 1)
-                       for filename in self._item_filenames(items)])
-
-    def chunked_paras(self, items=None):
-        return concat([BracketChunkCorpusView(filename, 1, 1, 1, 1)
-                       for filename in self._item_filenames(items)])
-
-    def _item_filenames(self, items):
-        if items is None: items = self.items
-        if isinstance(items, basestring): items = [items]
-        return [os.path.join(self._root, '%s%s' % (item, self._extension))
-                for item in items]
-        
-    def _read_block(self, stream):
-        return [chunk.tagstr2tree(t) for t in
-                read_blankline_block(stream)]
-
-class BracketChunkCorpusView(StreamBackedCorpusView):
-    def __init__(self, filename, tagged, group_by_sent,
-                 group_by_para, chunked):
-        StreamBackedCorpusView.__init__(self, filename)
-        self._tagged = tagged
-        self._group_by_sent = group_by_sent
-        self._group_by_para = group_by_para
-        self._chunked = chunked
-
-    def read_block(self, stream):
-        # Read the next paragraph.
-        block = ''
-        while True:
-            line = stream.readline()
-            # End of paragraph:
-            if re.match('======+\s*$', line):
-                if block.strip(): break
-            # End of file:
-            elif line == '':
-                if block.strip(): break
-                else: return []
-            # Content line:
-            else:
-                block += line
-
-        # Divide the block into sentences (look for words tagged with
-        # the end-of-word tag '.', such as './.' or '?/.')
-        sents = tokenize.regexp(block, r'(?<=/\.)\s', gaps=True)
-        
-        # Parse each sentence using tagstr2tree.
-        para = [chunk.tagstr2tree(sent) for sent in sents]
-
-        # If requested, throw away the tags.
-        if not self._tagged:
-            para = [self._untag(sent) for sent in para]
-
-        # If requested, throw away the chunks.
-        if not self._chunked:
-            para = [sent.leaves() for sent in para]
-
-        # If requested, concatenate the sentences together.
-        if not self._group_by_sent:
-            para = reduce((lambda a,b:a+b), para, [])
-
-        # Return the paragraph as a single token, or as a list of
-        # pieces, depending on whether we're grouping by para.
-        if self._group_by_para:
-            return [para]
+def tagged_treebank_para_block_reader(stream):
+    # Read the next paragraph.
+    para = ''
+    while True:
+        line = stream.readline()
+        # End of paragraph:
+        if re.match('======+\s*$', line):
+            if para.strip(): return [para]
+        # End of file:
+        elif line == '':
+            if para.strip(): return [para]
+            else: return []
+        # Content line:
         else:
-            return para
-
-    def _untag(self, tree):
-        for i, child in enumerate(tree):
-            if isinstance(child, Tree):
-                self._untag(child)
-            elif isinstance(child, tuple):
-                tree[i] = child[0]
-            else:
-                raise ValueError('expected child to be Tree or tuple')
-        return tree
-    
+            para += line
+            
 ######################################################################
 #{ Demo
 ######################################################################
 def demo():
-
-#     tb = BracketChunkCorpusReader('/Users/edloper/data/projects/nltk/corpora/treebank/tagged', '.*', '.pos')
-#     print tb.words('wsj_0120')
-#     for para in tb.chunked_paras('wsj_0120')[:3]:
-#         print '='*50
-#         for sent in para:
-#             print ' '.join(str(w) for w in sent)
-    
-
-
     from nltk.corpus import treebank
 
     print "Parsed:"
