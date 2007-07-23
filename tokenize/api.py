199111d034d85581eae6d9a17439ff2c99f0cd06
Edward Loper <edloper@ldc.upenn.edu>
2007-07-23 02:43:08 +0000

    added api file
    svn/trunk/nltk@4958

---
+++ tokenize/api.py
@@ -0,0 +1,23 @@
+# Natural Language Toolkit: Tokenizer Interface
+#
+# Copyright (C) 2001-2007 University of Pennsylvania
+# Author: Edward Loper <edloper@gradient.cis.upenn.edu>
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
+
+"""
+Tokenizer Interface
+"""
+
+class TokenizerI:
+    """
+    A procesing interface for I{tokenizing} a string, or dividing it
+    into a list of substrings.
+    """
+    def tokenize(self, s):
+        """
+        Divide the given string into a list of substrings.
+        
+        @return: C{list} of C{str}
+        """
+        raise NotImplementedError()
