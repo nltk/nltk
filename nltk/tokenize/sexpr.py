ca75614c055ec4c9b208c2a55cfe5c93a1056870
Edward Loper <edloper@ldc.upenn.edu>
2007-07-24 19:05:26 +0000

    Updated the nltk.tokenize package.   - All tokenizers are now available as subclasses of TokenizeI   - Selected tokenizers are also available as functions   - Function names are "whitespace_tokenize()", "wordpunct_tokenize()",     etc.  The old "tokenize.whitespace()" etc names are still available,     but should be considered deprecated!  They will be removed before     nltk 1.0.   - Simplified the code in regexp tokenizer.  It now delegates to     utilities.convert_regexp_to_nongrouping to convert grouping parens     to nongrouping ones.  Once this is done, it can simply use     re.findall() (for gaps=False) and re.split() (for gaps=True).     I also added a flag to control whether empty tokens are discarded     when gaps=True -- defaults to True (matching the original behavior).   - Changed implementation of sexpr tokenizer to something more efficient.     Tested on treebank text, it runs about twice as fast.  I also added     options for specifying custom parenthases (e.g., for [..[..]] style     trees) a 'strict' parameter to decide whether mismatched parens gives     an exception, or returns a list containing partial sexprs.
    svn/trunk@4988

--- nltk/tokenize/sexpr.py
+++ nltk/tokenize/sexpr.py
@@ -6,53 +6,112 @@
 # URL: <http://nltk.sourceforge.net>
 # For license information, see LICENSE.TXT
 
-import re
+"""
+A tokenizer that divides strings into s-expressions.  E.g.:
 
-def sexpr(s):
+    >>> sexpr_tokenize('(a b (c d)) e f (g)')
+    ['(a b (c d))', 'e', 'f', '(g)']
+"""
+
+import re
+from nltk.tokenize.api import *
+
+class SExprTokenizer(TokenizerI):
     """
-    Tokenize the text into s-expressions.  For example, the input
-    "(a b (c d)) e (f)" is tokenized into the following sequence:
-    "(a b (c d))", "e", "(f)".
+    A tokenizer that divides strings into X{s-expressions}.  An
+    s-expresion can be either:
+   
+      - A parenthasized expression, including any nested parenthasized
+        expressions.
+      - A sequence of non-whitespace non-parenthasis characters.
     
-    @param s: the string or string iterator to be tokenized
-    @type s: C{string} or C{iter(string)}
-    @return: An iterator over tokens (each of which is an s-expression)
+    For example, the string C{'(a (b c)) d e (f)'} consists of four
+    s-expressions: C{'(a (b c))'}, C{'d'}, C{'e'}, and C{'(f)'}.  
     """
-    def matching_paren(s,start=0):
-        count = 1
-        for (i,c) in enumerate(s[start+1:]):
-            if c == '(':
-                count += 1
-            elif c == ')':
-                count -= 1
-            if count == 0:
-                return i+1
-        return -1
-          
-    while s:
-        s = s.strip()
-        if s[0] == '(':
-            matching_paren_pos = matching_paren(s)
-            if matching_paren_pos == -1:
-                yield s
-                s = ''
-            else:
-                yield s[0:matching_paren_pos+1]
-                s = s[matching_paren_pos+1:]
-        else: 
-            space_pos = re.search("\s|$",s).start()
-            yield s[0:space_pos]
-            s = s[space_pos:]
+    def __init__(self, parens='()', strict=True):
+        """
+        Construct a new SExpr tokenizer.  By default, the characters
+        C{'('} and C{')'} are treated as open and close parenthases;
+        but alternative strings may be specified.
+
+        @param parens: A two-element sequence specifying the open and
+            close parenthases that should be used to find sexprs.  This
+            will typically be either a two-character string, or a list
+            of two strings.
+        @type parens: C{str} or C{list}
+        @param strict: If true, then raise an exception when tokenizing
+            an ill-formed sexpr.
+        """
+        if len(parens) != 2:
+            raise ValueError('parens must contain exactly two strings')
+        self._strict = strict
+        self._open_paren = parens[0]
+        self._close_paren = parens[1]
+        self._paren_regexp = re.compile('%s|%s' % (re.escape(parens[0]),
+                                                   re.escape(parens[1])))
+    
+    def tokenize(self, text):
+        """
+        Tokenize the text into s-expressions.  For example:
+    
+            >>> SExprTokenizer().tokenize('(a b (c d)) e f (g)')
+            ['(a b (c d))', 'e', 'f', '(g)']
+
+        All parenthases are assumed to mark sexprs.  In particular, no
+        special processing is done to exclude parenthases that occur
+        inside strings, or following backslash characters.
+
+        If the given expression contains non-matching parenthases,
+        then the behavior of the tokenizer depends on the C{strict}
+        parameter to the constructor.  If C{strict} is C{True}, then
+        raise a C{ValueError}.  If C{strict} is C{False}, then any
+        unmatched close parenthases will be listed as their own
+        s-expression; and the last partial sexpr with unmatched open
+        parenthases will be listed as its own sexpr:
+
+            >>> SExprTokenizer(strict=False).tokenize('c) d) e (f (g')
+            ['c', ')', 'd', ')', 'e', '(f (g']
+        
+        @param text: the string to be tokenized
+        @type text: C{string} or C{iter(string)}
+        @return: An iterator over tokens (each of which is an s-expression)
+        """
+        result = []
+        pos = 0
+        depth = 0
+        for m in self._paren_regexp.finditer(text):
+            paren = m.group()
+            if depth == 0:
+                result += text[pos:m.start()].split()
+                pos = m.start()
+            if paren == self._open_paren:
+                depth += 1
+            if paren == self._close_paren:
+                if self._strict and depth == 0:
+                    raise ValueError('Un-matched close paren at char %d'
+                                     % m.start())
+                depth = max(0, depth-1)
+                if depth == 0:
+                    result.append(text[pos:m.end()])
+                    pos = m.end()
+        if self._strict and depth > 0:
+            raise ValueError('Un-matched open paren at char %d' % pos)
+        if pos < len(text):
+            result.append(text[pos:])
+        return result
+
+sexpr_tokenize = SExprTokenizer().tokenize
 
 def demo():
     from nltk import tokenize
 
-    example = "a b d (d e (f)) r (t i) (iu a"
+    example = "d (d ((e) ((f) ss) a a c) d) r (t i) (iu a"
+    example = "d [d [[e] [[f] ss] a a c] d] r [t i]"
     print 'Input text:'
     print example
     print
     print 'Tokenize s-expressions:'
-    for x in tokenize.sexpr(example):
+    for x in SExprTokenizer('[]').tokenize(example):
 	print x
 
 if __name__ == '__main__':
