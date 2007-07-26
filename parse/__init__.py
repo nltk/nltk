a38b43d0cb5f4a79705940e4c5480ed0c5987fa7
Ewan Klein <ewan.klein@gmail.com>
2007-07-26 17:44:15 +0000

    Created util.py to contain testsuite code (for doctest)
    svn/trunk/nltk@5020

--- parse/__init__.py
+++ parse/__init__.py
@@ -45,7 +45,9 @@ from api import *
 from category import *
 from chart import *
 from featurechart import *
-from sr import *
-from rd import *
 from pchart import *
+from rd import *
+from sr import *
+from util import *
 from viterbi import *
+
