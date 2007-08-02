7168f1559d1880a90acdc819e4d292372f8ec744
Edward Loper <edloper@ldc.upenn.edu>
2007-08-02 01:05:40 +0000

    added docstrings; fixed bug when features dont always get values; made show_most_informative_features more general; handle unseen feature names & feature values in a reasonable way.
    svn/trunk/nltk@5040

--- classify/naivebayes.py
+++ classify/naivebayes.py
@@ -8,11 +8,28 @@
 # $Id: naivebayes.py 2063 2004-07-17 21:02:24Z edloper $
 
 """
-::
+A classifier based on the Naive Bayes algorithm.  In order to find the
+probability for a label, this algorithm first uses the Bayes rule to
+express P(label|features) in terms of P(label) and P(features|label)::
+
+                      P(label) * P(features|label)
+ P(label|features) = ------------------------------
+                             P(features)
+
+The algorithm then makes the 'naive' assumption that all features are
+independent, given the label::
+                             
                       P(label) * P(f1|label) * ... * P(fn|label)
- P(label|features) = -------------------------------------------
+ P(label|features) = --------------------------------------------
                                         P(features)
 
+Rather than computing P(featues) explicitly, the algorithm just
+calculates the denominator for each label, and normalizes them so they
+sum to one::
+                             
+                      P(label) * P(f1|label) * ... * P(fn|label)
+ P(label|features) = --------------------------------------------
+                       SUM[l]( P(l) * P(f1|l) * ... * P(fn|l) )
 """
 
 from nltk.classify.api import *
@@ -25,12 +42,40 @@ from nltk import defaultdict
 
 class NaiveBayesClassifier(ClassifyI):
     """
-    A Naive Bayes classifier.
+    A Naive Bayes classifier.  Naive Bayes classifiers are
+    paramaterized by two probability distributions:
+
+      - P(label) gives the probability that an input will receive each
+        label, given no information about the input's features.
+        
+      - P(fname=fval|label) gives the probability that a given feature
+        (fname) will receive a given value (fval), given that the
+        label (label).
+
+    If the classifier encounters an input with a feature that has
+    never been seen with any label, then rather than assigning a
+    probability of 0 to all labels, it will ignore that feature.
+
+    The feature value 'None' is reserved for unseen feature values;
+    you generally should not use 'None' as a feature value for one of
+    your own features.
     """
     def __init__(self, label_probdist, feature_probdist):
         """
-        @param label_probdist: P(label)
-        @param feature_probdist: P(fval|label,f)
+        @param label_probdist: P(label), the probability distribution
+            over labels.  It is expressed as a L{ProbDist} whose
+            samples are labels.  I.e., P(label) =
+            C{label_probdist.prob(label)}.
+        
+        @param feature_probdist: P(fname=fval|label), the probability
+            distribution for feature values, given labels.  It is
+            expressed as a dictionary whose keys are C{(label,fname)}
+            pairs and whose values are L{ProbDist}s over feature
+            values.  I.e., P(fname=fval|label) =
+            C{feature_probdist[label,fname].prob(fval)}.  If a given
+            C{(label,fname)} is not a key in C{feature_probdist}, then
+            it is assumed that the corresponding P(fname=fval|label)
+            is 0 for all values of C{fval}.
         """
         self._label_probdist = label_probdist
         self._feature_probdist = feature_probdist
@@ -49,33 +94,58 @@ class NaiveBayesClassifier(ClassifyI):
         if isinstance(featureset, list): # Handle batch mode.
             return [self.probdist(fs) for fs in featureset]
                 
+        # Discard any feature names that we've never seen before.
+        # Otherwise, we'll just assign a probability of 0 to
+        # everything.
+        featureset = featureset.copy()
+        for fname in featureset.keys():
+            for label in self._labels:
+                if (label, fname) in self._feature_probdist:
+                    break
+            else:
+                #print 'Ignoring unseen feature %s' % fname
+                del featureset[fname]
+
         # Find the log probabilty of each label, given the features.
+        # Start with the log probability of the label itself.
         logprob = {}
         for label in self._labels:
             logprob[label] = self._label_probdist.logprob(label)
-
+            
+        # Then add in the log probability of features given labels.
         for label in self._labels:
             for (fname, fval) in featureset.items():
                 if (label, fname) in self._feature_probdist:
                     feature_probs = self._feature_probdist[label,fname]
                     logprob[label] += feature_probs.logprob(fval)
-
+                else:
+                    # nb: This case will never come up if the
+                    # classifier was created by
+                    # NaiveBayesClassifier.train().
+                    logprob[label] += sum_logs([]) # = -INF.
+                    
         return DictionaryProbDist(logprob, normalize=True, log=True)
 
     def show_most_informative_features(self, n=10):
         # Determine the most relevant features, and display them.
         cpdist = self._feature_probdist
-        print '\nMost Informative Features    ',
-        print 'P(fval|Male) : P(fval|Female)\n'+'-'*60
+        print '\nMost Informative Features'
+
         for (fname, fval) in self.most_informative_features(10):
-            m = cpdist['male',fname].prob(fval)
-            f = cpdist['female',fname].prob(fval)
-            if m == f:
-                print '%14s = %-7r  %16d : %d' % (fname, fval, 1, 1)
-            if m > f:
-                print '%14s = %-7r  %16.1f : %d' % (fname, fval, m/f, 1)
+            def labelprob(l):
+                return cpdist[l,fname].prob(fval)
+            labels = sorted([l for l in self._labels
+                             if (l,fname) in cpdist], key=labelprob)
+            if len(labels) == 1: continue
+            l0 = labels[0]
+            l1 = labels[-1]
+            if cpdist[l0,fname].prob(fval) == 0:
+                ratio = 'INF'
             else:
-                print '%14s = %-7r  %16d : %.1f' % (fname, fval, 1, f/m)
+                ratio = '%.1f' % (cpdist[l1,fname].prob(fval) /
+                                  cpdist[l0,fname].prob(fval))
+            print ('%24s = %-7r  %s:%s = %s : 1.0' %
+                   (fname, fval, l1, l0, ratio))
 
     def most_informative_features(self, n=100):
         """
@@ -114,20 +184,36 @@ class NaiveBayesClassifier(ClassifyI):
     def train(labeled_featuresets, estimator=ELEProbDist):
         """
         @param labeled_featuresets: A list of classified featuresets,
-            i.e., tuples C{(featureset, label)}.
+            i.e., a list of tuples C{(featureset, label)}.
         """
         label_freqdist = FreqDist()
         feature_freqdist = defaultdict(FreqDist)
         feature_values = defaultdict(set)
+        fnames = set()
 
+        # Count up how many times each feature value occured, given
+        # the label and featurename.
         for featureset, label in labeled_featuresets:
             label_freqdist.inc(label)
-            
             for fname, fval in featureset.items():
                 # Increment freq(fval|label, fname)
                 feature_freqdist[label, fname].inc(fval)
                 # Record that fname can take the value fval.
                 feature_values[fname].add(fval)
+                # Keep a list of all feature names.
+                fnames.add(fname)
+
+        # If a feature didn't have a value given for an instance, then
+        # we assume that it gets the implicit value 'None.'  This loop
+        # counts up the number of 'missing' feature values for each
+        # (label,fname) pair, and increments the count of the fval
+        # 'None' by that amount.
+        for label in label_freqdist:
+            num_samples = label_freqdist[label]
+            for fname in fnames:
+                count = feature_freqdist[label, fname].N()
+                feature_freqdist[label, fname].inc(None, num_samples-count)
+                feature_values[fname].add(None)
 
         # Create the P(label) distribution
         label_probdist = estimator(label_freqdist)
