f9b81ec77423aecd4bde8a3825a042d3d94c36ab
Edward Loper <edloper@ldc.upenn.edu>
2007-08-11 21:46:30 +0000

    - Replaced the backup version of defaultdict with one that more   precisely mirrors the Python 2.5 implementation, including   behavior of copy() and str(), allowing keyword args to the constructor,   allowing the default_factory to be None, etc.
    svn/trunk@5067

--- nltk/compat.py
+++ nltk/compat.py
@@ -30,22 +30,46 @@ except ImportError:
     from nltk.etree import ElementTree
 
 # collections.defaultdict
-# contributed by Yoav Goldberg <yoav.goldberg@gmail.com>
-
+# originally contributed by Yoav Goldberg <yoav.goldberg@gmail.com>
+# new version by Jason Kirtland from Python cookbook.
+# <http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/523034>
 try:
     from collections import defaultdict
 except ImportError:
     class defaultdict(dict):
-        def __init__(self, default_constructor, *rest):
-            dict.__init__(self,*rest)
-            self.default_constructor = default_constructor
-
-        def __getitem__(self, s):
+        def __init__(self, default_factory=None, *a, **kw):
+            if (default_factory is not None and
+                not hasattr(default_factory, '__call__')):
+                raise TypeError('first argument must be callable')
+            dict.__init__(self, *a, **kw)
+            self.default_factory = default_factory
+        def __getitem__(self, key):
             try:
-                return dict.__getitem__(self, s)
+                return dict.__getitem__(self, key)
             except KeyError:
-                dict.__setitem__(self, s, self.default_constructor())
-                return dict.__getitem__(self,s)
+                return self.__missing__(key)
+        def __missing__(self, key):
+            if self.default_factory is None:
+                raise KeyError(key)
+            self[key] = value = self.default_factory()
+            return value
+        def __reduce__(self):
+            if self.default_factory is None:
+                args = tuple()
+            else:
+                args = self.default_factory,
+            return type(self), args, None, None, self.items()
+        def copy(self):
+            return self.__copy__()
+        def __copy__(self):
+            return type(self)(self.default_factory, self)
+        def __deepcopy__(self, memo):
+            import copy
+            return type(self)(self.default_factory,
+                              copy.deepcopy(self.items()))
+        def __repr__(self):
+            return 'defaultdict(%s, %s)' % (self.default_factory,
+                                            dict.__repr__(self))
 
     # [XX] to make pickle happy in python 2.4:
     import collections
