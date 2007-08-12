a2cb9ccf1ba28e00a453ea667d44801854a04600
Edward Loper <edloper@ldc.upenn.edu>
2007-08-12 06:28:05 +0000

    [branch] added some deprecated classes
    svn/branches/restructure_tag@5071

--- tag/__init__.py
+++ tag/__init__.py
@@ -14,8 +14,6 @@ which is known as X{tagging}, is defined by the L{TaggerI} interface.
 
 from api import *
 from util import *
-#from unigram import *
-#from ngram import *
 from sequential import *
 from brill import *
 
@@ -27,23 +25,39 @@ try:
 except ImportError:
     pass
 
-# # Deprecated:
-# from nltk.utilities import Deprecated
-# class TagI(TaggerI, Deprecated):
-#     'Use nltk.TaggerI instead.'
-# class SequentialBackoff(SequentialBackoffTagger, Deprecated):
-#     'Use nltk.SequentialBackoffTagger instead.'
-# class Unigram(Deprecated, SequentialBackoffTagger):
-#     """Use nltk.UnigramTagger instead.  Note: UnigramTagger.train()
-#     is now a static method."""
-#     def __init__(self, cutoff=1, backoff=None):
-#         self._cutoff = cutoff
-#         self._backoff = backoff
-#         self._tagger = None
-#     def train(self, tagged_corpus, verbose=False):
-#         self._tagger = UnigramTagger.train(tagged_corpus, verbose)
-#class LookupTagger(UnigramTagger, Deprecated):
-#    'Use UnigramTagger instead.'
+######################################################################
+#{ Deprecated
+######################################################################
+from nltk.utilities import Deprecated
+class TagI(TaggerI, Deprecated):
+    """Use nltk.TaggerI instead."""
+class SequentialBackoff(SequentialBackoffTagger, Deprecated):
+    """Use nltk.SequentialBackoffTagger instead.  Note: the methods
+    used to subclass SequentialBackoffTagger do not match those of
+    the old nltk.tag.SequentialBackoff; see the api docs for info."""
+class Ngram(SequentialBackoffTagger, Deprecated):
+    """Use nltk.NgramTagger instead.  Note: NgramTagger.train() is now
+    a static method."""
+    def __init__(self, n, cutoff=1, backoff=None):
+        self._n = n
+        self._cutoff = cutoff
+        self._backoff = backoff
+        self._tagger = None
+    def train(self, tagged_corpus, verbose=False):
+        self._tagger = NgramTagger.train(tagged_corpus, verbose)
+    def choose_tag(self, tokens, index, history):
+        return self._tagger.choose_tag(tokens, index, history)
+class Unigram(Ngram, Deprecated):
+    """Use nltk.UnigramTagger instead.  Note: UnigramTagger.train() is
+    now a static method."""
+class Bigram(Ngram, Deprecated):
+    """Use nltk.BigramTagger instead.  Note: BigramTagger.train() is
+    now a static method."""
+class Trigram(Ngram, Deprecated):
+    """Use nltk.TrigramTagger instead.  Note: TrigramTagger.train() is
+    now a static method."""
+class Lookup(UnigramTagger, Deprecated):
+    'Use UnigramTagger instead.'
 
     
     
