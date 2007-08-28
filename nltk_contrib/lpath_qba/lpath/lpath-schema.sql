45c3c46087b70ca66c27686141921a587979b3ec
Haejoong Lee <haejoong@ldc.upenn.edu>
2007-08-28 17:12:24 +0000

    moved from nltk_contrib/lpath
    svn/trunk@5315

---
+++ nltk_contrib/lpath_qba/lpath/lpath-schema.sql
@@ -0,0 +1,17 @@
+create table LPATH_TABLE (
+sid	int,
+tid	int,
+id	int,
+pid	int,
+l	int,
+r	int,
+d	int,
+type	varchar(32),
+name	varchar(32),
+value	varchar(128)
+);
+
+create index TABLE_all_idx on LPATH_TABLE (name,type,sid,tid,id,pid,l,r,d,value);
+create index TABLE_sid_idx on LPATH_TABLE (sid, tid, l, r, d);
+create index TABLE_value_idx on LPATH_TABLE (value, type, name, sid, tid, pid);
+
