9e794334bd6a8e8352d6ab1b71d2de4f4ea1c011
Steven Bird <stevenbird1@gmail.com>
2007-09-11 05:02:48 +0000

    /data/corpora/wordnet/cntlist.rev * added file giving counts for wordnet senses
    /nltk/wordnet/*.py
    * cleanups (1/0 to True/False)
    * a,b=x,y to a=x, b=y
    
    /nltk/wordnet/frequency.py
    * initial work on accessing sense count
    
    
    
    svn/trunk@5358

---
+++ nltk/wordnet/frequency.py
@@ -0,0 +1,34 @@
+# Natural Language Toolkit: Wordnet Interface: Sense frequency
+#
+# Copyright (C) 2001-2007 University of Pennsylvania
+# Author: Steven Bird <sb@csse.unimelb.edu.au>
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
+
+# Count of a tagged sense in a semantic concordance
+
+# Incomplete:
+# work out how to construct key
+# access method for individual wordsense, e.g. N['dog'][0].count()
+
+import types
+from util import *
+import nltk.data
+
+class SenseCount(object):
+    
+    def __init__(self):
+        self._loaded = False
+        
+    def load(self):
+        if not self._loaded:
+            path = nltk.data.find('corpora/wordnet/cntlist.rev')
+            self._file = open(path, FILE_OPEN_MODE)
+            self._loaded = True
+    
+    def count(self, key):
+        self.load()
+        line = binarySearchFile(self._file, key)
+        # incomplete
+
+S = SenseCount()
