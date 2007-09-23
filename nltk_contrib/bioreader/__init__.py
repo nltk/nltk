ce3ddb7a6ee60ef2722c1637d076972374ca4fa2
Ewan Klein <ewan.klein@gmail.com>
2007-09-23 12:12:53 +0000

    initial check-in of Carlos Rodriguez' BioReader code
    svn/trunk@5406

---
+++ nltk_contrib/bioreader/__init__.py
@@ -0,0 +1,9 @@
+# Natural Language Toolkit: Third-Party Contributions
+#
+# Contributions from: Carlos Rodriguez
+#
+# Copyright (C) 2001-2005 The original contributors
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
+#
+
