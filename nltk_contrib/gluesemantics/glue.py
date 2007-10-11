e5d9839f3249fb9826fb827b4c7b7719f2375e90
Dan Garrette <dhgarrette@gmail.com>
2007-10-11 23:41:00 +0000

    Updated to work with changes to the parser.
    svn/trunk@5465

--- nltk_contrib/gluesemantics/glue.py
+++ nltk_contrib/gluesemantics/glue.py
@@ -198,48 +198,48 @@ class FStructure(dict):
 
         self.parent = parent
         
-        if pt.node.head() == 'S':
-            if pt[0].node.head() == 'NP': # S -> NP VP
+        if head(pt.node) == 'S':
+            if head(pt[0].node) == 'NP': # S -> NP VP
                 self['subj'] = FStructure(pt[0], current_label, self)
-                if pt[1][0].node.head() == 'IV' or pt[1][0].node.head() == 'TV' or \
-                   pt[1][0].node.head() == 'DTV'  or pt[1][0].node.head() == 'EquiV' or \
-                   pt[1][0].node.head() == 'ObjEquiV' or pt[1][0].node.head() == 'TVComp' or \
-                   pt[1][0].node.head() == 'RaisingV':
+                if head(pt[1][0].node) == 'IV' or head(pt[1][0].node) == 'TV' or \
+                   head(pt[1][0].node) == 'DTV'  or head(pt[1][0].node) == 'EquiV' or \
+                   head(pt[1][0].node) == 'ObjEquiV' or head(pt[1][0].node) == 'TVComp' or \
+                   head(pt[1][0].node) == 'RaisingV':
                     self.pred = pt[1][0] # the verb
-                    if pt[1][0].node.head() == 'TV' or pt[1][0].node.head() == 'DTV' or \
-                       pt[1][0].node.head() == 'ObjEquiV':
+                    if head(pt[1][0].node) == 'TV' or head(pt[1][0].node) == 'DTV' or \
+                       head(pt[1][0].node) == 'ObjEquiV':
                         # OBJ for TV, DTV, ObjEquiV
                         self['obj'] = FStructure(pt[1][1], current_label, self)
-                        if pt[1][0].node.head() == 'DTV':
+                        if head(pt[1][0].node) == 'DTV':
                             # THEME for VP -> DTV NP [NP]
                             self['theme'] = FStructure(pt[1][2], current_label, self)
-                        elif pt[1][0].node.head() == 'ObjEquiV':
+                        elif head(pt[1][0].node) == 'ObjEquiV':
                             # XCOMP for VP -> ObjEquiV NP TO [VP]
                             self['xcomp'] = FStructure(pt[1][3], current_label, self)
                             # subj of XCOMP is subj of whole
                             #   ie "John persuades David to go" = "John persaudes David that David goes"
                             self['xcomp']['subj'] = self['obj']
-                    elif pt[1][0].node.head() == 'TVComp':
+                    elif head(pt[1][0].node) == 'TVComp':
                         # VP -> TVComp [S]
                         self['comp'] = FStructure(pt[1][1], current_label, self)
-                    elif pt[1][0].node.head() == 'EquiV':
+                    elif head(pt[1][0].node) == 'EquiV':
                         # VP -> EquiV TO [VP]
                         self['xcomp'] = FStructure(pt[1][2], current_label, self)
                         # subj of XCOMP is subj of whole
                         #   ie "John tries to go" = "John tries that John goes"
                         self['xcomp']['subj'] = self['subj']
-                    elif pt[1][0].node.head() == 'RaisingV':
+                    elif head(pt[1][0].node) == 'RaisingV':
                         # VP -> RaisingV TO [VP]
                         self['xcomp'] = FStructure(pt[1][2], current_label, self)
                         # subj of XCOMP is subj of whole
                         #   ie "John tries to go" = "John tries that John goes"
                         self['xcomp']['subj'] = self['subj']
-##                elif pt[1][0].node.head() == 'ADV':
+##                elif head(pt[1][0].node) == 'ADV':
 ##                    # VP -> ADV VP
 ##                    r = _get_v_and_adjs(pt[1], current_label)
 ##                    self.pred = r[0]
 ##                    if r[1] != []: self['adj'] = r[1]
-                elif pt[1][1].node.head() == 'CC':
+                elif head(pt[1][1].node) == 'CC':
                     # VP -> VP CC VP
                     self.pred = pt[1][1] # the CC
                     self['conjuncts'] = [FStructure(pt[1][0], current_label, self)]
@@ -248,70 +248,70 @@ class FStructure(dict):
                     
                     self['conjuncts'][0]['subj'] = FStructure(pt[0], current_label, self)
                     self['conjuncts'][1]['subj'] = self['conjuncts'][0]['subj']
-            elif pt[1].node.head() == 'CC': # S -o S CC S
+            elif head(pt[1].node) == 'CC': # S -o S CC S
                 self.pred = pt[1] # the CC
                 self['conjuncts'] = [FStructure(pt[0], current_label, self)]
                 self['conjuncts'].append(FStructure(pt[2], current_label, self))
              
-        elif pt.node.head() == 'NP':
-            if pt[0].node.head() == 'Det':
+        elif head(pt.node) == 'NP':
+            if head(pt[0].node) == 'Det':
                 # NP -> Det N
                 r = _get_n_and_adjs(pt[1], current_label, self)
                 self.pred = r[0]
                 if r[1] != []: self['adj'] = r[1]
                 self['spec'] = pt[0][0] # get the Det
-            elif pt[0].node.head() == 'PropN' or pt[0].node.head() == 'Pro':
+            elif head(pt[0].node) == 'PropN' or head(pt[0].node) == 'Pro':
                 # NP -> PropN | Pro
                 self.pred = pt[0]
-            elif pt[0].node.head() == 'N':
+            elif head(pt[0].node) == 'N':
                 # NP -> N[num=pl]
                 r = _get_n_and_adjs(pt[0], current_label, self)
                 self.pred = r[0]
                 if r[1] != []: self['adj'] = r[1]
-            elif pt[1].node.head() == 'CC': # NP -o NP CC NP
+            elif head(pt[1].node) == 'CC': # NP -o NP CC NP
                 self.pred = pt[1] # the CC
                 self['conjuncts'] = [FStructure(pt[0], current_label, self)]
                 self['conjuncts'].append(FStructure(pt[2], current_label, self))
 
-        elif pt.node.head() == 'VP':
-            if pt[0].node.head() == 'IV' or pt[0].node.head() == 'TV' or \
-               pt[0].node.head() == 'DTV'  or pt[0].node.head() == 'EquiV' or \
-               pt[0].node.head() == 'ObjEquiV' or pt[0].node.head() == 'RaisingV' or \
-               pt[0].node.head() == 'TVComp':
+        elif head(pt.node) == 'VP':
+            if head(pt[0].node) == 'IV' or head(pt[0].node) == 'TV' or \
+               head(pt[0].node) == 'DTV'  or head(pt[0].node) == 'EquiV' or \
+               head(pt[0].node) == 'ObjEquiV' or head(pt[0].node) == 'RaisingV' or \
+               head(pt[0].node) == 'TVComp':
                 self.pred = pt[0] # the verb
-                if pt[0].node.head() == 'TV' or pt[0].node.head() == 'DTV' or \
-                   pt[0].node.head() == 'ObjEquiV':
+                if head(pt[0].node) == 'TV' or head(pt[0].node) == 'DTV' or \
+                   head(pt[0].node) == 'ObjEquiV':
                     # OBJ for TV, DTV, ObjEquiV
                     self['obj'] = FStructure(pt[1], current_label, self)
-                    if pt[0].node.head() == 'DTV':
+                    if head(pt[0].node) == 'DTV':
                         # THEME for VP -o DTV NP [NP]
                         self['theme'] = FStructure(pt[2], current_label, self)
-                    elif pt[0].node.head() == 'ObjEquiV':
+                    elif head(pt[0].node) == 'ObjEquiV':
                         # XCOMP for VP -o ObjEquiV NP TO [VP]
                         self['xcomp'] = FStructure(pt[3], current_label, self)
                         # subj of XCOMP is obj of whole
                         #   ie "John persuades David to go" = "John persaudes David that David goes"
                         self['xcomp']['subj'] = self['obj']
-                elif pt[0].node.head() == 'TVComp':
+                elif head(pt[0].node) == 'TVComp':
                     # VP -> TVComp [S]
                     self['comp'] = FStructure(pt[1], current_label, self)
-                elif pt[0].node.head() == 'EquiV':
+                elif head(pt[0].node) == 'EquiV':
                     # VP -> EquiV TO [VP]
                     self['xcomp'] = FStructure(pt[2], current_label, self)
                     # subj of XCOMP is subj of whole
                     #   ie "John tries to go" = "John tries that John goes"
                     self['xcomp']['subj'] = self['subj']
-                elif pt[0].node.head() == 'RaisingV':
+                elif head(pt[0].node) == 'RaisingV':
                     # VP -> RaisingV TO [VP]
                     self['xcomp'] = FStructure(pt[2], current_label, self)
                     # subj of XCOMP is subj of whole
                     #   ie "John tries to go" = "John tries that John goes"
                     self['xcomp']['subj'] = self['subj']
-##                elif pt[0].node.head() == 'RB':
+##                elif head(pt[0].node) == 'RB':
 ##                    # VP -> RB VP
 ##                    self.pred = pt[1] # the verb
 ##                    self['adj'] = [FStructure(pt[0], current_label, self)]
-            elif pt[1].node.head() == 'CC':
+            elif head(pt[1].node) == 'CC':
                 # VP -> VP CC VP
                 self.pred = pt[1] # the CC
                 self['conjuncts'] = [FStructure(pt[0], current_label, self)]
@@ -320,12 +320,12 @@ class FStructure(dict):
                 self['conjuncts'][0]['subj'] = FStructure(pt[0], current_label, self)
                 self['conjuncts'][1]['subj'] = self['conjuncts'][0]['subj']
 
-        elif pt.node.head() == 'JJ':
+        elif head(pt.node) == 'JJ':
             if isinstance(pt[0], str):
                 ## JJ lexical entry
                 self.pred = pt
                 
-        elif pt.node.head() == 'ADV':
+        elif head(pt.node) == 'ADV':
             if isinstance(pt[0], str):
                 ## ADV lexical entry
                 self.pred = pt
@@ -438,15 +438,22 @@ class FStructure(dict):
             elif expression=='var':   return '%s%s' % (self.label.upper(), unique_var_id[0])
             else:                     return self[expression].label
 
+def head(node):
+    for (fname, fval) in sorted(node.items()):
+        display = getattr(fname, 'display', None)
+        if display == 'prefix':
+              return fval
+    return None 
+
 def _get_n_and_adjs(pt, current_label, parent):
     """ This function is here to deal with N -o JJ N rules
         since we don't know exactly where the noun is.
         Returns (noun_word, list_of_adj_fstructs) """
-    if pt.node.head() == 'N':
+    if head(pt.node) == 'N':
         if isinstance(pt[0], str):
             # N lexical entry
             return (pt, [])
-        else: #if self[0].node.head() == 'JJ':
+        else: #if head(self[0].node) == 'JJ':
             # N -o JJ N rule
             r = _get_n_and_adjs(pt[1], current_label, parent)
             jj_fstruct = FStructure(pt[0], current_label, parent)
@@ -524,13 +531,14 @@ def parse_to_glue(sentence='every big gray cat leaves'):
     return [fstruct_to_glue(f) for f in fstructs]
 
 def parse(sentence='a man sees Mary'):
-    from nltk.parse import GrammarFile
-    grammar = GrammarFile(r'glue_grammar.cfg')
-
-    return grammar.earley_parser(0).get_parse_list(list(tokenize.whitespace(sentence)))
+    from nltk.parse import load_earley
+    cp = load_earley(r'grammars/gluesemantics.fcfg')
+    tokens = sentence.split()
+    trees = cp.nbest_parse(tokens)
+    return trees
 
 def pt_to_fstruct(pt):
-    return FStructure(pt[0], [0])
+    return FStructure(pt, [0])
 
 def fstruct_to_glue(fstruct):
     glue_pos_dict = GlueDict()
