373b72becb77601ae3e0655ad587651191b50920
Ewan Klein <ewan.klein@gmail.com>
2007-12-13 16:39:10 +0000

    * Added more documentation * Changed 'assert' statement to a 'try' clause in 'attempt_proof()' * Moved location of input and output files for Prover9 to /tmp directory
    svn/trunk@5592

--- nltk_contrib/theorem_prover/prover.py
+++ nltk_contrib/theorem_prover/prover.py
@@ -1,25 +1,58 @@
-from nltk_contrib.drt import DRT
+# Natural Language Toolkit: Interface to Theorem Provers 
+#
+# Author: Dan Garrette <dhgarrette@gmail.com>
+#              Ewan Klein <ewan@inf.ed.ac.uk>
+
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
+
+from nltk_contrib.drt import ApplicationExpression, FolOperator, Parser
+
 import os
 from string import join
 
 def attempt_proof(expression, prover_name='Prover9'):
-    assert isinstance(expression, DRT.Expression)
+    """
+    Try to prove an expression of First Order Logic. 
+    
+    @param expression: Input expression, which should support the ``toProver9String()``
+    method
+    @type expression: L{DRT.Expression}
+    @type prover_name: C{str}
+    @param prover_name: Name of the prover to use.
+    
+    """
+    #assert isinstance(expression, DRT.Expression)
 
     if prover_name == 'tableau':
         return expression.tableau()
+    
     elif prover_name == 'Prover9':
-        return attempt_proof_prover9(expression)
+        config_prover9(verbose=False)
+        try:
+            p9_expression = expression.toProver9String()
+        except:
+            raise AttributeError("%s cannot be converted to Prover9 input syntax" % expression)
+        
+        return attempt_proof_prover9(p9_expression)
 
 _prover9_path = None
+_prover9_executable = None
+
 _prover9_search = ['.',
                    '/usr/local/bin',
-                   '/usr/share/prover9',
-                   '/usr/local/share/prover9',
-                   '/usr/share/nltk/tools/prover',]
+                   '/usr/local/prover9',
+                   '/usr/local/share/prover9']
 
-def config_prover9(path=None):
-    global _prover9_path
-    global _prover9_executable
+def config_prover9(path=None, verbose=True):
+    """
+    Configure the location of Prover9 Executable
+    
+    @param path: Path to the Prover9 executable
+    @type path: C{str}
+    """
+    
+    global _prover9_path, _prover9_executable
     _prover_path = None
 
     if path is not None:
@@ -35,34 +68,45 @@ def config_prover9(path=None):
         if os.path.exists(exe):
             _prover9_path = path
             _prover9_executable = exe
-            print '[Found Prover9: %s]' % _prover9_executable
+            if verbose:
+                print '[Found Prover9: %s]' % _prover9_executable
             break
   
     if _prover9_path is None:
         raise LookupError("Unable to find Prover9 executable in '%s'\n" 
-            "Use 'config_prover9(path=<path>) ',where '<path>' is valid path to the Prover9  directory,"
+            "Use 'config_prover9(path=<path>) '," 
             " or set the PROVER9HOME environment variable to a valid path." % join(searchpath)) 
         
 def attempt_proof_prover9(expression):
-    import os
-
+    """
+    Use Prover9 to prove an expression.
+    
+    @param expression: logic expression to be proved
+    @rtype: C{str}
+    @return: C{True} if the proof was successful (i.e. returns value of 0),
+    else C{False}
+    
+    """
+    if _prover9_executable is None:
+        config_prover9(verbose=False)
+        
     FILENAME = 'prove'
-
     f = None
+    
     try:
-        f = open('%s/%s.in' % (_prover9_path, FILENAME), 'w')
+        FILEPATH = os.path.join('/tmp', FILENAME)
+        
+        f = open('%s.in' % FILEPATH, 'w')
         f.write('formulas(goals).\n')
-
-        s = expression.infixify().toProver9String()
-
-        execute_string = \
-                       '%s/prover9/provers.src/prover9 -f %s/%s.in > %s/%s.out 2>> %s/%s.out' % \
-                       (PROVER_PATH, PROVER_PATH, FILENAME, \
-                        PROVER_PATH, FILENAME, \
-                        PROVER_PATH, FILENAME)
-
-        f.write('    %s.\n' % s)
+        #s = expression.infixify().toProver9String()
+        f.write('    %s.\n' % expression)
         f.write('end_of_list.\n')
+        f.close()
+        
+        execute_string = \
+                       '%s -f %s.in > %s.out 2>> %s.out' % \
+                       (_prover9_executable, FILEPATH, FILEPATH, FILEPATH)
+        
     finally:
         if f: f.close()
 
@@ -74,11 +118,19 @@ def demo_drt_glue_remove_duplicates():
     drt_glue.demo(remove_duplicates=True)
 
 def demo():
-    #from nltk_contrib.drt import DRT
+    from nltk_contrib.drt import DRT
 
-    #DRT.testTp_equals()
+    DRT.testTp_equals()
     #print '\n\n'
     #demo_drt_glue_remove_duplicates()
-    config_prover9('foo')
+    #lp = Parser()
+    #a = lp.parse(r'drs([x],[(man x), (walks x)])').simplify().toFol()
+    #b = lp.parse(r'drs([x],[(walks x), (man x)])').simplify().toFol()
+    #bicond = ApplicationExpression(ApplicationExpression(FolOperator('iff'), a), b)
+    
+    #print "Trying to prove:\n '%s <-> %s'" % (a, b)
+    #print attempt_proof(bicond)
+    
 
-if __name__ == '__main__': demo()
+if __name__ == '__main__': 
+    demo()
