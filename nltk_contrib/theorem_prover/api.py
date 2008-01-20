031f1b69f2eba27e9d76a8c9da781e2e2ee9c797
Ewan Klein <ewan.klein@gmail.com>
2008-01-20 10:04:03 +0000

    Added Prover9 class
    svn/trunk@5639

---
+++ nltk_contrib/theorem_prover/api.py
@@ -0,0 +1,30 @@
+# Natural Language Toolkit: Classifier Interface
+#
+# 
+# Author: Ewan Klein <ewan@inf.ed.ac.uk>
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
+
+"""
+Interfaces for provers.
+"""
+
+class ProverI(object):
+    """
+    Interface for trying to establish a proof from assumptions.
+    """
+    
+    def prove(self):
+        """
+        @return: Whether the proof was successful or not.
+        @rtype: C{bool} 
+        """
+        raise NotImplementedError()
+    
+    
+    def add_assumptions(self, assumptions):
+        """
+        @param assumptions: Assumptions for the proof
+        @type assumptions: C{list} of strings
+        """
+    
