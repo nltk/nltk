4fd788862659660aa10a9a73bd894fa78bf4d81d
Edward Loper <edloper@ldc.upenn.edu>
2008-02-01 16:44:44 +0000

    - Fixed bug in categorized corpus readers that caused selection of   individual files to fail
    svn/trunk@5733

--- nltk/corpus/reader/plaintext.py
+++ nltk/corpus/reader/plaintext.py
@@ -154,6 +154,8 @@ class CategorizedPlaintextCorpusReader(CategorizedCorpusReader,
             raise ValueError('Specify files or categories, not both')
         if categories is not None:
             return self.files(categories)
+        elif files is None:
+            return files
         else:
             return self._files
     def raw(self, files=None, categories=None):
