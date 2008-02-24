49b002b0daa7cb0ed1cc9d83ea66cbeb6c1ef916
Steven Bird <stevenbird1@gmail.com>
2008-02-24 23:38:52 +0000

    Added initial comment, copyright, etc
    svn/trunk@5781

--- nltk/inference/__init__.py
+++ nltk/inference/__init__.py
@@ -1,3 +1,16 @@
+# Natural Language Toolkit: Inference
+#
+# Copyright (C) 2001-2008 University of Pennsylvania
+# Author: Dan Garrette <dhgarrette@gmail.com>
+#         Ewan Klein <ewan@inf.ed.ac.uk>
+#         
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
+
+"""
+Classes and interfaces for theorem proving and model checking.
+"""
+
 from api import *
 from mace import *
 from prover9 import *
