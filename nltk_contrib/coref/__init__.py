4a9c04e47625fa8db41a6ea89f5fa5a742130697
Joseph Frazee <joseph.frazee@gmail.com>
2008-05-28 00:30:44 +0000

    Added nltk_contrib.coref directory
    svn/trunk@5979

---
+++ nltk_contrib/coref/__init__.py
@@ -0,0 +1,35 @@
+# Natural Language Toolkit (NLTK) Coreference Module
+#
+# Copyright (C) 2008 Joseph Frazee
+# Authors: Joseph Frazee <jfrazee@mail.utexas.edu>
+# URL:     http://nltk.org/
+# For license information, see LICENSE.TXT
+
+"""
+Classes and interfaces for coreference resolution.
+
+"""
+
+##//////////////////////////////////////////////////////
+##  Metadata
+##//////////////////////////////////////////////////////
+
+# Copyright notice
+__copyright__ = """\
+Copyright (C) 2008 Joseph Frazee.
+
+Distributed and Licensed under provisions of the GNU Public
+License, which is included by reference.
+"""
+
+# Maintainer, contributors, etc.
+__maintainer__ = "Joseph Frazee"
+__maintainer_email__ = "jfrazee@mail.utexas.edu"
+__author__ = __maintainer__
+__author_email__ = __maintainer_email__
+
+# Import top-level functionality into top-level namespace
+
+
+# Processing packages -- these all define __all__ carefully.
+
