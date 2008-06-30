562d39dcccf1134e709036acf23f19ffcf5058cb
Petro Verkhogliad <vpetro@gmail.com>
2008-06-30 23:00:35 +0000

    some refactoring and better handling of alt and opt. as well drawing through graphviz code
    svn/trunk@6053

---
+++ nltk_contrib/fuf/specialfs.py
@@ -0,0 +1,57 @@
+"""
+Handling for special feature names
+"""
+
+from sexp import *
+
+def _remove_specials(sexpl):
+    i = 0
+    length = len(sexpl)
+    while i < length:
+        sublist = sexpl[i]
+        if isinstance(sublist[0], basestring):
+            if sublist[0] in (":demo", "trace") or "%" in sublist[0]:
+                sexpl.remove(sublist)
+                i -= 1
+                length -= 1
+        i += 1
+    return sexpl
+
+
+def parse_alt(sexpl):
+    """
+    Convert the alt feature
+    """
+
+    feat, name, index, val = ('', '', '', '')
+    sexpl = _remove_specials(sexpl)
+
+    # named alt
+    if isinstance(sexpl[1], basestring):
+        # alt with index
+        if len(sexpl) == 4:
+            feat, name, index, val = sexpl
+        # alt without index
+        if len(sexpl) == 3:
+            feat, name, val = sexpl
+    # alt without name
+    elif isinstance(sexpl[1], SexpList):
+        if len(sexpl) == 3:
+            feat, index, val = sexpl
+        if len(sexpl) == 2:
+            feat, val = sexpl
+
+    if all(i == "" for i in (feat, name, index, val)):
+        return ValueError("Maformed alt: %s" % sexpl)
+    return (feat, name, index, val)
+
+def parse_opt(sexpl):
+    feat, name, index, val = ('','','','')
+    sexpl[0] = "alt"
+    #sexpl[-1].append(SexpList("(", ")"))
+    feat, name, index, val = parse_alt(sexpl)
+    #val.append(SexpList("(", ")"))
+    temp = SexpList('(', ')')
+    temp.append(val)
+    val = temp
+    return feat, name, index, val
