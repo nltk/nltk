8144b7eafb35ff12b7208608f04f03c82f60ce48
Steven Bird <stevenbird1@gmail.com>
2008-07-12 07:54:06 +0000

    renaming to follow NLTK conventions
    svn/trunk@6140

--- nltk_contrib/ccg/combinator.py
+++ nltk_contrib/ccg/combinator.py
@@ -1,5 +1,9 @@
-# Source file for CCG combinators
-# Author: Graeme Gange
+# Natural Language Toolkit: Combinatory Categorial Grammar
+#
+# Copyright (C) 2001-2008 NLTK Project
+# Author: Graeme Gange <ggange@csse.unimelb.edu.au>
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
 
 from api import *
 
--- nltk_contrib/ccg/testconj.py
+++ nltk_contrib/ccg/testconj.py
@@ -1,5 +1,10 @@
-# Various experiments on CCGs.
-# Author: Graeme Gange
+# Natural Language Toolkit: Combinatory Categorial Grammar
+#
+# Copyright (C) 2001-2008 NLTK Project
+# Author: Graeme Gange <ggange@csse.unimelb.edu.au>
+# URL: <http://nltk.sf.net>
+# For license information, see LICENSE.TXT
+
 from chart import CCGChartParser, ApplicationRuleSet, CompositionRuleSet, SubstitutionRuleSet, TypeRaiseRuleSet
 from chart import printCCGDerivation
 import lexicon
