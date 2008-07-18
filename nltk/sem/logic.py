754351d41079cf74cfaff0e532a5148bc5f4eb18
Dan Garrette <dhgarrette@gmail.com>
2008-07-18 03:06:47 +0000

    modified Expression.__repr__()
    svn/trunk@6214

--- nltk/sem/logic.py
+++ nltk/sem/logic.py
@@ -70,7 +70,7 @@ class Variable(object):
         return self.name
 
     def __repr__(self):
-        return self.__class__.__name__ + ': ' + self.name
+        return 'Variable(\'' + self.name + '\')'
 
 
 class SubstituteBindingsI(object):
@@ -161,7 +161,7 @@ class Expression(SubstituteBindingsI):
         return expr.simplify()
 
     def __repr__(self):
-        return self.__class__.__name__ + ': ' + str(self)
+        return '<' + self.__class__.__name__ + ' ' + str(self) + '>'
     
     def __str__(self):
         return self.str()
