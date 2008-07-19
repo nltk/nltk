7f3678a37915a3e6b1cf3e492e1b3c0b559c6682
Steven Bird <stevenbird1@gmail.com>
2008-07-19 22:22:33 +0000

    more work on probdist factory function argument
    svn/trunk@6222

--- nltk/model/ngram.py
+++ nltk/model/ngram.py
@@ -16,22 +16,33 @@ class NgramModel(ModelI):
     A processing interface for assigning a probability to the next word.
     """
 
-    def __init__(self, n, train, probdist_factory, supply_condition=False, *factory_args):
-        '''Create a new language model.'''
+    def __init__(self, n, train, estimator=None):
+        """
+        Creates an ngram language model to capture patterns in n consecutive
+        words of training text.  An estimator smooths the probabilities derived
+        from the text and may allow generation of ngrams not seen during training.
+
+        @param n: the order of the language model (ngram size)
+        @type n: C{int}
+        @param train: the training text
+        @type train: C{list} of C{list} of C{string}
+        @param estimator: a function for generating a probability distribution
+        @type estimator: a function that takes a C{ConditionalFreqDist} and returns
+              a C{ConditionalProbDist}
+        """
 
         self._n = n
-        self._candidate_set_size = 10
-        self._vocab = set()  # discard hapax legomena?
 
-        fd = ConditionalFreqDist()
+        if estimator == None:
+            estimator = lambda fdist, bins: MLEProbDist(fdist)
+
+        cfd = ConditionalFreqDist()
         for sentence in train:
             for index, token in enumerate(sentence):
                 context = self.context(sentence, index)
-                fd[context].inc(token)
-		self._vocab.add(token)
+                cfd[context].inc(token)
 
-        self._model = ConditionalProbDist(fd, probdist_factory,
-                                          supply_condition, *factory_args)
+        self._model = ConditionalProbDist(cfd, estimator, False, len(cfd))
         
     def context(self, tokens, index):
         return tuple(tokens[max(0,index-self._n+1):index])
@@ -44,37 +55,31 @@ class NgramModel(ModelI):
         '''Evaluate the log probability of this word in this context.'''
         return self._model[context].logprob(word)
 
-    def choose_random_word(self, context):
-        '''Randomly select a word that is likely to appear in this context.'''
-        # too simple: return self._model[context].max()
-
-        probs = [(self.prob(word, context), word) for word in self._vocab]
-        probs.sort()  # wasteful for finding n-best
-        return random.choice(probs[:self._candidate_set_size])
-
-    def generate(self, n):
-        tokens = []
+    def generate(self, n, context=()):
+        text = list(context)
         for i in range(n):
-            c = self.context(tokens, i)
-            tokens.append(self.choose_random_word(c))
-        return tokens
+            word = self._model[self.context(text, i)].generate()
+            text.append(word)
+        return text
 
-    def entropy(self, sentence):
-        '''Evaluate the total entropy of a message with respect to the model.
+    def entropy(self, text):
+        '''Evaluate the total entropy of a text with respect to the model.
         This is the sum of the log probability of each word in the message.'''
 
         e = 0.0
-        for index, token in enumerate(sentence):
-            context = self.context(sentence, index)
+        for index, token in enumerate(text):
+            context = self.context(text, index)
             e -= self.logprob(token, context)
         return e
 
     def __repr__(self):
-        return '<NgramModel with vocabulary of %d words>' % len(self._vocab)
+        return '<NgramModel with %d ngrams>' % len(self._model)
 
 def demo():
     from nltk.corpus import brown
-    lm = NgramModel(2, brown.sents(categories='a'), MLEProbDist)
+    from nltk.probability import LidstoneProbDist
+    estimator = lambda fdist, bins: LidstoneProbDist(fdist, 0.2, bins)
+    lm = NgramModel(3, brown.sents(categories='a'), estimator)
     print lm
     sent = brown.sents()[0]
     print sent
