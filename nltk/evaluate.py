fb425e45f616d438da8e206869c3aee8baa72ba9
Joseph Frazee <joseph.frazee@gmail.com>
2008-07-31 14:15:45 +0000

    Added approxrand() approximate randomization significance test.
    svn/trunk@6362

--- nltk/evaluate.py
+++ nltk/evaluate.py
@@ -11,9 +11,16 @@
 Utility functions for evaluating processing modules.
 """
 
+import sys
 import math
+import random
 
-from nltk.utilities import LazyZip
+try:
+    from scipy.stats.stats import betai
+except ImportError:
+    betai = None
+
+from nltk.utilities import LazyConcatenation, LazyMap, LazyZip
 from nltk.probability import FreqDist
 
 def accuracy(reference, test):
@@ -123,6 +130,76 @@ def log_likelihood(reference, test):
     total_likelihood = sum(dist.logprob(val)
                             for (val, dist) in zip(reference, test))
     return total_likelihood/len(reference)
+
+def approxrand(a, b, **kwargs):
+    """
+    Returns an approximate significance level between two lists of
+    independently generated test values.
+    
+    Approximate randomization calculates significance by randomly drawing
+    from a sample of the possible permutations. At the limit of the number
+    of possible permutations, the significance level is exact. The
+    approximate significance level is the sample mean number of times the
+    statistic of the permutated lists varies from the actual statistic of
+    the unpermuted argument lists.
+    
+    @return: a tuple containing an approximate significance level, the count
+             of the number of times the pseudo-statistic varied from the
+             actual statistic, and the number of shuffles
+    @rtype: C{tuple}
+    @param a: a list of test values
+    @type a: C{list}
+    @param b: another list of independently generated test values
+    @type b: C{list}
+    """
+    shuffles = kwargs.get('shuffles', 999)
+    # there's no point in trying to shuffle beyond all possible permutations
+    shuffles = \
+        min(shuffles, reduce(lambda x, y: x * y, xrange(1, len(a) + len(b) + 1)))
+    stat = kwargs.get('statistic', lambda lst: float(sum(lst)) / len(lst))
+    verbose = kwargs.get('verbose', False)
+    
+    if verbose:
+        print 'shuffles: %d' % shuffles
+    
+    actual_stat = math.fabs(stat(a) - stat(b))
+    
+    if verbose:
+        print 'actual statistic: %f' % actual_stat
+        print '-' * 60
+    
+    c = 1e-100
+    lst = LazyConcatenation([a, b])
+    indices = range(len(a) + len(b))
+
+    for i in range(shuffles):
+        if verbose and i % 10 == 0:
+            print 'shuffle: %d' % i
+
+        random.shuffle(indices)
+        
+        pseudo_stat_a = stat(LazyMap(lambda i: lst[i], indices[:len(a)]))
+        pseudo_stat_b = stat(LazyMap(lambda i: lst[i], indices[len(a):]))
+        pseudo_stat = math.fabs(pseudo_stat_a - pseudo_stat_b)
+        
+        if pseudo_stat >= actual_stat:
+            c += 1
+            
+        if verbose and i % 10 == 0:
+            print 'pseudo-statistic: %f' % pseudo_stat
+            print 'significance: %f' % (float(c + 1) / (i + 1))
+            print '-' * 60
+        
+    significance = float(c + 1) / (shuffles + 1)
+    
+    if verbose:
+        print 'significance: %f' % significance        
+        if betai:
+            for phi in [0.01, 0.05, 0.10, 0.15, 0.25, 0.50]:
+                print "prob(phi<=%f): %f" % (phi, betai(c, shuffles, phi))
+    
+    return (significance, c, shuffles)
+
 
 class ConfusionMatrix(object):
     """
