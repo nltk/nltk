86cafd313798886647927cc616a92354aa051387
Steven Bird <stevenbird1@gmail.com>
2008-08-14 05:18:22 +0000

    data/corpora/gutenberg * added Moby Dick
    book.py
    * definitions to help exposition in early chapters of book
    
    doc/en/ch01.rst
    * outline for new intro programming chapter
    
    
    svn/trunk@6433

---
+++ nltk/book.py
@@ -0,0 +1,14 @@
+# Natural Language Toolkit: Material for Book
+#
+# Copyright (C) 2001-2008 NLTK Project
+# Author: Steven Bird <sb@csse.unimelb.edu.au>
+# URL: <http://nltk.org>
+# For license information, see LICENSE.TXT
+#
+
+import nltk, re, pprint
+
+# Chapter 1
+
+moby = nltk.corpus.gutenberg.words('melville-moby_dick.txt')
+
