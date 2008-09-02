fc1c55fc6ece58b276b1aba5b521e9f0b476ae09
Steven Bird <stevenbird1@gmail.com>
2008-09-02 01:56:13 +0000

    doc/en/ch01.rst * new sections on variables and frequency distributions
    doc/images/tally.*
    * word tally diagram to give intuition about frequency distributions
    
    doc/images/fdist-moby.png
    * a screenshot of the cumulative frequency plot from FreqDist.plot()
    
    nltk/book.py
    * a couple more books
    
    nltk/probability.py
    * FreqDist.sorted() now has an optional argument, the samples
      to be sorted by frequency
    * FreqDist.plot()'s optional samples argument is now
      frequency-sorted before being plotted
    
    
    svn/trunk@6532

--- nltk/book.py
+++ nltk/book.py
@@ -1,11 +1,11 @@
 # Some texts for exploration in chapter 1 of the book
 
-from nltk.corpus import gutenberg, genesis, inaugural, nps_chat
+from nltk.corpus import gutenberg, genesis, inaugural, nps_chat, webtext, treebank
 from nltk.text import Text
 
 print "Introductory Examples for the NLTK Book"
 print
-print "Loading 6 texts..."
+print "Loading 8 texts..."
 
 text1 = Text(gutenberg.words('melville-moby_dick.txt'))
 print "text1:", text1.name
@@ -23,7 +23,30 @@ text5 = Text(nps_chat.words(['10-26-teens_706posts.xml',
                              '11-08-teens_706posts.xml',
                              '11-09-teens_706posts.xml']),
                              name="Teen Chat Corpus")
-print "text5:", text5.name
+print "text5", text5.name
 
 text6 = Text(gutenberg.words('chesterton-thursday.txt'))
 print "text6:", text6.name
+
+text7 = Text(treebank.words(), name="Wall Street Journal")
+print "text7:", text7.name
+
+text8 = Text(webtext.words('singles.txt'), name="Personals Corpus")
+print "text8:", text8.name
+
+sent1 = ["Call", "me", "Ishmael", "."]
+sent2 = ["The", "family", "of", "Dashwood", "had", "long",
+         "been", "settled", "in", "Sussex", "."]
+sent3 = ["In", "the", "beginning", "God", "created", "the",
+         "heaven", "and", "the", "earth", "."]
+sent4 = ["Fellow", "-", "Citizens", "of", "the", "Senate",
+         "and", "of", "the", "House", "of", "Representatives", ":"]
+sent5 = ["I", "have", "a", "problem", "with", "people",
+         "PMing", "me", "to", "lol", "JOIN"]
+sent6 = ["THE", "suburb", "of", "Saffron", "Park", "lay", "on", "the",
+         "sunset", "side", "of", "London", ",", "as", "red", "and",
+         "ragged", "as", "a", "cloud", "of", "sunset", "."]
+sent7 = ["Pierre", "Vinken", ",", "61", "years", "old", ",",
+         "will", "join", "the", "board", "as", "a", "nonexecutive",
+         "director", "Nov.", "29", "."]
+
