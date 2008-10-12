e60e050b43ba5f5eff1942b0d642dda2b67a80c0
Steven Bird <stevenbird1@gmail.com>
2008-10-12 07:46:42 +0000

    doc/en/ch02.rst * minor revisions
    doc/en/*.rst, */*.py
    * change FreqDist.sorted() to FreqDist.keys()
      since keys now produces sorted output
    
    nltk/probability.py
    * keys(), values(), items() and the iterator versions of these
      now reverse sort everything by value; this avoids the
      idiom fd.sorted() which adds confusion to the existing
      list.sort() and sorted(iterable) possibilities.
    
    
    svn/trunk@6678

--- nltk/draw/wordfreq.py
+++ nltk/draw/wordfreq.py
@@ -19,7 +19,7 @@ import sys
 
 def plot_word_freq_dist(text):
     fd = text.vocab()
-    samples = fd.sorted()[:50]
+    samples = fd.keys()[:50]
     values = [fd[sample] for sample in samples]
     values = [sum(values[:i+1]) * 100.0/fd.N() for i in range(len(values))]
     plot_values(samples, values)
