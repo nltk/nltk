89afbbd11889d9474a6872fba36fd860fb6ad046
Steven Bird <stevenbird1@gmail.com>
2008-10-20 01:45:23 +0000

    doc/en/ch02.rst * added section on Toolbox
    doc/en/ch06.rst
    * added note
    
    doc/en/ch13.rst
    * tweaked discussion of Toolbox to accommodate changes to ch02
    
    nltk/corpus/reader/toolbox.py
    * added entries and words methods to toolbox reader
      for consistency with other lexicons
      (perhaps we should have a superclass for lexicons
      in order to get some consistency)
    
    
    
    svn/trunk@6702

--- nltk/corpus/reader/toolbox.py
+++ nltk/corpus/reader/toolbox.py
@@ -30,7 +30,29 @@ class ToolboxCorpusReader(CorpusReader):
                errors='strict', unicode_fields=None):
         return concat([list(ToolboxData(filename,enc).fields(
                              strip, unwrap, encoding, errors, unicode_fields))
-                       for (filename, enc) in self.abspaths(files)])
+                       for (filename, enc)
+                       in self.abspaths(files, include_encoding=True)])
+
+    # should probably be done lazily:
+    def entries(self, files, **kwargs):
+        if 'key' in kwargs:
+            key = kwargs['key']
+            del kwargs['key']
+        else:
+            key = 'lx'  # the default key in MDF
+        entries = []
+        for marker, contents in self.fields(files, **kwargs):
+            if marker == key:
+                entries.append((contents, []))
+            else:
+                try:
+                    entries[-1][-1].append((marker, contents))
+                except IndexError:
+                    pass
+        return entries
+
+    def words(self, files, key='lx'):
+        return [contents for marker, contents in self.fields(files) if marker == key]
 
     def raw(self, files):
         if files is None: files = self._files
