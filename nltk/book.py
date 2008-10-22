c7b4449ca670c43303ea25cff3abc889e1eb797b
Steven Bird <stevenbird1@gmail.com>
2008-10-22 10:38:28 +0000

    doc/en/ch01.rst * implement first four pages of corrections from Ewan
    doc/en/ch02.rst
    * work on chapter intro
    
    doc/en/ch03.rst
    * moved chapter goal here from ch02
    
    nltk/book.py
    * implemented Ewan's suggestion for texts() and sents()
      functions to list contents
    
    doc/definitions.rst
    * added Python website URL
    
    
    
    svn/trunk@6704

--- nltk/book.py
+++ nltk/book.py
@@ -5,9 +5,10 @@ from nltk.text import Text
 from nltk import FreqDist, bigrams
 from nltk.wordnet import N
 
-print "Introductory Examples for the NLTK Book"
-print
-print "Loading 8 texts..."
+print "*** Introductory Examples for the NLTK Book ***"
+print "Loading text1, ..., text8 and sent1, ..., sent8"
+print "Type the name of the text or sentence to view it."
+print "Type: 'texts()' or 'sents()' to list the materials."
 
 text1 = Text(gutenberg.words('melville-moby_dick.txt'))
 print "text1:", text1.name
@@ -36,6 +37,16 @@ print "text7:", text7.name
 text8 = Text(webtext.words('singles.txt'), name="Personals Corpus")
 print "text8:", text8.name
 
+def texts():
+    print "text1:", text1.name
+    print "text2:", text2.name
+    print "text3:", text3.name
+    print "text4:", text4.name
+    print "text5:", text5.name
+    print "text6:", text6.name
+    print "text7:", text7.name
+    print "text8:", text8.name
+
 sent1 = ["Call", "me", "Ishmael", "."]
 sent2 = ["The", "family", "of", "Dashwood", "had", "long",
          "been", "settled", "in", "Sussex", "."]
@@ -53,3 +64,13 @@ sent7 = ["Pierre", "Vinken", ",", "61", "years", "old", ",",
          "director", "Nov.", "29", "."]
 sent8 = ['25', 'SEXY', 'MALE', ',', 'seeks', 'attrac', 'older',
          'single', 'lady', ',', 'for', 'discreet', 'encounters', '.']
+
+def sents():
+    print "sent1:", " ".join(sent1)
+    print "sent2:", " ".join(sent2)
+    print "sent3:", " ".join(sent3)
+    print "sent4:", " ".join(sent4)
+    print "sent5:", " ".join(sent5)
+    print "sent6:", " ".join(sent6)
+    print "sent7:", " ".join(sent7)
+    print "sent8:", " ".join(sent8)
