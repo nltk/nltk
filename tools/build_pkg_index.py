e4a9433148503c095ea367fa90da138c9ccfb843
Steven Bird <stevenbird1@gmail.com>
2008-11-24 02:58:54 +0000

    Stylesheet so corpus list is human-readable.
    svn/trunk@6961

--- tools/build_pkg_index.py
+++ tools/build_pkg_index.py
@@ -6,6 +6,10 @@ Build the corpus package index.  Usage:
   build_pkg_index.py <path-to-packages> <base-url> <output-file>
 """
 
+xml_header = """<?xml version="1.0"?>
+<?xml-stylesheet href="index.xsl" type="text/xsl"?>
+"""
+
 import sys
 from nltk.downloader import build_index
 from nltk.etree import ElementTree
@@ -20,6 +24,7 @@ ROOT, BASE_URL, OUT = sys.argv[1:]
 index = build_index(ROOT, BASE_URL)
 s = ElementTree.tostring(index)
 out = open(OUT, 'w')
+out.write(xml_header)
 out.write(s)
 out.close()
 
