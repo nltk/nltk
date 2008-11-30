ddb8e21189f00612524a53e607b98943fdf919e1
Steven Bird <stevenbird1@gmail.com>
2008-11-30 04:12:35 +0000

    Add www to nltk.org url globally. Tweaked comment in tools/global_replace.py
    svn/trunk@7066

--- nltk_contrib/coref/resolve.py
+++ nltk_contrib/coref/resolve.py
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2008 NLTK Project 
 # Author: Joseph Frazee <jfrazee@mail.utexas.edu>
-# URL: http://nltk.org/
+# URL: <http://www.nltk.org/>
 # For license information, see LICENSE.TXT
 
 import gzip
