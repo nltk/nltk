d7d99f3062a312015ac1acd9b0998d4e1890df5e
Steven Bird <stevenbird1@gmail.com>
2009-01-30 05:49:38 +0000

    Moving nltk.wordnet to nltk_contrib.wordnet
    svn/trunk@7489

--- nltk/__init__.py
+++ nltk/__init__.py
@@ -77,7 +77,7 @@ from misc import *
 
 from internals import config_java
 
-import chat, chunk, corpus, parse, metrics, sem, stem, tag, tokenize, wordnet
+import chat, chunk, corpus, parse, metrics, sem, stem, tag, tokenize
 
 # Import Tkinter-based modules if Tkinter is installed
 try:
