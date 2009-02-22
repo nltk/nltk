74865bceead9e2397132622738e0a56d7c582a4f
Steven Bird <stevenbird1@gmail.com>
2009-02-22 05:53:12 +0000

    Made a shell for Babelfish (still has some character encoding problems). Imported the babelize function into book.py for use in chapter 1. Wrote a section on MT, including babelize examples. Added lots of exercises to chapter 1. Wrote the further reading section for chapter 1. Added reading to chapter 4.
    svn/trunk@7719

--- nltk/book.py
+++ nltk/book.py
@@ -4,6 +4,7 @@ from nltk.corpus import gutenberg, genesis, inaugural,\
        nps_chat, webtext, treebank, wordnet
 from nltk.text import Text
 from nltk import FreqDist, bigrams
+from nltk.misc import babelize_shell
 
 print "*** Introductory Examples for the NLTK Book ***"
 print "Loading text1, ..., text9 and sent1, ..., sent9"
