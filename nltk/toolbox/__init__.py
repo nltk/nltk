72692fe300f52f363ed32212357ca2de0ab41c63
Greg Aumann <greg.aumann@gmail.com>
2009-04-01 07:33:23 +0000

    fixed module bug
    svn/trunk@8025

---
+++ nltk/toolbox/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+# Natural Language Toolkit: Toolbox Reader
+#
+# Copyright (C) 2001-2009 NLTK Project
+# Author: Greg Aumann <greg_aumann@sil.org>
+# URL: <http://nltk.org>
+# For license information, see LICENSE.TXT
+
+"""
+Module for reading, writing and manipulating 
+Toolbox databases and settings files.
+"""
+
+from toolbox import *
