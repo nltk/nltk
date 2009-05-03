8e23de141770768ba85f951e0c05086d7eb93e5f
Steven Bird <stevenbird1@gmail.com>
2009-05-03 09:42:14 +0000

    Applied patch from Joseph Frazee to improve performance for accessing large gzipped pickle files (issue 325).
    svn/trunk@8061

--- nltk/data.py
+++ nltk/data.py
@@ -40,7 +40,9 @@ import re
 import urllib2
 import zipfile
 import codecs
-import gzip
+
+from zlib import Z_SYNC_FLUSH
+from gzip import GzipFile, READ as GZ_READ, WRITE as GZ_WRITE
 
 try:
     import cPickle as pickle
@@ -172,24 +174,113 @@ class FileSystemPathPointer(PathPointer, str):
         return self._path
 
 
+class BufferedGzipFile(GzipFile):
+    """
+    A C{GzipFile} subclass that buffers calls to L{read()} and L{write()}.
+    This allows faster reads and writes of data to and from gzip-compressed 
+    files at the cost of using more memory.
+    
+    The default buffer size is 2mb.
+    
+    C{BufferedGzipFile} is useful for loading large gzipped pickle objects
+    as well as writing large encoded feature files for classifier training.
+    """    
+    SIZE = 2 * 2**20
+
+    def __init__(self, filename=None, mode=None, compresslevel=9, 
+                 fileobj=None, **kwargs):
+        """
+        @return: a buffered gzip file object
+        @rtype: C{BufferedGzipFile}
+        @param filename: a filesystem path
+        @type filename: C{str}
+        @param mode: a file mode which can be any of 'r', 'rb', 'a', 'ab', 
+            'w', or 'wb'
+        @type mode: C{str}
+        @param compresslevel: The compresslevel argument is an integer from 1
+            to 9 controlling the level of compression; 1 is fastest and 
+            produces the least compression, and 9 is slowest and produces the
+            most compression. The default is 9.
+        @type compresslevel: C{int}
+        @param fileobj: a StringIO stream to read from instead of a file.
+        @type fileobj: C{StringIO}
+        @kwparam size: number of bytes to buffer during calls to
+            L{read()} and L{write()}
+        @type size: C{int}
+        """   
+        GzipFile.__init__(self, filename, mode, compresslevel, fileobj)
+        self._size = kwargs.get('size', self.SIZE)
+        self._buffer = StringIO()
+        # cStringIO does not support len.
+        self._len = 0
+        
+    def _reset_buffer(self):
+        # For some reason calling StringIO.truncate() here will lead to 
+        # inconsistent writes so just set _buffer to a new StringIO object.
+        self._buffer = StringIO()
+        self._len = 0
+        
+    def _write_buffer(self, data):
+        # Simply write to the buffer and increment the buffer size.
+        if data is not None:
+            self._buffer.write(data)
+            self._len += len(data)
+    
+    def _write_gzip(self, data):
+        # Write the current buffer to the GzipFile.
+        GzipFile.write(self, self._buffer.getvalue())
+        # Then reset the buffer and write the new data to the buffer.
+        self._reset_buffer()
+        self._write_buffer(data)
+
+    def close(self):
+        # GzipFile.close() doesn't actuallly close anything.
+        if self.mode == GZ_WRITE:
+            self._write_gzip(None)
+            self._reset_buffer()
+        return GzipFile.close(self)
+
+    def flush(self, lib_mode=Z_SYNC_FLUSH):
+        self._buffer.flush()
+        GzipFile.flush(self, lib_mode)
+
+    def read(self, size=None):
+        if not size:
+            size = self._size
+            contents = StringIO()
+            while True:
+                blocks = GzipFile.read(self, size)
+                if not blocks:
+                    contents.flush()
+                    break
+                contents.write(blocks)
+            return contents.getvalue()
+        else:
+            return GzipFile.read(self, size)
+
+    def write(self, data, size=-1):
+        """
+        @param data: C{str} to write to file or buffer
+        @type data: C{str}
+        @param size: buffer at least size bytes before writing to file
+        @type size: C{int}
+        """
+        if not size: 
+            size = self._size
+        if self._len + len(data) <= size:
+            self._write_buffer(data)
+        else:
+            self._write_gzip(data)
+
+
 class GzipFileSystemPathPointer(FileSystemPathPointer):
     """
     A subclass of C{FileSystemPathPointer} that identifies a gzip-compressed
     file located at a given absolute path.  C{GzipFileSystemPathPointer} is
     appropriate for loading large gzip-compressed pickle objects efficiently.
     """
-    # 2MB
-    BLOCK_SIZE = 2 * 2**20
-    
     def open(self, encoding=None):
-        # Why do this? The default blocksize used by gzip.open readline() is
-        # too small which leads to poor performance loading large gzipped
-        # pickle objects.
-        stream = StringIO()
-        file = gzip.open(self._path, 'rb')
-        for line in iter(lambda: file.read(self.BLOCK_SIZE), ''):
-            stream.write(line)
-        stream = StringIO(stream.getvalue())
+        stream = BufferedGzipFile(self._path, 'rb')
         if encoding:
             stream = SeekableUnicodeStreamReader(stream, encoding)
         return stream
@@ -242,8 +333,10 @@ class ZipFilePathPointer(PathPointer):
     def open(self, encoding=None):
         data = self._zipfile.read(self._entry)
         stream = StringIO(data)
-        if encoding is not None:
-            stream = SeekableUnicodeStreamReader(stream, encoding)
+        if self._entry.endswith('.gz'):
+            stream = BufferedGzipFile(self._entry, fileobj=stream)                
+        elif encoding is not None:
+            stream = SeekableUnicodeStreamReader(stream, encoding)            
         return stream
 
     def file_size(self):
@@ -256,7 +349,7 @@ class ZipFilePathPointer(PathPointer):
     def __repr__(self):
         return 'ZipFilePathPointer(%r, %r)' % (
             self._zipfile.filename, self._entry)
-        
+      
 ######################################################################
 # Access Functions
 ######################################################################
@@ -488,7 +581,7 @@ def load(resource_url, format='auto', cache=True, verbose=False,
                              'on its file\nextension; use the "format" '
                              'argument to specify the format explicitly.'
                              % resource_url)
-        
+    
     # Load the resource.
     if format == 'pickle':
         resource_val = pickle.load(_open(resource_url))
