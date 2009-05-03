33377a072adc248a85906b125ab9faaa5e2b6573
Steven Bird <stevenbird1@gmail.com>
2009-05-03 09:14:20 +0000

    Fix bug reported in issue 372.
    svn/trunk@8060

--- nltk/text.py
+++ nltk/text.py
@@ -35,6 +35,7 @@ class ContextIndex(object):
         
     def __init__(self, tokens, context_func=None, filter=None, key=lambda x:x):
         self._key = key
+        self._tokens = tokens
         if not context_func:
             self._context_func = self._default_context
         if filter:
