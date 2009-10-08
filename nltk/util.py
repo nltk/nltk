adabc282b1f44611465a545f1f1a958c5d699951
Steven Bird <stevenbird1@gmail.com>
2009-10-08 23:06:37 +0000

    * removed stale content on Earley parsing from ch08-extras * fixed typo (ntlk) in ch11 * fixed indentation (4 spaces, not 3) in util.py * added extra substitution for &nbsp, re issue 23   (helps consistency with use of clean_html in book)
    svn/trunk@8376

--- nltk/util.py
+++ nltk/util.py
@@ -276,14 +276,16 @@ def clean_html(html):
     # Then we remove html comments. This has to be done before removing regular
     # tags since comments can contain '>' characters.
     cleaned = re.sub(r"(?s)<!--.*?-->", "", cleaned)
-    # Finally we can remove the remaining tags:
+    # Next we can remove the remaining tags:
     cleaned = re.sub(r"(?s)<.*?>", "", cleaned)
+    # Finally, we deal with whitespace
+    cleaned = re.sub(r"&nbsp;", " ", cleaned)
     return cleaned
 
 def clean_url(url):
-   from urllib import urlopen
-   html = urlopen(url).read()
-   return clean_html(html)
+    from urllib import urlopen
+    html = urlopen(url).read()
+    return clean_html(html)
 
 ##########################################################################
 # Ngram iteration
