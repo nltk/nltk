f3005c0c03104bd2a62d3b7aa08925d92ee5a155
Steven Bird <stevenbird1@gmail.com>
2009-11-09 01:43:10 +0000

    added machado corpus reader
    svn/trunk@8415

--- nltk/corpus/reader/plaintext.py
+++ nltk/corpus/reader/plaintext.py
@@ -178,6 +178,12 @@ class CategorizedPlaintextCorpusReader(CategorizedCorpusReader,
         return PlaintextCorpusReader.paras(
             self, self._resolve(fileids, categories))
 
+# is there a better way?
+class PortugueseCategorizedPlaintextCorpusReader(CategorizedPlaintextCorpusReader):
+    def __init__(self, *args, **kwargs):
+        CategorizedCorpusReader.__init__(self, kwargs)
+        kwargs['sent_tokenizer'] = nltk.data.LazyLoader('tokenizers/punkt/portuguese.pickle')
+        PlaintextCorpusReader.__init__(self, *args, **kwargs)
 
 class EuroparlCorpusReader(PlaintextCorpusReader):
 
