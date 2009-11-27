284b9cc400f2028773dc862ad316abc68fd90b51
Bjørn Arild Mæland <bjorn.maeland@gmail.com>
2009-11-27 20:56:40 +0000

    Slightly improved the clean_html function
    svn/trunk@8433

--- nltk/util.py
+++ nltk/util.py
@@ -275,12 +275,14 @@ def clean_html(html):
     cleaned = re.sub(r"(?is)<(script|style).*?>.*?(</\1>)", "", html.strip())
     # Then we remove html comments. This has to be done before removing regular
     # tags since comments can contain '>' characters.
-    cleaned = re.sub(r"(?s)<!--.*?-->", "", cleaned)
+    cleaned = re.sub(r"(?s)<!--(.*?)-->[\n]?", "", cleaned)
     # Next we can remove the remaining tags:
-    cleaned = re.sub(r"(?s)<.*?>", "", cleaned)
+    cleaned = re.sub(r"(?s)<.*?>", " ", cleaned)
     # Finally, we deal with whitespace
     cleaned = re.sub(r"&nbsp;", " ", cleaned)
-    return cleaned
+    cleaned = re.sub(r"  ", " ", cleaned)
+    cleaned = re.sub(r"  ", " ", cleaned)
+    return cleaned.strip()
 
 def clean_url(url):
     from urllib import urlopen
