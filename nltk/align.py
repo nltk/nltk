ff69f43b9350493bc980f7e111ec56556b8accbc
David Ross Coles <coles.david@gmail.com>
2010-10-16 13:55:15 +0000

    A version of IBM Model 1 as described in Koehn 2010.
    Ported from implementation in http://code.google.com/p/clir/.
    
    Unfortunately the documentation is a little unclear as to how to handle NULL 
    tokens in the algorithm. Most of the examples given ignore the NULL token 
    which makes it slightly difficult to come up with some more comprehensive 
    tests.
    
    
    svn/trunk@8695

--- nltk/align.py
+++ nltk/align.py
@@ -4,7 +4,11 @@
 # URL: <http://www.nltk.org/>
 # For license information, see LICENSE.TXT
 
+import sys
+import logging
+
 import nltk.metrics
+from nltk.util import defaultdict
 
 class AlignedSent(object):
     """
@@ -239,6 +243,138 @@ class Alignment(frozenset):
         self._index = [[] for _ in range(self._len + 1)]
         for p in self:
             self._index[p[0]].append(p)
+
+
+class EMIBMModel1(object):
+    '''
+    This class contains implementations of the Expectation Maximization
+    algorithm for IBM Model 1. The algorithm runs upon a sentence-aligned 
+    parallel corpus and generates word alignments in aligned sentence pairs.
+
+    The process is divided into 2 main stages.
+    Stage 1: Studies word-to-word translation probabilities by collecting
+    evidence of a English word been the translation of a foreign word from
+    the parallel corpus.
+
+    Stage 2: Based on the translation probabilities from Stage 1, generates 
+    word alignments for aligned sentence pairs.
+    '''
+
+    def __init__(self, aligned_sents, convergent_threshold=1e-2, debug=False):
+        '''
+        Initialize a new C{EMIBMModel1}.
+
+        @param aligned_sents: The parallel text corpus.Iteratable containing 
+            AlignedSent instances of aligned sentence pairs from the corpus.
+        @type aligned_sents: C{list} of L{AlignedSent} objects
+        @param convergent_threshold: The threshold value of convergence. An 
+            entry is considered converged if the delta from old_t to new_t
+            is less than this value. The algorithm terminates when all entries
+            are converged. This parameter is optional, default is 0.01
+        @type convergent_threshold: C{float}
+        '''
+        self.aligned_sents = aligned_sents
+        self.convergent_threshold = convergent_threshold
+        # Dictionary of translation probabilities t(e,f).
+        self.probabilities = None
+
+    def train(self):
+        '''
+        The train() function implements Expectation Maximization training
+        stage that learns word-to-word translation probabilities.
+
+        @return: Number of iterations taken to converge
+        '''
+
+        # Collect up sets of all English and foreign words
+        english_words = set()
+        foreign_words = set()
+        for aligned_sent in self.aligned_sents:
+            english_words.update(aligned_sent.words)
+            foreign_words.update(aligned_sent.mots)
+        # add the NULL token to the foreign word set.
+        foreign_words.add(None)
+        num_probs = len(english_words)*len(foreign_words)
+
+        # Initialise t(e|f) uniformly
+        t = defaultdict(lambda: float(1)/len(english_words))
+        s_total = defaultdict(float)
+        for e in english_words:
+            for f in foreign_words:
+                z = t[e,f]
+
+        globally_converged = False
+        iteration_count = 0
+        while not globally_converged:
+            # count(e|f)
+            count = defaultdict(float)
+            # total(f)
+            total = defaultdict(float)
+
+            for aligned_sent in self.aligned_sents:
+                # Compute normalization
+                for e_w in aligned_sent.words:
+                    s_total[e_w] = 0.0
+                    for f_w in aligned_sent.mots+[None]:
+                        s_total[e_w] += t[e_w, f_w]
+
+                # Collect counts
+                for e_w in aligned_sent.words:
+                    for f_w in aligned_sent.mots+[None]:
+                        cnt = t[e_w, f_w] / s_total[e_w]
+                        count[e_w, f_w] += cnt
+                        total[f_w] += cnt
+
+            # Estimate probabilities
+            num_converged = 0
+            for f_w in foreign_words:
+                for e_w in english_words:
+                    new_prob = count[e_w, f_w] / total[f_w]
+                    delta = abs(t[e_w, f_w] - new_prob)
+                    if delta < self.convergent_threshold:
+                        num_converged += 1
+                    t[e_w, f_w] = new_prob
+
+            # Have we converged
+            iteration_count += 1
+            if num_converged == num_probs:
+                globally_converged = True
+            logging.debug("%d/%d (%.2f%%) converged"%(
+                    num_converged, num_probs, 100.0*num_converged/num_probs))
+
+        self.probabilities = dict(t)
+        return iteration_count
+
+    def aligned(self):
+        '''
+        Returns a list of AlignedSents with Alignments calculated using 
+        IBM-Model 1.
+        '''
+        if self.probablities is None:
+            raise ValueError("No probabilities calculated")
+
+        aligned = []
+        # Alignment Learning from t(e|f)
+        for aligned_sent in self.aligned_sents:
+            alignment = []
+
+            # for every English word
+            for j, e_w in enumerate(aligned_sent.words):
+                # find the French word that gives maximized t(e|f)
+                # NULL token is the initial candidate
+                f_max = (self.probabilities[e_w, None], None)
+                for i, f_w in enumerate(aligned_sent.mots):
+                    f_max = max(f_max, (self.probabilities[e_w, f_w], i))
+
+                # only output alignment with non-NULL mapping
+                if f_max[1] is not None:
+                    alignment.append((j, f_max[1]))
+
+            # substitute the alignment of AlignedSent with the yielded one
+            aligned.append(AlignedSent(aligned_sent.words,
+                    aligned_sent.mots,  alignment))
+
+        return aligned
 
 
 def _giza2pair(pair_string):
