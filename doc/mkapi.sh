9f2c8acafc3175f8bd38892d6508b0b80cf98f9b
bobobo1618 <bobobo1618@gmail.com>
2011-10-08 23:48:28 +1100

    Added the script I used to generate the docs.

---
+++ doc/mkapi.sh
@@ -0,0 +1,42 @@
+echo ".. automodule:: nltk.app" >> app.rst 
+ echo ":members:" >> app.rst 
+echo ".. automodule:: nltk.chat" >> chat.rst 
+ echo ":members:" >> chat.rst 
+echo ".. automodule:: nltk.chunk" >> chunk.rst 
+ echo ":members:" >> chunk.rst 
+echo ".. automodule:: nltk.ccg" >> ccg.rst 
+ echo ":members:" >> ccg.rst 
+echo ".. automodule:: nltk.classify" >> classify.rst 
+ echo ":members:" >> classify.rst 
+echo ".. automodule:: nltk.corpus" >> corpus.rst 
+ echo ":members:" >> corpus.rst 
+echo ".. automodule:: nltk.reader" >> reader.rst 
+ echo ":members:" >> reader.rst 
+echo ".. automodule:: nltk.cluster" >> cluster.rst 
+ echo ":members:" >> cluster.rst 
+echo ".. automodule:: nltk.draw" >> draw.rst 
+ echo ":members:" >> draw.rst 
+echo ".. automodule:: nltk.examples" >> examples.rst 
+ echo ":members:" >> examples.rst 
+echo ".. automodule:: nltk.inference" >> inference.rst 
+ echo ":members:" >> inference.rst 
+echo ".. automodule:: nltk.metrics" >> metrics.rst 
+ echo ":members:" >> metrics.rst 
+echo ".. automodule:: nltk.misc" >> misc.rst 
+ echo ":members:" >> misc.rst 
+echo ".. automodule:: nltk.model" >> model.rst 
+ echo ":members:" >> model.rst 
+echo ".. automodule:: nltk.parse" >> parse.rst 
+ echo ":members:" >> parse.rst 
+echo ".. automodule:: nltk.sem" >> sem.rst 
+ echo ":members:" >> sem.rst 
+echo ".. automodule:: nltk.stem" >> stem.rst 
+ echo ":members:" >> stem.rst 
+echo ".. automodule:: nltk.tag" >> tag.rst 
+ echo ":members:" >> tag.rst 
+echo ".. automodule:: nltk.tokenize" >> tokenize.rst 
+ echo ":members:" >> tokenize.rst 
+echo ".. automodule:: nltk.toolbox" >> toolbox.rst 
+ echo ":members:" >> toolbox.rst 
+echo ".. automodule:: nltk.etree" >> etree.rst 
+ echo ":members:" >> etree.rst
