898842c0c589d5a4229cfc72223327a59bb2561a
bobobo1618 <bobobo1618@gmail.com>
2011-10-09 00:06:11 +1100

    More API work...

--- doc/mkapi.sh
+++ doc/mkapi.sh
@@ -1,42 +1,84 @@
+echo "nltk.app" > app.rst 
+echo "===================" >> app.rst 
 echo ".. automodule:: nltk.app" >> app.rst 
- echo ":members:" >> app.rst 
+echo "   :members:" >> app.rst 
+echo "nltk.chat" > chat.rst 
+echo "===================" >> chat.rst 
 echo ".. automodule:: nltk.chat" >> chat.rst 
- echo ":members:" >> chat.rst 
+echo "   :members:" >> chat.rst 
+echo "nltk.chunk" > chunk.rst 
+echo "===================" >> chunk.rst 
 echo ".. automodule:: nltk.chunk" >> chunk.rst 
- echo ":members:" >> chunk.rst 
+echo "   :members:" >> chunk.rst 
+echo "nltk.ccg" > ccg.rst 
+echo "===================" >> ccg.rst 
 echo ".. automodule:: nltk.ccg" >> ccg.rst 
- echo ":members:" >> ccg.rst 
+echo "   :members:" >> ccg.rst 
+echo "nltk.classify" > classify.rst 
+echo "===================" >> classify.rst 
 echo ".. automodule:: nltk.classify" >> classify.rst 
- echo ":members:" >> classify.rst 
+echo "   :members:" >> classify.rst 
+echo "nltk.corpus" > corpus.rst 
+echo "===================" >> corpus.rst 
 echo ".. automodule:: nltk.corpus" >> corpus.rst 
- echo ":members:" >> corpus.rst 
+echo "   :members:" >> corpus.rst 
+echo "nltk.reader" > reader.rst 
+echo "===================" >> reader.rst 
 echo ".. automodule:: nltk.reader" >> reader.rst 
- echo ":members:" >> reader.rst 
+echo "   :members:" >> reader.rst 
+echo "nltk.cluster" > cluster.rst 
+echo "===================" >> cluster.rst 
 echo ".. automodule:: nltk.cluster" >> cluster.rst 
- echo ":members:" >> cluster.rst 
+echo "   :members:" >> cluster.rst 
+echo "nltk.draw" > draw.rst 
+echo "===================" >> draw.rst 
 echo ".. automodule:: nltk.draw" >> draw.rst 
- echo ":members:" >> draw.rst 
+echo "   :members:" >> draw.rst 
+echo "nltk.examples" > examples.rst 
+echo "===================" >> examples.rst 
 echo ".. automodule:: nltk.examples" >> examples.rst 
- echo ":members:" >> examples.rst 
+echo "   :members:" >> examples.rst 
+echo "nltk.inference" > inference.rst 
+echo "===================" >> inference.rst 
 echo ".. automodule:: nltk.inference" >> inference.rst 
- echo ":members:" >> inference.rst 
+echo "   :members:" >> inference.rst 
+echo "nltk.metrics" > metrics.rst 
+echo "===================" >> metrics.rst 
 echo ".. automodule:: nltk.metrics" >> metrics.rst 
- echo ":members:" >> metrics.rst 
+echo "   :members:" >> metrics.rst 
+echo "nltk.misc" > misc.rst 
+echo "===================" >> misc.rst 
 echo ".. automodule:: nltk.misc" >> misc.rst 
- echo ":members:" >> misc.rst 
+echo "   :members:" >> misc.rst 
+echo "nltk.model" > model.rst 
+echo "===================" >> model.rst 
 echo ".. automodule:: nltk.model" >> model.rst 
- echo ":members:" >> model.rst 
+echo "   :members:" >> model.rst 
+echo "nltk.parse" > parse.rst 
+echo "===================" >> parse.rst 
 echo ".. automodule:: nltk.parse" >> parse.rst 
- echo ":members:" >> parse.rst 
+echo "   :members:" >> parse.rst 
+echo "nltk.sem" > sem.rst 
+echo "===================" >> sem.rst 
 echo ".. automodule:: nltk.sem" >> sem.rst 
- echo ":members:" >> sem.rst 
+echo "   :members:" >> sem.rst 
+echo "nltk.stem" > stem.rst 
+echo "===================" >> stem.rst 
 echo ".. automodule:: nltk.stem" >> stem.rst 
- echo ":members:" >> stem.rst 
+echo "   :members:" >> stem.rst 
+echo "nltk.tag" > tag.rst 
+echo "===================" >> tag.rst 
 echo ".. automodule:: nltk.tag" >> tag.rst 
- echo ":members:" >> tag.rst 
+echo "   :members:" >> tag.rst 
+echo "nltk.tokenize" > tokenize.rst 
+echo "===================" >> tokenize.rst 
 echo ".. automodule:: nltk.tokenize" >> tokenize.rst 
- echo ":members:" >> tokenize.rst 
+echo "   :members:" >> tokenize.rst 
+echo "nltk.toolbox" > toolbox.rst 
+echo "===================" >> toolbox.rst 
 echo ".. automodule:: nltk.toolbox" >> toolbox.rst 
- echo ":members:" >> toolbox.rst 
+echo "   :members:" >> toolbox.rst 
+echo "nltk.etree" > etree.rst 
+echo "===================" >> etree.rst 
 echo ".. automodule:: nltk.etree" >> etree.rst 
- echo ":members:" >> etree.rst
+echo "   :members:" >> etree.rst
