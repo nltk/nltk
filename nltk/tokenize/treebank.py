b4a4ed757927f8014dde0ce287defcc28b2715e5
Steven Bird <stevenbird1@gmail.com>
2012-01-12 15:38:51 +1100

    Don't use "flags" argument to re.sub as it requires Python 2.7.

--- nltk/tokenize/treebank.py
+++ nltk/tokenize/treebank.py
@@ -73,7 +73,7 @@ class TreebankWordTokenizer(TokenizerI):
             text = regexp.sub(r'\1 \2 \3', text)
 
         # Separate most punctuation
-        text = re.sub(r"([^\w\.\'\-\/,&])", r' \1 ', text, flags=re.U)
+        text = re.sub(r"(?u)([^\w\.\'\-\/,&])", r' \1 ', text)
 
         # Separate commas or single quotes if they're followed by space.
         # (E.g., don't separate 2,500)
