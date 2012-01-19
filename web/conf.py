50bf538507bffa7264935a89a6530727a8710440
Steven Bird <stevenbird1@gmail.com>
2012-01-19 21:32:07 +1100

    set left alignment

--- web/conf.py
+++ web/conf.py
@@ -99,7 +99,9 @@ html_theme = 'agogo'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    'textalign': 'left'
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
