0e8ef735a9c9143fd89cb16805edae48780459f7
Peter Ljunglöf <peter.ljunglof@heatherleaf.se>
2012-02-03 21:05:45 +0100

    Added author

--- nltk/tree.py
+++ nltk/tree.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
 # Natural Language Toolkit: Text Trees
 #
 # Copyright (C) 2001-2012 NLTK Project
 # Author: Edward Loper <edloper@gradient.cis.upenn.edu>
 #         Steven Bird <sb@csse.unimelb.edu.au>
+#         Peter Ljunglöf <peter.ljunglof@gu.se>
 #         Nathan Bodenstab <bodenstab@cslu.ogi.edu> (tree transforms)
 # URL: <http://www.nltk.org/>
 # For license information, see LICENSE.TXT
