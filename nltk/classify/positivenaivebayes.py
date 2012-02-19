e1c74713dfce1cdc91688cf73db43eedeb640ec7
Alessandro Presta <alessandro.presta@gmail.com>
2012-02-19 02:00:04 +0100

    Added docstrings

--- nltk/classify/positivenaivebayes.py
+++ nltk/classify/positivenaivebayes.py
@@ -1,3 +1,35 @@
+# Natural Language Toolkit: Positive Naive Bayes Classifier
+#
+# Copyright (C) 2012 NLTK Project
+# Author: Alessandro Presta <alessandro.presta@gmail.com>
+# URL: <http://www.nltk.org/>
+# For license information, see LICENSE.TXT
+
+"""
+A variant of the Naive Bayes Classifier that performs binary classification with
+partially-labeled training sets. In other words, assume we want to build a classifier
+that assigns each example to one of two complementary classes (e.g., male names and
+female names).
+If we have a training set with labeled examples for both classes, we can use a
+standard Naive Bayes Classifier. However, consider the case when we only have labeled
+examples for one of the classes, and other, unlabeled, examples.
+Then, assuming a prior distribution on the two labels, we can use the unlabeled set
+to estimate the frequencies of the various features.
+
+Let the two possible labels be 1 and 0, and let's say we only have examples labeled 1
+and unlabeled examples. We are also given an estimate of P(1).
+
+We compute P(feature|1) exactly as in the standard case.
+
+To compute P(feature|0), we first estimate P(feature) from the unlabeled set (we are
+assuming that the unlabeled examples are drawn according to the given prior distribution)
+and then express the conditional probability as:
+
+|                  P(feature) - P(feature|1) * P(1)
+|  P(feature|0) = ----------------------------------
+|                               P(0)
+"""
+
 from collections import defaultdict
 
 from nltk.probability import FreqDist, ConditionalFreqDist, DictionaryProbDist, \
@@ -5,15 +37,22 @@ from nltk.probability import FreqDist, ConditionalFreqDist, DictionaryProbDist, 
 
 from naivebayes import NaiveBayesClassifier
 
-class PositiveNaiveBayesClassifier(NaiveBayesClassifier):
-    """
+##//////////////////////////////////////////////////////
+##  Positive Naive Bayes Classifier
+##//////////////////////////////////////////////////////
 
-    """
+class PositiveNaiveBayesClassifier(NaiveBayesClassifier):
     @staticmethod
     def train(positive_featuresets, unlabeled_featuresets, positive_prob_prior=0.5,
               estimator=ELEProbDist):
         """
+        :param positive_featuresets: A list of featuresets that are known as positive
+            examples (i.e., their label is ``True``).
 
+        :param unlabeled_featuresets: A list of featuresets whose label is unknown.
+
+        :param positive_prob_prior: A prior estimate of the probability of the label
+            ``True`` (default 0.5).
         """
         positive_feature_freqdist = ConditionalFreqDist()
         unlabeled_feature_freqdist = ConditionalFreqDist()
@@ -71,7 +110,7 @@ class PositiveNaiveBayesClassifier(NaiveBayesClassifier):
 ##//////////////////////////////////////////////////////
 
 def demo():
-    from util import pnb_demo#from nltk.classify.util import pnb_demo
+    from nltk.classify.util import pnb_demo
     classifier = pnb_demo(PositiveNaiveBayesClassifier.train)
     classifier.show_most_informative_features()
 
