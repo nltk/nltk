31d0cba547511d10cc943c46b22cc99c51401de5
Dan Blanchard <dan.blanchard@gmail.com>
2012-05-15 17:20:33 -0300

    I just changed my email address to my work address, since I developed this on ETS time (and contributed it with their permission).

--- nltk/corpus/reader/lin.py
+++ nltk/corpus/reader/lin.py
@@ -1,7 +1,7 @@
 # Natural Language Toolkit: Lin's Thesaurus
 #
 # Copyright (C) 2001-2012 NLTK Project
-# Author: Dan Blanchard <dan.blanchard@gmail.com>
+# Author: Dan Blanchard <dblanchard@ets.org>
 # URL: <http://www.nltk.org/>
 # For license information, see LICENSE.txt
 
