2adec1f3f81ddc4f923d5171967bc675b46c3fc0
Dan Blanchard <dan.blanchard@gmail.com>
2012-06-04 11:08:43 -0300

    - Updated my email address to be my ETS address.

--- nltk/model/ngram.py
+++ nltk/model/ngram.py
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2001-2012 NLTK Project
 # Authors: Steven Bird <sb@csse.unimelb.edu.au>
-#          Daniel Blanchard <dan.blanchard@gmail.com>
+#          Daniel Blanchard <dblanchard@ets.org>
 # URL: <http://www.nltk.org/>
 # For license information, see LICENSE.TXT
 
