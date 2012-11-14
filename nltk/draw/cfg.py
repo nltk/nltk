0aa10ddea8f41567e13286e04e0b0dc95125705b
Krzysztof Langner <klangner@gmail.com>
2012-11-14 15:39:22 +0100

    from __future__ import print_function
    There are problems with test cases when this import is not the first
    line of the code

--- nltk/draw/cfg.py
+++ nltk/draw/cfg.py
@@ -4,6 +4,7 @@
 # Author: Edward Loper <edloper@gradient.cis.upenn.edu>
 # URL: <http://www.nltk.org/>
 # For license information, see LICENSE.TXT
+from __future__ import print_function
 
 """
 Visualization tools for CFGs.
@@ -48,7 +49,6 @@ Operations:
     - if connected to top & bottom, then disconnect
 """
 
-from __future__ import print_function
 import re
 
 from Tkinter import (Button, Canvas, Entry, Frame, IntVar, Label,
