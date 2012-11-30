86dedfbce59ba24b043a0bf018c952a41e53dbdc
Mikhail Korobov <kmike84@gmail.com>
2012-12-01 03:41:34 +0600

    Skip portuguese_en.doctest under Python 2.x. There is no way to fix this doctest it under Python 2.x and rewriting it as unittest would reduce readability. portuguese_en.doctest is more like a book chapter, not a test case, and so I think it is OK to run it only under Python 3.x.

--- nltk/test/portuguese_en_fixt.py
+++ nltk/test/portuguese_en_fixt.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
+from nltk.compat import PY3
+
 from nltk.corpus import teardown_module
+
+def setup_module(module):
+    from nose import SkipTest
+    if not PY3:
+        raise SkipTest("non-ascii doctests are not supported under Python 2.x")
