52cacec999d7f7d4ffef715f04fbf243b86925ba
Mikhail Korobov <kmike84@gmail.com>
2013-03-17 13:37:36 +0600

    PY3: fix nltk.classify.utils.names_demo (thanks Janosch Gräf for the report and a patch)

--- nltk/classify/util.py
+++ nltk/classify/util.py
@@ -198,7 +198,7 @@ def names_demo(trainer, features=names_demo_features):
         print('Avg. log likelihood: %6.4f' % (sum(ll)/len(test)))
         print()
         print('Unseen Names      P(Male)  P(Female)\n'+'-'*40)
-        for ((name, gender), pdist) in zip(test, pdists)[:5]:
+        for ((name, gender), pdist) in list(zip(test, pdists))[:5]:
             if gender == 'male':
                 fmt = '  %-15s *%6.4f   %6.4f'
             else:
