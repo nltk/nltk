5109ddffb449fa9fe944015e19da408af5c3852e
Steven Bird <stevenbird1@gmail.com>
2013-07-12 13:59:32 +1000

    pull fixes to setuptools, cf https://github.com/nltk/nltk/pull/437

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Distribute setup script for the Natural Language Toolkit
+# Setup script for the Natural Language Toolkit
 #
 # Copyright (C) 2001-2013 NLTK Project
 # Author: Steven Bird <stevenbird1@gmail.com>
@@ -29,9 +29,7 @@ version_file = os.path.join(os.path.dirname(__file__), 'nltk', 'VERSION')
 with open(version_file) as fh:
     nltk_version = fh.read().strip()
 
-import distribute_setup
-distribute_setup.use_setuptools()
-
+# setuptools
 from setuptools import setup, find_packages
 
 #
