72ac1c5593ec8da237dd7933fe2ab1b0d3b7b8eb
Steven Xu <>
2013-07-26 15:02:12 +1000

    avoid conflict of nltk.data.load's 'format' argument with the builtin 'format' function

--- nltk/sem/chat80.py
+++ nltk/sem/chat80.py
@@ -428,7 +428,7 @@ def _str2records(filename, rel):
     Read a file into memory and convert each relation clause into a list.
     """
     recs = []
-    contents = nltk.data.load("corpora/chat80/%s" % filename, format="text")
+    contents = nltk.data.load("corpora/chat80/%s" % filename, format_="text")
     for line in contents.splitlines():
         if line.startswith(rel):
             line = re.sub(rel+r'\(', '', line)
