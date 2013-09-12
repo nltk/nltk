77635dc16e7674222f9b6ea8f4db7d4281b639c6
nschneid <neatnate+git@gmail.com>
2013-09-11 21:28:12 -0700

    framenet.py: enhance frame_relations() with additional optional arguments for filtering: a second frame, frame relation type

--- nltk/corpus/reader/framenet.py
+++ nltk/corpus/reader/framenet.py
@@ -1380,10 +1380,14 @@ class FramenetCorpusReader(XMLCorpusReader):
             self._buildrelationindex()
         return self._freltyp_idx.values()
 
-    def frame_relations(self, frame=None):
+    def frame_relations(self, frame=None, frame2=None, type=None):
         """
-        :param frame: frame object, name, or ID; if specified, only relations involving 
+        :param frame: (optional) frame object, name, or ID; only relations involving 
         this frame will be returned
+        :param frame2: (optional; 'frame' must be a different frame) only show relations 
+        between the two specified frames, in either direction
+        :param type: (optional) frame relation type (name or object); show only relations
+        of this type
         :type frame: int or str or AttrDict
         :return: A list of all of the frame relations in framenet
         :rtype: list(dict)
@@ -1404,18 +1408,55 @@ class FramenetCorpusReader(XMLCorpusReader):
         >>> PrettyList(fn.frame_relations(fn.frame('Cooking_creation')), breakLines=True)
         [<Parent=Intentionally_create -- Inheritance -> Child=Cooking_creation>,
          <MainEntry=Apply_heat -- See_also -> ReferringEntry=Cooking_creation>, ...]
+        >>> PrettyList(fn.frame_relations('Cooking_creation', type='Inheritance'))
+        [<Parent=Intentionally_create -- Inheritance -> Child=Cooking_creation>]
+        >>> PrettyList(fn.frame_relations('Cooking_creation', 'Apply_heat'), breakLines=True)
+        [<MainEntry=Apply_heat -- See_also -> ReferringEntry=Cooking_creation>,
+         <Parent=Apply_heat -- Using -> Child=Cooking_creation>]
         """
         if not self._frel_idx:
             self._buildrelationindex()
-        if frame is None:
-            return PrettyList(self._frel_idx.values())
-        if not isinstance(frame,int):
-            if isinstance(frame,dict):
-                frame = frame.ID
-            else:
-                frame = self.frame_by_name(frame).ID
-        return PrettyList(self._frel_idx[frelID] for frelID in self._frel_f_idx[frame])
-
+        
+        rels = None
+        
+        if type is not None:
+            if not isinstance(type,dict):
+                type = [rt for rt in self.frame_relation_types() if rt.name==type][0]
+                assert isinstance(type,dict)
+        
+        # lookup by 'frame'
+        if frame is not None:
+            if not isinstance(frame,int):
+                if isinstance(frame,dict):
+                    frame = frame.ID
+                else:
+                    frame = self.frame_by_name(frame).ID
+            rels = PrettyList(self._frel_idx[frelID] for frelID in self._frel_f_idx[frame])
+        
+            # filter by 'type'
+            if type is not None:
+                rels = PrettyList(rel for rel in rels if rel.type is type)
+        elif type is not None:
+            # lookup by 'type'
+            rels = PrettyList(type.frameRelations)
+        else:
+            rels = PrettyList(self._frel_idx.values())
+        
+        # filter by 'frame2'
+        if frame2 is not None:
+            if frame is None:
+                raise FramenetError("frame_relations(frame=None, frame2=<value>) is not allowed")
+            if not isinstance(frame2,int):
+                if isinstance(frame2,dict):
+                    frame2 = frame2.ID
+                else:
+                    frame2 = self.frame_by_name(frame2).ID
+            if frame==frame2:
+                raise FramenetError("The two frame arguments to frame_relations() must be different frames")
+            rels = PrettyList(rel for rel in rels if rel.superFrame.ID==frame2 or rel.subFrame.ID==frame2)
+        
+        return rels
+        
     def fe_relations(self):
         """
         Obtain a list of frame element relations.
