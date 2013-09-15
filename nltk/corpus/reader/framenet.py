f470cfa3c2ce97ed927fb665c1a3ce2dc23c9df4
nschneid <neatnate+git@gmail.com>
2013-09-15 11:48:37 -0400

    framenet.py: string encoding for __repr__(); fixes #486

--- nltk/corpus/reader/framenet.py
+++ nltk/corpus/reader/framenet.py
@@ -35,9 +35,9 @@ def _pretty_longstring(defstr, prefix='', wrap_at=65):
     :rtype: str
     """
 
-    outstr = ""
+    outstr = u""
     for line in textwrap.fill(defstr, wrap_at).split('\n'):
-        outstr += "{0}{1}\n".format(prefix, line)
+        outstr += prefix + line + '\n'
     return outstr
 
 def _pretty_any(obj):
@@ -51,14 +51,14 @@ def _pretty_any(obj):
     :rtype: str
     """
 
-    outstr = ""
+    outstr = u""
     for k in obj:
         if isinstance(obj[k], string_types) and len(obj[k]) > 65:
-            outstr += "[{0}]\n".format(k)
-            outstr += "{0}".format(_pretty_longstring(obj[k], prefix='  '))
+            outstr += u"[{0}]\n".format(k)
+            outstr += u"{0}".format(_pretty_longstring(obj[k], prefix='  '))
             outstr += '\n'
         else:
-            outstr += "[{0}] {1}\n".format(k, obj[k])
+            outstr += u"[{0}] {1}\n".format(k, obj[k])
 
     return outstr
 
@@ -76,20 +76,20 @@ def _pretty_semtype(st):
     semkeys = st.keys()
     if len(semkeys) == 1: return "<None>"
 
-    outstr = ""
-    outstr += "semantic type ({0.ID}): {0.name}\n".format(st)
+    outstr = u""
+    outstr += u"semantic type ({0.ID}): {0.name}\n".format(st)
     if 'abbrev' in semkeys:
-        outstr += "[abbrev] {0}\n".format(st.abbrev)
+        outstr += u"[abbrev] {0}\n".format(st.abbrev)
     if 'definition' in semkeys:
-        outstr += "[definition]\n"
+        outstr += u"[definition]\n"
         outstr += _pretty_longstring(st.definition,'  ')
-    outstr += "[rootType] {0}({1})\n".format(st.rootType.name, st.rootType.ID)
+    outstr += u"[rootType] {0}({1})\n".format(st.rootType.name, st.rootType.ID)
     if st.superType is None:
         outstr += "[superType] <None>\n"
     else:
-        outstr += "[superType] {0}({1})\n".format(st.superType.name, st.superType.ID)
+        outstr += u"[superType] {0}({1})\n".format(st.superType.name, st.superType.ID)
     outstr += "[subTypes] {0} subtypes\n".format(len(st.subTypes))
-    outstr += "  " + ", ".join('{0}({1})'.format(x.name, x.ID) for x in st.subTypes) + '\n'*(len(st.subTypes)>0)
+    outstr += "  " + ", ".join(u'{0}({1})'.format(x.name, x.ID) for x in st.subTypes) + '\n'*(len(st.subTypes)>0)
     return outstr
 
 def _pretty_frame_relation_type(freltyp):
@@ -102,7 +102,7 @@ def _pretty_frame_relation_type(freltyp):
     :return: A nicely formated string representation of the frame relation type.
     :rtype: str
     """
-    outstr = "<frame relation type ({0.ID}): {0.superFrameName} -- {0.name} -> {0.subFrameName}>".format(freltyp)
+    outstr = u"<frame relation type ({0.ID}): {0.superFrameName} -- {0.name} -> {0.subFrameName}>".format(freltyp)
     return outstr
     
 def _pretty_frame_relation(frel):
@@ -115,7 +115,7 @@ def _pretty_frame_relation(frel):
     :return: A nicely formated string representation of the frame relation.
     :rtype: str
     """
-    outstr = "<{0.type.superFrameName}={0.superFrameName} -- {0.type.name} -> {0.type.subFrameName}={0.subFrameName}>".format(frel)
+    outstr = u"<{0.type.superFrameName}={0.superFrameName} -- {0.type.name} -> {0.type.subFrameName}={0.subFrameName}>".format(frel)
     return outstr
 
 def _pretty_fe_relation(ferel):
@@ -128,7 +128,7 @@ def _pretty_fe_relation(ferel):
     :return: A nicely formated string representation of the FE relation.
     :rtype: str
     """
-    outstr = "<{0.type.superFrameName}={0.frameRelation.superFrameName}.{0.superFEName} -- {0.type.name} -> {0.type.subFrameName}={0.frameRelation.subFrameName}.{0.subFEName}>".format(ferel)
+    outstr = u"<{0.type.superFrameName}={0.frameRelation.superFrameName}.{0.superFEName} -- {0.type.name} -> {0.type.subFrameName}={0.frameRelation.subFrameName}.{0.subFEName}>".format(ferel)
     return outstr
 
 def _pretty_lu(lu):
@@ -143,31 +143,31 @@ def _pretty_lu(lu):
     """
 
     lukeys = lu.keys()
-    outstr = ""
-    outstr += "lexical unit ({0.ID}): {0.name}\n\n".format(lu)
+    outstr = u""
+    outstr += u"lexical unit ({0.ID}): {0.name}\n\n".format(lu)
     if 'definition' in lukeys:
         outstr += "[definition]\n"
         outstr += _pretty_longstring(lu.definition,'  ')
     if 'frame' in lukeys:
-        outstr += "\n[frame] {0}({1})\n".format(lu.frame.name,lu.frame.ID)
+        outstr += u"\n[frame] {0}({1})\n".format(lu.frame.name,lu.frame.ID)
     if 'incorporatedFE' in lukeys:
-        outstr += "\n[incorporatedFE] {0}\n".format(lu.incorporatedFE)
+        outstr += u"\n[incorporatedFE] {0}\n".format(lu.incorporatedFE)
     if 'POS' in lukeys:
-        outstr += "\n[POS] {0}\n".format(lu.POS)
+        outstr += u"\n[POS] {0}\n".format(lu.POS)
     if 'status' in lukeys:
-        outstr += "\n[status] {0}\n".format(lu.status)
+        outstr += u"\n[status] {0}\n".format(lu.status)
     if 'totalAnnotated' in lukeys:
         outstr += "\n[totalAnnotated] {0} annotated examples\n".format(lu.totalAnnotated)
     if 'lexemes' in lukeys:
-        outstr += "\n[lexemes] {0}\n".format(' '.join('{0}/{1}'.format(lex.name,lex.POS) for lex in lu.lexemes))
+        outstr += u"\n[lexemes] {0}\n".format(' '.join('{0}/{1}'.format(lex.name,lex.POS) for lex in lu.lexemes))
     if 'semTypes' in lukeys:
         outstr += "\n[semTypes] {0} semantic types\n".format(len(lu.semTypes))
-        outstr += "  "*(len(lu.semTypes)>0) + ", ".join('{0}({1})'.format(x.name, x.ID) for x in lu.semTypes) + '\n'*(len(lu.semTypes)>0)
+        outstr += "  "*(len(lu.semTypes)>0) + ", ".join(u'{0}({1})'.format(x.name, x.ID) for x in lu.semTypes) + '\n'*(len(lu.semTypes)>0)
     if 'subCorpus' in lukeys:
         subc = [x.name for x in lu.subCorpus]
         outstr += "\n[subCorpus] {0} subcorpora\n".format(len(lu.subCorpus))
         for line in textwrap.fill(", ".join(sorted(subc)), 60).split('\n'): 
-            outstr += "  {0}\n".format(line)
+            outstr += u"  {0}\n".format(line)
 
     return outstr
 
@@ -182,8 +182,8 @@ def _pretty_fe(fe):
     :rtype: str
     """
     fekeys = fe.keys()
-    outstr = ""
-    outstr += "frame element ({0.ID}): {0.name}\n    of {1.name}({1.ID})\n".format(fe, fe.frame)
+    outstr = u""
+    outstr += u"frame element ({0.ID}): {0.name}\n    of {1.name}({1.ID})\n".format(fe, fe.frame)
     if 'definition' in fekeys:
         outstr += "[definition]\n"
         outstr += _pretty_longstring(fe.definition,'  ')
@@ -194,19 +194,19 @@ def _pretty_fe(fe):
         if fe.requiresFE is None:
             outstr += "<None>\n"
         else:
-            outstr += "{0}\n".format(fe.requiresFE.name)
+            outstr += u"{0}\n".format(fe.requiresFE.name)
     if 'excludesFE' in fekeys:
         outstr += "[excludesFE] "
         if fe.excludesFE is None:
             outstr += "<None>\n"
         else:
-            outstr += "{0}\n".format(fe.excludesFE.name)
+            outstr += u"{0}\n".format(fe.excludesFE.name)
     if 'semType' in fekeys:
         outstr += "[semType] "
         if fe.semType is None:
             outstr += "<None>\n"
         else:
-            outstr += "\n  " + '{0}({1})'.format(fe.semType.name, fe.semType.ID) + '\n'
+            outstr += "\n  " + u'{0}({1})'.format(fe.semType.name, fe.semType.ID) + '\n'
 
     return outstr
 
@@ -221,13 +221,13 @@ def _pretty_frame(frame):
     :rtype: str
     """
 
-    outstr = ""
-    outstr += "frame ({0.ID}): {0.name}\n\n".format(frame)
+    outstr = u""
+    outstr += u"frame ({0.ID}): {0.name}\n\n".format(frame)
     outstr += "[definition]\n"
     outstr += _pretty_longstring(frame.definition, '  ') + '\n'
 
     outstr += "[semTypes] {0} semantic types\n".format(len(frame.semTypes))
-    outstr += "  "*(len(frame.semTypes)>0) + ", ".join('{0}({1})'.format(x.name, x.ID) for x in frame.semTypes) + '\n'*(len(frame.semTypes)>0)
+    outstr += "  "*(len(frame.semTypes)>0) + ", ".join(u'{0}({1})'.format(x.name, x.ID) for x in frame.semTypes) + '\n'*(len(frame.semTypes)>0)
 
     outstr += "\n[frameRelations] {0} frame relations\n".format(len(frame.frameRelations))
     outstr += '  ' + '\n  '.join(repr(frel) for frel in frame.frameRelations) + '\n'
@@ -243,12 +243,12 @@ def _pretty_frame(frame):
     fes = {}
     for feName,fe in sorted(frame.FE.items()):
         try:
-            fes[fe.coreType].append('{0} ({1})'.format(feName, fe.ID))
+            fes[fe.coreType].append(u'{0} ({1})'.format(feName, fe.ID))
         except KeyError:
             fes[fe.coreType] = []
-            fes[fe.coreType].append('{0} ({1})'.format(feName, fe.ID))
+            fes[fe.coreType].append(u'{0} ({1})'.format(feName, fe.ID))
     for ct in sorted(fes.keys()):
-        outstr += '{0:>15}: {1}\n'.format(ct, ', '.join(sorted(fes[ct])))
+        outstr += u'{0:>15}: {1}\n'.format(ct, ', '.join(sorted(fes[ct])))
 
     outstr += "\n[FEcoreSets] {0} frame element core sets\n".format(len(frame.FEcoreSets))
     outstr += "  " + '\n  '.join(", ".join([x.name for x in coreSet]) for coreSet in frame.FEcoreSets) + '\n'
@@ -298,19 +298,18 @@ class AttrDict(dict):
             if self['_type'].endswith('relation'):
                 return self.__repr__()
             try:
-                return '<{0} ID={1} name={2}>'.format(self['_type'], self['ID'], self['name'])
+                return u'<{0} ID={1} name={2}>'.format(self['_type'], self['ID'], self['name'])
             except KeyError:    # no ID--e.g., for _type=lusubcorpus
-                return '<{0} name={1}>'.format(self['_type'], self['name'])
+                return u'<{0} name={1}>'.format(self['_type'], self['name'])
         else:
             return dict.__repr__(self)
         
-    def __str__(self):
+    def _str(self):
         outstr = ""
 
         if not '_type' in self:
-            return _pretty_any(self)
-
-        if self['_type'] == 'frame':
+            outstr = _pretty_any(self)
+        elif self['_type'] == 'frame':
             outstr = _pretty_frame(self)
         elif self['_type'] == 'fe':
             outstr = _pretty_fe(self)
@@ -327,8 +326,17 @@ class AttrDict(dict):
         else:
             outstr = _pretty_any(self)
 
+        # ensure result is unicode string prior to applying the 
+        # @python_2_unicode_compatible decorator (because non-ASCII characters 
+        # could in principle occur in the data and would trigger an encoding error when 
+        # passed as arguments to str.format()).
+        assert isinstance(outstr,unicode)
         return outstr
-    __repr__ = __str__
+        
+    def __str__(self):
+        return self._str()
+    def __repr__(self):
+        return self.__str__()
 
 class Future(object):
     """
@@ -364,7 +372,9 @@ class Future(object):
     def __str__(self):
         return self._data().__str__()
     def __repr__(self):
-        return self._data().__repr__()
+        s = self._data().__repr__()
+        assert isinstance(s,unicode)
+        return s
 
 
 @python_2_unicode_compatible
@@ -412,8 +422,8 @@ class PrettyList(list):
             pieces.append(elt._short_repr()) # key difference from inherited version: call to _short_repr()
             length += len(pieces[-1]) + 2
             if self._MAX_REPR_SIZE and length > self._MAX_REPR_SIZE and len(pieces) > 2:
-                return '[%s, ...]' % text_type(',\n ' if self._BREAK_LINES else ', ').join(pieces[:-1])
-        return '[%s]' % text_type(',\n ' if self._BREAK_LINES else ', ').join(pieces)
+                return u'[%s, ...]' % text_type(',\n ' if self._BREAK_LINES else ', ').join(pieces[:-1])
+        return u'[%s]' % text_type(',\n ' if self._BREAK_LINES else ', ').join(pieces)
 
 @python_2_unicode_compatible
 class PrettyLazyMap(LazyMap):
@@ -434,9 +444,9 @@ class PrettyLazyMap(LazyMap):
             pieces.append(elt._short_repr()) # key difference from inherited version: call to _short_repr()
             length += len(pieces[-1]) + 2
             if length > self._MAX_REPR_SIZE and len(pieces) > 2:
-                return '[%s, ...]' % text_type(', ').join(pieces[:-1])
+                return u'[%s, ...]' % text_type(', ').join(pieces[:-1])
         else:
-            return '[%s]' % text_type(', ').join(pieces)
+            return u'[%s]' % text_type(', ').join(pieces)
 
 class FramenetCorpusReader(XMLCorpusReader):
     """A corpus reader for the Framenet Corpus.
@@ -736,6 +746,9 @@ class FramenetCorpusReader(XMLCorpusReader):
         >>> f = fn.frame('Medical_specialties')
         >>> f.ID
         256
+        >>> # ensure non-ASCII character in definition doesn't trigger an encoding error:
+        >>> fn.frame('Imposing_obligation') # doctest: +ELLIPSIS 
+        frame (1494): Imposing_obligation...
 
         The dict that is returned from this function will contain the
         following information about the Frame:
