2f3cc6877a467967587d5444f51269e430201658
Mikhail Korobov <kmike84@gmail.com>
2013-09-19 00:52:23 +0600

    Revert "precompiling word tokenizer"
    This reverts commit 5a2066acc35fb91efe16605138937473f4dc1c0c.

--- nltk/tokenize/treebank.py
+++ nltk/tokenize/treebank.py
@@ -42,39 +42,6 @@ class TreebankWordTokenizer(TokenizerI):
         ['They', "'ll", 'save', 'and', 'invest', 'more', '.']
     """
 
-    # compile a sequence of regular expressions to make the overall
-    # processing faster in situations where we are tokenizing many
-    # text instances (e.g. tokenizing each document in the Brown
-    # corpus separately). These data structures have a compiled regex
-    # and the corresponding substitution pattern.
-    SUBSTITUTIONS1 = [
-        #starting quotes
-        (re.compile(r'^\"'), r'``')
-        (re.compile(r'(``)'), r' \1 ')
-        (re.compile(r'([ (\[{<])"'), r'\1 `` ')
-
-        #punctuation
-        (re.compile(r'([:,])([^\d])'), r' \1 \2')
-        (re.compile(r'\.\.\.'), r' ... ')
-        (re.compile(r'[;@#$%&]'), r' \g<0> ')
-        (re.compile(r'([^\.])(\.)([\]\)}>"\']*)\s*$'), r'\1 \2\3 ')
-        (re.compile(r'[?!]'), r' \g<0> ')
-
-        (re.compile(r"([^'])' "), r"\1 ' ")
-
-        #parens, brackets, etc.
-        (re.compile(r'[\]\[\(\)\{\}\<\>]'), r' \g<0> ')
-        (re.compile(r'--'), r' -- ')
-    ]
-    SUBSTITUTIONS2 = [
-        #ending quotes
-        (re.compile(r'"'), " '' ")
-        (re.compile(r'(\S)(\'\')'), r'\1 \2 ')
-
-        (re.compile(r"([^' ])('[sS]|'[mM]|'[dD]|') "), r"\1 \2 ")
-        (re.compile(r"([^' ])('ll|'LL|'re|'RE|'ve|'VE|n't|N'T) "), r"\1 \2 ")
-    ]
-
     # List of contractions adapted from Robert MacIntyre's tokenizer.
     CONTRACTIONS2 = [re.compile(r"(?i)\b(can)(not)\b"),
                      re.compile(r"(?i)\b(d)('ye)\b"),
@@ -90,15 +57,34 @@ class TreebankWordTokenizer(TokenizerI):
                      re.compile(r"(?i)\b(wha)(t)(cha)\b")]
 
     def tokenize(self, text):
+        #starting quotes
+        text = re.sub(r'^\"', r'``', text)
+        text = re.sub(r'(``)', r' \1 ', text)
+        text = re.sub(r'([ (\[{<])"', r'\1 `` ', text)
 
-        for regexp, replacement_str in self.SUBSTITUTIONS1:
-            text = regexp.sub(replacement_str, text)
+        #punctuation
+        text = re.sub(r'([:,])([^\d])', r' \1 \2', text)
+        text = re.sub(r'\.\.\.', r' ... ', text)
+        text = re.sub(r'[;@#$%&]', r' \g<0> ', text)
+        text = re.sub(r'([^\.])(\.)([\]\)}>"\']*)\s*$', r'\1 \2\3 ', text)
+        text = re.sub(r'[?!]', r' \g<0> ', text)
+
+        text = re.sub(r"([^'])' ", r"\1 ' ", text)
+
+        #parens, brackets, etc.
+        text = re.sub(r'[\]\[\(\)\{\}\<\>]', r' \g<0> ', text)
+        text = re.sub(r'--', r' -- ', text)
 
         #add extra space to make things easier
         text = " " + text + " "
 
-        for regexp, replacement_str in self.SUBSTITUTIONS2:
-            text = regexp.sub(replacement_str, text)
+        #ending quotes
+        text = re.sub(r'"', " '' ", text)
+        text = re.sub(r'(\S)(\'\')', r'\1 \2 ', text)
+
+        text = re.sub(r"([^' ])('[sS]|'[mM]|'[dD]|') ", r"\1 \2 ", text)
+        text = re.sub(r"([^' ])('ll|'LL|'re|'RE|'ve|'VE|n't|N'T) ", r"\1 \2 ",
+                      text)
 
         for regexp in self.CONTRACTIONS2:
             text = regexp.sub(r' \1 \2 ', text)
