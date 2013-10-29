2c24d60c2544d183d8498b8ef910c0f4023c3bad
Ewan Klein <ewan@inf.ed.ac.uk>
2013-10-29 16:29:34 +1100

    Fixed variable scoping bug.

--- nltk/sem/relextract.py
+++ nltk/sem/relextract.py
@@ -377,14 +377,14 @@ def ieer_headlines():
 
     from nltk.corpus import ieer
     from nltk.tree import Tree
-
+    
     print("IEER: First 20 Headlines")
-    print("=" * 45)
-
-    trees = [doc.headline for file in ieer.fileids() for doc in ieer.parsed_docs(file)]
+    print("=" * 45)  
+    
+    trees = [(doc.docno, doc.headline) for file in ieer.fileids() for doc in ieer.parsed_docs(file)]
     for tree in trees[:20]:
         print()
-        print("%s:\n%s" % (doc.docno, tree))
+        print("%s:\n%s" % tree)
 
 
 
