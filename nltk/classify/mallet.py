b7c7f02907bb68e3cff10af4c86a8a26d0b1ddf5
Ewan Klein <ewan@inf.ed.ac.uk>
2013-10-31 17:25:02 +1100

    Initial fix to call Mallet CRF SimpleTagger

--- nltk/classify/mallet.py
+++ nltk/classify/mallet.py
@@ -28,6 +28,7 @@ from nltk.internals import find_binary, java
 
 _mallet_home = None
 _mallet_classpath = None
+
 def config_mallet(mallet_home=None):
     """
     Configure NLTK's interface to the Mallet machine learning package.
@@ -38,26 +39,48 @@ def config_mallet(mallet_home=None):
         and if one is not found, it will raise a ``LookupError`` exception.
     """
     global _mallet_home, _mallet_classpath
-
-    # We don't actually care about this binary -- we just use it to
-    # make sure we've found the right directory.
-    mallethon_bin = find_binary(
-        'mallet', mallet_home,
-        env_vars=['MALLET',  'MALLET_HOME'],
-        binary_names=['mallethon'],
-        url='http://mallet.cs.umass.edu')
-    # Record the location where mallet lives.
-    bin_dir = os.path.split(mallethon_bin)[0]
-    _mallet_home = os.path.split(bin_dir)[0]
+    
+    if mallet_home is not None:
+        _mallet_home = mallet_home
+    
+    
+    # the following should work with an install via MacPorts as long as the
+    # environment variable is set correctly
+    else:
+        env_vars=['MALLET',  'MALLETHOME']
+        for ev in env_vars:
+            _env_dir = os.environ.get(ev)
+            if _env_dir is not None and os.path.isdir(_env_dir):
+                _mallet_home = _env_dir
+            break
+        
+        
+    if _mallet_home is None:      
+        # We don't actually care about this binary -- we just use it to
+        # make sure we've found the right directory.
+        
+        # EK: The following won't work if bin and lib files are not sibling directories
+        mallethon_bin = find_binary(
+            'mallet', mallet_home,
+            env_vars,
+            binary_names=['mallethon'],
+            url='http://mallet.cs.umass.edu')
+        # Record the location where mallet lives.
+        bin_dir = os.path.split(mallethon_bin)[0]
+        _mallet_home = os.path.split(bin_dir)[0]
     # Construct a classpath for using mallet.
     lib_dir = os.path.join(_mallet_home, 'lib')
+    class_dir = os.path.join(_mallet_home, 'class')
     if not os.path.isdir(lib_dir):
         raise ValueError('While configuring mallet: directory %r '
                          'not found.' % lib_dir)
-    _mallet_classpath = os.path.pathsep.join(os.path.join(lib_dir, filename)
-                                  for filename in sorted(os.listdir(lib_dir))
-                                  if filename.endswith('.jar'))
-
+        
+    #_mallet_classpath = os.pathsep.join(os.path.join(lib_dir, filename)
+                                  #for filename in sorted(os.listdir(lib_dir))
+                                  #if filename.endswith('.jar'))
+                                  
+    _mallet_classpath = os.pathsep.join((class_dir, os.path.join(lib_dir, "mallet-deps.jar")))
+    pass
 
 def call_mallet(cmd, classpath=None, stdin=None, stdout=None, stderr=None,
                 blocking=True):
@@ -68,7 +91,7 @@ def call_mallet(cmd, classpath=None, stdin=None, stdout=None, stderr=None,
 
     See `nltk.internals.java` for parameter and return value descriptions.
     """
-    if _mallet_classpath is None:
+    if _mallet_classpath is None or _mallet_classpath == "":
         config_mallet()
 
     # Set up the classpath
@@ -78,3 +101,20 @@ def call_mallet(cmd, classpath=None, stdin=None, stdout=None, stderr=None,
         classpath += os.path.pathsep + _mallet_classpath
     # Delegate to java()
     return java(cmd, classpath, stdin, stdout, stderr, blocking)
+
+ 
+
+
+def demo():
+    #from nltk.classify import mallet    
+    tagger = "cc.mallet.fst.SimpleTagger"
+      
+    stdout, stderr = call_mallet([tagger, 
+                    '--train', 'true',
+                    '--model-file', 'nouncrf',
+                    'sample.txt'], stdout='pipe')    
+
+if __name__ == '__main__':
+    demo()
+    
+    
