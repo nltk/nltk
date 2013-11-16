3e374a0cb49e233255300ad6f89933a10197ca90
Marcus Uneson <marcus.uneson@gmail.com>
2013-11-16 21:18:05 +0100

    added to demo.postagging

--- nltk/tag/brill/demo/postagging.py
+++ nltk/tag/brill/demo/postagging.py
@@ -10,6 +10,7 @@
 # For license information, see  LICENSE.TXT
 
 from __future__ import print_function, absolute_import, division
+import os
 
 import random
 import yaml
@@ -20,8 +21,6 @@ from nltk import tag
 from nltk.corpus import treebank
 
 from nltk.tag.brill.erroranalysis import error_list
-from nltk.tag.brill.trainer.fast import TaggerTrainer
-from nltk.tag.brill.demo import postagging_templates
 from nltk.tag.brill.template import Template
 from nltk.tag.brill.application.postagging import Word, Tag
 
@@ -46,7 +45,7 @@ def _demo_prepare_data(tagged_data, train, num_sents, randomize):
     return (training_data, gold_data, testing_data)
 
 
-def _demo_plot_learning_curve(learning_curve_output, trainstats, teststats, take=None):
+def _demo_plot(learning_curve_output, teststats, trainstats=None, take=None):
    testcurve = [teststats['initialerrors']]
    for rulescore in teststats['rulescores']:
        testcurve.append(testcurve[-1] - rulescore)
@@ -82,20 +81,27 @@ REGEXP_TAGGER = tag.RegexpTagger(
 
 
 
-def postag(templates=None,
-           tagged_data=None,
-           num_sents=1000,
-           max_rules=300,
-           min_score=3,
-           error_output="errors.out",
-           rule_output="rules.yaml",
-           learning_curve_output="learningcurve.png",
-           learning_curve_take=300,
-           baseline_backoff_tagger=None,
-           randomize=False,
-           train=.8,
-           trace=3,
-           ruleformat="str"):
+def postag(
+    templates=None,
+    tagged_data=None,
+    num_sents=1000,
+    max_rules=300,
+    min_score=3,
+    train=0.8,
+    trace=3,
+    randomize=False,
+    ruleformat="str",
+    incremental_stats=False,
+
+    #the following should rarely need modification for demo purposes
+    cache_baseline_tagger=None,
+    error_output="errors.out",
+    rule_output="rules.yaml",
+    learning_curve_output="learningcurve.png",
+    learning_curve_take=300,
+    baseline_backoff_tagger=None,
+    training_algorithm = "fast",
+    separate_baseline_data=True):
     """
     Brill Tagger Demonstration
     :param templates: how many sentences of training and testing data to use
@@ -113,8 +119,23 @@ def postag(templates=None,
     :param min_score: the minimum score for a rule in order for it to be considered
     :type min_score: L{int}
 
+    :param train: the fraction of the the corpus to be used for training (1=all)
+    :type train: L{float}
+
+    :param trace: the level of diagnostic tracing output to produce (0-4)
+    :type trace: L{int}
+
+    :param randomize: whether the training data should be a random subset of the corpus
+    :type randomize: boolean
+
+    :param ruleformat: rule output format, one of "str", "repr", "verbose"
+    :type ruleformat: L{str}
+
     :param error_output: the file where errors will be saved
     :type error_output: C{string}
+
+    :param cache_baseline_tagger: the file where rules will be saved
+    :type cache_baseline_tagger: C{string}
 
     :param learning_curve_output: filename of plotted learning curve (train and also test, if available)
     :type learning_curve_output: C{string}
@@ -122,82 +143,98 @@ def postag(templates=None,
     :param learning_curve_take: how many rules plotted
     :type learning_curve_take: C{int}
 
-    :param rule_output: the file where rules will be saved
-    :type rule_output: C{string}
-
     :param baseline_backoff_tagger: the file where rules will be saved
     :type baseline_backoff_tagger: tagger
 
-    :param randomize: whether the training data should be a random subset of the corpus
-    :type randomize: boolean
 
-    :param train: the fraction of the the corpus to be used for training (1=all)
-    :type train: L{float}
-
-    :param trace: the level of diagnostic tracing output to produce (0-4)
-    :type trace: L{int}
-
-    :param ruleformat: rule output format, one of "str", "repr", "verbose"
-    :type ruleformat: L{str}
     """
 
     # defaults
+    if training_algorithm == "fast":
+        from nltk.tag.brill.trainer.fast import TaggerTrainer
+    else:
+        from nltk.tag.brill.trainer.brillorig import TaggerTrainer
     baseline_backoff_tagger = baseline_backoff_tagger or REGEXP_TAGGER
     if templates is None:
-        #templates = postagging_templates.fntbl37()
-
         #Template.expand and Feature.expand are class methods facilitating
         #generating large amounts of templates -- see their documentation
-        #note -- training can easily fill all available memory
+        #note -- training with 500 templates can easily fill all available
+        #even on relatively small corpora
         wordtpls = Word.expand([-1,0,1], [1,2], excludezero=False)
-        tagtpls = Tag.expand([-2,-1,0,1,2], [1,2], excludezero=True)
+        tagtpls = Tag.expand([-2,-1,0,1], [1,2], excludezero=True)
         templates = list(Template.expand([wordtpls, tagtpls], combinations=(1,3)))
-        print("generated {} templates for transformation-based learning".format(len(templates)))
+        print("Generated {0} templates for transformation-based learning".format(len(templates)))
+
+        ## you could also try one of the pre-built sets, available through
+        #from nltk.tag.brill.demo import postagging_templates
+        ## for instance:
+        #templates = postagging_templates.fntbl37()
+
 
     (training_data, gold_data, testing_data) = _demo_prepare_data(tagged_data, train, num_sents, randomize)
-    #if we are to study the learning curve, then the baseline must be trained on separate data
-    #or it will be absurdly high
-    if learning_curve_output:
-        baseline_cutoff = len(training_data)//3
-        (baseline_data, training_data) = (training_data[:baseline_cutoff], training_data[baseline_cutoff:])
-    else:
+    # The data used to train the baseline tagger should be disjoint from that for the rule learner
+    # (and both disjoint from the test, of course). For pedagogical reasons, one can see what happens
+    # otherwise with separate_baseline_data=False: only a few rules learned, and better baseline
+    # but worse end result on unseen data.
+    if not separate_baseline_data:
         baseline_data = training_data
+    else:
+        baseline_cutoff = len(training_data) // 3
+        (baseline_data, training_data) = (training_data[:baseline_cutoff], training_data[baseline_cutoff:])
     (trainseqs, traintokens) = corpus_size(training_data)
     (testseqs, testtokens) = corpus_size(testing_data)
-    print("Read data (train %d sentences/%d words; test %d sentences/%d words)." % (
-        trainseqs, traintokens, testseqs, testtokens))
+    (bltrainseqs, bltraintokens) = corpus_size(baseline_data)
+    print("Read testing data ({0:d} sents/{1:d} wds)".format(testseqs, testtokens))
+    print("Read training data ({0:d} sents/{1:d} wds)".format(trainseqs, traintokens))
+    print("Read baseline data ({0:d} sents/{1:d} wds) {2:s}".format(
+        bltrainseqs, bltraintokens, "" if separate_baseline_data else "[reused the training set]"))
 
-    # creating a baseline tagger (unigram tagger)
-    tuni = time.time()
-    baseline_tagger = tag.UnigramTagger(baseline_data, backoff=baseline_backoff_tagger)
-    print("Trained unigram tagger in {:0.1f} seconds".format(time.time() - tuni))
+    # creating (or reloading from cache) a baseline tagger (unigram tagger)
+    if cache_baseline_tagger:
+        if not os.path.exists(cache_baseline_tagger):
+            baseline_tagger = tag.UnigramTagger(baseline_data, backoff=baseline_backoff_tagger)
+            with open(cache_baseline_tagger, 'w') as print_rules:
+                yaml.dump(baseline_tagger, print_rules)
+            print("Trained baseline tagger, wrote yaml to {0}".format(cache_baseline_tagger))
+        with open(cache_baseline_tagger, "r") as print_rules:
+            baseline_tagger= yaml.load(print_rules)
+            print("Reloaded YAML-serialized tagger from {0}".format(cache_baseline_tagger))
+    else:
+        baseline_tagger = tag.UnigramTagger(baseline_data, backoff=baseline_backoff_tagger)
+        print("Trained baseline tagger")
     if gold_data:
-        print("    [accuracy on test set: %f]" % baseline_tagger.evaluate(gold_data))
+        print("    Accuracy on test set: {0:0.4f}".format(baseline_tagger.evaluate(gold_data)))
 
     # creating a Brill tagger
     tbrill = time.time()
     trainer = TaggerTrainer(baseline_tagger, templates, trace, ruleformat=ruleformat)
+    print("Training brill tagger...")
     brill_tagger = trainer.train(training_data, max_rules, min_score)
-    print("Trained brill tagger in {:0.1f} seconds".format(time.time() - tbrill))
+    print("Trained brill tagger in {0:0.2f} seconds".format(time.time() - tbrill))
     if gold_data:
-        print("    [accuracy on test set: %f]" % brill_tagger.evaluate(gold_data))
+        print("    Accuracy on test set: %.4f" % brill_tagger.evaluate(gold_data))
 
-    # printing the learned rules (if learned silently)
-    if trace <= 1:
+    # printing the learned rules, if learned silently
+    if trace == 1:
         print("\nLearned rules: ")
-        for (ruleno, rule) in brill_tagger.rules():
-            print("{:3d} {:s}".format(ruleno, rule.format(ruleformat)))
+        for (ruleno, rule) in enumerate(brill_tagger.rules(),1):
+            print("{0:4d} {1:s}".format(ruleno, rule.format(ruleformat)))
 
-    # printing template statistics
-    brill_tagger.print_template_statistics()
-    (taggedtest, teststats) = brill_tagger.batch_tag_incremental(testing_data, gold_data)
-    trainstats = brill_tagger.train_stats()
-    brill_tagger.print_template_statistics(teststats['rulescores'])
 
-    # plotting learning curve
-    if learning_curve_output is not None:
-        _demo_plot_learning_curve(learning_curve_output, trainstats, teststats, take=learning_curve_take)
-        print("Wrote plot of learning curve to {}".format(learning_curve_output))
+    # printing template statistics (optionally including comparison with the training data)
+    if  incremental_stats:
+        print("Incrementally tagging the test data and collecting individual rule statistics")
+        (taggedtest, teststats) = brill_tagger.batch_tag_incremental(testing_data, gold_data)
+        print("    Rule statistics collected")
+        trainstats = brill_tagger.train_stats()
+        brill_tagger.print_template_statistics(teststats)
+        if learning_curve_output:
+            _demo_plot(learning_curve_output, teststats, trainstats, take=learning_curve_take)
+            print("Wrote plot of learning curve to {0}".format(learning_curve_output))
+    else:
+        brill_tagger.print_template_statistics()
+        print("Tagging the test data")
+        taggedtest = brill_tagger.batch_tag(testing_data)
 
     # writing error analysis to file
     if error_output is not None:
@@ -205,19 +242,21 @@ def postag(templates=None,
             f.write('Errors for Brill Tagger %r\n\n' % rule_output)
             for e in error_list(gold_data, taggedtest):
                 f.write(e+'\n')
-        print("Wrote tagger errors including context to {}".format(error_output))
+        print("Wrote tagger errors including context to {0}".format(error_output))
 
     # serializing the tagger to a yaml file
     if rule_output is not None:
         with open(rule_output, 'w') as print_rules:
             yaml.dump(brill_tagger, print_rules)
-        print("Wrote YAML-serialized tagger to {}".format(rule_output))
+        print("Wrote YAML-serialized tagger to {0}".format(rule_output))
         del brill_tagger
         with open(rule_output, "r") as print_rules:
             brill_tagger = yaml.load(print_rules)
-        print("Reloaded YAML-serialized tagger from {}".format(rule_output))
+        print("Reloaded YAML-serialized tagger from {0}".format(rule_output))
 
 
+def demo_train_stats():
+    postag(incremental_stats=True)
 
-def demo():
+def demo_all():
     postag()
