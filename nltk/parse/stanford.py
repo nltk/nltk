f2062c2aa91bb10c4f554d159426a4d69c13cb31
Steven Xu <>
2013-12-06 16:36:07 +1100

    added interface to stanford parser, c.f. https://github.com/nltk/nltk/issues/522

---
+++ nltk/parse/stanford.py
@@ -0,0 +1,152 @@
+from __future__ import print_function
+
+import tempfile
+from subprocess import PIPE
+
+from nltk import compat
+from nltk.tokenize import word_tokenize
+from nltk.internals import find_file, find_jar, config_java, java, _java_options
+
+from nltk.parse.api import ParserI
+from nltk.tree import Tree
+
+_stanford_url = 'http://nlp.stanford.edu/software/lex-parser.shtml'
+
+class StanfordParser(ParserI):
+    _MODEL_JAR = 'stanford-parser-3.3.0-models.jar'
+    _JAR = 'stanford-parser.jar'
+
+    def __init__(self, path_to_jar=None, path_to_models_jar=None,
+                 model_path='edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz',
+                 encoding='UTF-8', verbose=False, java_options='-mx1000m'):
+
+        self._stanford_jar = find_jar(
+                self._JAR, path_to_jar,
+                searchpath=(), url=_stanford_url,
+                verbose=verbose)
+
+        self._model_jar = find_jar(
+                self._MODEL_JAR, path_to_models_jar,
+                searchpath=(), url=_stanford_url,
+                verbose=verbose)
+
+        self.model_path = model_path
+        self._encoding = encoding
+        self.java_options = java_options
+
+    def parse(self, sentence, verbose=False):
+        """
+        Use StanfordParser to parse a sentence. Takes a sentence as a list of
+        words; it will be automatically tagged with this StanfordParser instance's
+        tagger.
+
+        :param sentence: Input sentence to parse
+        :type sentence: list(str)
+        :rtype: Tree
+        """
+        return self.batch_parse([sentence], verbose)[0]
+
+    def batch_parse(self, sentences, verbose=False):
+        """
+        Use StanfordParser to parse multiple sentences. Takes multiple sentences as a
+        list where each sentence is a list of words.
+        Each sentence will be automatically tagged with this StanfordParser instance's
+        tagger.
+
+        :param sentences: Input sentences to parse
+        :type sentences: list(list(str))
+        :rtype: list(Tree)
+        """
+
+    def raw_parse(self, sentence, verbose=False):
+        """
+        Use StanfordParser to parse a sentence. Takes a sentence as a string;
+        before parsing, it will be automatically tokenized and tagged by
+        the Stanford Parser.
+
+        :param sentence: Input sentence to parse
+        :type sentence: str
+        :rtype: Tree
+        """
+        return self.raw_batch_parse((sentence,), verbose)
+
+    def raw_batch_parse(self, sentences, verbose=False):
+        """
+        Use StanfordParser to parse multiple sentences. Takes multiple sentences as a
+        list of strings.
+        Each sentence will be automatically tokenized and tagged by the Stanford Parser.
+
+        :param sentences: Input sentences to parse
+        :type sentences: list(str)
+        :rtype: list(Tree)
+        """
+        cmd = [
+            'edu.stanford.nlp.parser.lexparser.LexicalizedParser',
+            '-model', self.model_path,
+            '-sentences', 'newline',
+            '-outputFormat', 'penn',
+        ]
+        output_ = self._execute(cmd, '\n'.join(sentences), verbose)
+        res = []
+        cur_lines = []
+        for line in output_.splitlines(False):
+            if line == '':
+                res.append(Tree.parse('\n'.join(cur_lines)))
+                cur_lines = []
+            else:
+                cur_lines.append(line)
+        return res
+
+    def tagged_parse(self, sentence, verbose=False):
+        """
+        Use StanfordParser to parse a sentence. Takes a sentence as a list of
+        (word, tag) tuples; the sentence must have already been tokenized and
+        tagged.
+
+        :param sentence: Input sentence to parse
+        :type sentence: list(tuple(str, str))
+        :rtype: Tree
+        """
+        return self.tagged_batch_parse([sentence], verbose)[0]
+
+    def tagged_batch_parse(self, sentences, verbose=False):
+        """
+        Use StanfordParser to parse multiple sentences. Takes multiple sentences
+        where each sentence is a list of (word, tag) tuples.
+        The sentences must have already been tokenized and tagged.
+
+        :param sentences: Input sentences to parse
+        :type sentence: list(list(tuple(str, str)))
+        :rtype: Tree
+        """
+
+    def _execute(self, cmd, input_, verbose=False):
+        encoding = self._encoding
+        cmd.extend(['-encoding', encoding])
+
+        default_options = ' '.join(_java_options)
+
+        #configure java
+        config_java(options=self.java_options, verbose=verbose)
+
+        with tempfile.NamedTemporaryFile(mode='wb', delete=False) as input_file:
+            # Write the actual sentences to the temporary input file
+            if isinstance(input_, compat.text_type) and encoding:
+                input_ = input_.encode(encoding)
+            input_file.write(input_)
+            input_file.flush()
+
+            cmd.append(input_file.name)
+
+            # Run the tagger and get the output
+            stdout, stderr = java(cmd, classpath=(self._stanford_jar, self._model_jar),
+                                  stdout=PIPE, stderr=PIPE)
+            stdout = stdout.decode(encoding)
+            if (not compat.PY3) and encoding == 'ascii':
+                stdout = str(stdout)
+
+        # Return java configurations to their default values
+        config_java(options=default_options, verbose=False)
+
+        return stdout
+
