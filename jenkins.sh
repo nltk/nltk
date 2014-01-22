25e61a079a8fd6bcb883f924ddd021b542122956
Steven Xu <>
2014-01-22 22:01:58 +1100

    curl suppress output

--- jenkins.sh
+++ jenkins.sh
@@ -14,7 +14,7 @@ pushd ${HOME}
 pushd 'third'
 
 #download nltk stanford dependencies
-stanford_parser_package_zip_name=$(curl 'http://nlp.stanford.edu/software/lex-parser.shtml' | grep -o 'stanford-parser-full-.*\.zip' | head -n1)
+stanford_parser_package_zip_name=$(curl -s 'http://nlp.stanford.edu/software/lex-parser.shtml' | grep -o 'stanford-parser-full-.*\.zip' | head -n1)
 [[ ${stanford_parser_package_zip_name} =~ (.+)\.zip ]]
 stanford_parser_package_name=${BASH_REMATCH[1]}
 if [[ ! -d ${stanford_parser_package_name} ]]; then
@@ -24,7 +24,7 @@ if [[ ! -d ${stanford_parser_package_name} ]]; then
 	ln -s ${stanford_parser_package_name} 'stanford-parser'
 fi
 
-stanford_tagger_package_zip_name=$(curl 'http://nlp.stanford.edu/downloads/tagger.shtml' | grep -o 'stanford-postagger-full-.*\.zip' | head -n1)
+stanford_tagger_package_zip_name=$(curl -s 'http://nlp.stanford.edu/downloads/tagger.shtml' | grep -o 'stanford-postagger-full-.*\.zip' | head -n1)
 [[ ${stanford_tagger_package_zip_name} =~ (.+)\.zip ]]
 stanford_tagger_package_name=${BASH_REMATCH[1]}
 if [[ ! -d ${stanford_tagger_package_name} ]]; then
