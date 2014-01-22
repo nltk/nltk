83d884d3fcb951b3737923518c981920f5288f80
Steven Xu <>
2014-01-22 21:14:35 +1100

    fixed up some strings

--- jenkins.sh
+++ jenkins.sh
@@ -24,14 +24,14 @@ if [[ ! -d ${stanford_parser_package_name} ]]; then
 	ln -s ${stanford_parser_package_name} 'stanford-parser'
 fi
 
-stanford_parser_package_zip_name=$(curl 'http://nlp.stanford.edu/downloads/tagger.shtml' | grep -o 'stanford-parser-full-.*\.zip' | head -n1)
-[[ ${stanford_parser_package_zip_name} =~ (.+)\.zip ]]
-stanford_parser_package_name=${BASH_REMATCH[1]}
-if [[ ! -d ${stanford_parser_package_name} ]]; then
-	wget -nv "http://nlp.stanford.edu/software/$stanford_parser_package_zip_name"
-	unzip ${stanford_parser_package_zip_name}
-	rm ${stanford_parser_package_name}
-	ln -s ${stanford_parser_package_name} 'stanford-postagger'
+stanford_tagger_package_zip_name=$(curl 'http://nlp.stanford.edu/downloads/tagger.shtml' | grep -o 'stanford-postagger-full-.*\.zip' | head -n1)
+[[ ${stanford_tagger_package_zip_name} =~ (.+)\.zip ]]
+stanford_tagger_package_name=${BASH_REMATCH[1]}
+if [[ ! -d ${stanford_tagger_package_name} ]]; then
+	wget -nv "http://nlp.stanford.edu/software/$stanford_tagger_package_zip_name"
+	unzip ${stanford_tagger_package_zip_name}
+	rm ${stanford_tagger_package_name}
+	ln -s ${stanford_tagger_package_name} 'stanford-postagger'
 fi
 
 popd
