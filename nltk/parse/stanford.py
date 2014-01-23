bfb55057aa20eb153ea1207b5fa375f150649baa
Steven Xu <>
2014-01-23 20:13:14 +1100

    changed single to double quotes

--- nltk/parse/stanford.py
+++ nltk/parse/stanford.py
@@ -27,11 +27,11 @@ class StanfordParser(ParserI):
     Interface to the Stanford Parser
 
     >>> parser=StanfordParser(
-    ...     model_path='edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz'
+    ...     model_path="edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz"
     ... )
     >>> parser.raw_batch_parse((
-    ...     'the quick brown fox jumps over the lazy dog',
-    ...     'the quick grey wolf jumps over the lazy fox'
+    ...     "the quick brown fox jumps over the lazy dog",
+    ...     "the quick grey wolf jumps over the lazy fox"
     ... ))
     [Tree('ROOT', [Tree('NP', [Tree('NP', [Tree('DT', ['the']), Tree('JJ', ['quick']), Tree('JJ', ['brown']),
     Tree('NN', ['fox'])]), Tree('NP', [Tree('NP', [Tree('NNS', ['jumps'])]), Tree('PP', [Tree('IN', ['over']),
@@ -41,8 +41,8 @@ class StanfordParser(ParserI):
     Tree('JJ', ['lazy']), Tree('NN', ['fox'])])])])])])]
 
     >>> parser.batch_parse((
-    ...     'I \'m a dog'.split(),
-    ...     'This is my friends \' cat ( the tabby )'.split(),
+    ...     "I 'm a dog".split(),
+    ...     "This is my friends ' cat ( the tabby )".split(),
     ... ))
     [Tree('ROOT', [Tree('S', [Tree('NP', [Tree('PRP', ['I'])]), Tree('VP', [Tree('VBP', ["'m"]),
     Tree('NP', [Tree('DT', ['a']), Tree('NN', ['dog'])])])])]), Tree('ROOT', [Tree('S', [Tree('NP',
@@ -52,16 +52,16 @@ class StanfordParser(ParserI):
 
     >>> parser.tagged_batch_parse((
     ...     (
-    ...         ('The', 'DT'),
-    ...         ('quick', 'JJ'),
-    ...         ('brown', 'JJ'),
-    ...         ('fox', 'NN'),
-    ...         ('jumped', 'VBD'),
-    ...         ('over', 'IN'),
-    ...         ('the', 'DT'),
-    ...         ('lazy', 'JJ'),
-    ...         ('dog', 'NN'),
-    ...         ('.', '.'),
+    ...         ("The", "DT"),
+    ...         ("quick", "JJ"),
+    ...         ("brown", "JJ"),
+    ...         ("fox", "NN"),
+    ...         ("jumped", "VBD"),
+    ...         ("over", "IN"),
+    ...         ("the", "DT"),
+    ...         ("lazy", "JJ"),
+    ...         ("dog", "NN"),
+    ...         (".", "."),
     ...     ),
     ... ))
     [Tree('ROOT', [Tree('S', [Tree('NP', [Tree('DT', ['The']), Tree('JJ', ['quick']), Tree('JJ', ['brown']),
--- nltk/tokenize/stanford.py
+++ nltk/tokenize/stanford.py
@@ -26,11 +26,11 @@ class StanfordTokenizer(TokenizerI):
     Interface to the Stanford Tokenizer
 
     >>> from nltk.tokenize.stanford import StanfordTokenizer
-    >>> s = 'Good muffins cost $3.88\nin New York.  Please buy me\ntwo of them.\nThanks.'
+    >>> s = "Good muffins cost $3.88\nin New York.  Please buy me\ntwo of them.\nThanks."
     >>> StanfordTokenizer().tokenize(s)
     ['Good', 'muffins', 'cost', '$', '3.88', 'in', 'New', 'York', '.', 'Please', 'buy', 'me', 'two', 'of', 'them', '.', 'Thanks', '.']
-    >>> s = 'The colour of the wall is blue.'
-    >>> StanfordTokenizer(options={'americanize': True}).tokenize(s)
+    >>> s = "The colour of the wall is blue."
+    >>> StanfordTokenizer(options={"americanize": True}).tokenize(s)
     ['The', 'color', 'of', 'the', 'wall', 'is', 'blue', '.']
     """
 
