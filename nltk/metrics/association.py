4d4d9fdf5f62775622a682677221282656f720d8
Ofer Helman <helmanofer@gmail.com>
2014-03-02 17:41:47 +0200

    new QuadgramAssocMeasures Calculates quadgram contingency table
    Signed-off-by: Ofer Helman <helmanofer@gmail.com>

--- nltk/metrics/association.py
+++ nltk/metrics/association.py
@@ -352,6 +352,73 @@ class QuadgramAssocMeasures(NgramAssocMeasures):
                 sum(contingency))
 
 
+class QuadgramAssocMeasures(NgramAssocMeasures):
+    """
+    A collection of quadgram association measures. Each association measure
+    is provided as a function with five arguments::
+
+        trigram_score_fn(n_iiii,
+                        (n_iiix, n_iixi, n_ixii, n_xiii),
+                        (n_iixx, n_ixix, n_ixxi, n_xixi, n_xxii, n_xiix),
+                        (n_ixxx, n_xixx, n_xxix, n_xxxi),
+                        n_all)
+
+    The arguments constitute the marginals of a contingency table, counting
+    the occurrences of particular events in a corpus. The letter i in the
+    suffix refers to the appearance of the word in question, while x indicates
+    the appearance of any word. Thus, for example:
+    n_iiii counts (w1, w2, w3, w4), i.e. the quadgram being scored
+    n_ixxi counts (w1, *, *, w4)
+    n_xxxx counts (*, *, *, *), i.e. any quadgram
+    """
+
+    _n = 4
+
+    @staticmethod
+    def _contingency(n_iiii, n_iiix_tuple, n_iixx_tuple, n_ixxx_tuple, n_xxxx):
+        """Calculates values of a quadgram contingency table from
+        marginal values.
+        """
+        (n_iiix, n_iixi, n_ixii, n_xiii) = n_iiix_tuple
+        (n_iixx, n_ixix, n_ixxi, n_xixi, n_xxii, n_xiix) = n_iixx_tuple
+        (n_ixxx, n_xixx, n_xxix, n_xxxi) = n_ixxx_tuple
+        n_oiii = n_xiii - n_iiii
+        n_ioii = n_ixii - n_iiii
+        n_iioi = n_iixi - n_iiii
+        n_ooii = n_xxii - n_iiii - n_oiii - n_ioii
+        n_oioi = n_xixi - n_iiii - n_oiii - n_iioi
+        n_iooi = n_ixxi - n_iiii - n_ioii - n_iioi
+        n_oooi = n_xxxi - n_iiii - n_oiii - n_ioii - n_iioi - n_ooii - n_iooi - n_oioi
+        n_iiio = n_iiix - n_iiii
+        n_oiio = n_xiix - n_iiii - n_oiii - n_iiio
+        n_ioio = n_ixix - n_iiii - n_ioii - n_iiio
+        n_ooio = n_xxix - n_iiii - n_oiii - n_ioii - n_iiio - n_ooii - n_ioio - n_oiio
+        n_iioo = n_iixx - n_iiii - n_iioi - n_iiio
+        n_oioo = n_xixx - n_iiii - n_oiii - n_iioi - n_iiio - n_oioi - n_oiio - n_iioo
+        n_iooo = n_ixxx - n_iiii - n_ioii - n_iioi - n_iiio - n_iooi - n_iioo - n_ioio
+        n_oooo = n_xxxx - n_iiii - n_oiii - n_ioii - n_iioi - n_ooii - n_oioi - n_iooi - \
+                 n_oooi - n_iiio - n_oiio - n_ioio - n_ooio - n_iioo - n_oioo - n_iooo
+
+        return (n_iiii, n_oiii, n_ioii, n_ooii, n_iioi,
+                n_oioi, n_iooi, n_oooi, n_iiio, n_oiio,
+                n_ioio, n_ooio, n_iioo, n_oioo, n_iooo, n_oooo)
+
+    # @staticmethod
+    # def _marginals(*contingency):
+    #     """Calculates values of contingency table marginals from its values.
+    #     >>> TrigramAssocMeasures._marginals(1, 0, 0, 0, 0, 72, 0, 1927)
+    #     (1, (1, 1, 1), (1, 73, 1), 2000)
+    #     """
+    #     n_iiii, n_oiii, n_ioii, n_ooii, n_iioi, n_oioi, n_iooi, n_oooi, n_iiio, n_oiio, n_ioio, n_ooio, \
+    #     n_iioo, n_oioo, n_iooo, n_oooo = contingency
+    #     return (n_iiii,
+    #             (n_iiii + n_oiii, n_iiii + n_ioii, n_iiii + n_iioi),
+    #             (n_iiii + n_ooii + n_iio + n_ioo,
+    #              n_iiii + n_oii + n_iio + n_oio,
+    #              n_iiii + n_oii + n_ioi + n_ooi),
+    #             sum(contingency))
+
+
 class ContingencyMeasures(object):
     """Wraps NgramAssocMeasures classes such that the arguments of association
     measures are contingency table values rather than marginals.
