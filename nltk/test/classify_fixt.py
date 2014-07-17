06b5488baa85369b433a881e8e66fedff2cdc927
Dmitrijs Milajevs <dimazest@gmail.com>
2014-07-17 18:18:15 +0100

    classify tests also require sklearn.svm.SVC

--- nltk/test/classify_fixt.py
+++ nltk/test/classify_fixt.py
@@ -1,10 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+
 # most of classify.doctest requires numpy
 def setup_module(module):
     from nose import SkipTest
     try:
         import numpy
+        from sklearn.svm import SVC
     except ImportError:
-        raise SkipTest("classify.doctest requires numpy")
+        raise SkipTest("classify.doctest requires numpy and sklearn.svm.SVC")
