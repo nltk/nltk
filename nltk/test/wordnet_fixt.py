18f9a3bd3438b896c950178727c4c01e23d26d7e
Dmitrijs Milajevs <dimazest@gmail.com>
2014-07-21 11:29:45 +0200

    Skipping Wordnet tests as they take too much time…

--- nltk/test/wordnet_fixt.py
+++ nltk/test/wordnet_fixt.py
@@ -1,6 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 
+
+def setup_module(module):
+    from nose import SkipTest
+
+    raise SkipTest("Wordnet tests take too much time...")
+
+
 def teardown_module(module=None):
     from nltk.corpus import wordnet
     wordnet._unload()
