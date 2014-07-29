9918f52214c099dc9fad56d3a8747db6de7a2db4
Ewan Klein <ewan.klein@gmail.com>
2014-07-29 15:32:14 +0100

    Tidying up, removing blank lines, etc.

--- nltk/tokenize/twitter.py
+++ nltk/tokenize/twitter.py
@@ -2,7 +2,7 @@
 #
 # Copyright (C) 2001-2014 NLTK Project
 # Author: Christopher Potts <cgpotts@stanford.edu>
-#         Ewan Klein <ewan@inf.ed.ac.uk>
+#         Ewan Klein <ewan@inf.ed.ac.uk> (modifications)
 # URL: <http://nltk.org/>
 # For license information, see LICENSE.TXT
 
@@ -64,47 +64,48 @@ emoticon_string = r"""
       <3                         # heart
     )"""
 
-# URL pattern due to John Gruber, modified by Tom Winzig https://gist.github.com/winzig/8894715
+# URL pattern due to John Gruber, modified by Tom Winzig. See
+# https://gist.github.com/winzig/8894715
 
-url_string = r"""				# Capture 1: entire matched URL
+url_string = r"""			# Capture 1: entire matched URL
   (?:
   https?:				# URL protocol and colon
     (?:
-      /{1,3}						# 1-3 slashes
-      |								#   or
-      [a-z0-9%]						# Single letter or digit or '%'
-      								# (Trying not to match e.g. "URI::Escape")
+      /{1,3}				# 1-3 slashes
+      |					#   or
+      [a-z0-9%]				# Single letter or digit or '%'
+                                       # (Trying not to match e.g. "URI::Escape")
     )
-    |							#   or
-    							# looks like domain name followed by a slash:
+    |					#   or
+                                       # looks like domain name followed by a slash:
     [a-z0-9.\-]+[.]
     (?:[a-z]{2,13})
     /
   )
-  (?:							# One or more:
-    [^\s()<>{}\[\]]+						# Run of non-space, non-()<>{}[]
-    |								#   or
-    \([^\s()]*?\([^\s()]+\)[^\s()]*?\)  # balanced parens, one level deep: (…(…)…)
+  (?:					# One or more:
+    [^\s()<>{}\[\]]+			# Run of non-space, non-()<>{}[]
+    |					#   or
+    \([^\s()]*?\([^\s()]+\)[^\s()]*?\) # balanced parens, one level deep: (…(…)…)
     |
-    \([^\s]+?\)							# balanced parens, non-recursive: (…)
+    \([^\s]+?\)				# balanced parens, non-recursive: (…)
   )+
-  (?:							# End with:
-    \([^\s()]*?\([^\s()]+\)[^\s()]*?\)  # balanced parens, one level deep: (…(…)…)
+  (?:					# End with:
+    \([^\s()]*?\([^\s()]+\)[^\s()]*?\) # balanced parens, one level deep: (…(…)…)
     |
-    \([^\s]+?\)							# balanced parens, non-recursive: (…)
-    |									#   or
-    [^\s`!()\[\]{};:'".,<>?«»“”‘’]		# not a space or one of these punct chars
+    \([^\s]+?\)				# balanced parens, non-recursive: (…)
+    |					#   or
+    [^\s`!()\[\]{};:'".,<>?«»“”‘’]	# not a space or one of these punct chars
   )
   |					# OR, the following to match naked domains:
   (?:
-  	(?<!@)			# not preceded by a @, avoid matching foo@_gmail.com_
+  	(?<!@)			        # not preceded by a @, avoid matching foo@_gmail.com_
     [a-z0-9]+
     (?:[.\-][a-z0-9]+)*
     [.]
     (?:[a-z]{2,13})
     \b
     /?
-    (?!@)			# not succeeded by a @, avoid matching "foo.na" in "foo.na@example.com"
+    (?!@)			        # not succeeded by a @, avoid matching "foo.na" in "foo.na@example.com"
   )
 """
 
@@ -153,9 +154,6 @@ regex_strings = (
     |
     (?:\S)                         # Everything else that isn't whitespace.
     """
-    #,
-    ## URLs
-    #url_string
     )
 
 ######################################################################
@@ -176,12 +174,13 @@ amp = "&amp;"
 
 class TweetTokenizer:
     def __init__(self, preserve_case=True):
-        self.preserve_case = preserve_case
+        self.preserve_case=preserve_case
 
     def tokenize(self, s):
         """
-        Argument: s -- any string or unicode object
-        Value: a tokenize list of strings; conatenating this list returns the original string if preserve_case=False
+        :parameter s: str :rtype: list(str) :returns: a tokenized list of
+        strings; concatenating this list returns the original string if
+        preserve_case=False
         """        
         # Try to ensure unicode:
         try:
@@ -201,8 +200,8 @@ class TweetTokenizer:
 
     def __html2unicode(self, s):
         """
-        Internal method that seeks to replace all the HTML entities in
-        s with their corresponding unicode characters.
+        Try to replace all the HTML entities in `s` with their corresponding
+        Unicode characters.
         """
         # First the digits:
         ents = set(html_entity_digit_re.findall(s))
@@ -229,22 +228,12 @@ class TweetTokenizer:
 ###############################################################################
 
 if __name__ == '__main__':
-    
-    def test(s, toks):
-        tokenise = TweetTokenizer().tokenize
-        if tokenise(s) == toks:
-            return True
-        return tokenise(s)
-    
-    
     s0 = "This is a cooool #dummysmiley: :-) :-P <3"
     s1 = "Naps are a must \ud83d\ude34\ud83d\ude34"
     s2 = "Renato fica com muito medo de ouvir meus \u00e1udios perto da gaja dele, pois s\u00f3 falo merda KKK"
     s3 = "\u0412\u043b\u0430\u0434\u0435\u043b\u0435\u0446 20th Century Fox \u043d\u0430\u043c\u0435\u0440\u0435\u043d \u043a\u0443\u043f\u0438\u0442\u044c Warner Bros."
     s4 = "RT @facugambande: Ya por arrancar a grabar !!! #TirenTirenTiren vamoo !!"
     s5 = "http://t.co/7r8d5bVKyA http://t.co/hZpwZe1uKt http://t.co/ZKb7GKWocy Ничто так не сближает людей"
-    
-   
     
     t0 = ['This', 'is', 'a', 'cooool', '#dummysmiley', ':', ':-)', ':-P', '<3']
     t1 = ['Naps', 'are', 'a', 'must', '\ud83d', '\ude34', '\ud83d', '\ude34']
@@ -259,13 +248,7 @@ if __name__ == '__main__':
     for (s, t) in zip(tweets, toks):
         print(test(s, t))
 
-    #print(tokenise(s0) == ['This', 'is', 'a', 'cooool', '#dummysmiley', ':', ':-)', ':-P', '<3'])
-    #print(tokenise(s1) == ['Naps', 'are', 'a', 'must', '\ud83d', '\ude34', '\ud83d', '\ude34'])
-    #print(tokenise(s2) == ['Renato', 'fica', 'com', 'muito', 'medo', 'de', 'ouvir', 'meus', 'áudios', 'perto', 'da', 'gaja', 'dele', ',', 'pois', 'só', 'falo', 'merda', 'KKK'])
-    #print(tokenise(s3) == ['Владелец', '20th', 'Century', 'Fox', 'намерен', 'купить', 'Warner', 'Bros', '.'])
-    #print(tokenise(s4) == ['RT', '@facugambande', ':', 'Ya', 'por', 'arrancar', 'a', 'grabar', '!', '!', '!', '#TirenTirenTiren', 'vamoo', '!', '!'])
-    #print(tokenise(s5) == ['http://t.co/7r8d5bVKyA', 'http://t.co/hZpwZe1uKt', 'http://t.co/ZKb7GKWocy', 'Ничто', 'так', 'не', 'сближает', 'людей']    )
-    
+
 
     
     
