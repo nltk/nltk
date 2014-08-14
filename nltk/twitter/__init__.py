c2e55f675558ae268341e22e9903f22615f64cae
Ewan Klein <ewan.klein@gmail.com>
2014-08-14 13:25:06 +0100

    Added imports to __init__

---
+++ nltk/twitter/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Natural Language Toolkit: Twitter
+#
+# Copyright (C) 2001-2014 NLTK Project
+# Author: Ewan Klein <ewan@inf.ed.ac.uk>
+# URL: <http://nltk.org/>
+# For license information, see LICENSE.TXT
+
+"""
+NLTK Twitter Package
+
+This package contains classes for retrieving Tweet documents using the
+Twitter API.
+
+"""
+try:
+    from twython import Twython, TwythonStreamer
+except ImportError as exc:
+    print("""The twitterclient module requires the Twython package.
+    See https://twython.readthedocs.org/ for installation instructions.""")
+
+    raise
+
+from nltk.util import credsfromfile
+from nltk.twitter.twitterclient import Streamer, Query,Twitter, TweetViewer,\
+     TweetWriter
+
+# skip doctests from this package
+def setup_module(module):
+    from nose import SkipTest
+    raise SkipTest("nltk.twitter examples are not doctests")
+
