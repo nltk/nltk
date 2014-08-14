8af9e739fd33c41f29627b9a19dfc0944a58e9a7
alvas <alvations@gmail.com>
2014-08-14 07:19:06 +0200

    removed comments that are transparent in the code.

--- nltk/align/phrase_based.py
+++ nltk/align/phrase_based.py
@@ -98,26 +98,25 @@ def phrase_extraction(srctext, trgtext, alignment):
                 return {}
 
         # Add phrase pairs (incl. additional unaligned f)
-        # Remark:  how to interpret "additional unaligned f"?
         phrases = set()
         fs = f_start
-        # repeat-
         while True:
             fe = f_end
-            # repeat-
             while True:
                 # add phrase pair ([e_start, e_end], [fs, fe]) to set E
                 # Need to +1 in range  to include the end-point.
-                src_phrase = " ".join(srctext[i] for i in range(e_start,e_end+1))
+                src_phrase = " ".join(srctext[i] for i in 
+                                      range(e_start,e_end+1))
                 trg_phrase = " ".join(trgtext[i] for i in range(fs,fe+1))
                 # Include more data for later ordering.
-                phrases.add(((e_start, e_end+1), (f_start, f_end+1), src_phrase, trg_phrase))
-                fe += 1 # fe++
-                # -until fe aligned or out-of-bounds
+                phrases.add(((e_start, e_end+1), (f_start, f_end+1), 
+                             src_phrase, trg_phrase))
+                fe += 1
+                # if fe is in word alignment or out-of-bounds
                 if fe in f_aligned or fe == trglen:
                     break
-            fs -=1  # fe--
-            # -until fs aligned or out-of- bounds
+            fs -=1 
+            # if fs is in word alignment or out-of-bounds
             if fs in f_aligned or fs < 0:
                 break
         return phrases
@@ -132,10 +131,8 @@ def phrase_extraction(srctext, trgtext, alignment):
     f_aligned = [j for _,j in alignment]
 
     bp = set() # set of phrase pairs BP
-    # for e start = 1 ... length(e) do
     # Index e_start from 0 to len(e) - 1
     for e_start in range(srclen):
-        # for e end = e start ... length(e) do
         # Index e_end from e_start to len(e) - 1
         for e_end in range(e_start, srclen):
             # // find the minimally matching foreign phrase
@@ -144,7 +141,6 @@ def phrase_extraction(srctext, trgtext, alignment):
             f_start, f_end = trglen-1 , -1  #  0-based indexing
             # for all (e,f) ∈ A do
             for e,f in alignment:
-                # if e start ≤ e ≤ e end then
                 if e_start <= e <= e_end:
                     f_start = min(f, f_start)
                     f_end = max(f, f_end)
