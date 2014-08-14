bc6f405558e2a2ea37d495a8b7b96317a558bdbd
Ewan Klein <ewan.klein@gmail.com>
2014-08-14 13:43:12 +0100

    temporary unittest file in twitter package

---
+++ nltk/twitter/test_twitter.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+import unittest
+
+from nltk.twitter import Streamer, Query, Twitter, TweetViewer,\
+     TweetWriter
+
+class TestInitClient(unittest.TestCase):
+
+
+class TestCredentials(unittest.TestCase):
+
+    def test_missingfile(self):
+        pass
