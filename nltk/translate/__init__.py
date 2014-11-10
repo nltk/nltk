af577e16d39627ca2dd442d67a5d3933deaeb7aa
alvas <alvations@gmail.com>
2014-11-10 13:19:33 +0100

    added init and util for translate module

---
+++ nltk/translate/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Natural Language Toolkit: Machine Translation
+#
+# Copyright (C) 2001-2014 NLTK Project
+# Author: Liling Tan
+# URL: <http://nltk.org/>
+# For license information, see LICENSE.TXT
+#
+
+"""
+Basic Machine Translation (MT) components.
+These interfaces are subjected to change.
+"""
+
+from nltk.translate.model import *
+from nltk.translate.stack_decoder import monotone_stack_decode
