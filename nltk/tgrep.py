578cb42b9cb56c6cf64e62b1580aa75941a6a064
Will Roberts <will.roberts@rz.hu-berlin.de>
2015-02-19 13:15:30 +0100

    tgrep_tokenize: also treat bytes and str the same

--- nltk/tgrep.py
+++ nltk/tgrep.py
@@ -537,6 +537,8 @@ def tgrep_tokenize(tgrep_string):
     Tokenizes a TGrep search string into separate tokens.
     '''
     parser = _build_tgrep_parser(False)
+    if isinstance(tgrep_string, bytes):
+        tgrep_string = tgrep_string.decode()
     return list(parser.parseString(tgrep_string))
 
 def tgrep_compile(tgrep_string):
