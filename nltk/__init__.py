06b1fa4ec62944e9ecb32c6d2751e3fb06294747
Steven Bird <stevenbird1@gmail.com>
2015-03-11 15:33:51 +1100

    remove import of model package

--- nltk/__init__.py
+++ nltk/__init__.py
@@ -119,7 +119,6 @@ from nltk.chunk import *
 from nltk.classify import *
 from nltk.inference import *
 from nltk.metrics import *
-from nltk.model import *
 from nltk.parse import *
 from nltk.tag import *
 from nltk.tokenize import *
@@ -166,7 +165,7 @@ else:
 
 from nltk import align, ccg, chunk, classify, collocations
 from nltk import data, featstruct, grammar, help, inference, metrics
-from nltk import misc, model, parse, probability, sem, stem, wsd
+from nltk import misc, parse, probability, sem, stem, wsd
 from nltk import tag, tbl, text, tokenize, tree, treetransforms, util
 
 # override any accidentally imported demo
