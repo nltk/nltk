7e5859551374e90a57a1177da8638d49dde76bab
Ewan Klein <ewan.klein@gmail.com>
2015-06-07 22:57:30 +0100

    * Use TemporaryDirectory for writing files rather than user filespace. * PEP8 line-wrapping. * Pass a file-like object rather than filename to json2csv

--- nltk/test/unit/test_json2csv_corpus.py
+++ nltk/test/unit/test_json2csv_corpus.py
@@ -1,117 +1,148 @@
-'''
-Created on 6 de may. de 2015
+# -*- coding: utf-8 -*-
+# Natural Language Toolkit: Twitter client
+#
+# Copyright (C) 2001-2015 NLTK Project
+# Author: Lorenzo Rubio <lrnzcig@gmail.com>
+# URL: <http://nltk.org/>
+# For license information, see LICENSE.TXT
 
-@author: lorenzorubio
-'''
-import unittest
-import os
+
 import filecmp
-from nltk.twitter.util import guess_path, json2csv, json2csv_entities
+import os
+from tempfile import TemporaryDirectory
+import unittest
+
+from nltk.corpus import tweets
+from nltk.twitter.util import json2csv, json2csv_entities
+
 
 class Test(unittest.TestCase):
 
-
     def setUp(self):
-        from nltk.corpus import tweets
-        self.inpf = tweets.abspath("tweets.20150430-223406.json")
-        pass
+        self.infile = open(tweets.abspath("tweets.20150430-223406.json"))
 
 
     def tearDown(self):
-        pass
+        self.infile.close()
 
 
-    def testTextOutput(self):
-        outf = os.path.join(guess_path("twitter-files"), 'tweets.20150430-223406.text.csv')
-        json2csv(self.inpf, outf,
-                 ['text'])
+    def test_textoutput(self):
+        with TemporaryDirectory() as tempdir:
+            outfn = os.path.join(tempdir, 'tweets.20150430-223406.text.csv')
+            json2csv(self.infile, outfn, ['text'])
 
-        assert filecmp.cmp(outf,
-                       'tweets.20150430-223406.text.csv.ref'), 'Error in csv file'
+            assert filecmp.cmp(outfn, 'tweets.20150430-223406.text.csv.ref'),\
+                       'Error in csv file'
 
-    def testTweetMetadata(self):
-        outf = os.path.join(guess_path("twitter-files"), 'tweets.20150430-223406.tweet.csv')
-        json2csv(self.inpf, outf,
-                 ['created_at', 'favorite_count', 'id', 'in_reply_to_status_id', 
-                 'in_reply_to_user_id', 'retweet_count', 'retweeted', 
-                 'text', 'truncated', {'user' : {'id'}}])
+    def test_tweet_metadata(self):
+        fields = ['created_at', 'favorite_count', 'id',
+                  'in_reply_to_status_id', 'in_reply_to_user_id', 'retweet_count',
+                  'retweeted', 'text', 'truncated', {'user' : 'id'}]
 
-        assert filecmp.cmp(outf,
-                       'tweets.20150430-223406.tweet.csv.ref'), 'Error in csv file'
+        with TemporaryDirectory() as tempdir:
+            outfn = os.path.join(tempdir, 'tweets.20150430-223406.tweet.csv')
 
-    def testUserMetadata(self):
-        outf = os.path.join(guess_path("twitter-files"), 'tweets.20150430-223406.user.csv')
-        json2csv(self.inpf, outf,
-                 ['id', 'text', {'user' : ['id', 'followers_count', 'friends_count']}])
+            json2csv(self.infile, outfn, fields)
 
-        assert filecmp.cmp(outf,
-                       'tweets.20150430-223406.user.csv.ref'), 'Error in csv file'
-                       
-    def testTweetHashtag(self):
-        outf = os.path.join(guess_path("twitter-files"), 'tweets.20150430-223406.hashtag.csv')
-        json2csv_entities(self.inpf, outf,
-                          ['id', 'text'], 'hashtags', ['text'])
-                 
-        assert filecmp.cmp(outf,
-                       'tweets.20150430-223406.hashtag.csv.ref'), 'Error in csv file'
-                 
-    def testTweetUserMention(self):
-        outf = os.path.join(guess_path("twitter-files"), 'tweets.20150430-223406.usermention.csv')
-        json2csv_entities(self.inpf, outf,
-                          ['id', 'text'], 'user_mentions', ['id', 'screen_name'])
-                 
-        assert filecmp.cmp(outf,
-                       'tweets.20150430-223406.usermention.csv.ref'), 'Error in csv file'
-                       
-    def testTweetMedia(self):
-        outf = os.path.join(guess_path("twitter-files"), 'tweets.20150430-223406.media.csv')
-        json2csv_entities(self.inpf, outf,
-                          ['id'], 'media', ['media_url', 'url'])
-                 
-        assert filecmp.cmp(outf,
-                       'tweets.20150430-223406.media.csv.ref'), 'Error in csv file'
-                       
-    def testTweetUrl(self):
-        outf = os.path.join(guess_path("twitter-files"), 'tweets.20150430-223406.url.csv')
-        json2csv_entities(self.inpf, outf,
-                          ['id'], 'urls', ['url', 'expanded_url'])
-                 
-        assert filecmp.cmp(outf,
-                       'tweets.20150430-223406.url.csv.ref'), 'Error in csv file'
+            assert filecmp.cmp(outfn, 'tweets.20150430-223406.tweet.csv.ref'),\
+                   'Error in csv file'
 
-    def testUserUrl(self):
-        outf = os.path.join(guess_path("twitter-files"), 'tweets.20150430-223406.userurl.csv')
-        json2csv_entities(self.inpf, outf,
-                          ['id', 'screen_name'], {'user' : 'urls'}, ['url', 'expanded_url'])
-                 
-        assert filecmp.cmp(outf,
-                       'tweets.20150430-223406.userurl.csv.ref'), 'Error in csv file'
+    def test_user_metadata(self):
+        fields = ['id', 'text', {'user' : ['id', 'followers_count', 'friends_count']}]
+        with TemporaryDirectory() as tempdir:
+            outfn = os.path.join(tempdir, 'tweets.20150430-223406.user.csv')
+            json2csv(self.infile, outfn, fields)
 
-    def testTweetPlace(self):
-        outf = os.path.join(guess_path("twitter-files"), 'tweets.20150430-223406.place.csv')
-        json2csv_entities(self.inpf, outf,
-                          ['id', 'text'], 'place', ['name', 'country'])
-                 
-        assert filecmp.cmp(outf,
-                       'tweets.20150430-223406.place.csv.ref'), 'Error in csv file'
+            assert filecmp.cmp(outfn, 'tweets.20150430-223406.user.csv.ref'),\
+                   'Error in csv file'
 
-    def testTweetPlaceBoundingBox(self):
-        outf = os.path.join(guess_path("twitter-files"), 'tweets.20150430-223406.placeboundingbox.csv')
-        json2csv_entities(self.inpf, outf,
-                          ['id', 'name'], {'place' : 'bounding_box'}, ['coordinates'])
-                 
-        assert filecmp.cmp(outf,
-                       'tweets.20150430-223406.placeboundingbox.csv.ref'), 'Error in csv file'
+    def test_tweet_hashtag(self):
+        with TemporaryDirectory() as tempdir:
+            outfn = os.path.join(tempdir,
+                                'tweets.20150430-223406.hashtag.csv')
 
-    def testRetweetOriginalTweet(self):
-        outf = os.path.join(guess_path("twitter-files"), 'tweets.20150430-223406.retweet.csv')
-        json2csv_entities(self.inpf, outf,
-                          ['id'], 'retweeted_status', ['created_at', 'favorite_count', 
-                          'id', 'in_reply_to_status_id', 'in_reply_to_user_id', 'retweet_count',
-                          'text', 'truncated', {'user' : {'id'}}])
-                 
-        assert filecmp.cmp(outf,
-                       'tweets.20150430-223406.retweet.csv.ref'), 'Error in csv file'
+            json2csv_entities(self.infile, outfn,
+                              ['id', 'text'], 'hashtags', ['text'])
+
+            assert filecmp.cmp(outfn, 'tweets.20150430-223406.hashtag.csv.ref'),\
+                   'Error in csv file'
+
+    def test_tweet_usermention(self):
+        with TemporaryDirectory() as tempdir:
+            outfn = os.path.join(tempdir,
+                                'tweets.20150430-223406.usermention.csv')
+
+            json2csv_entities(self.infile, outfn,
+                              ['id', 'text'], 'user_mentions', ['id', 'screen_name'])
+
+            assert filecmp.cmp(outfn,
+                           'tweets.20150430-223406.usermention.csv.ref'), 'Error in csv file'
+
+    def test_tweet_media(self):
+        with TemporaryDirectory() as tempdir:
+            outfn = os.path.join(tempdir,
+                                'tweets.20150430-223406.media.csv')
+
+            json2csv_entities(self.infile, outfn,
+                              ['id'], 'media', ['media_url', 'url'])
+
+            assert filecmp.cmp(outfn, 'tweets.20150430-223406.media.csv.ref'),\
+                   'Error in csv file'
+
+    def test_tweet_url(self):
+        with TemporaryDirectory() as tempdir:
+            outfn = os.path.join(tempdir,
+                                'tweets.20150430-223406.url.csv')
+
+            json2csv_entities(self.infile, outfn,
+                              ['id'], 'urls', ['url', 'expanded_url'])
+
+            assert filecmp.cmp(outfn, 'tweets.20150430-223406.url.csv.ref'),\
+                   'Error in csv file'
+
+    def test_userurl(self):
+        with TemporaryDirectory() as tempdir:
+            outfn = os.path.join(tempdir,
+                                'tweets.20150430-223406.userurl.csv')
+
+            json2csv_entities(self.infile, outfn, ['id', 'screen_name'],
+                              {'user' : 'urls'}, ['url', 'expanded_url'])
+
+            assert filecmp.cmp(outfn, 'tweets.20150430-223406.userurl.csv.ref'),\
+                   'Error in csv file'
+
+    def test_tweet_place(self):
+        with TemporaryDirectory() as tempdir:
+            outfn = os.path.join(tempdir,
+                                'tweets.20150430-223406.place.csv')
+            json2csv_entities(self.infile, outfn,
+                              ['id', 'text'], 'place', ['name', 'country'])
+
+            assert filecmp.cmp(outfn, 'tweets.20150430-223406.place.csv.ref'),\
+                   'Error in csv file'
+
+    def test_tweet_place_boundingbox(self):
+        with TemporaryDirectory() as tempdir:
+            outfn = os.path.join(tempdir,
+                                'tweets.20150430-223406.placeboundingbox.csv')
+
+            json2csv_entities(self.infile, outfn,
+                              ['id', 'name'], {'place' : 'bounding_box'}, ['coordinates'])
+
+            assert filecmp.cmp(outfn, 'tweets.20150430-223406.placeboundingbox.csv.ref'),\
+               'Error in csv file'
+
+    def test_retweet_original_tweet(self):
+        with TemporaryDirectory() as tempdir:
+            outfn = os.path.join(tempdir, 'tweets.20150430-223406.retweet.csv')
+
+            json2csv_entities(self.infile, outfn, ['id'], 'retweeted_status',
+                              ['created_at', 'favorite_count', 'id', 'in_reply_to_status_id',
+                               'in_reply_to_user_id', 'retweet_count', 'text', 'truncated',
+                               {'user' : {'id'}}])
+
+            assert filecmp.cmp(outfn, 'tweets.20150430-223406.retweet.csv.ref'),\
+                   'Error in csv file'
 
 if __name__ == "__main__":
     unittest.main()
