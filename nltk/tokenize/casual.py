aa55e269812b7fe43ae75929fa01abc308e8b225
Ewan Klein <ewan.klein@gmail.com>
2015-07-14 10:35:30 +0100

    PEP8 fixes, plus tweaks to docstrings.

--- nltk/tokenize/casual.py
+++ nltk/tokenize/casual.py
@@ -5,6 +5,7 @@
 # Copyright (C) 2001-2015 NLTK Project
 # Author: Christopher Potts <cgpotts@stanford.edu>
 #         Ewan Klein <ewan@inf.ed.ac.uk> (modifications)
+#         Pierpaolo Pantone <> (modifications)
 # URL: <http://nltk.org/>
 # For license information, see LICENSE.TXT
 #
@@ -112,7 +113,8 @@ URLS = r"""			# Capture 1: entire matched URL
     (?:[a-z]{2,13})
     \b
     /?
-    (?!@)			        # not succeeded by a @, avoid matching "foo.na" in "foo.na@example.com"
+    (?!@)			        # not succeeded by a @,
+                            # avoid matching "foo.na" in "foo.na@example.com"
   )
 """
 
@@ -177,7 +179,7 @@ WORD_RE = re.compile(r"""(%s)""" % "|".join(REGEXPS), re.VERBOSE | re.I
 EMOTICON_RE = re.compile(EMOTICONS, re.VERBOSE | re.I | re.UNICODE)
 
 # These are for regularizing HTML entities to Unicode:
-_ent_re = re.compile(r'&(#?(x?))([^&;\s]+);')
+ENT_RE = re.compile(r'&(#?(x?))([^&;\s]+);')
 
 
 ######################################################################
@@ -192,25 +194,38 @@ def _str_to_unicode(text, encoding=None, errors='strict'):
     return text
 
 def _replace_html_entities(text, keep=(), remove_illegal=True, encoding='utf-8'):
-    u"""Remove entities from the given `text` by converting them to their
+    """
+    Remove entities from text by converting them to their
     corresponding unicode character.
-    `text` can be a unicode string or a byte string encoded in the given
+
+    :param text: a unicode string or a byte string encoded in the given
     `encoding` (which defaults to 'utf-8').
-    If `keep` is passed (with a list of entity names) those entities will
-    be kept (they won't be removed).
-    It supports both numeric entities (``&#nnnn;`` and ``&#hhhh;``)
+
+    :param list keep:  list of entity names which should not be replaced.\
+    This supports both numeric entities (``&#nnnn;`` and ``&#hhhh;``)
     and named entities (such as ``&nbsp;`` or ``&gt;``).
-    If `remove_illegal` is ``True``, entities that can't be converted are removed.
-    If `remove_illegal` is ``False``, entities that can't be converted are kept "as
-    is". For more information see the tests.
-    Always returns a unicode string (with the entities removed).
+
+    :param bool remove_illegal: If `True`, entities that can't be converted are\
+    removed. Otherwise, entities that can't be converted are kept "as
+    is".
+
+    :returns: A unicode string with the entities removed.
+
+    See https://github.com/scrapy/w3lib/blob/master/w3lib/html.py
+
+        >>> from nltk.tokenize.casual import _replace_html_entities
+        >>> _replace_html_entities(b'Price: &pound;100')
+        'Price: \\xa3100'
+        >>> print(_replace_html_entities(b'Price: &pound;100'))
+        Price: £100
+        >>>
     """
 
-    def convert_entity(m):
-        entity_body = m.group(3)
-        if m.group(1):
+    def _convert_entity(match):
+        entity_body = match.group(3)
+        if match.group(1):
             try:
-                if m.group(2):
+                if match.group(2):
                     number = int(entity_body, 16)
                 else:
                     number = int(entity_body, 10)
@@ -224,7 +239,7 @@ def _replace_html_entities(text, keep=(), remove_illegal=True, encoding='utf-8')
                 number = None
         else:
             if entity_body in keep:
-                return m.group(0)
+                return match.group(0)
             else:
                 number = htmlentitydefs.name2codepoint.get(entity_body)
         if number is not None:
@@ -233,9 +248,9 @@ def _replace_html_entities(text, keep=(), remove_illegal=True, encoding='utf-8')
             except ValueError:
                 pass
 
-        return u'' if remove_illegal else m.group(0)
+        return u'' if remove_illegal else match.group(0)
 
-    return _ent_re.sub(convert_entity, _str_to_unicode(text, encoding))
+    return ENT_RE.sub(_convert_entity, _str_to_unicode(text, encoding))
 
 
 ######################################################################
@@ -244,37 +259,39 @@ class TweetTokenizer:
     r"""
     Tokenizer for tweets.
 
-    >>> from nltk.tokenize import TweetTokenizer
-    >>> s0 = "This is a cooool #dummysmiley: :-) :-P <3 and some arrows < > -> <--"
-    >>> TweetTokenizer().tokenize(s0)
-    ['This', 'is', 'a', 'cooool', '#dummysmiley', ':', ':-)', ':-P', '<3', 'and', 'some', 'arrows', '<', '>', '->', '<--']
-    >>> s1 = "Naps are a must \ud83d\ude34\ud83d\ude34"
-    >>> TweetTokenizer().tokenize(s1)
-    ['Naps', 'are', 'a', 'must', '\ud83d', '\ude34', '\ud83d', '\ude34']
-    >>> s2 = "Renato fica com muito medo de ouvir meus \u00e1udios perto da gaja dele, pois s\u00f3 falo merda KKK"
-    >>> TweetTokenizer().tokenize(s2)
-    ['Renato', 'fica', 'com', 'muito', 'medo', 'de', 'ouvir', 'meus', 'áudios', 'perto', 'da', 'gaja', 'dele', ',', 'pois', 'só', 'falo', 'merda', 'KKK']
-    >>> s3 = "\u0412\u043b\u0430\u0434\u0435\u043b\u0435\u0446 20th Century Fox \u043d\u0430\u043c\u0435\u0440\u0435\u043d \u043a\u0443\u043f\u0438\u0442\u044c Warner Bros."
-    >>> TweetTokenizer().tokenize(s3)
-    ['Владелец', '20th', 'Century', 'Fox', 'намерен', 'купить', 'Warner', 'Bros', '.']
-    >>> s4 = "RT @facugambande: Ya por arrancar a grabar !!! #TirenTirenTiren vamoo !!"
-    >>> TweetTokenizer().tokenize(s4)
-    ['RT', '@facugambande', ':', 'Ya', 'por', 'arrancar', 'a', 'grabar', '!', '!', '!', '#TirenTirenTiren', 'vamoo', '!', '!']
-    >>> s5 = "http://t.co/7r8d5bVKyA http://t.co/hZpwZe1uKt http://t.co/ZKb7GKWocy Ничто так не сближает людей"
-    >>> TweetTokenizer().tokenize(s5)
-    ['http://t.co/7r8d5bVKyA', 'http://t.co/hZpwZe1uKt', 'http://t.co/ZKb7GKWocy', 'Ничто', 'так', 'не', 'сближает', 'людей']
+        >>> from nltk.tokenize import TweetTokenizer
+        >>> tknzr = TweetTokenizer()
+        >>> s0 = "This is a cooool #dummysmiley: :-) :-P <3 and some arrows < > -> <--"
+        >>> tknzr.tokenize(s0)
+        ['This', 'is', 'a', 'cooool', '#dummysmiley', ':', ':-)', ':-P', '<3', 'and', 'some', 'arrows', '<', '>', '->', '<--']
+        >>> s1 = "Naps are a must \ud83d\ude34\ud83d\ude34"
+        >>> tknzr.tokenize(s1)
+        ['Naps', 'are', 'a', 'must', '\ud83d', '\ude34', '\ud83d', '\ude34']
+        >>> s2 = "Renato fica com muito medo de ouvir meus \u00e1udios perto da gaja dele, pois s\u00f3 falo merda KKK"
+        >>> tknzr.tokenize(s2)
+        ['Renato', 'fica', 'com', 'muito', 'medo', 'de', 'ouvir', 'meus', 'áudios', 'perto', 'da', 'gaja', 'dele', ',', 'pois', 'só', 'falo', 'merda', 'KKK']
+        >>> s3 = "\u0412\u043b\u0430\u0434\u0435\u043b\u0435\u0446 20th Century Fox \u043d\u0430\u043c\u0435\u0440\u0435\u043d \u043a\u0443\u043f\u0438\u0442\u044c Warner Bros."
+        >>> tknzr.tokenize(s3)
+        ['Владелец', '20th', 'Century', 'Fox', 'намерен', 'купить', 'Warner', 'Bros', '.']
+        >>> s4 = "RT @facugambande: Ya por arrancar a grabar !!! #TirenTirenTiren vamoo !!"
+        >>> tknzr.tokenize(s4)
+        ['RT', '@facugambande', ':', 'Ya', 'por', 'arrancar', 'a', 'grabar', '!', '!', '!', '#TirenTirenTiren', 'vamoo', '!', '!']
+        >>> s5 = "http://t.co/7r8d5bVKyA http://t.co/hZpwZe1uKt http://t.co/ZKb7GKWocy Ничто так не сближает людей"
+        >>> tknzr.tokenize(s5)
+        ['http://t.co/7r8d5bVKyA', 'http://t.co/hZpwZe1uKt', 'http://t.co/ZKb7GKWocy', 'Ничто', 'так', 'не', 'сближает', 'людей']
 
-    Examples using strip_handles and reduce_len parameters:
-    >>> tokenizer = TweetTokenizer(strip_handles=True, reduce_len=True)
-    >>> s6 = '@remy: This is waaaaayyyy too much for you!!!!!!'
-    >>> tokenizer.tokenize(s6)
-    [':', 'This', 'is', 'waaayyy', 'too', 'much', 'for', 'you', '!', '!', '!']
-    >>> s7 = '@_willy65: No place for @chuck tonight. Sorry.'
-    >>> tokenizer.tokenize(s7)
-    [':', 'No', 'place', 'for', 'tonight', '.', 'Sorry', '.']
-    >>> s8 = '@mar_tin is a great developer. Contact him at mar_tin@email.com'
-    >>> tokenizer.tokenize(s8)
-    ['is', 'a', 'great', 'developer', '.', 'Contact', 'him', 'at', 'mar_tin', '@email', '.', 'com']
+    Examples using `strip_handles` and `reduce_len parameters`:
+
+        >>> tknzr = TweetTokenizer(strip_handles=True, reduce_len=True)
+        >>> s6 = '@remy: This is waaaaayyyy too much for you!!!!!!'
+        >>> tknzr.tokenize(s6)
+        [':', 'This', 'is', 'waaayyy', 'too', 'much', 'for', 'you', '!', '!', '!']
+        >>> s7 = '@_willy65: No place for @chuck tonight. Sorry.'
+        >>> tknzr.tokenize(s7)
+        [':', 'No', 'place', 'for', 'tonight', '.', 'Sorry', '.']
+        >>> s8 = '@mar_tin is a great developer. Contact him at mar_tin@email.com'
+        >>> tknzr.tokenize(s8)
+        ['is', 'a', 'great', 'developer', '.', 'Contact', 'him', 'at', 'mar_tin', '@email', '.', 'com']
     """
 
     def __init__(self, preserve_case=True, reduce_len=False, strip_handles=False):
@@ -286,17 +303,17 @@ class TweetTokenizer:
         """
         :param text: str
         :rtype: list(str)
-        :return: a tokenized list of strings; concatenating this list returns
-        the original string if preserve_case=False
+        :return: a tokenized list of strings; concatenating this list returns\
+        the original string if `preserve_case=False`
         """
         # Fix HTML character entities:
         text = _replace_html_entities(text)
         # Remove username handles
         if self.strip_handles:
-          text = remove_handles(text)
+            text = remove_handles(text)
         # Normalize word lengthening
         if self.reduce_len:
-          text = reduce_lengthening(text)
+            text = reduce_lengthening(text)
         # Tokenize:
         words = WORD_RE.findall(text)
         # Possibly alter the case, but avoid changing emoticons like :D into :d:
@@ -310,17 +327,17 @@ class TweetTokenizer:
 ######################################################################
 
 def reduce_lengthening(text):
-    '''
+    """
     Replace repeated character sequences of length 3 or greater with sequences
     of length 3.
-    '''
+    """
     pattern = re.compile(r"(.)\1{2,}")
     return pattern.sub(r"\1\1\1", text)
 
 def remove_handles(text):
-    '''
+    """
     Remove Twitter username handles from text.
-    '''
+    """
     pattern = re.compile(r"(^|(?<=[^\w.-]))@[A-Za-z_]+\w+")
     return pattern.sub('', text)
 
@@ -333,7 +350,7 @@ def casual_tokenize(text, preserve_case=True, reduce_len=False, strip_handles=Fa
     Convenience function for wrapping the tokenizer.
     """
     return TweetTokenizer(preserve_case=preserve_case, reduce_len=reduce_len,
-      strip_handles=strip_handles).tokenize(text)
+                          strip_handles=strip_handles).tokenize(text)
 
 ###############################################################################
 
