bc02af159739f63b7b599b4ca9a06e20e51e0984
Pierpaolo Pantone <24alsecondo@gmail.com>
2015-07-20 13:12:27 +0200

    implement ProsConsCorpusReader for Pros and Cons dataset

---
+++ nltk/corpus/reader/pros_cons.py
@@ -0,0 +1,102 @@
+# Natural Language Toolkit: Product Reviews Corpus Reader
+#
+# Copyright (C) 2001-2015 NLTK Project
+# Author: Pierpaolo Pantone <24alsecondo@gmail.com>
+# URL: <http://nltk.org/>
+# For license information, see LICENSE.TXT
+
+"""
+CorpusReader for the Pros and Cons dataset.
+
+- Pros and Cons dataset information -
+
+Contact: Bing Liu, liub@cs.uic.edu
+        http://www.cs.uic.edu/~liub
+
+Distributed with permission.
+
+Related papers:
+
+- Murthy Ganapathibhotla and Bing Liu. "Mining Opinions in Comparative Sentences".
+    Proceedings of the 22nd International Conference on Computational Linguistics
+    (Coling-2008), Manchester, 18-22 August, 2008.
+
+- Bing Liu, Minqing Hu and Junsheng Cheng. "Opinion Observer: Analyzing and Comparing
+    Opinions on the Web". Proceedings of the 14th international World Wide Web
+    conference (WWW-2005), May 10-14, 2005, in Chiba, Japan.
+"""
+import re
+
+from nltk.compat import string_types
+from nltk.corpus.reader.api import *
+from nltk.tokenize import *
+
+
+class ProsConsCorpusReader(CategorizedCorpusReader, CorpusReader):
+    """
+    Reader for the Pros and Cons sentence dataset.
+    """
+    CorpusView = StreamBackedCorpusView
+
+    def __init__(self, root, fileids,
+                 word_tokenizer=WordPunctTokenizer(),
+                 encoding='utf8', *args, **kwargs):
+
+        CorpusReader.__init__(self, root, fileids, encoding)
+        CategorizedCorpusReader.__init__(self, kwargs)
+        self._word_tokenizer = word_tokenizer
+
+    def _resolve(self, fileids, categories):
+        if fileids is not None and categories is not None:
+            raise ValueError('Specify fileids or categories, not both')
+        if categories is not None:
+            return self.fileids(categories)
+        else:
+            return fileids
+
+    def sents(self, fileids=None, categories=None):
+        """
+        :return: the given file(s) as a list of tokenized sentences.
+            Each sentence is tokenized using the specified word_tokenizer.
+        :rtype: list(list(str))
+        """
+        fileids = self._resolve(fileids, categories)
+        if fileids is None: fileids = self._fileids
+        elif isinstance(fileids, compat.string_types): fileids = [fileids]
+        return concat([self.CorpusView(path, self._read_sent_block, encoding=enc)
+            for (path, enc, fileid) in self.abspaths(fileids, True, True)])
+
+    def words(self, fileids=None, categories=None):
+        """
+        :return: the given file(s) as a list of words and punctuation symbols.
+        :rtype: list(str)
+        """
+        fileids = self._resolve(fileids, categories)
+        if fileids is None: fileids = self._fileids
+        elif isinstance(fileids, compat.string_types): fileids = [fileids]
+        return concat([self.CorpusView(path, self._read_word_block, encoding=enc)
+            for (path, enc, fileid) in self.abspaths(fileids, True, True)])
+
+    def _read_sent_block(self, stream):
+        sents = []
+        for i in range(20): # Read 20 lines at a time.
+            line = stream.readline()
+            if not line:
+                continue
+            sent = re.match(r"^(?!\n)\s*<(Pros|Cons)>(.*)</(?:Pros|Cons)>", line)
+            if sent:
+                sents.append(self._word_tokenizer.tokenize(sent.group(2).strip()))
+        return sents
+
+    def _read_word_block(self, stream):
+        import pdb
+        words = []
+        for sent in self._read_sent_block(stream):
+            pdb.set_trace()
+            words.extend(sent)
+
+if __name__ == '__main__':
+    from nltk.corpus.util import LazyCorpusLoader
+    procons = LazyCorpusLoader('pros_cons', ProsConsCorpusReader, r'Integrated(Cons|Pros)\.txt',
+        cat_pattern=r'Integrated(Cons|Pros)\.txt', encoding='ISO-8859-2')
+    procons.sents(categories='Cons')
