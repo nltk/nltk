9875e205b33cea065b3ed19ddd936e75fb714e9f
Pierpaolo Pantone <24alsecondo@gmail.com>
2015-07-25 16:31:59 +0200

    add docstrings to util module

--- nltk/sentiment/util.py
+++ nltk/sentiment/util.py
@@ -1,3 +1,16 @@
+# coding: utf-8
+#
+# Natural Language Toolkit: Sentiment Analyzer
+#
+# Copyright (C) 2001-2015 NLTK Project
+# Author: Pierpaolo Pantone <24alsecondo@gmail.com>
+# URL: <http://nltk.org/>
+# For license information, see LICENSE.TXT
+
+"""
+Utility methods for Sentiment Analysis.
+"""
+
 from copy import deepcopy
 import codecs
 import csv
@@ -23,7 +36,7 @@ from nltk.twitter.util import outf_writer_compat, extract_fields
 #{ Regular expressions
 #////////////////////////////////////////////////////////////
 
-# Regular expression by Christopher Potts
+# Regular expression for negation by Christopher Potts
 NEGATION = r"""
     (?:
         ^(?:never|no|nothing|nowhere|noone|none|not|
@@ -34,10 +47,12 @@ NEGATION = r"""
     |
     n't"""
 
-CLAUSE_PUNCT = r'^[.:;!?]$'
-
 NEGATION_RE = re.compile(NEGATION, re.VERBOSE)
+
+CLAUSE_PUNCT = r'^[.:;!?]$'
 CLAUSE_PUNCT_RE = re.compile(CLAUSE_PUNCT)
+
+# Happy and sad emoticons
 
 HAPPY = {
     ':-)', ':)', ';)', ':o)', ':]', ':3', ':c)', ':>', '=]', '8)', '=)', ':}',
@@ -53,8 +68,10 @@ SAD = {
     ':c', ':{', '>:\\', ';('
     }
 
-# Define @timer decorator
 def timer(method):
+    """
+    A timer decorator to measure execution performance of methods.
+    """
     def timed(*args, **kw):
         start = time.time()
         result = method(*args, **kw)
@@ -75,12 +92,28 @@ def timer(method):
 #{ Feature extractor functions
 #////////////////////////////////////////////////////////////
 """
-Feature extractor functions are declared outside the SentimentAnalyzer class
-because users should have the possibility to create their own feature extractors
+Feature extractor functions are declared outside the SentimentAnalyzer class.
+Users should have the possibility to create their own feature extractors
 without modifying SentimentAnalyzer.
 """
 
 def extract_unigram_feats(document, unigrams, handle_negation=False):
+    """
+    Populate a dictionary of unigram features, reflecting the presence/absence in
+    the document of each of the tokens in `unigrams`.
+
+    :param document: a list of words/tokens.
+    :param unigrams: a list of words/tokens whose presence/absence has to be
+        checked in `document`.
+    :param handle_negation: if `handle_negation == True` apply `mark_negation`
+        method to `document` before checking for unigram presence/absence.
+    :return: a dictionary of unigram features {unigram : boolean}.
+
+    >>> words = ['ice', 'police', 'riot']
+    >>> document = 'ice is melting due to global warming'.split()
+    >>> sorted(extract_unigram_feats(document, words).items())
+    [('contains(ice)', True), ('contains(police)', False), ('contains(riot)', False)]
+    """
     features = {}
     if handle_negation:
         document = mark_negation(document)
@@ -90,8 +123,20 @@ def extract_unigram_feats(document, unigrams, handle_negation=False):
 
 def extract_bigram_feats(document, bigrams):
     """
-    Populate bigram features for the document. This extractor function only considers
-    contiguous bigrams obtained by `nltk.bigrams`.
+    Populate a dictionary of bigram features, reflecting the presence/absence in
+    the document of each of the tokens in `bigrams`. This extractor function only
+    considers contiguous bigrams obtained by `nltk.bigrams`.
+
+    :param document: a list of words/tokens.
+    :param unigrams: a list of bigrams whose presence/absence has to be
+        checked in `document`.
+    :return: a dictionary of bigram features {bigram : boolean}.
+
+    >>> bigrams = [('global', 'warming'), ('police', 'prevented'), ('love', 'you')]
+    >>> document = 'ice is melting due to global warming'.split()
+    >>> sorted(extract_bigram_feats(document, bigrams).items())
+    [('contains(global - warming)', True), ('contains(love - you)', False),
+    ('contains(police - prevented)', False)]
     """
     features = {}
     for bigr in bigrams:
@@ -104,15 +149,20 @@ def extract_bigram_feats(document, bigrams):
 
 def mark_negation(document, double_neg_flip=False, shallow=False):
     '''
-    Append a specific suffix to words that appear in the scope between a negation
+    Append _NEG suffix to words that appear in the scope between a negation
     and a punctuation mark.
+
+    :param document: a list of words/tokens, or a tuple (words, label).
     :param shallow: if True, the method will modify the original document in place.
-    :param double_neg_flip: if True, double negation is considered affirmation (we
-        activate/deactivate negation scope everytime we find a negation).
+    :param double_neg_flip: if True, double negation is considered affirmation
+        (we activate/deactivate negation scope everytime we find a negation).
+    :return: if `shallow == True` the method will modify the original document
+        and return it. If `shallow == False` the method will return a modified
+        document, leaving the original unmodified.
     '''
     if not shallow:
         document = deepcopy(document)
-    # check if the document is labeled. If so, only consider the unlabeled document
+    # check if the document is labeled. If so, do not consider the label.
     labeled = document and isinstance(document[0], (tuple, list))
     if labeled:
         doc = document[0]
@@ -134,6 +184,9 @@ def mark_negation(document, double_neg_flip=False, shallow=False):
     return document
 
 def output_markdown(filename, **kwargs):
+    """
+    Write the output of an analysis to a file.
+    """
     with codecs.open(filename, 'at') as outfile:
         text = '\n*** \n\n'
         text += '{} \n\n'.format(time.strftime("%d/%m/%Y, %H:%M"))
@@ -142,13 +195,24 @@ def output_markdown(filename, **kwargs):
         outfile.write(text)
 
 def save_file(content, filename):
+    """
+    Store `content` in `filename`. Can be used to store a SentimentAnalyzer.
+    """
     print("Saving", filename)
     with codecs.open(filename, 'wb') as storage_file:
-        # pickle.dump(content, storage_file) # This will break on python2.x
-        pickle.dump(content, storage_file, protocol=2) # protocol = 2 is for python2 compatibility
+        # The protocol=2 parameter is for python2 compatibility
+        pickle.dump(content, storage_file, protocol=2)
 
 def split_train_test(all_instances, n=None):
-    # Randomly split n instances of the dataset into train and test sets
+    """
+    Randomly split `n` instances of the dataset into train and test sets.
+
+    :param all_instances: a list of instances (e.g. documents) that will be split.
+    :param n: the number of instances to consider (in case we want to use only a
+        subset).
+    :return: two lists of instances. Train set is 8/10 of the total and test set
+        is 2/10 of the total.
+    """
     random.seed(12345)
     random.shuffle(all_instances)
     if not n or n > len(all_instances):
@@ -169,7 +233,7 @@ def _show_plot(x_values, y_values, x_labels=None, y_labels=None):
         plt.xticks(x_values, x_labels, rotation='vertical')
     if y_labels:
         plt.yticks([-1, 0, 1], y_labels, rotation='horizontal')
-    # Pad margins so that markers don't get clipped by the axes
+    # Pad margins so that markers are not clipped by the axes
     plt.margins(0.2)
     plt.show()
 
@@ -184,9 +248,27 @@ def json2csv_preprocess(json_file, outfile, fields, encoding='utf8', errors='rep
     """
     Convert json file to csv file, preprocessing each row to obtain a suitable
     dataset for tweets Semantic Analysis.
-    """
-    with codecs.open(json_file, encoding='utf-8') as fp:
 
+    :param json_file: the original json file containing tweets.
+    :param outfile: the output csv filename.
+    :param fields: a list of fields that will be extracted from the json file and
+        kept in the output csv file.
+    :param encoding: the encoding of the files.
+    :param errors: the error handling strategy for the output writer.
+    :param gzip_compress: if True, create a compressed GZIP file.
+
+    :param skip_retweets: if True, remove retweets.
+    :param skip_tongue_tweets: if True, remove tweets containing ":P" and ":-P"
+        emoticons.
+    :param skip_ambiguous_tweets: if True, remove tweets containing both happy
+        and sad emoticons.
+    :param strip_off_emoticons: if True, strip off emoticons from all tweets.
+    :param remove_duplicates: if True, remove tweets appearing more than once.
+    :param limit: an integer to set the number of tweets to convert. After the
+        limit is reached the conversion will stop. It can be useful to create
+        subsets of the original tweets json data.
+    """
+    with codecs.open(json_file, encoding=encoding) as fp:
         (writer, outf) = outf_writer_compat(outfile, encoding, errors, gzip_compress)
         # write the list of fields as header
         writer.writerow(fields)
@@ -233,13 +315,18 @@ def json2csv_preprocess(json_file, outfile, fields, encoding='utf8', errors='rep
 def parse_tweets_set(filename, label, word_tokenizer, sent_tokenizer=None,
                      skip_header=True):
     '''
-    Parse training file and output train and test sets in (text, label) format.
-    :param tokenizer: the tokenizer method that will be used to tokenize the text
-    E.g. WordPunctTokenizer.tokenize
-         BlanklineTokenizer.tokenize
+    Parse csv file containing tweets and output data a list of (text, label) tuples.
 
-    N.B.: word_tokenize is actually a shortcut that combines PunktSentenceTokenizer
-    and TreebankWordTokenizer().tokenize
+    :param filename: the input csv filename.
+    :param label: the label to be appended to each tweet contained in the csv file.
+    :param word_tokenizer: the tokenizer instance that will be used to tokenize
+        each sentence into tokens (e.g. WordPunctTokenizer() or BlanklineTokenizer())
+    :param sent_tokenizer: the tokenizer that will be used to split each tweet into
+        sentences.
+    :param skip_header: if True, skip the first line of the csv file (which usually
+        contains headers).
+
+    :return: a list of (text, label) tuples.
     '''
     tweets = []
     if not sent_tokenizer:
@@ -250,7 +337,7 @@ def parse_tweets_set(filename, label, word_tokenizer, sent_tokenizer=None,
         with codecs.open(filename, 'rt') as csvfile:
             reader = csv.reader(csvfile)
             if skip_header == True:
-                next(reader, None) # skip the header generated by json2csv
+                next(reader, None) # skip the header
             i = 0
             for tweet_id, text in reader:
                 # text = text[1]
@@ -265,7 +352,7 @@ def parse_tweets_set(filename, label, word_tokenizer, sent_tokenizer=None,
         with codecs.open(filename) as csvfile:
             reader = csv.reader(csvfile)
             if skip_header == True:
-                next(reader, None) # skip the header generated by json2csv
+                next(reader, None) # skip the header
             i = 0
             for row in reader:
                 unicode_row = [x.decode('utf8') for x in row]
@@ -280,6 +367,9 @@ def parse_tweets_set(filename, label, word_tokenizer, sent_tokenizer=None,
     return tweets
 
 def parse_subjectivity_dataset(filename, word_tokenizer, label=None):
+    """
+    Parse the Subjectivity Dataset by Pang and Lee.
+    """
     with codecs.open(filename, 'rb') as inputfile:
         docs = []
         for line in inputfile:
@@ -298,6 +388,8 @@ def demo_tweets(trainer):
     Features are composed of:
         - 1000 most frequent unigrams
         - 100 top bigrams (using BigramAssocMeasures.pmi)
+
+    :param trainer: `train` method of a classifier.
     '''
     from nltk.tokenize import TweetTokenizer
     from sentiment_analyzer import SentimentAnalyzer
@@ -361,11 +453,13 @@ def demo_tweets(trainer):
 
 def demo_movie_reviews(trainer):
     '''
-    Train Naive Bayes classifier on all instances of the Movie Reviews dataset.
+    Train classifier on all instances of the Movie Reviews dataset.
     The corpus has been preprocessed using the default sentence tokenizer and
     WordPunctTokenizer.
     Features are composed of:
         - 1000 most frequent unigrams
+
+    :param trainer: `train` method of a classifier.
     '''
     from nltk.corpus import movie_reviews
     from sentiment_analyzer import SentimentAnalyzer
@@ -405,6 +499,15 @@ def demo_movie_reviews(trainer):
                     Tokenizer='WordPunctTokenizer', Feats=extr, Accuracy=accuracy)
 
 def demo_subjectivity(trainer):
+    """
+    Train and test a classifier on instances of the Subjective Dataset by Pang and
+    Lee. The dataset is made of 5000 subjective and 5000 objective sentences.
+    All tokens (words and punctuation marks) are separated by a whitespace, so
+    we use the basic WhitespaceTokenizer to parse the data.
+
+    :param trainer: `train` method of a classifier.
+    """
+
     from sentiment_analyzer import SentimentAnalyzer
     from nltk.tokenize import regexp
 
@@ -453,20 +556,30 @@ def demo_subjectivity(trainer):
 
 def demo_sent_subjectivity(text):
     """
-    Classify a single sentence using a stored SentimentAnalyzer
+    Classify a single sentence as subjective or objective using a stored
+    SentimentAnalyzer.
+
+    :param text: a sentence whose subjectivity has to be classified.
     """
     from nltk.tokenize import regexp
     word_tokenizer = regexp.WhitespaceTokenizer()
     sentim_analyzer = load('sa_subjectivity.pickle')
 
-    # tokenized_text = word_tokenizer.tokenize(text)
+    # Tokenize and convert to lower case
     tokenized_text = [word.lower() for word in word_tokenizer.tokenize(text)]
+    # Apply the features that have already been stored in the SentimentAnalyzer
     text_feats = sentim_analyzer.apply_features([tokenized_text], labeled=False)
     print(sentim_analyzer.classifier.classify(text_feats[0]))
 
 def demo_liu_hu_lexicon(sentence, plot=False):
     """
-    Very basic example of sentiment classification using Liu and Hu opinion lexicon
+    Basic example of sentiment classification using Liu and Hu opinion lexicon.
+    This function simply counts the number of positive, negative and neutral words
+    in the sentence and classifies it depending on which polarity is more represented.
+    Words that do not appear in the lexicon are considered as neutral.
+
+    :param sentence: a sentence whose polarity has to be classified.
+    :param plot: if True, plot a visual representation of the sentence polarity.
     """
     from nltk.corpus import LazyCorpusLoader
     from nltk.corpus import OpinionLexiconCorpusReader
@@ -507,6 +620,11 @@ def demo_liu_hu_lexicon(sentence, plot=False):
             print("matplotlib not installed. Graph generation not available.")
 
 def demo_vader(text):
+    """
+    Output polarity scores for a text using Vader approach.
+
+    :param text: a text whose polarity has to be evaluated.
+    """
     from vader import SentimentIntensityAnalyzer
     sia = SentimentIntensityAnalyzer()
     print(sia.polarity_scores(text))
@@ -521,9 +639,9 @@ if __name__ == '__main__':
     svm = SklearnClassifier(LinearSVC()).train
     maxent = MaxentClassifier.train
 
-    demo_tweets(naive_bayes)
+    # demo_tweets(naive_bayes)
     # demo_movie_reviews(svm)
     # demo_subjectivity(svm)
     # demo_sent_subjectivity("she's an artist , but hasn't picked up a brush in a year . ")
     # demo_liu_hu_lexicon("This movie was actually neither that funny, nor super witty.", plot=True)
-    # demo_vader("This movie was actually neither that funny, nor super witty.")
+    demo_vader("This movie was actually neither that funny, nor super witty.")
