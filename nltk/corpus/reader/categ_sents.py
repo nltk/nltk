758d6790599fdaad170e0dc17f4deaa5ebccb392
Pierpaolo Pantone <24alsecondo@gmail.com>
2015-07-31 11:58:24 +0200

    add doctests and more docstrings to categ_sents module

--- nltk/corpus/reader/categ_sents.py
+++ nltk/corpus/reader/categ_sents.py
@@ -46,14 +46,49 @@ class CategorizedSentencesCorpusReader(CategorizedCorpusReader, CorpusReader):
     Since many corpora allow rows that contain more than one sentence, it is
     possible to specify a sentence tokenizer to retrieve all sentences instead
     than all rows.
+
+    Examples using the Subjectivity Dataset:
+
+    >>> from nltk.corpus.util import LazyCorpusLoader
+    >>> subj_corpus = LazyCorpusLoader('rotten_imdb', CategorizedSentencesCorpusReader,
+            r'(quote.tok.gt9_subj|plot.tok.gt9_obj)\.5000',
+            cat_pattern=r'.*_(subj|obj)\.5000', encoding='latin-1')
+    >>> subj_corpus.sents()[23]
+    ['television', 'made', 'him', 'famous', ',', 'but', 'his', 'biggest', 'hits',
+    'happened', 'off', 'screen', '.']
+    >>> subj.categories()
+    ['obj', 'subj']
+    >>> subj_corpus.words(categories='subj')
+    ['smart', 'and', 'alert', ',', 'thirteen', ...]
+
+    Examples using the Sentence Polarity Dataset:
+
+    >>> polar_corpus = LazyCorpusLoader('rt-polaritydata', CategorizedSentencesCorpusReader,
+            r'rt-polarity\.(neg|pos)', cat_pattern=r'rt-polarity\.(neg|pos)',
+            encoding='utf-8')
+    >>> polar_corpus.sents()
+    [['simplistic', ',', 'silly', 'and', 'tedious', '.'], ["it's", 'so', 'laddish',
+    'and', 'juvenile', ',', 'only', 'teenage', 'boys', 'could', 'possibly', 'find',
+    'it', 'funny', '.'], ...]
+    >>> polar_corpus.categories()
+    ['neg', 'pos']
+    >>> polar_corpus.words(categories='pos')
+    ['the', 'rock', 'is', 'destined', 'to', 'be', 'the', ...]
     """
 
     CorpusView = StreamBackedCorpusView
 
-    def __init__(self, root, fileids,
-                 word_tokenizer=WhitespaceTokenizer(),
-                 sent_tokenizer=None,
-                 encoding='utf8', *args, **kwargs):
+    def __init__(self, root, fileids, word_tokenizer=WhitespaceTokenizer(),
+                 sent_tokenizer=None, encoding='utf8', **kwargs):
+        """
+        :param root: The root directory for the corpus.
+        :param fileids: a list or regexp specifying the fileids in the corpus.
+        :param word_tokenizer: a tokenizer for breaking sentences or paragraphs
+            into words. Default: `WhitespaceTokenizer`
+        :param sent_tokenizer: a tokenizer for breaking paragraphs into sentences.
+        :param encoding: the encoding that should be used to read the corpus.
+        :param kwargs: additional parameters passed to CategorizedCorpusReader.
+        """
 
         CorpusReader.__init__(self, root, fileids, encoding)
         CategorizedCorpusReader.__init__(self, kwargs)
@@ -88,6 +123,8 @@ class CategorizedSentencesCorpusReader(CategorizedCorpusReader, CorpusReader):
 
     def sents(self, fileids=None, categories=None):
         """
+        Return all sentences in the corpus or in the specified file(s).
+
         :return: the given file(s) as a list of sentences.
             Each sentence is tokenized using the specified word_tokenizer.
         :rtype: list(list(str))
@@ -102,6 +139,9 @@ class CategorizedSentencesCorpusReader(CategorizedCorpusReader, CorpusReader):
 
     def words(self, fileids=None, categories=None):
         """
+        Return all words and punctuation symbols in the corpus or in the specified
+        file(s).
+
         :return: the given file(s) as a list of words and punctuation symbols.
         :rtype: list(str)
         """
@@ -121,7 +161,7 @@ class CategorizedSentencesCorpusReader(CategorizedCorpusReader, CorpusReader):
                 continue
             if self._sent_tokenizer:
                 sents.extend([self._word_tokenizer.tokenize(sent)
-                          for sent in self._sent_tokenizer.tokenize(line)])
+                              for sent in self._sent_tokenizer.tokenize(line)])
             else:
                 sents.append(self._word_tokenizer.tokenize(line))
         return sents
