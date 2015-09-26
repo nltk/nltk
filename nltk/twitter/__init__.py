ead5792006cadbf680914a6f45cc35896809d7f2
Ewan Klein <ewan@raw-text.io>
2015-09-26 16:46:08 +0100

    Refactoring of twitter package utilities to avoid unnecessary dependencies on Twython.

--- nltk/twitter/__init__.py
+++ nltk/twitter/__init__.py
@@ -17,9 +17,12 @@ try:
     import twython
 except ImportError:
     import warnings
-    warnings.warn("nltk.twitter package not loaded "
-                  "(please install twython library).")
+    warnings.warn("The twython library has not been installed. "
+                  "Some functionality from the twitter package will not be available.")
+else:
+    from nltk.twitter.util import Authenticate, credsfromfile
+    from nltk.twitter.twitterclient import Streamer, Query, Twitter,\
+         TweetViewer, TweetWriter
 
-from nltk.twitter.util import Authenticate, credsfromfile, json2csv
-from nltk.twitter.twitterclient import Streamer, Query, Twitter,\
-     TweetViewer, TweetWriter
+
+from nltk.twitter.common import json2csv
