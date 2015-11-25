5c104cc1cf84927ce32d9b2c700e273a03cfa774
David Kamholz <lautgesetz@gmail.com>
2015-11-25 15:12:50 -0800

    make panlex corpus reader partially functional

--- nltk/corpus/reader/panlex.py
+++ nltk/corpus/reader/panlex.py
@@ -9,14 +9,15 @@
 Module docstring from readme
 """
 
+import os
 import sqlite3
 
 from nltk.corpus.reader.util import *
 from nltk.corpus.reader.api import *
 
 class PanLexCorpusReader(CorpusReader):
-    LOOKUP_Q = """
-        SELECT dnx2.mn, dnx2.uq, dnx2.ap, ex2.tt, ex2.lv
+    MEANING_Q = """
+        SELECT dnx2.mn, dnx2.uq, dnx2.ap, dnx2.ui, ex2.tt, ex2.lv
         FROM dnx
         JOIN ex ON (ex.ex = dnx.ex)
         JOIN dnx dnx2 ON (dnx2.mn = dnx.mn)
@@ -26,19 +27,30 @@ class PanLexCorpusReader(CorpusReader):
     """
 
     def __init__(self):
-        _conn = sqlite3.connect('db.sqlite')
-        _c = _conn.cursor()
+        self._conn = sqlite3.connect(os.environ['PANLEX_LITE'])
+        self._c = self._conn.cursor()
 
-        _uid_lv = {}
-        _lv_uid = {}
+        self._uid_lv = {}
+        self._lv_uid = {}
 
-        for row in _c.execute('SELECT uid, lv, lc FROM lv'):
-            _uid_lv[row[0]] = row[1]
-            _lv_uid[row[1]] = row[0]
+        for row in self._c.execute('SELECT uid, lv, lc FROM lv'):
+            self._uid_lv[row[0]] = row[1]
+            self._lv_uid[row[1]] = row[0]
 
-    def language_varieties(self, optional arg for 3-letter language code prefix):
+    def language_varieties(self, lc=None):
         """given "eng" returns ["eng-001", ...]
         """
+
+        lvs = []
+
+        if lc == None:
+            for row in self._c.execute('SELECT uid FROM lv'):
+                lvs.append(row[0])
+        else:
+            for row in self._c.execute('SELECT uid FROM lv WHERE lc = ?', (lc,)):
+                lvs.append(row[0])
+
+        return sorted(lvs)
 
     def meanings(self, expr_tt, expr_uid):
         """
@@ -46,35 +58,50 @@ class PanLexCorpusReader(CorpusReader):
 
         """
 
-        d = {"quality": 3, "expressions": { ... }}
-        Meaning(**d)
+        expr_lv = self._uid_lv[expr_uid]
 
-    lv = uid_lv[lc_to_uid(uid)]
-    concepts = {}
+        mn_info = {}
 
-    for i in c.execute(lookup_q, (tt,lv)):
-        mn = i[0]
-        uid = lv_uid[i[4]]
+        for i in self._c.execute(self.MEANING_Q, (expr_tt, expr_lv)):
+            mn = i[0]
+            uid = self._lv_uid[i[5]]
 
-        if not mn in concepts:
-            concepts[mn] = { 'quality': i[1], 'ap': i[2] }
+            if not mn in mn_info:
+                mn_info[mn] = { 'uq': i[1], 'ap': i[2], 'ui': i[3], 'ex': {} }
 
-        if not uid in concepts[mn]:
-            concepts[mn][uid] = []
+            if not uid in mn_info[mn]['ex']:
+                mn_info[mn]['ex'][uid] = []
 
-        concepts[mn][uid].append(i[3])
+            mn_info[mn]['ex'][uid].append(i[4])
 
-    for i in concepts.keys():
-        print i
-        print repr(concepts[i])
-        print
+        mns = []
 
+        for mn in mn_info:
+            mns.append(Meaning(mn, mn_info[mn]))
 
+        return mns
 
 class Meaning(object):
-    def __init__(self, **kwargs):
-        self = dict.__init__(**kwargs)
+    def __init__(self, mn, attr):
+        self = {
+            'mn': mn,
+            'uq': attr['uq'],
+            'ap': attr['ap'],
+            'ui': attr['ui'],
+            'ex': attr['ex']
+        }
+
+    def id(self):
+        return self['mn']
 
     def quality(self):
-        return self["quality"]
+        return self['uq']
 
+    def source(self):
+        return self['ap']
+
+    def source_group(self):
+        return self['ui']
+
+    def expressions(self):
+        return self['ex']
