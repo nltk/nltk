4fb1221296d4a4f67e4f54263814e827cc9fe3f1
Mike Recachinas <mgr3yp@virginia.edu>
2015-12-07 21:17:50 -0500

    Removed float used with division in draw/table.py

--- nltk/draw/table.py
+++ nltk/draw/table.py
@@ -9,6 +9,8 @@
 Tkinter widgets for displaying multi-column listboxes and tables.
 """
 
+from __future__ import division
+
 import nltk.compat
 import operator
 
@@ -184,12 +186,12 @@ class MultiListbox(Frame):
 
     def _resize_column_motion_cb(self, event):
         lb = self._listboxes[self._resize_column_index]
-        charwidth = lb.winfo_width() / float(lb['width'])
+        charwidth = lb.winfo_width() / lb['width']
 
         x1 = event.x + event.widget.winfo_x()
         x2 = lb.winfo_x() + lb.winfo_width()
 
-        lb['width'] = max(3, lb['width'] + int((x1-x2)/charwidth))
+        lb['width'] = max(3, lb['width'] + (x1-x2) // charwidth)
 
     def _resize_column_buttonrelease_cb(self, event):
         event.widget.unbind('<ButtonRelease-%d>' % event.num)
