3cdb5527eb74b84664363b96f5af34e0ddb916b0
alvations <alvations@gmail.com>
2016-03-14 14:20:01 +0100

    Added new fringe test cases

--- nltk/test/unit/translate/test_bleu.py
+++ nltk/test/unit/translate/test_bleu.py
@@ -30,7 +30,7 @@ class TestBLEU(unittest.TestCase):
         self.assertAlmostEqual(hyp1_unigram_precision, 0.28571428, places=4)
         
         # Testing modified bigram precision.
-        assert(modified_precision(references, hyp1, n=2) == 0.0)
+        assert(float(modified_precision(references, hyp1, n=2)) == 0.0)
         
         
         # Example 2: the "of the" example.
@@ -46,10 +46,10 @@ class TestBLEU(unittest.TestCase):
         
         references = [ref1, ref2, ref3] 
         # Testing modified unigram precision.
-        assert (modified_precision(references, hyp1, n=1) == 1.0)
+        assert (float(modified_precision(references, hyp1, n=1)) == 1.0)
         
         # Testing modified bigram precision.
-        assert(modified_precision(references, hyp1, n=2) == 1.0)
+        assert(float(modified_precision(references, hyp1, n=2)) == 1.0)
         
 
         # Example 3: Proper MT outputs.
@@ -70,7 +70,6 @@ class TestBLEU(unittest.TestCase):
         assert (round(hyp1_unigram_precision, 4) == 0.9444)
         assert (round(hyp2_unigram_precision, 4) == 0.5714)
         
-        
         # Bigram precision
         hyp1_bigram_precision = float(modified_precision(references, hyp1, n=2))
         hyp2_bigram_precision = float(modified_precision(references, hyp2, n=2))
@@ -81,8 +80,58 @@ class TestBLEU(unittest.TestCase):
         assert (round(hyp1_bigram_precision, 4) == 0.5882)
         assert (round(hyp2_bigram_precision, 4) == 0.0769)
         
-    def test_fringe_cases(self):
-        pass
+    def test_zero_matches(self):
+        # Test case where there's 0 matches
+        references = ['The candidate has no alignment to any of the references'.split()]
+        hypothesis = 'John loves Mary'.split()
+        
+        # Test BLEU to nth order of n-grams, where n is len(hypothesis). 
+        for n in range(1,len(hypothesis)):
+            weights = [1.0/n] * n # Uniform weights.
+            assert(sentence_bleu(references, hypothesis, weights) == 0)
+    
+    def test_full_matches(self):    
+        # Test case where there's 100% matches
+        references = ['John loves Mary'.split()]
+        hypothesis = 'John loves Mary'.split()
+    
+        # Test BLEU to nth order of n-grams, where n is len(hypothesis). 
+        for n in range(1,len(hypothesis)):
+            weights = [1.0/n] * n # Uniform weights.
+            assert(sentence_bleu(references, hypothesis, weights) == 1.0)
+    
+    def test_partial_matches_hypothesis_longer_than_reference(self):
+        references = ['John loves Mary'.split()]
+        hypothesis = 'John loves Mary who loves Mike'.split()
+        self.assertAlmostEqual(sentence_bleu(references, hypothesis), 0.4729, places=4)
+            
+
+@unittest.skip("Skipping fringe cases for BLEU.")            
+class TestBLEUFringeCases(unittest.TestCase):
+
+    def test_case_where_n_is_bigger_than_hypothesis_length(self):
+        # Test BLEU to nth order of n-grams, where n > len(hypothesis).
+        # TODO: Currently this test breaks the BLEU implementation (13.03.2016)
+        references = ['John loves Mary'.split()]
+        hypothesis = 'John loves Mary'.split()
+        n = len(hypothesis) + 1 # 
+        weights = [1.0/n] * n # Uniform weights.
+        assert(sentence_bleu(references, hypothesis, weights) == 1.0)
+    
+    def test_empty_hypothesis(self):
+        # Test case where there's hypothesis is empty.
+        # TODO: Currently this test breaks the BLEU implementation (13.03.2016)
+        references = ['The candidate has no alignment to any of the references'.split()]
+        hypothesis = []
+        assert(sentence_bleu(references, hypothesis) == 0)
+        
+    def test_empty_references(self):
+        # Test case where there's reference is empty.
+        # TODO: Currently this test breaks the BLEU implementation (13.03.2016)
+        references = [[]]
+        hypothesis = 'John loves Mary'.split()
+        assert(sentence_bleu(references, hypothesis) == 0)
+        
         
     def test_brevity_penalty(self):
         pass
