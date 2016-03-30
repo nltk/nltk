2d2f12dc54f63d8616d727fbfae29c67fa8693e3
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2016-03-30 18:30:24 +0200

    Updated copyright.

--- nltk/model/__init__.py
+++ nltk/model/__init__.py
@@ -1,8 +1,9 @@
 # Natural Language Toolkit: Language Models
 #
-# Copyright (C) 2001-2014 NLTK Project
-# Author: Steven Bird <stevenbird1@gmail.com>
-# URL: <http://nltk.org/>
+# Copyright (C) 2001-2016 NLTK Project
+# Authors: Steven Bird <stevenbird1@gmail.com>
+#          Ilia Kurenkov <ilia.kurenkov@gmail.com>
+# URL: <http://nltk.org/
 # For license information, see LICENSE.TXT
 
 from nltk.model.ngram import MLENgramModel
