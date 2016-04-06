d3a0112a55e8bbe4df24af00e19d329cac9e9f67
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2016-04-06 22:38:52 +0200

    Added BaseNgramModel, a class that folks can inherit from when creating their models.

---
+++ nltk/model/ngram.py
@@ -0,0 +1,95 @@
+# Natural Language Toolkit: Language Models
+#
+# Copyright (C) 2001-2016 NLTK Project
+# Authors: Steven Bird <stevenbird1@gmail.com>
+#          Daniel Blanchard <dblanchard@ets.org>
+#          Ilia Kurenkov <ilia.kurenkov@gmail.com>
+# URL: <http://nltk.org/>
+# For license information, see LICENSE.TXT
+
+from __future__ import unicode_literals
+
+from math import log
+
+from nltk.model.counter import NgramCounter
+
+from nltk import compat
+
+
+NEG_INF = -1e6
+
+
+@compat.python_2_unicode_compatible
+class BaseNgramModel(object):
+    """An example of how to consume NgramCounter to create a language model.
+
+    This class isn't intended to be used directly, folks should inherit from it
+    when writing their own ngram models.
+    """
+
+    def __init__(self, ngram_counts):
+
+        self.ngram_counts = ngram_counts
+
+        self.ngrams = ngram_counts.ngrams[ngram_counts.order]
+
+        self._normalize = self.ngram_counts.check_against_vocab
+
+    def score(self, word, context):
+        """
+        This is a dummy implementation. Child classes should define their own
+        implementations.
+
+        :param word: the word to get the probability of
+        :type word: str
+        :param context: the context the word is in
+        :type context: Tuple[str]
+        """
+        return 0.5
+
+    def logscore(self, word, context):
+        """
+        Evaluate the log probability of this word in this context.
+
+        This implementation actually works, child classes don't have to
+        redefine it.
+
+        :param word: the word to get the probability of
+        :type word: str
+        :param context: the context the word is in
+        :type context: Tuple[str]
+        """
+        score = self.score(word, context)
+        if score == 0.0:
+            return NEG_INF
+        return log(score, 2)
+
+    def entropy(self, text):
+        """
+        Calculate the approximate cross-entropy of the n-gram model for a
+        given evaluation text.
+        This is the average log probability of each word in the text.
+
+        :param text: words to use for evaluation
+        :type text: Iterable[str]
+        """
+
+        H = 0.0     # entropy is conventionally denoted by "H"
+        normed_text = (self._normalize(word) for word in text)
+        processed_ngrams = 0.0
+        for ngram in self.counter.padded_ngrams(normed_text):
+            context, word = tuple(ngram[:-1]), ngram[-1]
+            H += self.logprob(word, context)
+            processed_ngrams += 1.0
+        return H / processed_ngrams
+
+    def perplexity(self, text):
+        """
+        Calculates the perplexity of the given text.
+        This is simply 2 ** cross-entropy for the text.
+
+        :param text: words to calculate perplexity of
+        :type text: Iterable[str]
+        """
+
+        return pow(2.0, self.entropy(text))
