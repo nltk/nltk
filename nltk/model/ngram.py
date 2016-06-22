f10e623a0210e2eebd3da105cd123a8a833359cf
Ilia Kurenkov <ilia.kurenkov@gmail.com>
2016-06-22 23:36:32 +0200

    Adds Lidstone ngram model class.

--- nltk/model/ngram.py
+++ nltk/model/ngram.py
@@ -105,3 +105,24 @@ class MLENgramModel(BaseNgramModel):
         - context is expected to be something reasonably convertible to a tuple
         """
         return self.ngrams[tuple(context)].freq(word)
+
+
+@compat.python_2_unicode_compatible
+class LidstoneNgramModel(BaseNgramModel):
+    """Provides Lidstone-smoothed scores.
+
+    In addition to initialization arguments from BaseNgramModel also requires
+    a number by which to increase the counts, gamma.
+    """
+
+    def __init__(self, gamma, *args):
+        super(LidstoneNgramModel, self).__init__(*args)
+        self.gamma = gamma
+        # This gets added to the denominator to normalize the effect of gamma
+        self.gamma_norm = len(self.ngram_counter.vocabulary) * gamma
+
+    def score(self, word, context):
+        context_freqdist = self.ngrams[tuple(context)]
+        word_count = context_freqdist[word]
+        ctx_count = context_freqdist.N()
+        return (word_count + self.gamma) / (ctx_count + self.gamma_norm)
