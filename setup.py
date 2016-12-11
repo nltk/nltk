b3f339463907f9b79064db91a08b69b35a244849
Pierpaolo Pantone <24alsecondo@gmail.com>
2016-12-11 18:53:32 +0100

    Remove minimum version from six dependency

--- setup.py
+++ setup.py
@@ -95,7 +95,7 @@ natural language processing.  NLTK requires Python 2.7, or 3.4+.""",
     'Topic :: Text Processing :: Linguistic',
     ],
     package_data = {'nltk': ['test/*.doctest', 'VERSION']},
-    install_requires = ['six>=1.10.0'],
+    install_requires = ['six'],
     extras_require = extras_require,
     packages = find_packages(),
     zip_safe=False, # since normal files will be present too?
