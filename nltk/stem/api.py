cbff3b6ceb8bc85bae6b9783c281d1b6198f1efe
Naoya Kanai <naopon@gmail.com>
2016-12-29 04:00:28 -0800

    Abstract base class style for Stemmer

--- nltk/stem/api.py
+++ nltk/stem/api.py
@@ -7,12 +7,17 @@
 # URL: <http://nltk.org/>
 # For license information, see LICENSE.TXT
 
+from abc import ABCMeta, abstractmethod
+from six import add_metaclass
+
+@add_metaclass(ABCMeta)
 class StemmerI(object):
     """
     A processing interface for removing morphological affixes from
     words.  This process is known as stemming.
 
     """
+    @abstractmethod
     def stem(self, token):
         """
         Strip affixes from the token and return the stem.
@@ -20,6 +25,4 @@ class StemmerI(object):
         :param token: The token that should be stemmed.
         :type token: str
         """
-        raise NotImplementedError()
-
-
+        pass
