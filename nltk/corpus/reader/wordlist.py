243ca7ee2181dc02739cb33757464ab00508d853
alvations <alvations@gmail.com>
2017-01-16 17:52:10 +0800

    added more explicit information about the corpus

--- nltk/corpus/reader/wordlist.py
+++ nltk/corpus/reader/wordlist.py
@@ -111,14 +111,14 @@ class UnicharsCorpusReader(WordListCorpusReader):
 
 class MWAPPDBCorpusReader(WordListCorpusReader):
     """
-    This class is used to read lists of word pairs from the subset of lexical
+    This class is used to read the list of word pairs from the subset of lexical
     pairs of The Paraphrase Database (PPDB) XXXL used in the Monolingual Word
     Alignment (MWA) algorithm described in Sultan et al. (2014a, 2014b, 2015):
      - http://acl2014.org/acl2014/Q14/pdf/Q14-1017
      - http://www.aclweb.org/anthology/S14-2039
      - http://www.aclweb.org/anthology/S15-2027
 
-    The original source of the PPDB corpus can be found on
+    The original source of the full PPDB corpus can be found on
     http://www.cis.upenn.edu/~ccb/ppdb/
 
     :return: a list of tuples of similar lexical terms.
