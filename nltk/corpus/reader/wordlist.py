40e8c0b1eafd1099f3a1e11a1cfa82dcd12a4fbc
alvations <alvations@gmail.com>
2017-01-16 17:24:43 +0800

    added first version of reader for MWA subset of PPDB

--- nltk/corpus/reader/wordlist.py
+++ nltk/corpus/reader/wordlist.py
@@ -42,8 +42,8 @@ class SwadeshCorpusReader(WordListCorpusReader):
 class NonbreakingPrefixesCorpusReader(WordListCorpusReader):
     """
     This is a class to read the nonbreaking prefixes textfiles from the
-    Moses Machine Translation toolkit. These lists are used in the Python port 
-    of the Moses' word tokenizer.  
+    Moses Machine Translation toolkit. These lists are used in the Python port
+    of the Moses' word tokenizer.
     """
     available_langs = {'catalan': 'ca', 'czech': 'cs', 'german': 'de',
                         'greek': 'el', 'english': 'en', 'spanish': 'es',
@@ -54,22 +54,22 @@ class NonbreakingPrefixesCorpusReader(WordListCorpusReader):
                         'slovenian': 'sl', 'swedish': 'sv',  'tamil': 'ta'}
     # Also, add the lang IDs as the keys.
     available_langs.update({v:v for v in available_langs.values()})
-    
+
     def words(self, lang=None, fileids=None, ignore_lines_startswith='#'):
         """
         This module returns a list of nonbreaking prefixes for the specified
         language(s).
-        
+
         >>> from nltk.corpus import nonbreaking_prefixes as nbp
         >>> nbp.words('en')[:10] == [u'A', u'B', u'C', u'D', u'E', u'F', u'G', u'H', u'I', u'J']
         True
         >>> nbp.words('ta')[:5] == [u'\u0b85', u'\u0b86', u'\u0b87', u'\u0b88', u'\u0b89']
         True
-        
+
         :return: a list words for the specified language(s).
         """
         # If *lang* in list of languages available, allocate apt fileid.
-        # Otherwise, the function returns non-breaking prefixes for 
+        # Otherwise, the function returns non-breaking prefixes for
         # all languages when fileids==None.
         if lang in self.available_langs:
             lang = self.available_langs[lang]
@@ -79,21 +79,21 @@ class NonbreakingPrefixesCorpusReader(WordListCorpusReader):
 
 class UnicharsCorpusReader(WordListCorpusReader):
     """
-    This class is used to read lists of characters from the Perl Unicode 
+    This class is used to read lists of characters from the Perl Unicode
     Properties (see http://perldoc.perl.org/perluniprops.html).
-    The files in the perluniprop.zip are extracted using the Unicode::Tussle 
+    The files in the perluniprop.zip are extracted using the Unicode::Tussle
     module from http://search.cpan.org/~bdfoy/Unicode-Tussle-1.11/lib/Unicode/Tussle.pm
     """
     # These are categories similar to the Perl Unicode Properties
-    available_categories = ['Close_Punctuation', 'Currency_Symbol', 
-                            'IsAlnum', 'IsAlpha', 'IsLower', 'IsN', 'IsSc', 
+    available_categories = ['Close_Punctuation', 'Currency_Symbol',
+                            'IsAlnum', 'IsAlpha', 'IsLower', 'IsN', 'IsSc',
                             'IsSo', 'Open_Punctuation']
-    
+
     def chars(self, category=None, fileids=None):
         """
         This module returns a list of characters from  the Perl Unicode Properties.
         They are very useful when porting Perl tokenizers to Python.
-        
+
         >>> from nltk.corpus import perluniprops as pup
         >>> pup.chars('Open_Punctuation')[:5] == [u'(', u'[', u'{', u'\u0f3a', u'\u0f3c']
         True
@@ -101,9 +101,31 @@ class UnicharsCorpusReader(WordListCorpusReader):
         True
         >>> pup.available_categories
         ['Close_Punctuation', 'Currency_Symbol', 'IsAlnum', 'IsAlpha', 'IsLower', 'IsN', 'IsSc', 'IsSo', 'Open_Punctuation']
-        
-        :return: a list of characters given the specific unicode character category 
+
+        :return: a list of characters given the specific unicode character category
         """
         if category in self.available_categories:
             fileids = [category+'.txt']
         return list(self.raw(fileids).strip())
+
+
+class MWAPPDBCorpusReader(WordListCorpusReader):
+    """
+    This class is used to read lists of word pairs from the subset of lexical
+    pairs of The Paraphrase Database (PPDB) XXXL used in the Monolingual Word
+    Alignment (MWA) algorithm described in Sultan et al. (2014a, 2014b, 2015):
+     - http://acl2014.org/acl2014/Q14/pdf/Q14-1017
+     - http://www.aclweb.org/anthology/S14-2039
+     - http://www.aclweb.org/anthology/S15-2027
+
+    The original source of the PPDB corpus can be found on
+    http://www.cis.upenn.edu/~ccb/ppdb/
+
+    :return: a list of tuples of similar lexical terms.
+    """
+    mwa_ppdb_xxxl_file = 'ppdb-1.0-xxxl-lexical.extended.synonyms.uniquepairs'
+    def entries(self, fileids=mwa_ppdb_xxxl_file):
+        """
+        :return: a tuple of synonym word pairs.
+        """
+        return [tuple(line.split('\t')) for line in line_tokenize(self.raw(fileids))]
