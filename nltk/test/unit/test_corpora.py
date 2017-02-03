7430a33ccbc0b91abc5de02e1665c200a9f1d62e
Steven Bird <stevenbird1@gmail.com>
2017-02-04 06:43:51 +1100

    Skipping test for mwa_ppdb

--- nltk/test/unit/test_corpora.py
+++ nltk/test/unit/test_corpora.py
@@ -182,19 +182,19 @@ class TestPTB(unittest.TestCase):
             ['Thirty-three', 'Scotty', 'did', 'not', 'go', 'back']
         )
 
+@unittest.skip("Skipping test for mwa_ppdb.")
+class TestMWAPPDB(unittest.TestCase):
+    def test_fileids(self):
+        self.assertEqual(mwa_ppdb.fileids(),
+            ['ppdb-1.0-xxxl-lexical.extended.synonyms.uniquepairs'])
 
-# class TestMWAPPDB(unittest.TestCase):
-#     def test_fileids(self):
-#         self.assertEqual(mwa_ppdb.fileids(),
-#             ['ppdb-1.0-xxxl-lexical.extended.synonyms.uniquepairs'])
-# 
-#     def test_entries(self):
-#         self.assertEqual(mwa_ppdb.entries()[:10],
-#             [('10/17/01', '17/10/2001'), ('102,70', '102.70'),
-#             ('13,53', '13.53'), ('3.2.5.3.2.1', '3.2.5.3.2.1.'),
-#             ('53,76', '53.76'), ('6.9.5', '6.9.5.'),
-#             ('7.7.6.3', '7.7.6.3.'), ('76,20', '76.20'),
-#             ('79,85', '79.85'), ('93,65', '93.65')] )
+    def test_entries(self):
+        self.assertEqual(mwa_ppdb.entries()[:10],
+            [('10/17/01', '17/10/2001'), ('102,70', '102.70'),
+            ('13,53', '13.53'), ('3.2.5.3.2.1', '3.2.5.3.2.1.'),
+            ('53,76', '53.76'), ('6.9.5', '6.9.5.'),
+            ('7.7.6.3', '7.7.6.3.'), ('76,20', '76.20'),
+            ('79,85', '79.85'), ('93,65', '93.65')] )
 
 # unload corpora
 from nltk.corpus import teardown_module
