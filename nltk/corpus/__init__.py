bfe311e223e89422cfdd0b3a51bfda8ad17104ff
Steven Bird <stevenbird1@gmail.com>
2017-02-03 20:32:16 +1100

    drop mwa_ppdb for now, as the tests still fail, resolves https://github.com/nltk/nltk/issues/1606

--- nltk/corpus/__init__.py
+++ nltk/corpus/__init__.py
@@ -286,8 +286,8 @@ nonbreaking_prefixes = LazyCorpusLoader(
 perluniprops = LazyCorpusLoader(
     'perluniprops', UnicharsCorpusReader, r'(?!README|\.).*', nltk_data_subdir='misc', encoding='utf8')
 
-mwa_ppdb = LazyCorpusLoader(
-    'mwa_ppdb', MWAPPDBCorpusReader, r'(?!README|\.).*', nltk_data_subdir='misc', encoding='utf8')
+# mwa_ppdb = LazyCorpusLoader(
+#     'mwa_ppdb', MWAPPDBCorpusReader, r'(?!README|\.).*', nltk_data_subdir='misc', encoding='utf8')
 
 
 def demo():
