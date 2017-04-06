07fc35d3e7aa719636e9052d2422ea338d544c76
Guthrie Armstrong <guthrie.armstrong@gmail.com>
2017-04-06 17:31:11 -0400

    Reformat nltk/draw/ for PEP 8 compliance

--- nltk/draw/__init__.py
+++ nltk/draw/__init__.py
@@ -7,20 +7,22 @@
 # For license information, see LICENSE.TXT
 
 # Import Tkinter-based modules if Tkinter is installed
-import nltk.compat
+
 try:
     import tkinter
 except ImportError:
     import warnings
+
     warnings.warn("nltk.draw package not loaded "
                   "(please install Tkinter library).")
 else:
     from nltk.draw.cfg import ProductionList, CFGEditor, CFGDemo
     from nltk.draw.tree import (TreeSegmentWidget, tree_to_treesegment,
-                      TreeWidget, TreeView, draw_trees)
+                                TreeWidget, TreeView, draw_trees)
     from nltk.draw.table import Table
 
 from nltk.draw.dispersion import dispersion_plot
+
 
 # skip doctests from this package
 def setup_module(module):
