3fd77700a8f688aa284ad0db30e09c22d93d3d36
alvations <alvations@gmail.com>
2017-05-04 16:43:43 +0800

    use more explicit test cases

--- nltk/tokenize/util.py
+++ nltk/tokenize/util.py
@@ -200,16 +200,24 @@ def align_tokens(tokens, sentence):
 
         >>> from nltk.tokenize import TreebankWordTokenizer
         >>> from nltk.tokenize.util import align_tokens
-        >>> s = '''The plane, bound for St Petersburg, crashed in Egypt's
-        ... Sinai desert just 23 minutes after take-off from Sharm el-Sheikh
-        ... on Saturday.'''
+        >>> s = str("The plane, bound for St Petersburg, crashed in Egypt's "
+        ... "Sinai desert just 23 minutes after take-off from Sharm el-Sheikh "
+        ... "on Saturday.")
+        >>> tokens = TreebankWordTokenizer().tokenize(s)
         >>> expected = [(0, 3), (4, 9), (9, 10), (11, 16), (17, 20), (21, 23),
         ... (24, 34), (34, 35), (36, 43), (44, 46), (47, 52), (52, 54),
         ... (55, 60), (61, 67), (68, 72), (73, 75), (76, 83), (84, 89),
         ... (90, 98), (99, 103), (104, 109), (110, 119), (120, 122),
         ... (123, 131), (131, 132)]
-        >>> expected == list(align_tokens(TreebankWordTokenizer().tokenize(s), s))
+        >>> output = list(align_tokens(tokens, s))
+        # Check that length of tokens and tuples are the same.
+        >>> len(tokens) == len(expected) == len(output)
         True
+        # Check that the output is as expected.
+        >>> expected == list(align_tokens(, s))
+        True
+        # Check that the slices of the string corresponds to the tokens.
+        >>> tokens == [s[start:end] for start, end in output]
 
     :param tokens: The list of strings that are the result of tokenization
     :type tokens: list(str)
