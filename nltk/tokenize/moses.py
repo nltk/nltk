6a039305307d88698bdd4bed3fad69edfda3d0c3
alvas <alvations@gmail.com>
2017-05-20 17:18:58 +0800

    added the update of the token when checking for quotes.

--- nltk/tokenize/moses.py
+++ nltk/tokenize/moses.py
@@ -563,7 +563,7 @@ class MosesDetokenizer(TokenizerI):
                 normalized_quo = token
                 if re.search(r'^[„“”]+$', token):
                     normalized_quo = '"'
-                quote_counts.get(normalized_quo, 0)
+                quote_counts[normalized_quo] = quote_counts.get(normalized_quo, 0)
 
                 if self.lang == 'cs' and token == u"„":
                     quote_counts[normalized_quo] = 0
