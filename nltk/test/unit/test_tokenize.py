85108ad14884530ef6f7b9d5c6f4b79e6a9a9c27
Albert <albertauyeung@gmail.com>
2017-06-12 21:13:24 +0800

    added test for span_tokenize on strings with double quotes

--- nltk/test/unit/test_tokenize.py
+++ nltk/test/unit/test_tokenize.py
@@ -5,7 +5,7 @@ See also nltk/test/tokenize.doctest
 """
 
 from __future__ import unicode_literals
-from nltk.tokenize import TweetTokenizer, StanfordSegmenter
+from nltk.tokenize import TweetTokenizer, StanfordSegmenter, TreebankWordTokenizer
 from nose import SkipTest
 import unittest
 import os
@@ -106,4 +106,44 @@ class TestTokenize(unittest.TestCase):
         test7 = "@abcdefghijklmnopqrstu@abcde @abcdefghijklmnopqrst@abcde @abcdefghijklmnopqrst_@abcde @abcdefghijklmnopqrst5@abcde"
         expected = ['u', '@abcde', '@abcdefghijklmnopqrst', '@abcde', '_', '@abcde', '5', '@abcde']
         result = tokenizer.tokenize(test7)
+        self.assertEqual(result, expected)
+
+    def test_treebank_span_tokenizer(self):
+        """
+        Test TreebankWordTokenizer.span_tokenize function
+        """
+
+        tokenizer = TreebankWordTokenizer()
+
+        # Test case in the docstring
+        test1 = "Good muffins cost $3.88\nin New (York).  Please (buy) me\ntwo of them.\n(Thanks)."
+        expected = [
+            (0, 4), (5, 12), (13, 17), (18, 19), (19, 23),
+            (24, 26), (27, 30), (31, 32), (32, 36), (36, 37), (37, 38),
+            (40, 46), (47, 48), (48, 51), (51, 52), (53, 55), (56, 59),
+            (60, 62), (63, 68), (69, 70), (70, 76), (76, 77), (77, 78)
+        ]
+        result = tokenizer.span_tokenize(test1)
+        self.assertEqual(result, expected)
+
+        # Test case with double quotation
+        test2 = "The DUP is similar to the \"religious right\" in the United States and takes a hardline stance on social issues"
+        expected = [
+            (0, 3), (4, 7), (8, 10), (11, 18), (19, 21), (22, 25), (26, 27),
+            (27, 36), (37, 42), (42, 43), (44, 46), (47, 50), (51, 57), (58, 64),
+            (65, 68), (69, 74), (75, 76), (77, 85), (86, 92), (93, 95), (96, 102),
+            (103, 109)
+        ]
+        result = tokenizer.span_tokenize(test2)
+        self.assertEqual(result, expected)
+
+        # Test case with double qoutation as well as converted quotations
+        test3 = "The DUP is similar to the \"religious right\" in the United States and takes a ``hardline'' stance on social issues"
+        expected = [
+            (0, 3), (4, 7), (8, 10), (11, 18), (19, 21), (22, 25), (26, 27),
+            (27, 36), (37, 42), (42, 43), (44, 46), (47, 50), (51, 57), (58, 64),
+            (65, 68), (69, 74), (75, 76), (77, 79), (79, 87), (87, 89), (90, 96),
+            (97, 99), (100, 106), (107, 113)
+        ]
+        result = tokenizer.span_tokenize(test3)
         self.assertEqual(result, expected)
