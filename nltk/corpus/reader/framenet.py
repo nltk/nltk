72f90053ec825d4b6734ec70944dbf158eaff3da
Nathan Schneider <neatnate+git@gmail.com>
2017-08-27 22:03:26 -0400

    framenet.py: citation; fix typo in method documentation

--- nltk/corpus/reader/framenet.py
+++ nltk/corpus/reader/framenet.py
@@ -1003,6 +1003,10 @@ class FramenetCorpusReader(XMLCorpusReader):
 
 
         msg = """
+Citation: Nathan Schneider and Chuck Wooters (2017), 
+"The NLTK FrameNet API: Designing for Discoverability with a Rich Linguistic Resource". 
+Proceedings of EMNLP: System Demonstrations. https://arxiv.org/abs/1703.07438
+
 Use the following methods to access data in FrameNet.
 Provide a method name to `help()` for more information.
 
@@ -1023,7 +1027,7 @@ fes() to get frame elements (a.k.a. roles) matching a name pattern, optionally c
 LEXICAL UNITS
 =============
 
-lu() to look up a frame by its ID
+lu() to look up an LU by its ID
 lus() to get lexical units matching a name pattern, optionally constrained by frame
 lu_ids_and_names() to get a mapping from LU IDs to names
 
