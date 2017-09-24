68390bdb53576a5c2a6a48b76d6195dc59e381fe
Steven Bird <stevenbird1@gmail.com>
2017-09-24 20:08:02 +0930

    changed naming of lemmatize function and lemmatize flag cf #1775

--- nltk/classify/rte_classify.py
+++ nltk/classify/rte_classify.py
@@ -32,7 +32,7 @@ def ne(token):
         return True
     return False
 
-def _lemmatize(word):
+def lemmatize(word):
     """
     Use morphy from WordNet to find the base form of verbs.
     """
@@ -46,7 +46,7 @@ class RTEFeatureExtractor(object):
     This builds a bag of words for both the text and the hypothesis after
     throwing away some stopwords, then calculates overlap and difference.
     """
-    def __init__(self, rtepair, stop=True, lemmatize=False):
+    def __init__(self, rtepair, stop=True, use_lemmatize=False):
         """
         :param rtepair: a ``RTEPair`` from which features should be extracted
         :param stop: if ``True``, stopwords are thrown away.
@@ -69,9 +69,9 @@ class RTEFeatureExtractor(object):
         self.text_words = set(self.text_tokens)
         self.hyp_words = set(self.hyp_tokens)
 
-        if lemmatize:
-            self.text_words = set(_lemmatize(token) for token in self.text_tokens)
-            self.hyp_words = set(_lemmatize(token) for token in self.hyp_tokens)
+        if use_lemmatize:
+            self.text_words = set(lemmatize(token) for token in self.text_tokens)
+            self.hyp_words = set(lemmatize(token) for token in self.hyp_tokens)
 
         if self.stop:
             self.text_words = self.text_words - self.stopwords
