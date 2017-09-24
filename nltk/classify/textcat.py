6dc39a0680204932b42cc03b246e4efaab3d9fec
MohamedAlFahim <malfahim8@gmail.com>
2017-09-23 21:03:49 -0700

    Improved PEP8 and readability in textcat.py

--- nltk/classify/textcat.py
+++ nltk/classify/textcat.py
@@ -149,8 +149,10 @@ class TextCat(object):
         return distances
 
     def guess_language(self, text):
-        ''' Find the language with the min distance
-            to the text and return its ISO 639-3 code '''
+        """
+        Find the language with the min distance
+        to the text and return its ISO 639-3 code
+        """
         self.last_distances = self.lang_dists(text)
 
         return min(self.last_distances, key=self.last_distances.get)
