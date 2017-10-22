e314135ccf12fab2550afdea2f39ed603a520be1
Björn Mattsson <bamattsson@gmail.com>
2017-10-22 12:27:23 +0100

    Adding more explenations to the doctests

--- nltk/translate/bleu_score.py
+++ nltk/translate/bleu_score.py
@@ -57,29 +57,32 @@ def sentence_bleu(references, hypothesis, weights=(0.25, 0.25, 0.25, 0.25),
     >>> sentence_bleu([reference1, reference2, reference3], hypothesis1) # doctest: +ELLIPSIS
     0.5045...
 
-    If no ngram overlap exists for any of the orders that are used to evaluate BLEU
-    it will return the value zero. This since BLEU is a geometric mean of the precisions
-    for the different ngram orders (no overlap means a precision of 0). The following example
-    has zero 3gram overlaps:
+    If there is no ngrams overlap for any order of n-grams, BLEU returns the
+    value 0. This is because the precision for the order of n-grams without
+    overlap is 0, and the geometric mean in the final BLEU score computation
+    multiplies the 0 with the precision of other n-grams. This results in 0
+    (independently of the precision of the othe n-gram orders). The following
+    example has zero 3-gram and 4-gram overlaps:
 
     >>> round(sentence_bleu([reference1, reference2, reference3], hypothesis2),4) # doctest: +ELLIPSIS
     0.0
 
-    To avoid this harsh behaviour when no ngram overlaps are found a smoothing function can be used.
+    To avoid this harsh behaviour when no ngram overlaps are found a smoothing
+    function can be used.
 
     >>> chencherry = SmoothingFunction()
     >>> sentence_bleu([reference1, reference2, reference3], hypothesis2,
     ...     smoothing_function=chencherry.method1) # doctest: +ELLIPSIS
     0.0370...
 
-    The default BLEU calculates a score for up to 4grams using uniform
-    weights. To evaluate your translations with higher/lower order ngrams,
-    use customized weights. E.g. when accounting for up to 5grams with uniform
-    weights:
+    The default BLEU calculates a score for up to 4-grams using uniform
+    weights (this is called BLEU-4). To evaluate your translations with
+    higher/lower order ngrams, use customized weights. E.g. when accounting
+    for up to 5-grams with uniform weights (this is called BLEU-5) use:
 
-    >>> weights = (0.1666, 0.1666, 0.1666, 0.1666, 0.1666)
+    >>> weights = (1./5., 1./5., 1./5., 1./5., 1./5.)
     >>> sentence_bleu([reference1, reference2, reference3], hypothesis1, weights) # doctest: +ELLIPSIS
-    0.4583...
+    0.3920...
 
     :param references: reference sentences
     :type references: list(list(str))
