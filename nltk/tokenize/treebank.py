4b21300999e11ba6f91952c05a936ccec0673e2e
Yibin Lin <lyyb46@gmail.com>
2017-11-29 11:55:48 -0800

    revert changes and revise as alvations suggested

--- nltk/tokenize/treebank.py
+++ nltk/tokenize/treebank.py
@@ -67,7 +67,7 @@ class TreebankWordTokenizer(TokenizerI):
     STARTING_QUOTES = [
         (re.compile(r'^\"'), r'``'),
         (re.compile(r'(``)'), r' \1 '),
-        (re.compile(r'([ (\[{<])"'), r'\1 `` '),
+        (re.compile(r"([ (\[{<])(\"|\'{2})"), r'\1 `` '),
     ]
 
     #punctuation
@@ -147,46 +147,54 @@ class TreebankWordTokenizer(TokenizerI):
     def span_tokenize(self, text):
         """
         Uses the post-hoc nltk.tokens.align_tokens to return the offset spans.
+
+            >>> from nltk.tokenize import TreebankWordTokenizer
+            >>> s = '''Good muffins cost $3.88\\nin New (York).  Please (buy) me\\ntwo of them.\\n(Thanks).'''
+            >>> expected = [(0, 4), (5, 12), (13, 17), (18, 19), (19, 23),
+            ... (24, 26), (27, 30), (31, 32), (32, 36), (36, 37), (37, 38),
+            ... (40, 46), (47, 48), (48, 51), (51, 52), (53, 55), (56, 59),
+            ... (60, 62), (63, 68), (69, 70), (70, 76), (76, 77), (77, 78)]
+            >>> TreebankWordTokenizer().span_tokenize(s) == expected
+            True
+            >>> expected = ['Good', 'muffins', 'cost', '$', '3.88', 'in',
+            ... 'New', '(', 'York', ')', '.', 'Please', '(', 'buy', ')',
+            ... 'me', 'two', 'of', 'them.', '(', 'Thanks', ')', '.']
+            >>> [s[start:end] for start, end in TreebankWordTokenizer().span_tokenize(s)] == expected
+            True
+
+            Additional example
             >>> from nltk.tokenize import TreebankWordTokenizer
             >>> s = '''I said, "I'd like to buy some ''good muffins" which cost $3.88\\n each in New (York)."'''
             >>> expected = [(0, 1), (2, 6), (6, 7), (8, 9), (9, 10), (10, 12),
-            ... (13, 17), (18, 20), (21, 24), (25, 29), (30, 36), (37, 44),
-            ... (44, 45), (46, 51), (52, 56), (57, 58), (58, 62), (64, 68),
-            ... (69, 71), (72, 75), (76, 77), (77, 81), (81, 82), (82, 83),
-            ... (83, 84)]
+            ... (13, 17), (18, 20), (21, 24), (25, 29), (30, 32), (32, 36),
+            ... (37, 44), (44, 45), (46, 51), (52, 56), (57, 58), (58, 62),
+            ... (64, 68), (69, 71), (72, 75), (76, 77), (77, 81), (81, 82),
+            ... (82, 83), (83, 84)]
             >>> TreebankWordTokenizer().span_tokenize(s) == expected
             True
             >>> expected = ['I', 'said', ',', '"', 'I', "'d", 'like', 'to',
-            ... 'buy', 'some', "''good", 'muffins', '"', 'which', 'cost', '$',
-            ... '3.88', 'each', 'in', 'New', '(', 'York', ')', '.', '"']
+            ... 'buy', 'some', "''", "good", 'muffins', '"', 'which', 'cost',
+            ... '$', '3.88', 'each', 'in', 'New', '(', 'York', ')', '.', '"']
             >>> [s[start:end] for start, end in TreebankWordTokenizer().span_tokenize(s)] == expected
             True
 
         """
+        raw_tokens = self.tokenize(text)
+
         # Convert converted quotes back to original double quotes
-        ix = 0
+        # Do this only if original text contains double quote(s) or double
+        # single-quotes (because '' might be transformed to `` if it is
+        # treated as starting quotes).
+        if ('"' in text) or ("''" in text):
+            # Find double quotes and converted quotes
+            matched = [m.group() for m in re.finditer(r"``|'{2}|\"", text)]
+            
+            # Replace converted quotes back to double quotes
+            tokens = [matched.pop(0) if tok in ['"', "``", "''"] else tok for tok in raw_tokens]
+        else:
+            tokens = raw_tokens
 
-        spans = []
-        for word_token in self.tokenize(text):
-            if word_token in ('``', "''"):
-                orig_idx = text.find(word_token, ix)
-                quote_idx = text.find('"', ix)
-                if orig_idx < 0:
-                    real_token = '"'
-                elif quote_idx < 0:
-                    real_token = word_token
-                elif orig_idx < quote_idx:
-                    real_token = word_token
-                else:
-                    real_token = '"'
-            else:
-                real_token = word_token
-            ix = text.find(real_token, ix)
-            end = ix + len(real_token)
-            spans.append((ix, end))
-            ix = end
-
-        return spans
+        return align_tokens(tokens, text)
 
 
 class TreebankWordDetokenizer(TokenizerI):
