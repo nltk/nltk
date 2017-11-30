33012d53fd2016bc941e1b7c699f212619f89874
Dougal J. Sutherland <dougal@gmail.com>
2017-11-30 18:07:05 +0000

    look for data in sys.prefix/share/nltk_data too

--- nltk/data.py
+++ nltk/data.py
@@ -95,6 +95,7 @@ if sys.platform.startswith('win'):
     path += [
         str(r'C:\nltk_data'), str(r'D:\nltk_data'), str(r'E:\nltk_data'),
         os.path.join(sys.prefix, str('nltk_data')),
+        os.path.join(sys.prefix, str('share'), str('nltk_data')),
         os.path.join(sys.prefix, str('lib'), str('nltk_data')),
         os.path.join(
             os.environ.get(str('APPDATA'), str('C:\\')), str('nltk_data'))
@@ -107,6 +108,7 @@ else:
         str('/usr/lib/nltk_data'),
         str('/usr/local/lib/nltk_data'),
         os.path.join(sys.prefix, str('nltk_data')),
+        os.path.join(sys.prefix, str('share'), str('nltk_data')),
         os.path.join(sys.prefix, str('lib'), str('nltk_data'))
     ]
 
