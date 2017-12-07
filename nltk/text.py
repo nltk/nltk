807b95f151aef5253989a971beee0d0a44857800
chickenrun <chickenrun4@yahoo.com.tw>
2017-12-07 16:05:28 +0800

    inverse document frequency(idf): log+1 instead of log

--- nltk/text.py
+++ nltk/text.py
@@ -574,7 +574,8 @@ class TextCollection(Text):
         if idf is None:
             matches = len([True for text in self._texts if term in text])
             # FIXME Should this raise some kind of error instead?
-            idf = (log(len(self._texts) / matches) if matches else 0.0)
+            # the idf is computed as idf(term) = log [ len(documents) / match ] + 1 (if ``smooth_idf=False``)
+            idf = (log(len(self._texts) / matches) + 1 if matches else 0.0)
             self._idf_cache[term] = idf
         return idf
 
