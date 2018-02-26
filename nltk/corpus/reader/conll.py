73600b609aa541bc96921e52dc8265a2332202b8
Jaehoon Hwang <jaehoon217@gmail.com>
2018-02-26 00:43:50 -0800

    Implementation of Better CoNLL Reader: from https://stackoverflow.com/a/46875652/610569

--- nltk/corpus/reader/conll.py
+++ nltk/corpus/reader/conll.py
@@ -521,3 +521,23 @@ class ConllChunkCorpusReader(ConllCorpusReader):
             self, root, fileids, ('words', 'pos', 'chunk'),
             chunk_types=chunk_types, encoding=encoding,
             tagset=tagset)
+
+class BetterConllReader(ConllCorpusReader):
+    def iob_words(self, fileids=None, tagset=None, column="chunk"):
+        """
+        :return: a list of word/tag/IOB tuples
+        :rtype: list(tuple)
+        :param fileids: the list of fileids that make up this corpus
+        :type fileids: None or str or list
+        """
+        self._require(self.WORDS, self.POS, self.CHUNK)
+        def get_iob_words(grid):
+            return self._get_iob_words(grid, tagset, column)
+        return LazyConcatenation(LazyMap(get_iob_words, self._grids(fileids)))
+
+    def _get_iob_words(self, grid, tagset=None, column="chunk"):
+        pos_tags = self._get_column(grid, self._colmap['pos'])
+        if tagset and tagset != self._tagset:
+            pos_tags = [map_tag(self._tagset, tagset, t) for t in pos_tags]
+        return list(zip(self._get_column(grid, self._colmap['words']), pos_tags,
+                   self._get_column(grid, self._colmap[column])))
