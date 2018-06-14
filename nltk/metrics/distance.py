cbca3e51769814f725d3eff9c711dfbf7183f4a3
alvations <alvations@gmail.com>
2018-06-14 10:19:21 +0800

    Cleaned up the demo code

--- nltk/metrics/distance.py
+++ nltk/metrics/distance.py
@@ -285,20 +285,25 @@ def jaro_winkler_similarity(s1, s2, p=0.1, max_l=None):
     return jaro_sim + ( l * p * (1 - jaro_sim) )
 
 
+
 def demo():
-    edit_distance_examples = [
+    string_distance_examples = [
         ("rain", "shine"), ("abcdef", "acbdef"), ("language", "lnaguaeg"),
         ("language", "lnaugage"), ("language", "lngauage")]
-    for s1, s2 in edit_distance_examples:
-        print("Edit dist btwn '%s' and '%s':" % (s1, s2),
+
+    for s1, s2 in string_distance_examples:
+        print("Edit distance btwn '%s' and '%s':" % (s1, s2),
               edit_distance(s1, s2))
-    for s1, s2 in edit_distance_examples:
         print("Edit dist with transpositions btwn '%s' and '%s':" % (s1, s2),
               edit_distance(s1, s2, transpositions=True))
-    for s1, s2 in edit_distance_examples:
-        print("Jaro-Winkler_distance: ", jaro_winkler_distance(s1, s2))
-    for s1, s2 in edit_distance_examples:
-        print("Jaro-Winkler_similarity: ", 1-jaro_winkler_distance(s1, s2))
+        print("Jaro similarity btwn '%s' and '%s':" % (s1, s2),
+              jaro_similarity(s1, s2))
+        print("Jaro-Winkler similarity btwn '%s' and '%s':" % (s1, s2),
+              jaro_winkler_similarity(s1, s2))
+        print("Jaro-Winkler similarity btwn '%s' and '%s':" % (s1, s2),
+              1 - jaro_winkler_similarity(s1, s2))
+        print()
+
     s1 = set([1, 2, 3, 4])
     s2 = set([3, 4, 5])
     print("s1:", s1)
