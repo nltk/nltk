783a383f38482b5bf25aa76ac3420918f603ea25
alvations <alvations@gmail.com>
2018-08-23 15:57:08 +0800

    Fixed typo

--- nltk/tokenize/__init__.py
+++ nltk/tokenize/__init__.py
@@ -107,7 +107,7 @@ improved_open_single_quote_regex = re.compile(r"(?i)(\')(?!re|ve|ll|m|t|s|d)(\w|
 improved_close_quote_regex = re.compile(u'([»”’])', re.U)
 improved_punct_regex = re.compile(r'([^\.])(\.)([\]\)}>"\'' u'»”’ ' r']*)\s*$', re.U)
 _treebank_word_tokenizer.STARTING_QUOTES.insert(0, (improved_open_quote_regex, r' \1 '))
-_treebank_word_tokenizer.STARTING_QUOTS.append((impoved_open_single_quote_regex, r'\1 \2'))
+_treebank_word_tokenizer.STARTING_QUOTES.append((impoved_open_single_quote_regex, r'\1 \2'))
 _treebank_word_tokenizer.ENDING_QUOTES.insert(0, (improved_close_quote_regex, r' \1 '))
 _treebank_word_tokenizer.PUNCTUATION.insert(0, (improved_punct_regex, r'\1 \2 \3 '))
 
