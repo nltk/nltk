42b1abcce9fccef3e90ca1a639b01c4dee8c18ae
Steven Bird <stevenbird1@gmail.com>
2018-09-10 09:25:43 +0930

    favour sys.prefix paths, resolves #2107

--- nltk/data.py
+++ nltk/data.py
@@ -93,12 +93,12 @@ if 'APPENGINE_RUNTIME' not in os.environ and os.path.expanduser('~/') != '~/':
 if sys.platform.startswith('win'):
     # Common locations on Windows:
     path += [
-        os.path.join(
-            os.environ.get(str('APPDATA'), str('C:\\')), str('nltk_data')),
-        str(r'C:\nltk_data'), str(r'D:\nltk_data'), str(r'E:\nltk_data'),
         os.path.join(sys.prefix, str('nltk_data')),
         os.path.join(sys.prefix, str('share'), str('nltk_data')),
-        os.path.join(sys.prefix, str('lib'), str('nltk_data'))
+        os.path.join(sys.prefix, str('lib'), str('nltk_data')),
+        os.path.join(
+            os.environ.get(str('APPDATA'), str('C:\\')), str('nltk_data')),
+        str(r'C:\nltk_data'), str(r'D:\nltk_data'), str(r'E:\nltk_data')
     ]
 else:
     # Common locations on UNIX & OS X:
