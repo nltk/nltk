45ebefd2d2d91343bebc3e7d862ba71ff6484bc5
alvations <alvations@gmail.com>
2018-09-10 15:13:36 +0800

    of course, we forgot to add the .sh file in tools/travis -_-|||

---
+++ tools/travis/coverage-pylint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e # Exit immediately if a command exits with a non-zero status.
+set -x # Print all executed commands to the terminal.
+
+# Check python versions
+python --version
+
+#coverage
+coverage erase
+coverage run --source=nltk nltk/test/runtests.py -v --with-xunit
+coverage xml --omit=nltk/test/*
+iconv -c -f utf-8 -t utf-8 nosetests.xml > nosetests_scrubbed.xml
+
+# Create a default pylint configuration file.
+touch $HOME/.pylintrc
+pylint -f parseable nltk > pylintoutput
+
+#script always succeeds
+true
