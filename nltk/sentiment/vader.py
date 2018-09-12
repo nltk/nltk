43819f72466bfa247242eecfea95fade26e02891
alvations <alvations@gmail.com>
2018-09-12 08:57:15 +0800

    pylint cleanup
    nltk/nltk/sentiment/vader.py:346: [W0311(bad-indentation), ] Bad indentation. Found 20 spaces, expected 16
    nltk/nltk/sentiment/util.py:741: [W0311(bad-indentation), ] Bad indentation. Found 12 spaces, expected 8

--- nltk/sentiment/vader.py
+++ nltk/sentiment/vader.py
@@ -343,7 +343,7 @@ class SentimentIntensityAnalyzer(object):
     def _never_check(self, valence, words_and_emoticons, start_i, i):
         if start_i == 0:
             if negated([words_and_emoticons[i-1]]):
-                    valence = valence*N_SCALAR
+                valence = valence*N_SCALAR
         if start_i == 1:
             if words_and_emoticons[i-2] == "never" and\
                (words_and_emoticons[i-1] == "so" or
