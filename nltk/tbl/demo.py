587be0ce5cb266e7f6b13decf7ce8f7faf5b761d
alvations <alvations@gmail.com>
2018-09-12 08:54:42 +0800

    pylint cleanup
    nltk/nltk/tbl/demo.py:328: [W0311(bad-indentation), ] Bad indentation. Found 3 spaces, expected 4
    nltk/nltk/tbl/demo.py:329: [W0311(bad-indentation), ] Bad indentation. Found 3 spaces, expected 4
    nltk/nltk/tbl/demo.py:330: [W0311(bad-indentation), ] Bad indentation. Found 7 spaces, expected 8
    nltk/nltk/tbl/demo.py:331: [W0311(bad-indentation), ] Bad indentation. Found 3 spaces, expected 4
    nltk/nltk/tbl/demo.py:333: [W0311(bad-indentation), ] Bad indentation. Found 3 spaces, expected 4
    nltk/nltk/tbl/demo.py:334: [W0311(bad-indentation), ] Bad indentation. Found 3 spaces, expected 4
    nltk/nltk/tbl/demo.py:335: [W0311(bad-indentation), ] Bad indentation. Found 7 spaces, expected 8
    nltk/nltk/tbl/demo.py:336: [W0311(bad-indentation), ] Bad indentation. Found 3 spaces, expected 4
    nltk/nltk/tbl/demo.py:338: [W0311(bad-indentation), ] Bad indentation. Found 3 spaces, expected 4
    nltk/nltk/tbl/demo.py:339: [W0311(bad-indentation), ] Bad indentation. Found 3 spaces, expected 4
    nltk/nltk/tbl/demo.py:340: [W0311(bad-indentation), ] Bad indentation. Found 3 spaces, expected 4
    nltk/nltk/tbl/demo.py:341: [W0311(bad-indentation), ] Bad indentation. Found 3 spaces, expected 4
    nltk/nltk/tbl/demo.py:342: [W0311(bad-indentation), ] Bad indentation. Found 3 spaces, expected 4

--- nltk/tbl/demo.py
+++ nltk/tbl/demo.py
@@ -325,21 +325,21 @@ def _demo_prepare_data(tagged_data, train, num_sents, randomize, separate_baseli
 
 
 def _demo_plot(learning_curve_output, teststats, trainstats=None, take=None):
-   testcurve = [teststats['initialerrors']]
-   for rulescore in teststats['rulescores']:
-       testcurve.append(testcurve[-1] - rulescore)
-   testcurve = [1 - x/teststats['tokencount'] for x in testcurve[:take]]
+    testcurve = [teststats['initialerrors']]
+    for rulescore in teststats['rulescores']:
+        testcurve.append(testcurve[-1] - rulescore)
+    testcurve = [1 - x/teststats['tokencount'] for x in testcurve[:take]]
 
-   traincurve = [trainstats['initialerrors']]
-   for rulescore in trainstats['rulescores']:
-       traincurve.append(traincurve[-1] - rulescore)
-   traincurve = [1 - x/trainstats['tokencount'] for x in traincurve[:take]]
+    traincurve = [trainstats['initialerrors']]
+    for rulescore in trainstats['rulescores']:
+        traincurve.append(traincurve[-1] - rulescore)
+    traincurve = [1 - x/trainstats['tokencount'] for x in traincurve[:take]]
 
-   import matplotlib.pyplot as plt
-   r = list(range(len(testcurve)))
-   plt.plot(r, testcurve, r, traincurve)
-   plt.axis([None, None, None, 1.0])
-   plt.savefig(learning_curve_output)
+    import matplotlib.pyplot as plt
+    r = list(range(len(testcurve)))
+    plt.plot(r, testcurve, r, traincurve)
+    plt.axis([None, None, None, 1.0])
+    plt.savefig(learning_curve_output)
 
 
 NN_CD_TAGGER = RegexpTagger(
