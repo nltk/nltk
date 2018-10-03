517270fe784ee26ba34f2f909462c461c543446d
alvations <alvations@gmail.com>
2018-10-03 13:17:03 +0800

    Give the appropriate comments.

--- tools/travis/travis-ci-commenter.sh
+++ tools/travis/travis-ci-commenter.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+# This will fire a comment on Github after the build from pull-request builds in Travis.
+# Note: this can be used in after_success but it's not used for anything now.
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
-    -d "{\"body\": \"[CI: retest]\"}" \
+    -d "{\"body\": \"<Some message>\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 fi
