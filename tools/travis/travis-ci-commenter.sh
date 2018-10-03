e941586a0ad1a84fae1d986aaa5708e061360f13
alvations <alvations@gmail.com>
2018-10-03 08:47:10 +0800

    Corrected the syntax of the shell.

--- tools/travis/travis-ci-commenter.sh
+++ tools/travis/travis-ci-commenter.sh
@@ -4,3 +4,4 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     curl -H "Authorization: token ${GITHUB_TOKEN}" -X POST \
     -d "{\"body\": \"[CI: retest]\"}" \
     "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+fi
