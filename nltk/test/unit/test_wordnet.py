632559ecab42140a0fc2302095e3562d49d34112
purificant <purificant@users.noreply.github.com>
2018-10-13 14:52:09 +0100

    fix unit test

--- nltk/test/unit/test_wordnet.py
+++ nltk/test/unit/test_wordnet.py
@@ -125,9 +125,9 @@ class WordnNetDemo(unittest.TestCase):
 
     def test_in_topic_domains(self):
         # Test in domains.
-        self.assertEqual(S('computer_science.n.01').in_topic_domains()[0], [S('access.n.05')])
-        self.assertEqual(S('germany.n.01').in_region_domains()[23], [S('trillion.n.02')])
-        self.assertEqual(S('slang.n.02').in_usage_domains()[1], [S('airhead.n.01')])
+        self.assertEqual(S('computer_science.n.01').in_topic_domains()[0], S('access.n.05'))
+        self.assertEqual(S('germany.n.01').in_region_domains()[23], S('trillion.n.02'))
+        self.assertEqual(S('slang.n.02').in_usage_domains()[1], S('airhead.n.01'))
 
     def test_wordnet_similarities(self):
         # Path based similarities.
