fe8e86800a3e9d1f0c38053e750b1d9ba1b44f1c
alvations <alvations@gmail.com>
2018-10-23 13:29:12 +0800

    changed back the position kwargs

--- nltk/tag/__init__.py
+++ nltk/tag/__init__.py
@@ -97,8 +97,7 @@ def _get_tagger(lang=None):
         tagger = PerceptronTagger()
     return tagger
 
-
-def _pos_tag(tokens, tagger, tagset=None, lang=None):
+def _pos_tag(tokens, tagset=None, tagger=None, lang=None):
     # Currently only supoorts English and Russian.
     if lang not in ['eng', 'rus']:
         raise NotImplementedError("Currently, NLTK pos_tag only supports English and Russian "
