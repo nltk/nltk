41b7ab3869510f6a627c0cabdeed6c4a11d3dedf
itsmemala <itsmemala@users.noreply.github.com>
2019-04-18 15:27:29 +0530

    Update vader.py
    Added a test comment

--- nltk/sentiment/vader.py
+++ nltk/sentiment/vader.py
@@ -20,7 +20,7 @@ Hutto, C.J. & Gilbert, E.E. (2014). VADER: A Parsimonious Rule-based Model for
 Sentiment Analysis of Social Media Text. Eighth International Conference on
 Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
 """
-
+#This is a test edit
 import math
 import re
 import string
