530a3853c426b99a746c248f1681a237e19616ca
itsmemala <itsmemala@users.noreply.github.com>
2019-04-18 16:00:07 +0530

    Update vader.py
    Added another test comment

--- nltk/sentiment/vader.py
+++ nltk/sentiment/vader.py
@@ -21,6 +21,7 @@ Sentiment Analysis of Social Media Text. Eighth International Conference on
 Weblogs and Social Media (ICWSM-14). Ann Arbor, MI, June 2014.
 """
 #This is a test edit
+#This is another test edit
 import math
 import re
 import string
