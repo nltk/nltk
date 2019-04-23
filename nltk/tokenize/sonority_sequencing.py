6003980dfcf6ea6ecab2020f35c7c4edd477916e
alvations <alvations@gmail.com>
2019-04-23 15:43:17 +0800

    handle punctuations like consonants

--- nltk/tokenize/sonority_sequencing.py
+++ nltk/tokenize/sonority_sequencing.py
@@ -45,16 +45,21 @@ class SyllableTokenizer(TokenizerI):
     """
     Syllabifies words based on the Sonority Sequencing Principle (SSP).
 
-    >>> SSP = SyllableTokenizer()
-    >>> SSP.tokenize('justification')
-    ['jus', 'ti', 'fi', 'ca', 'tion']
+        >>> from nltk import word_tokenize
+        >>> SSP = SyllableTokenizer()
+        >>> SSP.tokenize('justification')
+        ['jus', 'ti', 'fi', 'ca', 'tion']
+        >>> text = "This is a foobar-like sentence."
+        >>> [SSP.tokenize(token) for token in word_tokenize(text)]
+        [['This'], ['is'], ['a'], ['foo', 'bar', '-', 'li', 'ke'], ['sen', 'ten', 'ce'], ['.']]
     """
 
     def __init__(self, lang='en', sonority_hierarchy=False):
         """
         :param lang: Language parameter, default is English, 'en'
         :type lang: str
-        :param sonority_hierarchy: Sonority hierarchy according to the Sonority Sequencing Principle.
+        :param sonority_hierarchy: Sonority hierarchy according to the
+                                   Sonority Sequencing Principle.
         :type sonority_hierarchy: list(str)
         """
         # Sonority hierarchy should be provided in descending order.
@@ -78,7 +83,14 @@ class SyllableTokenizer(TokenizerI):
 
     def assign_values(self, token):
         """
-        Assigns each phoneme its value from the sonority hierarchy
+        Assigns each phoneme its value from the sonority hierarchy.
+        Note: Sentence/text has to be tokenized first.
+
+        :param token: Single word or token
+        :type token: str
+        :return: List of tuples, first element is character/phoneme and
+                 second is the soronity value.
+        :rtype: list(tuple(str, int))
         """
         syllables_values = []
         for c in token:
@@ -86,20 +98,31 @@ class SyllableTokenizer(TokenizerI):
                 syllables_values.append((c, self.phoneme_map[c]))
             except KeyError:
                 if c not in punctuation:
-                    warnings.warn("Character not defined in sonority_hierarchy, assigning as vowel: '{}'".format(c))
+                    warnings.warn("Character not defined in sonority_hierarchy,"
+                                  " assigning as vowel: '{}'".format(c))
                     syllables_values.append((c, max(self.phoneme_map.values())))
                     self.vowels += c
+                else: # If it's a punctuation, assing -1.
+                    syllables_values.append((c, -1))
         return syllables_values
 
-    def validate_syllables(self, syllabified):
+    def validate_syllables(self, syllable_list):
         """
         Ensures each syllable has at least one vowel.
         If the following syllable doesn't have vowel, add it to the current one.
+
+        :param syllable_list: Single word or token broken up into syllables.
+        :type syllable_list: list(str)
+        :return: Single word or token broken up into syllables
+                 (with added syllables if necessary)
+        :rtype: list(str)
         """
         valid_syllables = []
         front = ""
-        for i, syllable in enumerate(syllabified):
-
+        for i, syllable in enumerate(syllable_list):
+            if syllable in punctuation:
+                valid_syllables.append(syllable)
+                continue
             if not re.search('|'.join(self.vowels), syllable):
                 if len(valid_syllables) == 0:
                     front += syllable
@@ -116,10 +139,16 @@ class SyllableTokenizer(TokenizerI):
     def tokenize(self, token):
         """
         Apply the SSP to return a list of syllables.
+        Note: Sentence/text has to be tokenized first.
+
+        :param token: Single word or token
+        :type token: str
+        :return syllable_list: Single word or token broken up into syllables.
+        :rtype: list(str)
         """
         # assign values from hierarchy
         syllables_values = self.assign_values(token)
-        
+
         # if only one vowel return word
         if sum(token.count(x) for x in self.vowels) <= 1:
             return [token]
@@ -133,8 +162,12 @@ class SyllableTokenizer(TokenizerI):
             # Focal phoneme.
             focal_phoneme = phonemes[1]
 
-            # these cases trigger syllable break
-            if prev_value >= focal_value == next_value:
+            # These cases trigger syllable break.
+            if focal_value == -1: # If it's a punctuation, just break.
+                syllable_list.append(syllable)
+                syllable_list.append(focal_phoneme)
+                syllable = ""
+            elif prev_value >= focal_value == next_value:
                 syllable += focal_phoneme
                 syllable_list.append(syllable)
                 syllable = ""
