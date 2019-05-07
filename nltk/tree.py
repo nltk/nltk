19acb1bb9adfa273d75a169047dec209b3011663
alvations <alvations@gmail.com>
2019-05-07 12:14:03 +0800

    Added link to official gs site

--- nltk/tree.py
+++ nltk/tree.py
@@ -800,7 +800,8 @@ class Tree(list):
                     )
                     except LookupError:
                         print("The Ghostscript executable isn't found.\n"
-                              "If you're using a Mac, you can try installing"
+                              "See http://web.mit.edu/ghostscript/www/Install.htm\n"
+                              "If you're using a Mac, you can try installing\n"
                               "https://docs.brew.sh/Installation then `brew install ghostscript`",
                              file=sys.stderr)
                         raise LookupError
