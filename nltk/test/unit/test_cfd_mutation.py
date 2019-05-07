829b5308f3a5cb6cbd10a6261c61448f5cf5a9a2
Iris Zhou <iriszhoux@gmail.com>
2019-05-07 02:18:41 -0700

    Remove typo and create test case

---
+++ nltk/test/unit/test_cfd_mutation.py
@@ -0,0 +1,23 @@
+import unittest
+from nltk import ConditionalFreqDist
+from nose.tools import assert_raises
+
+class TestEmptyCondFreq(unittest.TestCase):
+    def test_tabulate(self):
+        empty = ConditionalFreqDist()
+        self.assertEqual(empty.conditions(),[])
+        try:
+            empty.tabulate(conditions="BUG") # nonexistent keys shouldn't be added
+        except:
+            pass
+        self.assertEqual(empty.conditions(), [])
+
+
+    def test_plot(self):
+        empty = ConditionalFreqDist()
+        self.assertEqual(empty.conditions(),[])
+        try:
+            empty.plot(conditions="BUG") # nonexistent keys shouldn't be added
+        except:
+            pass
+        self.assertEqual(empty.conditions(),[])
