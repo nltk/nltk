1216074fdedce1865adb8b6d7f13bf3b60143458
alvations <alvations@gmail.com>
2019-05-07 09:11:38 +0800

    Retrying Cythonize

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 #
 # Setup script for the Natural Language Toolkit
 #
-# Copyright (C) 2001-2019 NLTK Project
+# Copyright (C) 2001-2018 NLTK Project
 # Author: Steven Bird <stevenbird1@gmail.com>
 #         Edward Loper <edloper@gmail.com>
 #         Ewan Klein <ewan@inf.ed.ac.uk>
@@ -12,12 +12,11 @@
 # Work around mbcs bug in distutils.
 # http://bugs.python.org/issue10945
 import codecs
-
 try:
     codecs.lookup('mbcs')
 except LookupError:
     ascii = codecs.lookup('ascii')
-    func = lambda name, enc=ascii: {True: enc}.get(name == 'mbcs')
+    func = lambda name, enc=ascii: {True: enc}.get(name=='mbcs')
     codecs.register(func)
 
 import os
@@ -32,73 +31,99 @@ from setuptools import setup, find_packages
 
 # Specify groups of optional dependencies
 extras_require = {
-    'machine_learning': ['gensim', 'numpy', 'python-crfsuite', 'scikit-learn', 'scipy'],
-    'plot': ['matplotlib'],
-    'tgrep': ['pyparsing'],
-    'twitter': ['twython'],
-    'corenlp': ['requests'],
+    'machine_learning': [
+        'gensim',
+        'numpy',
+        'python-crfsuite',
+        'scikit-learn',
+        'scipy'
+    ],
+    'plot': [
+        'matplotlib',
+    ],
+    'tgrep': [
+        'pyparsing',
+    ],
+    'twitter': [
+        'twython',
+    ],
+    'corenlp': [
+        'requests',
+    ],
 }
 
 # Add a group made up of all optional dependencies
-extras_require['all'] = set(
-    package for group in extras_require.values() for package in group
-)
+extras_require['all'] = set(package for group in extras_require.values() for package in group)
+
+	MODULES_TO_COMPILE = [
+    'nltk.grammar',
+    'nltk.parse.chart',
+    'nltk.tokenize.*'
+]
+
+
+def compile_modules(modules):
+    """
+    Compile the named modules using Cython, using the clearer Python 3 semantics.
+    """
+    import Cython
+    from Cython.Build import cythonize
+    files = [name.replace('.', os.path.sep) + '.py' for name in modules]
+    print("Compiling %d modules using Cython %s" % (len(modules), Cython.__version__))
+    return cythonize(files, language_level=3)
+
+
+if os.getenv('CYTHONIZE_NLTK') == 'true':
+    ext_modules = compile_modules(MODULES_TO_COMPILE)
+else:
+    ext_modules = None
 
 setup(
-    name="nltk",
-    description="Natural Language Toolkit",
-    version=nltk_version,
-    url="http://nltk.org/",
-    long_description="""\
+    name = "nltk",
+    description = "Natural Language Toolkit",
+    version = nltk_version,
+    url = "http://nltk.org/",
+    long_description = """\
 The Natural Language Toolkit (NLTK) is a Python package for
-natural language processing.  NLTK requires Python 2.7, 3.5, 3.6, or 3.7.""",
-    license="Apache License, Version 2.0",
-    keywords=[
-        'NLP',
-        'CL',
-        'natural language processing',
-        'computational linguistics',
-        'parsing',
-        'tagging',
-        'tokenizing',
-        'syntax',
-        'linguistics',
-        'language',
-        'natural language',
-        'text analytics',
+natural language processing.  NLTK requires Python 2.7, 3.4, 3.5, or 3.6.""",
+    license = "Apache License, Version 2.0",
+    keywords = ['NLP', 'CL', 'natural language processing',
+                'computational linguistics', 'parsing', 'tagging',
+                'tokenizing', 'syntax', 'linguistics', 'language',
+                'natural language', 'text analytics'],
+    maintainer = "Steven Bird",
+    maintainer_email = "stevenbird1@gmail.com",
+    author = "Steven Bird",
+    author_email = "stevenbird1@gmail.com",
+    classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Developers',
+    'Intended Audience :: Education',
+    'Intended Audience :: Information Technology',
+    'Intended Audience :: Science/Research',
+    'License :: OSI Approved :: Apache Software License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python :: 2.7',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Topic :: Scientific/Engineering',
+    'Topic :: Scientific/Engineering :: Artificial Intelligence',
+    'Topic :: Scientific/Engineering :: Human Machine Interfaces',
+    'Topic :: Scientific/Engineering :: Information Analysis',
+    'Topic :: Text Processing',
+    'Topic :: Text Processing :: Filters',
+    'Topic :: Text Processing :: General',
+    'Topic :: Text Processing :: Indexing',
+    'Topic :: Text Processing :: Linguistic',
     ],
-    maintainer="Steven Bird",
-    maintainer_email="stevenbird1@gmail.com",
-    author="Steven Bird",
-    author_email="stevenbird1@gmail.com",
-    classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
-        'Intended Audience :: Education',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: Apache Software License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Topic :: Scientific/Engineering',
-        'Topic :: Scientific/Engineering :: Artificial Intelligence',
-        'Topic :: Scientific/Engineering :: Human Machine Interfaces',
-        'Topic :: Scientific/Engineering :: Information Analysis',
-        'Topic :: Text Processing',
-        'Topic :: Text Processing :: Filters',
-        'Topic :: Text Processing :: General',
-        'Topic :: Text Processing :: Indexing',
-        'Topic :: Text Processing :: Linguistic',
-    ],
-    package_data={'nltk': ['test/*.doctest', 'VERSION']},
-    install_requires=[
-        'six',
-        'singledispatch; python_version < "3.4"'
-    ],
-    extras_require=extras_require,
-    packages=find_packages(),
-    zip_safe=False,  # since normal files will be present too?
-)
+    package_data = {'nltk': ['test/*.doctest', 'VERSION']},
+   install_requires = [
+       'six',
+       "singledispatch"
+   ],
+    extras_require = extras_require,
+    packages = find_packages(),
+    ext_modules=ext_modules,
+    zip_safe=False, # since normal files will be present too?
+    )
