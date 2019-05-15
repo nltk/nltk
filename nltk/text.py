3f59f65af69879558ad45bfa9c47249696aa3d5f
alvations <alvations@gmail.com>
2019-05-15 13:18:55 +0800

    add generate()

--- nltk/text.py
+++ nltk/text.py
@@ -19,15 +19,19 @@ from math import log
 from collections import defaultdict, Counter, namedtuple
 from functools import reduce
 import re
+import sys
 
 from six import text_type
 
+from nltk.lm import MLE
+from nltk.lm.preprocessing import padded_everygram_pipeline
 from nltk.probability import FreqDist
 from nltk.probability import ConditionalFreqDist as CFD
 from nltk.util import tokenwrap, LazyConcatenation
 from nltk.metrics import f_measure, BigramAssocMeasures
 from nltk.collocations import BigramCollocationFinder
 from nltk.compat import python_2_unicode_compatible
+from nltk.tokenize import sent_tokenize
 
 ConcordanceLine = namedtuple(
     'ConcordanceLine',
@@ -530,16 +534,54 @@ class Text(object):
         from nltk.draw import dispersion_plot
 
         dispersion_plot(self, words)
-
-    def generate(self, words):
+        
+    def build_ngram_lm(self, tokenized_sents, n=3):
+        train_data, padded_sents = padded_everygram_pipeline(n, tokenized_sents)
+        model = MLE(order=n)
+        model.fit(train_data, padded_sents)
+        return model
+        
+    def generate(self, length=100, text_seed=None, random_seed=42):
         """
-        Issues a reminder to users following the book online
-        """
-        import warnings
+        Print random text, generated using a trigram language model.
 
-        warnings.warn(
-            'The generate() method is no longer available.', DeprecationWarning
-        )
+        See also: 
+        
+            >>> from nltk import lm
+            >>> help(lm)
+        
+        :param length: The length of text to generate (default=100)
+        :type length: int
+        
+        :param text_seed: Generation can be conditioned on preceding context.
+        :type text_seed: list(str)
+        
+        :param random_seed: A random seed or an instance of `random.Random`. If provided,
+        makes the random sampling part of generation reproducible.
+        :type random_seed: int
+        
+        """
+        # Create the model when using it the first time.
+        self.tokenized_sents = [sent.split(' ') for sent in 
+                                sent_tokenize(' '.join(self.tokens))]
+        if not hasattr(self, 'trigram_model'):
+            print("Building ngram index...", file=sys.stderr)
+            self.trigram_model = self.build_ngram_lm(self.tokenized_sents, n=3)
+            
+        generated_tokens = []
+        
+        assert length > 0, 'The `length` must be more than 0.'
+        while len(generated_tokens) < length:
+            for idx, token in enumerate(self.trigram_model.generate(length, text_seed=text_seed, random_seed=random_seed)):
+                if token == '<s>':
+                    continue
+                if token == '</s>':
+                    break
+                generated_tokens.append(token)
+            random_seed += 1
+        
+        prefix = ' '.join(text_seed) + ' ' if text_seed else ''
+        print(prefix + tokenwrap(generated_tokens[:length]))
 
     def plot(self, *args):
         """
