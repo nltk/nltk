53c9cba3daf0b60d2cc34b382c3faf5093306bcd
Oleg Höfling <oleg.hoefling@gmail.com>
2019-09-05 18:05:31 +0200

    write pydistutils.cfg to speed up compilation of cythonized extensions
    Signed-off-by: Oleg Höfling <oleg.hoefling@gmail.com>

--- tools/travis/install.sh
+++ tools/travis/install.sh
@@ -8,3 +8,8 @@ pip install --upgrade https://github.com/PyCQA/pylint/archive/master.zip
 
 #download nltk data packages
 python -c "import nltk; nltk.download('all')" || echo "NLTK data download failed: $?"
+
+# speed up the compilation of cythonized C extensions by instructing distutils to use multiple cores
+# travis jobs have two cores available, see
+# https://docs.travis-ci.com/user/reference/overview/#virtualisation-environment-vs-operating-system
+printf '[build_ext]\nparallel=3' > ~/.pydistutils.cfg
