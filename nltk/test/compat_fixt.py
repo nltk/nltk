7fc88d8fba129c4b7e03ff377684b11a3a5a5949
Pyfisch <pyfisch@posteo.org>
2020-02-24 10:29:01 +0100

    Remove Python 2 compatibility tests and docs

--- nltk/test/compat_fixt.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from nltk.compat import PY3
-
-
-def setup_module(module):
-    from nose import SkipTest
-
-    if PY3:
-        raise SkipTest("compat.doctest is for Python 2.x")
