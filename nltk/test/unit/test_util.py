19b02516ccb951c0fcd79f1f51a1341e7fbf3e8c
Denali Molitor <dmolitor@ucla.edu>
2020-07-15 22:24:23 -0700

    Remove repeated test

--- nltk/test/unit/test_util.py
+++ nltk/test/unit/test_util.py
@@ -13,29 +13,6 @@ class TestEverygrams(unittest.TestCase):
         """Form test data for tests."""
         self.test_data = 'a b c'.split()
 
-    def test_everygrams_pad_left(self):
-        expected_output = set(
-            [
-                (None),
-                (None, None),
-                (None, None, 'a'),
-                (None),
-                (None, 'a'),
-                (None, 'a', 'b'),
-                ('a',),
-                ('a', 'b'),
-                ('a', 'b', 'c'),
-                ('b',),
-                ('b', 'c'),
-                ('c',),
-            ]
-        )
-        output = list(everygrams(self.test_data, max_len=3, pad_left=True))
-        # Test for expected output.
-        self.assertEqual(set(output), expected_output)
-        # Test that no grams are repeated.
-        self.assertEqual(len(output), len(expected_output))
-
     def test_everygrams(self):
         test_data = 'a b c'.split()
         # Test everygrams with defaults.
