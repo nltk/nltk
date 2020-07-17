4ad2c9ba63ae8bb24c548e3941d2f152e68ff2e8
Denali Molitor <dmolitor@ucla.edu>
2020-07-17 10:25:23 -0700

    Split test functions

--- nltk/test/unit/test_util.py
+++ nltk/test/unit/test_util.py
@@ -11,10 +11,9 @@ class TestEverygrams(unittest.TestCase):
 
     def setUp(self):
         """Form test data for tests."""
-        self.test_data = 'a b c'.split()
+        self.test_data = iter('a b c'.split())
 
-    def test_everygrams(self):
-        # Test everygrams without padding.
+    def test_everygrams_without_padding(self):
         expected_output = [
             ('a',),
             ('a', 'b'),
@@ -23,17 +22,17 @@ class TestEverygrams(unittest.TestCase):
             ('b', 'c'),
             ('c',),
         ]
-        output = everygrams(iter(self.test_data))
+        output = everygrams(self.test_data)
         self.assertCountEqual(output, expected_output)
 
-        # Test everygrams with a max length of 2.
+    def test_everygrams_max_len(self):
         expected_output = [('a',), ('a', 'b'), ('b',), ('b', 'c'),('c',),]
-        output = everygrams(iter(self.test_data), max_len=2)
+        output = everygrams(self.test_data, max_len=2)
         self.assertCountEqual(output, expected_output)
 
-        # Test everygrams with a min length of 2.
+    def test_everygrams_min_len(self):
         expected_output = [('a', 'b'), ('b', 'c'), ('a', 'b', 'c'),]
-        output = everygrams(iter(self.test_data), min_len=2)
+        output = everygrams(self.test_data, min_len=2)
         self.assertCountEqual(list(output), expected_output)
 
     def test_everygrams_pad_right(self):
@@ -51,7 +50,7 @@ class TestEverygrams(unittest.TestCase):
             (None, None),
             (None,),
         ]
-        output = everygrams(iter(self.test_data), max_len=3, pad_right=True)
+        output = everygrams(self.test_data, max_len=3, pad_right=True)
         self.assertCountEqual(list(output), expected_output)
 
     def test_everygrams_pad_left(self):
@@ -69,5 +68,5 @@ class TestEverygrams(unittest.TestCase):
             ('b', 'c'),
             ('c',),
         ]
-        output = everygrams(iter(self.test_data), max_len=3, pad_left=True)
+        output = everygrams(self.test_data, max_len=3, pad_left=True)
         self.assertCountEqual(list(output), expected_output)
