01e0a7ad22150eb819745a237a475bde14cc4cd9
CubieDev <cubiegamedev@gmail.com>
2020-10-06 12:26:42 +0200

    Reduced precision of tagger evaluation test
    Rather than skipping, reducing the precision to between 70% and 80%, rather than between 73% and 74% seems like the best solution for preserving the test, while keeping the test easily readable.

--- nltk/tag/__init__.py
+++ nltk/tag/__init__.py
@@ -58,8 +58,8 @@ of ``None``.
 
 We evaluate a tagger on data that was not seen during training:
 
-    >>> tagger.evaluate(brown.tagged_sents(categories='news')[500:600]) # doctest: +SKIP
-    0.73...
+    >>> tagger.evaluate(brown.tagged_sents(categories='news')[500:600])
+    0.7...
 
 For more information, please consult chapter 5 of the NLTK Book.
 """
