54b8705aafd95a1d69d9bf293a096e554e0644bb
CubieDev <cubiegamedev@gmail.com>
2020-10-07 22:08:08 +0200

    Implement BFS with cycle prevention
    This time not returning self as the first item in the list

--- nltk/corpus/reader/wordnet.py
+++ nltk/corpus/reader/wordnet.py
@@ -564,14 +564,20 @@ class Synset(_WordNetObject):
             Synset('physical_entity.n.01'), Synset('entity.n.01')]
 
         """
-        from nltk.util import breadth_first
 
         synset_offsets = []
-        for synset in breadth_first(self, rel, depth):
+        queue = deque([(self, 0)])
+        while queue:
+            synset, cur_depth = queue.popleft()
+
+            synset_offsets.append(synset._offset)
             if synset._offset != self._offset:
-                if synset._offset not in synset_offsets:
-                    synset_offsets.append(synset._offset)
-                    yield synset
+                yield synset
+
+            if cur_depth == depth:
+                break
+
+            queue.extend((child, depth + 1) for child in rel(synset) if child._offset not in synset_offsets)
 
     def hypernym_paths(self):
         """
