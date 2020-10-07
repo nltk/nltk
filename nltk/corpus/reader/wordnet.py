1613d161abc210e6560d95f26f936bdc44f2238b
CubieDev <cubiegamedev@gmail.com>
2020-10-07 21:47:18 +0200

    Implement BFS with cycle prevention
    The previously used BFS implementation will just loop forever on certain parameters.

--- nltk/corpus/reader/wordnet.py
+++ nltk/corpus/reader/wordnet.py
@@ -564,14 +564,19 @@ class Synset(_WordNetObject):
             Synset('physical_entity.n.01'), Synset('entity.n.01')]
 
         """
-        from nltk.util import breadth_first
 
         synset_offsets = []
-        for synset in breadth_first(self, rel, depth):
-            if synset._offset != self._offset:
-                if synset._offset not in synset_offsets:
-                    synset_offsets.append(synset._offset)
-                    yield synset
+        queue = deque([(self, 0)])
+        while queue:
+            synset, cur_depth = queue.popleft()
+
+            synset_offsets.append(synset._offset)
+            yield synset
+
+            if cur_depth == depth:
+                break
+
+            queue.extend((child, depth + 1) for child in rel(synset) if child._offset not in synset_offsets and child._offset != self._offset)
 
     def hypernym_paths(self):
         """
