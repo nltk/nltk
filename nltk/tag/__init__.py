385fb3f22031ebfa87b5c5571f32008f967264fa
Tom Aarsen <37621491+CubieDev@users.noreply.github.com>
2020-10-08 05:45:26 +0200

    Skip inconsistent test (#2605)
    * Skip inconsistent test
    
    There is a ~1.8% chance that this test will randomly fail, despite working correctly.
    
    * Reduced precision of tagger evaluation test
    
    Rather than skipping, reducing the precision to between 70% and 80%, rather than between 73% and 74% seems like the best solution for preserving the test, while keeping the test easily readable.

--- nltk/tag/__init__.py
+++ nltk/tag/__init__.py
@@ -59,7 +59,7 @@ of ``None``.
 We evaluate a tagger on data that was not seen during training:
 
     >>> tagger.evaluate(brown.tagged_sents(categories='news')[500:600])
-    0.73...
+    0.7...
 
 For more information, please consult chapter 5 of the NLTK Book.
 """
