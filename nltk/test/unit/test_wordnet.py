9366ab199e6e5380049fe1aa99fd1fdbf487d1ff
yangliustonybrook <yang.liu.11@stonybrook.edu>
2020-12-30 03:16:32 -0500

    wup similarity

--- nltk/test/unit/test_wordnet.py
+++ nltk/test/unit/test_wordnet.py
@@ -173,13 +173,15 @@ class WordnNetDemo(unittest.TestCase):
         self.assertAlmostEqual(S('cat.n.01').path_similarity(S('cat.n.01')), 1.0)
         self.assertAlmostEqual(S('dog.n.01').path_similarity(S('cat.n.01')), 0.2)
         self.assertAlmostEqual(S('car.n.01').path_similarity(S('automobile.v.01')),
-                               S('automobile.v.01').path_similarity(S('car.n.01')) )
+                               S('automobile.v.01').path_similarity(S('car.n.01')))
         self.assertAlmostEqual(
             S('dog.n.01').lch_similarity(S('cat.n.01')), 2.028, places=3
         )
         self.assertAlmostEqual(
             S('dog.n.01').wup_similarity(S('cat.n.01')), 0.8571, places=3
         )
+        self.assertAlmostEqual(S('car.n.01').wup_similarity(S('automobile.v.01')),
+                               S('automobile.v.01').wup_similarity(S('car.n.01')))
         # Information Content similarities.
         brown_ic = wnic.ic('ic-brown.dat')
         self.assertAlmostEqual(
