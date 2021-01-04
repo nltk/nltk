2d29a5d507bff09532729e1945bd230089c64a39
yangliustonybrook <yang.liu.11@stonybrook.edu>
2021-01-04 16:55:51 -0500

    test

--- nltk/test/unit/test_wordnet.py
+++ nltk/test/unit/test_wordnet.py
@@ -173,9 +173,9 @@ class WordnNetDemo(unittest.TestCase):
         self.assertAlmostEqual(S('cat.n.01').path_similarity(S('cat.n.01')), 1.0)
         self.assertAlmostEqual(S('dog.n.01').path_similarity(S('cat.n.01')), 0.2)
         self.assertAlmostEqual(S('car.n.01').path_similarity(S('automobile.v.01')),
-                               S('automobile.v.01').path_similarity(S('car.n.01'))
-                               S('big.a.01').path_similarity(S('apple.n.01'))
-                               S('white.a.01').path_similarity(S('grey.a.01')))
+                               S('automobile.v.01').path_similarity(S('car.n.01')))
+        self.assertAlmostEqual(S('big.a.01').path_similarity(S('dog.n.01')),
+                               S('dog.n.01').path_similarity(S('big.a.01')))
         self.assertAlmostEqual(
             S('dog.n.01').lch_similarity(S('cat.n.01')), 2.028, places=3
         )
@@ -183,9 +183,9 @@ class WordnNetDemo(unittest.TestCase):
             S('dog.n.01').wup_similarity(S('cat.n.01')), 0.8571, places=3
         )
         self.assertAlmostEqual(S('car.n.01').wup_similarity(S('automobile.v.01')),
-                               S('automobile.v.01').wup_similarity(S('car.n.01'))
-                               S('big.a.01').wup_similarity(S('apple.n.01'))
-                               S('white.a.01').wup_similarity(S('grey.a.01')))
+                               S('automobile.v.01').wup_similarity(S('car.n.01')))
+        self.assertAlmostEqual(S('big.a.01').wup_similarity(S('dog.n.01')),
+                               S('dog.n.01').wup_similarity(S('big.a.01')))
         # Information Content similarities.
         brown_ic = wnic.ic('ic-brown.dat')
         self.assertAlmostEqual(
