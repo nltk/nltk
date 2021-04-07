0dea25ea57ed0748fa4913881e849a1f526545bb
RubenCart <ruben.cartuyvels@gmail.com>
2021-04-07 17:54:58 +0200

    Changes cls._n * ... in nltk.NgramAssocMeasures.likelihood_ratio to 2 * ..., because multiplying by other factors for TrigramAssocMeasures or QuadgramAssocMeasures gave wrong results

--- nltk/metrics/association.py
+++ nltk/metrics/association.py
@@ -145,7 +145,7 @@ class NgramAssocMeasures(metaclass=ABCMeta):
         """Scores ngrams using likelihood ratios as in Manning and Schutze 5.3.4.
         """
         cont = cls._contingency(*marginals)
-        return cls._n * sum(
+        return 2 * sum(
             obs * _ln(obs / (exp + _SMALL) + _SMALL)
             for obs, exp in zip(cont, cls._expected_values(cont))
         )
