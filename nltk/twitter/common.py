5d615b13afe9826d7454fbb6118d26cf8bf37521
Danny Sepler <dannysepler@gmail.com>
2021-07-06 00:13:33 -0400

    Pytest-ify a few more tests

--- nltk/twitter/common.py
+++ nltk/twitter/common.py
@@ -48,9 +48,7 @@ def _add_field_to_out(json, field, out):
 
 
 def _is_composed_key(field):
-    if HIER_SEPARATOR in field:
-        return True
-    return False
+    return HIER_SEPARATOR in field
 
 
 def _get_key_value_composed(field):
