b11966d37e42e5275012ee51fec68e94668699e0
antoine <antoine.venant@gmail.com>
2021-09-13 20:34:58 -0400

    pre commit fails when pushing

--- nltk/test/unit/test_distance.py
+++ nltk/test/unit/test_distance.py
@@ -1,9 +1,9 @@
 import unittest
+
 from nltk.metrics.distance import edit_distance
 
 
 class TestEditDistanceABC(unittest.TestCase):
-
     def setUp(self):
         self.left = "abc"
         self.right = "ca"
@@ -18,17 +18,35 @@ class TestEditDistanceABC(unittest.TestCase):
 
 
 class TestEditDistanceWithHigherSubCost(unittest.TestCase):
-
     def setUp(self):
         self.left = "wants"
         self.right = "Wasp"
         self.sub_cost = 2
 
     def test_with_transpositions(self):
-        self.assertEqual(edit_distance(self.left, self.right, substitution_cost=2, transpositions=True), 4)
-        self.assertEqual(edit_distance(self.right, self.left, substitution_cost=2, transpositions=True), 4)
+        self.assertEqual(
+            edit_distance(
+                self.left, self.right, substitution_cost=2, transpositions=True
+            ),
+            4,
+        )
+        self.assertEqual(
+            edit_distance(
+                self.right, self.left, substitution_cost=2, transpositions=True
+            ),
+            4,
+        )
 
     def test_without_transpositions(self):
-        self.assertEqual(edit_distance(self.left, self.right, substitution_cost=2, transpositions=False), 5)
-        self.assertEqual(edit_distance(self.right, self.left, substitution_cost=2, transpositions=False), 5)
-
+        self.assertEqual(
+            edit_distance(
+                self.left, self.right, substitution_cost=2, transpositions=False
+            ),
+            5,
+        )
+        self.assertEqual(
+            edit_distance(
+                self.right, self.left, substitution_cost=2, transpositions=False
+            ),
+            5,
+        )
