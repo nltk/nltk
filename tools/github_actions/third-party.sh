c495c63a15d913ea9213a5c30f5b4601bf72b1b0
Tom Aarsen <Cubiegamedev@gmail.com>
2021-09-20 20:39:45 +0200

    Specified sh to third-party call

--- tools/github_actions/third-party.sh
+++ tools/github_actions/third-party.sh
@@ -13,8 +13,8 @@ stanford_corenlp_package_zip_name="stanford-corenlp-full-2017-06-09.zip"
 [[ ${stanford_corenlp_package_zip_name} =~ (.+)\.zip ]]
 stanford_corenlp_package_name=${BASH_REMATCH[1]}
 if [[ ! -d ${stanford_corenlp_package_name} ]]; then
-	#curl -L "https://nlp.stanford.edu/software/$stanford_corenlp_package_zip_name" -o ${stanford_corenlp_package_zip_name}
-	wget -nv "http://nlp.stanford.edu/software/$stanford_corenlp_package_zip_name"
+	curl -L "https://nlp.stanford.edu/software/$stanford_corenlp_package_zip_name" -o ${stanford_corenlp_package_zip_name}
+	# wget -nv "http://nlp.stanford.edu/software/$stanford_corenlp_package_zip_name"
 	unzip ${stanford_corenlp_package_zip_name}
 	rm ${stanford_corenlp_package_zip_name}
 	mv ${stanford_corenlp_package_name} 'stanford-corenlp'
@@ -26,8 +26,8 @@ stanford_parser_package_zip_name="stanford-parser-full-2017-06-09.zip"
 [[ ${stanford_parser_package_zip_name} =~ (.+)\.zip ]]
 stanford_parser_package_name=${BASH_REMATCH[1]}
 if [[ ! -d ${stanford_parser_package_name} ]]; then
-	#curl -L "https://nlp.stanford.edu/software/$stanford_parser_package_zip_name" -o ${stanford_parser_package_zip_name}
-	wget -nv "https://nlp.stanford.edu/software/$stanford_parser_package_zip_name"
+	curl -L "https://nlp.stanford.edu/software/$stanford_parser_package_zip_name" -o ${stanford_parser_package_zip_name}
+	# wget -nv "https://nlp.stanford.edu/software/$stanford_parser_package_zip_name"
 	unzip ${stanford_parser_package_zip_name}
 	rm ${stanford_parser_package_zip_name}
 	mv ${stanford_parser_package_name} 'stanford-parser'
@@ -38,8 +38,8 @@ stanford_tagger_package_zip_name="stanford-postagger-full-2017-06-09.zip"
 [[ ${stanford_tagger_package_zip_name} =~ (.+)\.zip ]]
 stanford_tagger_package_name=${BASH_REMATCH[1]}
 if [[ ! -d ${stanford_tagger_package_name} ]]; then
-	# curl -L "https://nlp.stanford.edu/software/$stanford_tagger_package_zip_name" -o ${stanford_tagger_package_zip_name}
-	wget -nv "https://nlp.stanford.edu/software/$stanford_tagger_package_zip_name"
+	curl -L "https://nlp.stanford.edu/software/$stanford_tagger_package_zip_name" -o ${stanford_tagger_package_zip_name}
+	# wget -nv "https://nlp.stanford.edu/software/$stanford_tagger_package_zip_name"
 	unzip ${stanford_tagger_package_zip_name}
 	rm ${stanford_tagger_package_zip_name}
 	mv ${stanford_tagger_package_name} 'stanford-postagger'
@@ -49,8 +49,8 @@ fi
 senna_file_name=$(curl -s 'https://ronan.collobert.com/senna/download.html' | grep -o 'senna-v.*.tgz' | head -n1)
 senna_folder_name='senna'
 if [[ ! -d $senna_folder_name ]]; then
-	# curl -L "https://ronan.collobert.com/senna/$senna_file_name" -o ${senna_file_name}
-	wget -nv "https://ronan.collobert.com/senna/$senna_file_name"
+	curl -L "https://ronan.collobert.com/senna/$senna_file_name" -o ${senna_file_name}
+	# wget -nv "https://ronan.collobert.com/senna/$senna_file_name"
 	tar -xvzf ${senna_file_name}
 	rm ${senna_file_name}
 fi
