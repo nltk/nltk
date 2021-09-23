6849c0b6c15eea3c938a6beb79f4de7086fceacd
Tom Aarsen <Cubiegamedev@gmail.com>
2021-09-23 17:31:33 +0200

    Added automatic download for MEGAM and TADM

--- tools/github_actions/third-party.sh
+++ tools/github_actions/third-party.sh
@@ -8,6 +8,7 @@ pushd ${HOME}
 pushd 'third'
 
 # Download nltk stanford dependencies
+# Downloaded to ~/third/stanford-corenlp
 #stanford_corenlp_package_zip_name=$(curl -s 'https://stanfordnlp.github.io/CoreNLP/' | grep -o 'stanford-corenlp-full-.*\.zip' | head -n1)
 stanford_corenlp_package_zip_name="stanford-corenlp-full-2017-06-09.zip"
 [[ ${stanford_corenlp_package_zip_name} =~ (.+)\.zip ]]
@@ -21,6 +22,7 @@ if [[ ! -d ${stanford_corenlp_package_name} ]]; then
 fi
 
 
+# Downloaded to ~/third/stanford-parser
 #stanford_parser_package_zip_name=$(curl -s 'https://nlp.stanford.edu/software/lex-parser.shtml' | grep -o 'stanford-parser-full-.*\.zip' | head -n1)
 stanford_parser_package_zip_name="stanford-parser-full-2017-06-09.zip"
 [[ ${stanford_parser_package_zip_name} =~ (.+)\.zip ]]
@@ -33,6 +35,7 @@ if [[ ! -d ${stanford_parser_package_name} ]]; then
 	mv ${stanford_parser_package_name} 'stanford-parser'
 fi
 
+# Downloaded to ~/third/stanford-postagger
 #stanford_tagger_package_zip_name=$(curl -s 'https://nlp.stanford.edu/software/tagger.shtml' | grep -o 'stanford-postagger-full-.*\.zip' | head -n1)
 stanford_tagger_package_zip_name="stanford-postagger-full-2017-06-09.zip"
 [[ ${stanford_tagger_package_zip_name} =~ (.+)\.zip ]]
@@ -45,7 +48,7 @@ if [[ ! -d ${stanford_tagger_package_name} ]]; then
 	mv ${stanford_tagger_package_name} 'stanford-postagger'
 fi
 
-# Download SENNA
+# Download SENNA to ~/third/senna
 senna_file_name=$(curl -s 'https://ronan.collobert.com/senna/download.html' | grep -o 'senna-v.*.tgz' | head -n1)
 senna_folder_name='senna'
 if [[ ! -d $senna_folder_name ]]; then
@@ -55,7 +58,7 @@ if [[ ! -d $senna_folder_name ]]; then
 	rm ${senna_file_name}
 fi
 
-# Download PROVER9
+# Download PROVER9 to ~/third/prover9
 prover9_file_name="p9m4-v05.tar.gz"
 [[ ${prover9_file_name} =~ (.+)\.tar\.gz ]]
 prover9_folder_name=${BASH_REMATCH[1]}
@@ -66,20 +69,29 @@ if [[ ! -d ${prover9_folder_name} ]]; then
 	rm ${prover9_file_name}
 fi
 
-# Setup the Environment variable
-# echo "CLASSPATH=$(pwd)/${stanford_corenlp_package_name}:$(pwd)/${stanford_parser_package_name}:$(pwd)/${stanford_tagger_package_name}" >> $GITHUB_ENV
-# cat > ./envs.sh <<EOL
-# #!/bin/bash
-# echo "CORENLP=$(pwd)/stanford-corenlp" >> ~/.bashrc
-# echo "CORENLP_MODELS=$(pwd)/stanford-corenlp" >> ~/.bashrc
-# echo "STANFORD_PARSER=$(pwd)/stanford-parser" >> ~/.bashrc
-# echo "STANFORD_MODELS=$(pwd)/stanford-postagger" >> ~/.bashrc
-# echo "STANFORD_POSTAGGER=$(pwd)/stanford-postagger" >> ~/.bashrc
-# echo "SENNA=$(pwd)/senna" >> ~/.bashrc
-# echo "PROVER9=$(pwd)/prover9/bin" >> ~/.bashrc
-# EOL
+# Download MEGAM to ~/third/megam
+megam_file_name="megam_i686.opt.gz"
+[[ ${megam_file_name} =~ (.+)\.gz ]]
+megam_folder_name=${BASH_REMATCH[1]}
+if [[ ! -d ${megam_folder_name} ]]; then
+	curl -L "http://hal3.name/megam/$megam_file_name" -o ${megam_file_name}
+	gunzip -vf ${megam_file_name}
+	mv ${megam_folder_name} 'megam'
+	rm ${megam_file_name}
+fi
 
-# chmod +x ./envs.sh
+# Download TADM to ~/third/tadm
+tadm_file_name="tadm-0.9.8.tgz"
+[[ ${tadm_file_name} =~ (.+)\.tgz ]]
+tadm_folder_name=${BASH_REMATCH[1]}
+if [[ ! -d ${tadm_folder_name} ]]; then
+	curl -L "https://master.dl.sourceforge.net/project/tadm/tadm/tadm%200.9.8/$tadm_file_name?viasf=1" -o ${tadm_file_name}
+	tar -xvzf ${tadm_file_name}
+	mv ${tadm_folder_name} 'tadm'
+	rm ${tadm_file_name}
+fi
+
+ls ~/third
 
 popd
 popd
