48752a693242380b220cf083966e842826f19145
Tom Aarsen <Cubiegamedev@gmail.com>
2021-09-26 22:40:36 +0200

    Move megam executable to megam directory

--- tools/github_actions/third-party.sh
+++ tools/github_actions/third-party.sh
@@ -10,64 +10,64 @@ pushd 'third'
 # Download nltk stanford dependencies
 # Downloaded to ~/third/stanford-corenlp
 #stanford_corenlp_package_zip_name=$(curl -s 'https://stanfordnlp.github.io/CoreNLP/' | grep -o 'stanford-corenlp-full-.*\.zip' | head -n1)
-stanford_corenlp_package_zip_name="stanford-corenlp-full-2017-06-09.zip"
-[[ ${stanford_corenlp_package_zip_name} =~ (.+)\.zip ]]
-stanford_corenlp_package_name=${BASH_REMATCH[1]}
-if [[ ! -d ${stanford_corenlp_package_name} ]]; then
-	curl -L "https://nlp.stanford.edu/software/$stanford_corenlp_package_zip_name" -o ${stanford_corenlp_package_zip_name}
-	# wget -nv "http://nlp.stanford.edu/software/$stanford_corenlp_package_zip_name"
-	unzip -q ${stanford_corenlp_package_zip_name}
-	rm ${stanford_corenlp_package_zip_name}
-	mv ${stanford_corenlp_package_name} 'stanford-corenlp'
-fi
+# stanford_corenlp_package_zip_name="stanford-corenlp-full-2017-06-09.zip"
+# [[ ${stanford_corenlp_package_zip_name} =~ (.+)\.zip ]]
+# stanford_corenlp_package_name=${BASH_REMATCH[1]}
+# if [[ ! -d ${stanford_corenlp_package_name} ]]; then
+# 	curl -L "https://nlp.stanford.edu/software/$stanford_corenlp_package_zip_name" -o ${stanford_corenlp_package_zip_name}
+# 	# wget -nv "http://nlp.stanford.edu/software/$stanford_corenlp_package_zip_name"
+# 	unzip -q ${stanford_corenlp_package_zip_name}
+# 	rm ${stanford_corenlp_package_zip_name}
+# 	mv ${stanford_corenlp_package_name} 'stanford-corenlp'
+# fi
 
 
-# Downloaded to ~/third/stanford-parser
-#stanford_parser_package_zip_name=$(curl -s 'https://nlp.stanford.edu/software/lex-parser.shtml' | grep -o 'stanford-parser-full-.*\.zip' | head -n1)
-stanford_parser_package_zip_name="stanford-parser-full-2017-06-09.zip"
-[[ ${stanford_parser_package_zip_name} =~ (.+)\.zip ]]
-stanford_parser_package_name=${BASH_REMATCH[1]}
-if [[ ! -d ${stanford_parser_package_name} ]]; then
-	curl -L "https://nlp.stanford.edu/software/$stanford_parser_package_zip_name" -o ${stanford_parser_package_zip_name}
-	# wget -nv "https://nlp.stanford.edu/software/$stanford_parser_package_zip_name"
-	unzip -q ${stanford_parser_package_zip_name}
-	rm ${stanford_parser_package_zip_name}
-	mv ${stanford_parser_package_name} 'stanford-parser'
-fi
+# # Downloaded to ~/third/stanford-parser
+# #stanford_parser_package_zip_name=$(curl -s 'https://nlp.stanford.edu/software/lex-parser.shtml' | grep -o 'stanford-parser-full-.*\.zip' | head -n1)
+# stanford_parser_package_zip_name="stanford-parser-full-2017-06-09.zip"
+# [[ ${stanford_parser_package_zip_name} =~ (.+)\.zip ]]
+# stanford_parser_package_name=${BASH_REMATCH[1]}
+# if [[ ! -d ${stanford_parser_package_name} ]]; then
+# 	curl -L "https://nlp.stanford.edu/software/$stanford_parser_package_zip_name" -o ${stanford_parser_package_zip_name}
+# 	# wget -nv "https://nlp.stanford.edu/software/$stanford_parser_package_zip_name"
+# 	unzip -q ${stanford_parser_package_zip_name}
+# 	rm ${stanford_parser_package_zip_name}
+# 	mv ${stanford_parser_package_name} 'stanford-parser'
+# fi
 
-# Downloaded to ~/third/stanford-postagger
-#stanford_tagger_package_zip_name=$(curl -s 'https://nlp.stanford.edu/software/tagger.shtml' | grep -o 'stanford-postagger-full-.*\.zip' | head -n1)
-stanford_tagger_package_zip_name="stanford-postagger-full-2017-06-09.zip"
-[[ ${stanford_tagger_package_zip_name} =~ (.+)\.zip ]]
-stanford_tagger_package_name=${BASH_REMATCH[1]}
-if [[ ! -d ${stanford_tagger_package_name} ]]; then
-	curl -L "https://nlp.stanford.edu/software/$stanford_tagger_package_zip_name" -o ${stanford_tagger_package_zip_name}
-	# wget -nv "https://nlp.stanford.edu/software/$stanford_tagger_package_zip_name"
-	unzip -q ${stanford_tagger_package_zip_name}
-	rm ${stanford_tagger_package_zip_name}
-	mv ${stanford_tagger_package_name} 'stanford-postagger'
-fi
+# # Downloaded to ~/third/stanford-postagger
+# #stanford_tagger_package_zip_name=$(curl -s 'https://nlp.stanford.edu/software/tagger.shtml' | grep -o 'stanford-postagger-full-.*\.zip' | head -n1)
+# stanford_tagger_package_zip_name="stanford-postagger-full-2017-06-09.zip"
+# [[ ${stanford_tagger_package_zip_name} =~ (.+)\.zip ]]
+# stanford_tagger_package_name=${BASH_REMATCH[1]}
+# if [[ ! -d ${stanford_tagger_package_name} ]]; then
+# 	curl -L "https://nlp.stanford.edu/software/$stanford_tagger_package_zip_name" -o ${stanford_tagger_package_zip_name}
+# 	# wget -nv "https://nlp.stanford.edu/software/$stanford_tagger_package_zip_name"
+# 	unzip -q ${stanford_tagger_package_zip_name}
+# 	rm ${stanford_tagger_package_zip_name}
+# 	mv ${stanford_tagger_package_name} 'stanford-postagger'
+# fi
 
-# Download SENNA to ~/third/senna
-senna_file_name=$(curl -s 'https://ronan.collobert.com/senna/download.html' | grep -o 'senna-v.*.tgz' | head -n1)
-senna_folder_name='senna'
-if [[ ! -d $senna_folder_name ]]; then
-	curl -L "https://ronan.collobert.com/senna/$senna_file_name" -o ${senna_file_name}
-	# wget -nv "https://ronan.collobert.com/senna/$senna_file_name"
-	tar -xzf ${senna_file_name}
-	rm ${senna_file_name}
-fi
+# # Download SENNA to ~/third/senna
+# senna_file_name=$(curl -s 'https://ronan.collobert.com/senna/download.html' | grep -o 'senna-v.*.tgz' | head -n1)
+# senna_folder_name='senna'
+# if [[ ! -d $senna_folder_name ]]; then
+# 	curl -L "https://ronan.collobert.com/senna/$senna_file_name" -o ${senna_file_name}
+# 	# wget -nv "https://ronan.collobert.com/senna/$senna_file_name"
+# 	tar -xzf ${senna_file_name}
+# 	rm ${senna_file_name}
+# fi
 
-# Download PROVER9 to ~/third/prover9
-prover9_file_name="p9m4-v05.tar.gz"
-[[ ${prover9_file_name} =~ (.+)\.tar\.gz ]]
-prover9_folder_name=${BASH_REMATCH[1]}
-if [[ ! -d ${prover9_folder_name} ]]; then
-	curl -L "https://www.cs.unm.edu/~mccune/prover9/gui/$prover9_file_name" -o ${prover9_file_name}
-	tar -xzf ${prover9_file_name}
-	mv ${prover9_folder_name} 'prover9'
-	rm ${prover9_file_name}
-fi
+# # Download PROVER9 to ~/third/prover9
+# prover9_file_name="p9m4-v05.tar.gz"
+# [[ ${prover9_file_name} =~ (.+)\.tar\.gz ]]
+# prover9_folder_name=${BASH_REMATCH[1]}
+# if [[ ! -d ${prover9_folder_name} ]]; then
+# 	curl -L "https://www.cs.unm.edu/~mccune/prover9/gui/$prover9_file_name" -o ${prover9_file_name}
+# 	tar -xzf ${prover9_file_name}
+# 	mv ${prover9_folder_name} 'prover9'
+# 	rm ${prover9_file_name}
+# fi
 
 # Download MEGAM to ~/third/megam
 megam_file_name="megam_i686.opt.gz"
@@ -76,9 +76,8 @@ megam_folder_name=${BASH_REMATCH[1]}
 if [[ ! -d ${megam_folder_name} ]]; then
 	curl -L "http://hal3.name/megam/$megam_file_name" -o ${megam_file_name}
 	gunzip -vf ${megam_file_name}
-	mv ${megam_folder_name} "megam"
-	tree ./
-	chmod -R 711 "./megam/$megam_folder_name"
+	mv ${megam_folder_name} "megam/${megam_folder_name}"
+	chmod -R 711 "megam/$megam_folder_name"
 fi
 
 # TADM requires `libtaopetsc.so` from PETSc v2.3.3, and likely has more
