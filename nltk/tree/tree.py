d775e1eb44d8a717d0cb9e6b28f9d1381ac20ba3
mohaned mashaly <mohaned_boss@outlook.com>
2021-12-02 11:36:23 +0200

    add deprecated decorator

--- nltk/tree/tree.py
+++ nltk/tree/tree.py
@@ -17,8 +17,7 @@ syntax trees and morphological trees.
 
 import re
 import sys
-import warnings
-
+from nltk.internals import deprecated
 from nltk.grammar import Nonterminal, Production
 
 ######################################################################
@@ -202,22 +201,13 @@ class Tree(list):
     # ////////////////////////////////////////////////////////////
     # Basic tree operations
     # ////////////////////////////////////////////////////////////
-
+    @deprecated("_get_node() is deprecated, use label() instead") 
     def _get_node(self):
         """Outdated method to access the node value; use the label() method instead."""
-        warnings.warn(
-            "_get_node() is deprecated, use label() instead",
-            DeprecationWarning,
-            stacklevel=2,
-        )
 
+    @deprecated("_set_node() is deprecated, use set_label() instead") 
     def _set_node(self, value):
         """Outdated method to set the node value; use the set_label() method instead."""
-        warnings.warn(
-            "_set_node() is deprecated, use set_label() instead",
-            DeprecationWarning,
-            stacklevel=2,
-        )
 
     node = property(_get_node, _set_node)
 
