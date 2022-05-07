b494145ebed4ff32c4b83e28abaca2887a06ad72
cestwc <80936226+cestwc@users.noreply.github.com>
2022-05-07 22:52:31 +0800

    Update skolemize.py
    add a function, richardize (just a name trying to make it not confused with 'simplify' or 'reduce' ), modified from skolemize

--- nltk/sem/skolemize.py
+++ nltk/sem/skolemize.py
@@ -132,6 +132,119 @@ def skolemize(expression, univ_scope=None, used_variables=None):
         raise Exception("'%s' cannot be skolemized" % expression)
 
 
+def richardize(expression, univ_scope=None, used_variables=None):
+    """
+    richardize? the expression and convert to a simplest equivalent form FOL
+    """
+    if univ_scope is None:
+        univ_scope = set()
+    if used_variables is None:
+        used_variables = set()
+
+    if isinstance(expression, AllExpression):
+        term = richardize(
+            expression.term,
+            univ_scope | {expression.variable},
+            used_variables | {expression.variable},
+        )
+        return AllExpression(expression.variable, term)
+        # term.replace(
+        #     expression.variable,
+        #     VariableExpression(unique_variable(ignore=used_variables)),
+        # )
+    elif isinstance(expression, AndExpression):
+        return richardize(expression.first, univ_scope, used_variables) & richardize(
+            expression.second, univ_scope, used_variables
+        )
+    elif isinstance(expression, OrExpression):
+        return to_cnf(
+            richardize(expression.first, univ_scope, used_variables),
+            richardize(expression.second, univ_scope, used_variables),
+        )
+    elif isinstance(expression, ImpExpression):
+        return to_cnf(
+            richardize(-expression.first, univ_scope, used_variables),
+            richardize(expression.second, univ_scope, used_variables),
+        )
+    elif isinstance(expression, IffExpression):
+        return to_cnf(
+            richardize(-expression.first, univ_scope, used_variables),
+            richardize(expression.second, univ_scope, used_variables),
+        ) & to_cnf(
+            richardize(expression.first, univ_scope, used_variables),
+            richardize(-expression.second, univ_scope, used_variables),
+        )
+    elif isinstance(expression, EqualityExpression):
+        return expression
+    elif isinstance(expression, NegatedExpression):
+        negated = expression.term
+        if isinstance(negated, AllExpression):
+            term = richardize(
+                -negated.term, univ_scope, used_variables | {negated.variable}
+            )
+            if univ_scope:
+                return term.replace(negated.variable, skolem_function(univ_scope))
+            else:
+                # skolem_constant = VariableExpression(
+                #     unique_variable(ignore=used_variables)
+                # )
+                return ExistsExpression(negated.variable, term)#.replace(negated.variable, skolem_constant)
+        elif isinstance(negated, AndExpression):
+            return to_cnf(
+                richardize(-negated.first, univ_scope, used_variables),
+                richardize(-negated.second, univ_scope, used_variables),
+            )
+        elif isinstance(negated, OrExpression):
+            return richardize(-negated.first, univ_scope, used_variables) & richardize(
+                -negated.second, univ_scope, used_variables
+            )
+        elif isinstance(negated, ImpExpression):
+            return richardize(negated.first, univ_scope, used_variables) & richardize(
+                -negated.second, univ_scope, used_variables
+            )
+        elif isinstance(negated, IffExpression):
+            return to_cnf(
+                richardize(-negated.first, univ_scope, used_variables),
+                richardize(-negated.second, univ_scope, used_variables),
+            ) & to_cnf(
+                richardize(negated.first, univ_scope, used_variables),
+                richardize(negated.second, univ_scope, used_variables),
+            )
+        elif isinstance(negated, EqualityExpression):
+            return expression
+        elif isinstance(negated, NegatedExpression):
+            return richardize(negated.term, univ_scope, used_variables)
+        elif isinstance(negated, ExistsExpression):
+            term = richardize(
+                -negated.term,
+                univ_scope | {negated.variable},
+                used_variables | {negated.variable},
+            )
+            return AllExpression(negated.variable, term)
+            # term.replace(
+            #     negated.variable,
+            #     VariableExpression(unique_variable(ignore=used_variables)),
+            # )
+        elif isinstance(negated, ApplicationExpression):
+            return expression
+        else:
+            raise Exception("'%s' cannot be richardized" % expression)
+    elif isinstance(expression, ExistsExpression):
+        term = richardize(
+            expression.term, univ_scope, used_variables | {expression.variable}
+        )
+        if univ_scope:
+            return term.replace(expression.variable, skolem_function(univ_scope))
+        else:
+            return ExistsExpression(expression.variable, term)
+            # skolem_constant = VariableExpression(unique_variable(ignore=used_variables))
+            # return term.replace(expression.variable, skolem_constant)
+    elif isinstance(expression, ApplicationExpression):
+        return expression
+    else:
+        raise Exception("'%s' cannot be richardized" % expression)
+
+        
 def to_cnf(first, second):
     """
     Convert this split disjunction to conjunctive normal form (CNF)
