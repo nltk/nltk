c32be62505208fc7df043c4ac66f36c984c1ed04
cestwc <80936226+cestwc@users.noreply.github.com>
2022-05-29 22:11:48 +0800

    Update skolemize.py

--- nltk/sem/skolemize.py
+++ nltk/sem/skolemize.py
@@ -307,16 +307,16 @@ def conj_elim(expression):
     elif "first" in tree:
         firsts = conj_elim(tree["first"])
         seconds = conj_elim(tree["second"])
-        if isinstance(expression, AndExpression):
-            firsts, firsts_ = itertools.tee(firsts)
-            yield from firsts_
-            seconds, seconds_ = itertools.tee(seconds)
-            yield from seconds_
         for first in firsts:
             seconds, seconds_ = itertools.tee(seconds)
             for second in seconds_:
                 tree["first"] = first
                 tree["second"] = second
                 yield form[0](**tree)
+        if isinstance(expression, AndExpression):
+            firsts, firsts_ = itertools.tee(firsts)
+            yield from firsts_
+            seconds, seconds_ = itertools.tee(seconds)
+            yield from seconds_
     else:
         yield expression
