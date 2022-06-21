952119e959e66bcf6a702abad79555e0f860b638
Tom Aarsen <Cubiegamedev@gmail.com>
2022-06-21 10:04:44 +0200

    Add new sent_tokenize tests that I'd like to see pass

--- nltk/test/unit/test_tokenize.py
+++ nltk/test/unit/test_tokenize.py
@@ -13,6 +13,7 @@ from nltk.tokenize import (
     TreebankWordTokenizer,
     TweetTokenizer,
     punkt,
+    sent_tokenize,
     word_tokenize,
 )
 
@@ -809,3 +810,21 @@ class TestTokenize:
         )
         # The sentence should be split into two sections,
         # with one split and hence one decision.
+
+    @pytest.mark.parametrize(
+        "sentences, expected",
+        [
+            (
+                "this is a test. . new sentence.",
+                ["this is a test.", ".", "new sentence."],
+            ),
+            ("This... That", ["This... That"]),
+            ("This. . . That", ["This.", ".", ".", "That"]),
+            (
+                "1. This is R .\n2. This is A .\n3. That's all",
+                ["1.", "This is R .", "2.", "This is A .", "3.", "That's all"],
+            ),
+        ],
+    )
+    def test_sent_tokenize(self, sentences: str, expected: List[str]):
+        assert sent_tokenize(sentences) == expected
