25d25a7999db66df8184070b621735f80769b4eb
Jan Lennartz <jan.lennartz@ing.com>
2022-08-08 09:11:11 +0200

    fixed pytest (initialised missing object)

--- nltk/draw/util.py
+++ nltk/draw/util.py
@@ -80,7 +80,7 @@ class CanvasWidget(metaclass=ABCMeta):
     arguments of the form ``attribute=value``:
 
         >>> from nltk.draw.util import TextWidget
-        >>> cn = TextWidget(c, 'test', color='red')
+        >>> cn = TextWidget(Canvas(), 'test', color='red')
 
     Attribute values can also be changed after a canvas widget has
     been constructed, using the ``__setitem__`` operator:
@@ -90,7 +90,7 @@ class CanvasWidget(metaclass=ABCMeta):
     The current value of an attribute value can be queried using the
     ``__getitem__`` operator:
 
-        >>> cn['color']
+        >>> print(cn['color'])
         red
 
     For a list of the attributes supported by a type of canvas widget,
@@ -2216,7 +2216,7 @@ class ColorizedList:
         """
         Set up any colortags that will be used by this colorized list.
         E.g.:
-            >>> textwidget.tag_config('terminal', foreground='black')
+            textwidget.tag_config('terminal', foreground='black')
         """
 
     @abstractmethod
