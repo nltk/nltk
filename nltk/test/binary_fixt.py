2ddf14af90b2bfb5329c0a5f7b548da706121bc1
Eric Kafe <kafe.eric@gmail.com>
2022-09-05 08:39:06 +0200

    Generic fixture to skip doctest when a required binary is not available

---
+++ nltk/test/binary_fixt.py
@@ -0,0 +1,10 @@
+from nltk.internals import find_binary
+
+
+def check_binary(binary):
+    import pytest
+
+    try:
+        find_binary(binary)
+    except:
+        pytest.skip("Skipping test because the {binary} binary was not found")
