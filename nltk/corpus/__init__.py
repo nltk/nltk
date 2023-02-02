add0f1b5a90b9ae0db78e02df9cb9fe42e3a3d41
Eric Kafe <kafe.eric@gmail.com>
2023-02-02 08:46:23 +0100

    Add comment, to restart the tests

--- nltk/corpus/__init__.py
+++ nltk/corpus/__init__.py
@@ -405,6 +405,7 @@ wordnet2021: WordNetCorpusReader = LazyCorpusLoader(
     WordNetCorpusReader,
     LazyCorpusLoader("omw-1.4", CorpusReader, r".*/wn-data-.*\.tab", encoding="utf8"),
 )
+# Latest Open English Wordnet:
 wordnet2022: WordNetCorpusReader = LazyCorpusLoader(
     "wordnet2022",
     WordNetCorpusReader,
