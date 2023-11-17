7e609fe20e4389d83249d9cdccac2fd3afaab5a6
Konstantin Chernyshev <k4black@ya.ru>
2023-11-17 08:57:52 +0100

    docs: add 3.12 python to readme and metadata

--- setup.py
+++ setup.py
@@ -67,7 +67,7 @@ setup(
     },
     long_description="""\
 The Natural Language Toolkit (NLTK) is a Python package for
-natural language processing.  NLTK requires Python 3.7, 3.8, 3.9, 3.10 or 3.11.""",
+natural language processing.  NLTK requires Python 3.7, 3.8, 3.9, 3.10, 3.11 or 3.12.""",
     license="Apache License, Version 2.0",
     keywords=[
         "NLP",
@@ -100,6 +100,7 @@ natural language processing.  NLTK requires Python 3.7, 3.8, 3.9, 3.10 or 3.11."
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
         "Topic :: Scientific/Engineering :: Human Machine Interfaces",
