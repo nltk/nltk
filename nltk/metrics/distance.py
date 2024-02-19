14d2d0f01cdb6e7ceba5add83c7f547d8a6477cf
Ash Vardanian <1983160+ashvardanian@users.noreply.github.com>
2024-02-19 06:11:45 +0000

    Faster SIMD-accelerated Levenshtein distances
    This PR replaces the native Python implementation of
    the Levenshtein distance with a SIMD-accelerated version
    from the StringZilla library.
    
    On ~5 letter words from a typical English corpus - StringZilla
    is over 10x faster than current Python implementation in NLTK.
    On a multilingual corpus with longer words - same result.

--- nltk/metrics/distance.py
+++ nltk/metrics/distance.py
@@ -22,6 +22,8 @@ As metrics, they must satisfy the following three requirements:
 import operator
 import warnings
 
+import stringzilla as sz
+
 
 def _edit_dist_init(len1, len2):
     lev = []
@@ -85,6 +87,12 @@ def edit_distance(s1, s2, substitution_cost=1, transpositions=False):
     :type transpositions: bool
     :rtype: int
     """
+
+    # StringZilla currently doesn't support transpositions and cost-parameterized substitutions.
+    # It's C implementation is 10x faster than a pure Python implementation.
+    if not transpositions and substitution_cost == 1:
+        return sz.edit_distance_unicode(s1, s2)
+
     # set up a 2-D array
     len1 = len(s1)
     len2 = len(s2)
