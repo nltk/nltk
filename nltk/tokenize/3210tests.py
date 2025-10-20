5480c647d07c5bcaac80c23810be2a116dbd533f
unknown <jwaring@uoguelph.ca>
2025-10-20 02:41:59 -0400

    Updated test case, will add automatic checking later

--- nltk/tokenize/3210tests.py
+++ nltk/tokenize/3210tests.py
@@ -8,13 +8,16 @@ import nltk
 from nltk import pos_tag, word_tokenize
 from nltk.tokenize.treebank import TreebankWordDetokenizer
 
-nltk.download('averaged_perceptron_tagger')
-nltk.download('punkt')
-
-
 text = "Lorem ipsum dolor sit amet. consectetur adipiscing elit."
-# d = TreebankWordDetokenizer()
+text2 = "Lorem ipsum. . d.o.l.o.r sit amet@   @. consectetur!!!!!!!! adipiscing.... elit??."
+
 tagged_words = pos_tag(word_tokenize(text))
 words = [word for word, tag in tagged_words]
+
+tagged_words2 = pos_tag(word_tokenize(text2))
+words2 = [word for word, tag in tagged_words2]
 # print('x'+words[5]+'x')
+# print(words)
 print(TreebankWordDetokenizer().detokenize(words))
+# print(words2)
+print(TreebankWordDetokenizer().detokenize(words2))
