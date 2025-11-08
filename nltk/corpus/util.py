b76ba59372cc8c5ea3c714a4ac67ec0ab343ca3e
Eric Kafe <kafe@megadoc.net>
2025-11-08 13:51:09 +0100

    Assume the class swap is the cause of the segfaults

--- nltk/corpus/util.py
+++ nltk/corpus/util.py
@@ -105,7 +105,8 @@ class LazyCorpusLoader:
         def _unload(self):
             lazy_reader = LazyCorpusLoader(name, reader_cls, *args, **kwargs)
             self.__dict__ = lazy_reader.__dict__
-            self.__class__ = lazy_reader.__class__
+
+        #    self.__class__ = lazy_reader.__class__
 
         #            gc.collect()
 
