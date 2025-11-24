04e50ec35a8ce9736824bb115c15c3d3d58d65ef
HeixCS <mmasoo08@uoguelph.ca>
2025-11-24 18:30:28 -0500

    split two test cases into clear box and black box testing. Testing if the pip install works with the entry points

--- nltk/test/test_data_clear_box.py
+++ nltk/test/test_data_clear_box.py
@@ -1,7 +1,7 @@
 import os
 import zipfile
 import pytest
-
+from nltk.data import split_resource_url
 import nltk.data as data
 
 
@@ -112,3 +112,18 @@ class ReopenableZipFile(zipfile.ZipFile):
         if not isinstance(filename, str):
             raise TypeError("ReopenableZipFile filename must be a string")
         zipfile.ZipFile.__init__(self, filename)
+
+@pytest.mark.parametrize(
+    "input_url, expected",
+    [
+        ("corpora/wordnet", ("nltk", "corpora/wordnet")),
+        ("nltk:home/nltk", ("nltk", "home/nltk")),
+        ("file:/dir/file", ("file", "/dir/file")),
+        ("https://example.com/dir/file", ("https", "example.com/dir/file")),
+        ("http:/example.com/path", ("http", "example.com/path")),
+        ("nltk:tokenizers/punkt/english.pickle", ("nltk", "tokenizers/punkt/english.pickle")),
+        ("custom:some:extra/path", ("custom", "some:extra/path")),
+    ],
+)
+def test_split_resource_url_variants(input_url, expected):
+    assert split_resource_url(input_url) == expected
